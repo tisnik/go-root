d5320783e984285069afbb8271cf04663624e922
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-07 11:38:17 +0200

    Example 8: check for changing type of variables

---
+++ article_01/08_no_true_dynamic_type.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	fmt.Println(a)
+	b := "hello"
+	fmt.Println(b)
+	c := true
+	fmt.Println(c)
+
+	a = "world"
+	fmt.Println(a)
+	b = 0
+	fmt.Println(b)
+	c = nil
+	fmt.Println(c)
+}
+
