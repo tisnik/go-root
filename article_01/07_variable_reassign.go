4e3e642fb7a299793723919258918faa39069789
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-07 11:37:43 +0200

    Example 7: correct variable reassign

---
+++ article_01/07_variable_reassign.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	fmt.Println(a)
+	b := "hello"
+	fmt.Println(b)
+	c := true
+	fmt.Println(c)
+
+	a = 20
+	fmt.Println(a)
+	b = "world"
+	fmt.Println(b)
+	c = false
+	fmt.Println(c)
+}
+
