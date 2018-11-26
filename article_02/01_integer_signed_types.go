7cefabc2a349af0e2bfc9a18ccaecb46b61c81bb
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-11-26 09:57:53 +0100

    Updated 1st example

--- article_02/01_integer_signed_types.go
+++ article_02/01_integer_signed_types.go
@@ -15,8 +15,18 @@ func main() {
 	var c int32 = -10000
 	var d int32 = -1000000
 
+        var r1 rune = 'a'
+        var r2 rune = '\x40'
+        var r3 rune = '\n'
+        var r4 rune = '\u03BB'
+
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
 	fmt.Println(d)
+
+	fmt.Println(r1)
+	fmt.Println(r2)
+	fmt.Println(r3)
+	fmt.Println(r4)
 }
