229b89d01ecbaf06610fa1feaedd5e5cfd6fd462
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-03 10:44:40 +0100

    Example 4: array with user type items

---
+++ article_03/04_typed_array_B.go
@@ -0,0 +1,30 @@
+// Seriál "Programovací jazyk Go"
+//
+// Třetí část
+//
+// Demonstrační příklad číslo 4:
+//    Pole s prvky definovaného typu.
+
+package main
+
+import "fmt"
+
+type Mesic string
+
+func main() {
+	mesice := [12]Mesic{
+		"Leden",
+		"Únor",
+		"Březen",
+		"Duben",
+		"Květen",
+		"Červen",
+		"Červenec",
+		"Srpen",
+		"Září",
+		"Říjen",
+		"Listopad",
+		"Prosinec"}
+
+	fmt.Println(mesice)
+}
