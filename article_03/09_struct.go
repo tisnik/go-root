0409718f50bfcfacfa3c5478102908e13fb53f8d
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-03 10:52:32 +0100

    Example 9: structures

---
+++ article_03/09_struct.go
@@ -0,0 +1,28 @@
+// Seriál "Programovací jazyk Go"
+//
+// Třetí část
+//
+// Demonstrační příklad číslo 9:
+//    Uživatelsky definovaná struktura.
+
+package main
+
+import "fmt"
+
+type User struct {
+	id      uint32
+	name    string
+	surname string
+}
+
+func main() {
+	var user1 User
+
+	fmt.Println(user1)
+
+	user1.id = 1
+	user1.name = "Pepek"
+	user1.surname = "Vyskoč"
+
+	fmt.Println(user1)
+}
