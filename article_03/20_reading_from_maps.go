6aab0243a09be5ee06a3a465279f47956e81872c
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-03 11:01:39 +0100

    Example 20: reading from maps

---
+++ article_03/20_reading_from_maps.go
@@ -0,0 +1,41 @@
+// Seriál "Programovací jazyk Go"
+//
+// Třetí část
+//
+// Demonstrační příklad číslo 20:
+//    Čtení hodnot z mapy.
+
+package main
+
+import "fmt"
+
+func main() {
+	m1 := make(map[string]int)
+	fmt.Println(m1)
+
+	m1["nula"] = 0
+	m1["jedna"] = 1
+	m1["dva"] = 2
+	m1["tri"] = 3
+	m1["ctyri"] = 4
+	m1["pet"] = 5
+	m1["sest"] = 6
+
+	fmt.Println(m1)
+
+	value, exist := m1["dva"]
+
+	if exist {
+		fmt.Println("Found:", value)
+	} else {
+		fmt.Println("Not found")
+	}
+
+	value, exist = m1["sto"]
+
+	if exist {
+		fmt.Println("Found:", value)
+	} else {
+		fmt.Println("Not found")
+	}
+}
