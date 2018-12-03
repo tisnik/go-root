6d0ff1f53d30c906acb349060f360868e43d4921
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-03 10:59:01 +0100

    Example 14: array of structs, second solution

---
+++ article_03/14_array_of_structs.go
@@ -0,0 +1,50 @@
+// Seriál "Programovací jazyk Go"
+//
+// Třetí část
+//
+// Demonstrační příklad číslo 14:
+//    Pole struktur.
+
+package main
+
+import "fmt"
+
+type User struct {
+	id      uint32
+	name    string
+	surname string
+}
+
+func main() {
+	var users = [3]User{
+		User{
+			id:      1,
+			name:    "Pepek",
+			surname: "Vyskoč"},
+		User{
+			id:      2,
+			name:    "Pepek",
+			surname: "Vyskoč"},
+		User{
+			id:      3,
+			name:    "Josef",
+			surname: "Vyskočil"},
+	}
+	fmt.Println(users)
+
+	var users2 = []User{
+		User{
+			id:      1,
+			name:    "Pepek",
+			surname: "Vyskoč"},
+		User{
+			id:      2,
+			name:    "Pepek",
+			surname: "Vyskoč"},
+		User{
+			id:      3,
+			name:    "Josef",
+			surname: "Vyskočil"},
+	}
+	fmt.Println(users2)
+}
