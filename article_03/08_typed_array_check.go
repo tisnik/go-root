63fcb5f0c038f466ff0f173499ac5f30ec45f4cb
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-03 10:50:54 +0100

    Example 8: type check in compile time

---
+++ article_03/08_typed_array_check.go
@@ -0,0 +1,33 @@
+// Seriál "Programovací jazyk Go"
+//
+// Třetí část
+//
+// Demonstrační příklad číslo 8:
+//    Pole s prvky definovaného typu + kontrola typů.
+
+package main
+
+import "fmt"
+
+type Mesic string
+
+func main() {
+	mesice := [12]Mesic{
+		"Leden",
+		"Únor",
+		"Březen",
+		"Duben",
+		"Květen",
+		"Červen",
+		"Červenec",
+		"Srpen",
+		"Září",
+		"Říjen",
+		"Listopad",
+		"Prosinec"}
+
+	var mesice2 [12]string = mesice
+
+	fmt.Println(mesice)
+	fmt.Println(mesice2)
+}
