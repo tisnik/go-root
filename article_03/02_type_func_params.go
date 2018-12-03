31b38c5491cf8829984122330f48b5b27f536d9b
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-03 10:42:06 +0100

    Example 2: passing user types to function

---
+++ article_03/02_type_func_params.go
@@ -0,0 +1,26 @@
+// Seriál "Programovací jazyk Go"
+//
+// Třetí část
+//
+// Demonstrační příklad číslo 2:
+//    Kontrola parametrů funkcí.
+
+package main
+
+import "fmt"
+
+type Id uint32
+type Name string
+type Surname string
+
+func register_user(id Id, name Name, surname Surname) {
+	fmt.Printf("Registering: %d %s %s", id, name, surname)
+}
+
+func main() {
+	var i Id = 1
+	var n Name = "Jan"
+	var s Surname = "Novák"
+
+	register_user(i, n, s)
+}
