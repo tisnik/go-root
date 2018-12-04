817b7f48de1474717158de11161ac914898d6242
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-04 12:09:18 +0100

    Example 27: functions as data types

---
+++ article_03/27_func_type_2.go
@@ -0,0 +1,31 @@
+// Seriál "Programovací jazyk Go"
+//
+// Třetí část
+//
+// Demonstrační příklad číslo 27:
+//    Datový typ "funkce s parametry"
+
+package main
+
+import "fmt"
+
+func funkce1(x int, y int) int {
+	return x + y
+}
+
+func funkce2(x int, y int) int {
+	return x * y
+}
+
+func main() {
+	var a func(int, int) int
+	fmt.Println(a)
+
+	a = funkce1
+	fmt.Println(a)
+	fmt.Println(a(10, 20))
+
+	a = funkce2
+	fmt.Println(a)
+	fmt.Println(a(10, 20))
+}
