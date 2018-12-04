3e96b48ba9f3f72b2ccd5867d279c605ecb99753
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-04 12:07:12 +0100

    Example 21: delete item from map

---
+++ article_03/21_delete_from_map.go
@@ -0,0 +1,44 @@
+// Seriál "Programovací jazyk Go"
+//
+// Třetí část
+//
+// Demonstrační příklad číslo 21:
+//    Odstranění položek z mapy.
+
+package main
+
+import "fmt"
+
+type Key struct {
+	id   uint32
+	role string
+}
+
+type User struct {
+	id      uint32
+	name    string
+	surname string
+}
+
+func main() {
+	m1 := make(map[Key]User)
+	fmt.Println(m1)
+
+	m1[Key{1, "admin"}] = User{
+		id:      1,
+		name:    "Pepek",
+		surname: "Vyskoč"}
+
+	m1[Key{2, "user"}] = User{
+		id:      2,
+		name:    "Josef",
+		surname: "Vyskočil"}
+
+	fmt.Println(m1)
+
+	delete(m1, Key{2, "user"})
+	fmt.Println(m1)
+
+	delete(m1, Key{1000, "nekdo jiny"})
+	fmt.Println(m1)
+}
