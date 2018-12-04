6680552f00fb4d1285e5cfc9336aa69714c66f20
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-04 12:07:26 +0100

    Example 22: basic pointer usage

---
+++ article_03/22_basic_pointers.go
@@ -0,0 +1,29 @@
+// Seriál "Programovací jazyk Go"
+//
+// Třetí část
+//
+// Demonstrační příklad číslo 22:
+//    Základní práce s ukazateli.
+
+package main
+
+import "fmt"
+
+func main() {
+	var i int = 42
+
+	fmt.Println(i)
+
+	var p_i *int
+	fmt.Println(p_i)
+
+	p_i = &i
+
+	fmt.Println(p_i)
+	fmt.Println(*p_i)
+
+	*p_i++
+
+	fmt.Println(i)
+	fmt.Println(*p_i)
+}
