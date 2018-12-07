53b06af5aef36fbbffac37a06db11613061a094c
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-07 08:56:18 +0100

    Example 1: method declaration and invocation

---
+++ article_04/01_methods.go
@@ -0,0 +1,31 @@
+// Seriál "Programovací jazyk Go"
+//
+// Čtvrtá část
+//
+// Demonstrační příklad číslo 1:
+//    Definice a zavolání metody navázané na strukturu Line.
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Line struct {
+	x1, y1 float64
+	x2, y2 float64
+}
+
+func (line Line) length() float64 {
+	return math.Hypot(line.x1-line.x2, line.y1-line.y2)
+}
+
+func main() {
+	l1 := Line{x1: 0, y1: 0, x2: 100, y2: 100}
+
+	fmt.Println(l1)
+
+	line_length := l1.length()
+	fmt.Println(line_length)
+}
