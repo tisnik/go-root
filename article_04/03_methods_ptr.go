644c0ebdb002516f2facc0888119394c49525ed4
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-07 08:58:49 +0100

    Example 3: modify struct in method (pointer usage)

---
+++ article_04/03_methods_ptr.go
@@ -0,0 +1,41 @@
+// Seriál "Programovací jazyk Go"
+//
+// Čtvrtá část
+//
+// Demonstrační příklad číslo 3:
+//    Metody s parametry, předání struktury odkazem.
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Line struct {
+	x1, y1 float64
+	x2, y2 float64
+}
+
+func (line Line) length() float64 {
+	return math.Hypot(line.x1-line.x2, line.y1-line.y2)
+}
+
+func (line *Line) translate(dx, dy float64) {
+	fmt.Printf("Translating line %v by %f %f\n", *line, dx, dy)
+	(*line).x1 += dx
+	(*line).y1 += dy
+	(*line).x2 += dx
+	(*line).y2 += dy
+}
+
+func main() {
+	l1 := Line{x1: 0, y1: 0, x2: 100, y2: 100}
+
+	fmt.Println(l1)
+	l1.translate(5, 5)
+	fmt.Println(l1)
+
+	line_length := l1.length()
+	fmt.Println(line_length)
+}
