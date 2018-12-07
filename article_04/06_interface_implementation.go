fbd8c234ac5939c17728853a8eaaefc689933fd4
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-07 09:01:33 +0100

    Example 6: interface declaration (not finished)

---
+++ article_04/06_interface_implementation.go
@@ -0,0 +1,35 @@
+// Seriál "Programovací jazyk Go"
+//
+// Čtvrtá část
+//
+// Demonstrační příklad číslo 6:
+//    Implementace jednoduchého rozhraní nazvaného OpenShape.
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type OpenShape interface {
+	length() float64
+}
+
+type Line struct {
+	x1, y1 float64
+	x2, y2 float64
+}
+
+func length(shape OpenShape) float64 {
+	return shape.length()
+}
+
+func main() {
+	l1 := Line{x1: 0, y1: 0, x2: 100, y2: 100}
+
+	fmt.Println(l1)
+
+	line_length := length(l1)
+	fmt.Println(line_length)
+}
