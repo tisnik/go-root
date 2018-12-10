683e72cd40484963349edb1f71875ede33a1f75b
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-10 09:17:04 +0100

    Example 1B: names are not unique

---
+++ article_04/01_B_unique_names.go
@@ -0,0 +1,43 @@
+// Seriál "Programovací jazyk Go"
+//
+// Čtvrtá část
+//
+// Demonstrační příklad číslo 1B:
+//    Unikátní jména metod.
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	x1, y1 float64
+}
+
+type Line struct {
+	x1, y1 float64
+	x2, y2 float64
+}
+
+func length() float64 {
+	return 0
+}
+
+func (point Point) length() float64 {
+	return 0
+}
+
+func (line Line) length() float64 {
+	return math.Hypot(line.x1-line.x2, line.y1-line.y2)
+}
+
+func main() {
+	l1 := Line{x1: 0, y1: 0, x2: 100, y2: 100}
+
+	fmt.Println(l1)
+
+	line_length := l1.length()
+	fmt.Println(line_length)
+}
