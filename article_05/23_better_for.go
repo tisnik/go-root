37d22491cf96e54b7b5262004e5a552f95b10782
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-17 10:52:43 +0100

    Example 23: better variant, local variable

---
+++ article_05/23_better_for.go
@@ -0,0 +1,14 @@
+// Seriál "Programovací jazyk Go"
+//
+// Pátá část
+//
+// Demonstrační příklad číslo 23:
+//    Zjednodušená forma programové smyčky for odvozené od C.
+
+package main
+
+func main() {
+	for i := 0; i < 10; i++ {
+		println(i)
+	}
+}
