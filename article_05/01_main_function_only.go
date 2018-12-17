1993bf4d7cb22c921757d3649811f0c356d56592
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-17 10:45:16 +0100

    Example 1: main function only

---
+++ article_05/01_main_function_only.go
@@ -0,0 +1,12 @@
+// Seriál "Programovací jazyk Go"
+//
+// Pátá část
+//
+// Demonstrační příklad číslo 1:
+//    Funkce main() jako jediný uživatelsky definovaný prvek aplikace.
+
+package main
+
+func main() {
+	println("Hello world!")
+}
