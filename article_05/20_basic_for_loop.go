e311d8c615bb9a3650fa82d118e6197985bd969a
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-17 10:51:48 +0100

    Example 20: basic for loop

---
+++ article_05/20_basic_for_loop.go
@@ -0,0 +1,14 @@
+// Seriál "Programovací jazyk Go"
+//
+// Pátá část
+//
+// Demonstrační příklad číslo 20:
+//    Základní forma programové smyčky for s podmínkou.
+
+package main
+
+func main() {
+	for true {
+		println("Diamonds are forever")
+	}
+}
