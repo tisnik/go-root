f188e7d928a76554b335deabaf31382776e6aca2
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-17 10:47:19 +0100

    Example 8: bad type of expression in if statement

---
+++ article_05/08_if_statement_nil.go
@@ -0,0 +1,36 @@
+// Seriál "Programovací jazyk Go"
+//
+// Pátá část
+//
+// Demonstrační příklad číslo 8:
+//    Řídicí konstrukce if a hodnoty, které nejsou typu boolean.
+
+package main
+
+func main() {
+	if nil {
+		println("true")
+	}
+
+	if !nil {
+		println("false")
+	}
+
+	if "" {
+		println("true")
+	}
+
+	if !"" {
+		println("false")
+	}
+
+	var b1 bool = nil
+
+	if b1 {
+		println("true")
+	}
+
+	if !b1 {
+		println("false")
+	}
+}
