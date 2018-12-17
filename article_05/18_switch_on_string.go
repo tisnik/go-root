c0c6779f1c19ee3e2518a83f65e19fba39f12b6e
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-17 10:51:11 +0100

    Example 18: Switch on string values

---
+++ article_05/18_switch_on_string.go
@@ -0,0 +1,35 @@
+// Seriál "Programovací jazyk Go"
+//
+// Pátá část
+//
+// Demonstrační příklad číslo 18:
+//    Řídicí konstrukce switch s vyhodnocovaným výrazem typu string.
+
+package main
+
+func command(x string) string {
+	switch x {
+	case "":
+		return "missing command"
+	case "help":
+	case "info":
+		return "help"
+	case "bye":
+		fallthrough
+	case "exit":
+		fallthrough
+	case "quit":
+		return "quit"
+	default:
+		return "unknown command"
+	}
+	return "unknown command"
+}
+
+func main() {
+	println(command(""))
+	println(command("bzz bzz bzz"))
+	println(command("bye"))
+	println(command("quit"))
+	println(command("exit"))
+}
