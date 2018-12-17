173fdd9cea473f25dc50f9a955f7b0c42f64fa8c
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-17 10:52:01 +0100

    Example 21: for loop with condition

---
+++ article_05/21_for_loop_with_condition.go
@@ -0,0 +1,16 @@
+// Seriál "Programovací jazyk Go"
+//
+// Pátá část
+//
+// Demonstrační příklad číslo 21:
+//    Základní forma programové smyčky for s podmínkou.
+
+package main
+
+func main() {
+	i := 10
+	for i != 0 {
+		println(i)
+		i--
+	}
+}
