f01ebd8b05321e62e6a90b5da0e9283678388925
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-17 10:51:33 +0100

    Example 19: simplest form of for loop

---
+++ article_05/19_simplest_for_loop.go
@@ -0,0 +1,14 @@
+// Seriál "Programovací jazyk Go"
+//
+// Pátá část
+//
+// Demonstrační příklad číslo 19:
+//    Základní forma programové smyčky for.
+
+package main
+
+func main() {
+	for {
+		println("Diamonds are forever")
+	}
+}
