3ba3240adef83e8a52366eef7c9727203a9cdfc8
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-17 10:52:58 +0100

    Example 24: loop variable is really local

---
+++ article_05/24_better_for_local_variable.go
@@ -0,0 +1,15 @@
+// Seriál "Programovací jazyk Go"
+//
+// Pátá část
+//
+// Demonstrační příklad číslo 24:
+//    Zjednodušená forma programové smyčky for odvozené od C.
+
+package main
+
+func main() {
+	for i := 0; i < 10; i++ {
+		println(i)
+	}
+	println(i)
+}
