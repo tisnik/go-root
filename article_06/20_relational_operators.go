5d3bb6bbf7f773abaaf46e76a7785c20c2fad552
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-24 12:12:37 +0100

    Example 20: relational operators

---
+++ article_06/20_relational_operators.go
@@ -0,0 +1,44 @@
+// Seriál "Programovací jazyk Go"
+//
+// Šestá část
+//
+// Demonstrační příklad číslo 20:
+//    Relační operátory.
+
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := 0
+
+	fmt.Println(x < y)
+	fmt.Println(x <= y)
+	fmt.Println(x == y)
+	fmt.Println(x >= y)
+	fmt.Println(x > y)
+	fmt.Println(x != y)
+
+	fmt.Println()
+
+	fx := 1e10
+	fy := -2.3e56
+
+	fmt.Println(fx < fy)
+	fmt.Println(fx <= fy)
+	fmt.Println(fx == fy)
+	fmt.Println(fx >= fy)
+	fmt.Println(fx > fy)
+	fmt.Println(fx != fy)
+
+	fmt.Println()
+
+	bx := true
+	by := false
+
+	fmt.Println(bx == by)
+	fmt.Println(bx != by)
+
+	fmt.Println()
+}
