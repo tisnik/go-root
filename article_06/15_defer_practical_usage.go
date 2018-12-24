d6194403435eba6e68badf5910acdf05a7196ec8
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-24 12:12:37 +0100

    Example 15: practical usage of defer

---
+++ article_06/15_defer_practical_usage.go
@@ -0,0 +1,48 @@
+// Seriál "Programovací jazyk Go"
+//
+// Šestá část
+//
+// Demonstrační příklad číslo 15:
+//    Praktické použití konstrukce defer.
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func copyFile(srcName, dstName string) (written int64, err error) {
+	src, err := os.Open(srcName)
+	if err != nil {
+		return
+	}
+	defer src.Close()
+
+	dst, err := os.Create(dstName)
+	if err != nil {
+		return
+	}
+	defer dst.Close()
+
+	return io.Copy(dst, src)
+}
+
+func testCopyFile(srcName, dstName string) {
+	copied, err := copyFile(srcName, dstName)
+	if err != nil {
+		fmt.Printf("copyFile('%s', '%s') failed!!!\n", srcName, dstName)
+	} else {
+		fmt.Printf("Copied %d bytes\n", copied)
+	}
+	fmt.Println()
+}
+
+func main() {
+	testCopyFile("14_defer_practical_usage.go", "new.go")
+	testCopyFile("tento_soubor_neexistuje", "new.go")
+	testCopyFile("14_defer_practical_usage.go", "")
+	testCopyFile("14_defer_practical_usage.go", "/dev/full")
+	testCopyFile("/dev/null", "new2.go")
+}
