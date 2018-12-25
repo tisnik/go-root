3116f6c1ef153550bac33502e5b2ae254a698292
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-25 21:21:48 +0100

    More usages for relational operators

--- article_06/20_relational_operators.go
+++ article_06/20_relational_operators.go
@@ -41,4 +41,25 @@ func main() {
 	fmt.Println(bx != by)
 
 	fmt.Println()
+
+	cx := 1 + 1i
+	cy := 0 + 0i
+
+	fmt.Println(cx == cy)
+	fmt.Println(cx != cy)
+
+	fmt.Println()
+
+	sx := "Hello"
+	sy := "World"
+
+	fmt.Println(sx < sy)
+	fmt.Println(sx <= sy)
+	fmt.Println(sx == sy)
+	fmt.Println(sx >= sy)
+	fmt.Println(sx > sy)
+	fmt.Println(sx != sy)
+
+	fmt.Println()
+
 }
