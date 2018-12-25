eaf933f52712c26dc09520794c80f38e75c8db2b
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-25 20:08:57 +0100

    Example 7: defer statement

---
+++ article_06/07_defer.go
@@ -0,0 +1,23 @@
+// Seriál "Programovací jazyk Go"
+//
+// Šestá část
+//
+// Demonstrační příklad číslo 7:
+//    Použití příkazu defer.
+
+package main
+
+import "fmt"
+
+func on_finish() {
+	fmt.Println("Finished")
+}
+
+func main() {
+	defer on_finish()
+
+	for i := 10; i >= 0; i-- {
+		fmt.Printf("%2d\n", i)
+	}
+	fmt.Println("Finishing main() function")
+}
