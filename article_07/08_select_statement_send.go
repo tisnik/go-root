01442d8ed9b1a3e2c06d25f79c0f574146e6b389
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-01 12:01:16 +0100

    Example 8: select statement: send

---
+++ article_07/08_select_statement_send.go
@@ -0,0 +1,36 @@
+// Seriál "Programovací jazyk Go"
+//
+// Sedmá část
+//
+// Demonstrační příklad číslo 8:
+//    Použití příkazu select pro zápis do kanálu.
+
+package main
+
+import "fmt"
+
+func worker(channel chan int) {
+	for true {
+		value, ok := <-channel
+		if ok {
+			fmt.Printf("Přijata hodnota %d\n", value)
+		} else {
+			fmt.Printf("Kanál je uzavřen\n")
+		}
+	}
+}
+
+func main() {
+	ch1 := make(chan int)
+
+	go worker(ch1)
+
+	for i := 0; i < 10; i++ {
+		select {
+		case ch1 <- 0:
+			fmt.Println("Poslána nula")
+		case ch1 <- 1:
+			fmt.Println("Poslána jednička")
+		}
+	}
+}
