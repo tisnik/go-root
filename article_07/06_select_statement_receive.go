ed2ce87d54afa21045c8182de8017a0285a3bca3
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-01 12:00:40 +0100

    Example 6: select statement: receive

---
+++ article_07/06_select_statement_receive.go
@@ -0,0 +1,35 @@
+// Seriál "Programovací jazyk Go"
+//
+// Sedmá část
+//
+// Demonstrační příklad číslo 6:
+//    Použití příkazu select pro čtení z kanálů.
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func worker(channel chan int, worker int) {
+	fmt.Printf("Worker %d spuštěn\n", worker)
+	time.Sleep(2 * time.Second)
+	channel <- 1
+	fmt.Printf("Worker %d ukončen\n", worker)
+}
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go worker(ch1, 1)
+	go worker(ch2, 2)
+
+	select {
+	case <-ch1:
+		fmt.Println("Data z kanálu 1")
+	case <-ch2:
+		fmt.Println("Data z kanálu 2")
+	}
+}
