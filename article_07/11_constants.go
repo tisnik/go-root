5dbef165fc33b35a0eec40808976c555ee4d2127
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-08 09:43:03 +0100

    Example 11+12: constants in the Go programming language

---
+++ article_07/11_constants.go
@@ -0,0 +1,28 @@
+// Seriál "Programovací jazyk Go"
+//
+// Sedmá část
+//
+// Demonstrační příklad číslo 11:
+//    Konstanty.
+
+package main
+
+import "fmt"
+
+const Pi float64 = 3.1415927
+const E = 2.71828
+
+const z0 int = 0
+const z1 = 0
+
+const z2 = z0 + z1
+
+func main() {
+	fmt.Printf("Pi = %f\n", Pi)
+	fmt.Printf("e = %f\n", E)
+
+	fmt.Printf("z0 = %d\n", z0)
+	fmt.Printf("z1 = %d\n", z1)
+
+	fmt.Printf("z2 = %d\n", z2)
+}
