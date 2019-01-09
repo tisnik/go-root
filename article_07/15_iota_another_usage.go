7b4bb0e5c732b7772827996516e36db9e95bf0b6
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-09 09:05:16 +0100

    Example 15: another iota usage

---
+++ article_07/15_iota_another_usage.go
@@ -0,0 +1,26 @@
+// Seriál "Programovací jazyk Go"
+//
+// Sedmá část
+//
+// Demonstrační příklad číslo 15:
+//    Konstanty a identifikátor iota.
+
+package main
+
+import "fmt"
+
+const (
+	a = iota + 0.5i
+	b = iota + 0.5i
+	c = 1.0 / iota
+	d = 1 << iota
+	e = iota ^ 0xff
+)
+
+func main() {
+	fmt.Printf("%f\n", a)
+	fmt.Printf("%f\n", b)
+	fmt.Printf("%f\n", c)
+	fmt.Printf("%d\n", d)
+	fmt.Printf("%d\n", e)
+}
