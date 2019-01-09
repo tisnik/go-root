e880d7415c7802a846241ef725d087407c156e9a
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-09 09:05:31 +0100

    Example 16: variadic functions

---
+++ article_07/16_variadic_function.go
@@ -0,0 +1,35 @@
+// Seriál "Programovací jazyk Go"
+//
+// Sedmá část
+//
+// Demonstrační příklad číslo 16:
+//    Variadické funkce.
+
+package main
+
+import "fmt"
+
+func f1(msg string) {
+	fmt.Printf("%s\n", msg)
+}
+
+func f2(parts ...string) {
+	for _, val := range parts {
+		fmt.Printf("%s ", val)
+	}
+	fmt.Println()
+}
+
+func f3(prefix string, parts ...string) {
+	fmt.Println(prefix)
+	for _, val := range parts {
+		fmt.Printf("%s ", val)
+	}
+	fmt.Println()
+}
+
+func main() {
+	f1("Hello")
+	f2("Hello", "world", "!")
+	f3("Message:", "Hello", "world", "again", "!")
+}
