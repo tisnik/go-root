dcc70186cf5b307625d86efa26d24c6ebe6e8af5
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-15 09:30:31 +0100

    Example 6: enum as a type

---
+++ article_08/06_enum_as_type.go
@@ -0,0 +1,55 @@
+// Seriál "Programovací jazyk Go"
+//
+// Osmá část
+//
+// Demonstrační příklad číslo 6:
+//    Výčet jako datový typ: nemožnost použití operátoru ++.
+
+package main
+
+import "fmt"
+
+type Enum int
+
+const (
+	Pondeli Enum = iota
+	Utery
+	Streda
+	Ctvrtek
+	Patek
+	Sobota
+	Nedele
+)
+
+type Den struct {
+	X Enum
+}
+
+func (day Enum) String() string {
+	days := []string{
+		"Pondeli",
+		"Utery",
+		"Streda",
+		"Ctvrtek",
+		"Patek",
+		"Sobota",
+		"Nedele"}
+	if day < Pondeli || day > Nedele {
+		return "Unknown day"
+	}
+	return days[day]
+}
+
+func reservation(day Den) {
+	fmt.Printf("Reservation for %s\n", day)
+}
+
+func main() {
+	reservation(Den{Pondeli})
+	reservation(Den{Sobota})
+	reservation(Den{Nedele})
+
+	day := Den{Pondeli}
+	day++
+	reservation(day)
+}
