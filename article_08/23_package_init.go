edf20b2970812eb76ee6fe64c239a1084b57b8d2
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-16 20:13:14 +0100

    Example 23: initialization of package imported as _

---
+++ article_08/23_package_init.go
@@ -0,0 +1,15 @@
+// Seriál "Programovací jazyk Go"
+//
+// Osmá část
+//
+// Demonstrační příklad číslo 23:
+//    Inicializace balíčku.
+
+package main
+
+import (
+	_ "hello4"
+)
+
+func main() {
+}
