ab536802203bc739be8c0753b94a9e83f84e3cbf
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-16 20:13:06 +0100

    Example 22: initialization of package

---
+++ article_08/22_package_init.go
@@ -0,0 +1,16 @@
+// Seriál "Programovací jazyk Go"
+//
+// Osmá část
+//
+// Demonstrační příklad číslo 22:
+//    Inicializace balíčku.
+
+package main
+
+import (
+	"hello4"
+)
+
+func main() {
+	hello4.Hello_world()
+}
