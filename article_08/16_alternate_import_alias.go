ecda0bb54a74f5b634fff6ff1382b822537fab6d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-16 20:12:05 +0100

    Example 16: alternate import, alias

---
+++ article_08/16_alternate_import_alias.go
@@ -0,0 +1,16 @@
+// Seriál "Programovací jazyk Go"
+//
+// Osmá část
+//
+// Demonstrační příklad číslo 16:
+//    Alternativní způsob importu balíčku.
+
+package main
+
+import (
+	formatter "fmt"
+)
+
+func main() {
+	formatter.Println("Hello world!")
+}
