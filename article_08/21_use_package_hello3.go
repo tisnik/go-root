d5af07792fe95f8ad8cf9947b57f0d2ed11dc3c7
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-16 20:12:54 +0100

    Example 21: use package hello_3

---
+++ article_08/21_use_package_hello3.go
@@ -0,0 +1,16 @@
+// Seriál "Programovací jazyk Go"
+//
+// Osmá část
+//
+// Demonstrační příklad číslo 21:
+//    Import vlastního balíčku.
+
+package main
+
+import (
+	"hello3"
+)
+
+func main() {
+	hello3.hello_world()
+}
