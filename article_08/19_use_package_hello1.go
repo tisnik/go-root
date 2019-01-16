554c321377a27f45dcc4f5c3ac17bfa5ad935f3b
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-16 20:12:38 +0100

    Example 19: use package hello_1

---
+++ article_08/19_use_package_hello1.go
@@ -0,0 +1,16 @@
+// Seriál "Programovací jazyk Go"
+//
+// Osmá část
+//
+// Demonstrační příklad číslo 19:
+//    Import vlastního balíčku.
+
+package main
+
+import (
+	"hello1"
+)
+
+func main() {
+	hello1.Hello_world()
+}
