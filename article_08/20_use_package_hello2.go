5ce17601f7b6a492416cf4066bb8d35f88780ccb
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-16 20:12:47 +0100

    Example 20: use package hello_2

---
+++ article_08/20_use_package_hello2.go
@@ -0,0 +1,16 @@
+// Seriál "Programovací jazyk Go"
+//
+// Osmá část
+//
+// Demonstrační příklad číslo 20:
+//    Import vlastního balíčku.
+
+package main
+
+import (
+	"hello2"
+)
+
+func main() {
+	hello2.Hello_world()
+}
