d96dc11000f89e1ff232293a94de5c7224c9d548
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-16 20:12:16 +0100

    Example 17: dot in import

---
+++ article_08/17_alternate_import_dot.go
@@ -0,0 +1,14 @@
+// Seriál "Programovací jazyk Go"
+//
+// Osmá část
+//
+// Demonstrační příklad číslo 17:
+//    Alternativní způsob importu balíčku.
+
+package main
+
+import . "fmt"
+
+func main() {
+	Println("Hello world!")
+}
