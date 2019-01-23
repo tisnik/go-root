0f6fb1b72c94dc58f236134f81a6180f4c3b992b
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-23 08:59:26 +0100

    Example 2: parsing boolean values

---
+++ article_09/02_string_parsing_bool.go
@@ -0,0 +1,40 @@
+// Seriál "Programovací jazyk Go"
+//
+// Devátá část
+//
+// Demonstrační příklad číslo 2:
+//    Základní funkce pro parsing řetězců: ParseBool.
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func tryToParseBool(s string) {
+	i, err := strconv.ParseBool(s)
+	if err == nil {
+		fmt.Printf("%t\n", i)
+	} else {
+		fmt.Printf("%v\n", err)
+	}
+}
+
+func main() {
+	tryToParseBool("true")
+	tryToParseBool("True")
+	tryToParseBool("TRUE")
+	tryToParseBool("T")
+	tryToParseBool("t")
+	tryToParseBool("false")
+	tryToParseBool("False")
+	tryToParseBool("FALSE")
+	tryToParseBool("F")
+	tryToParseBool("f")
+	tryToParseBool("Foobar")
+	tryToParseBool("0")
+	tryToParseBool("1")
+	tryToParseBool("no")
+	tryToParseBool("")
+}
