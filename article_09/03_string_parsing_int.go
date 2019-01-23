41bc1f92080be528e1f48e80d63f3396d7a61246
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-23 08:59:26 +0100

    Example 3: parsing integer values

---
+++ article_09/03_string_parsing_int.go
@@ -0,0 +1,37 @@
+// Seriál "Programovací jazyk Go"
+//
+// Devátá část
+//
+// Demonstrační příklad číslo 3:
+//    Základní funkce pro parsing řetězců: ParseInt.
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func tryToParseInteger(s string, base int) {
+	i, err := strconv.ParseInt(s, base, 32)
+	if err == nil {
+		fmt.Printf("%d\n", i)
+	} else {
+		fmt.Printf("%v\n", err)
+	}
+}
+
+func main() {
+	tryToParseInteger("42", 10)
+	tryToParseInteger("42", 0)
+	tryToParseInteger("42", 16)
+	tryToParseInteger("42", 2)
+	tryToParseInteger("42x", 10)
+	println()
+	tryToParseInteger("-42", 10)
+	tryToParseInteger("-42", 0)
+	tryToParseInteger("-42", 16)
+	tryToParseInteger("-42", 2)
+	tryToParseInteger("-42x", 10)
+	println()
+}
