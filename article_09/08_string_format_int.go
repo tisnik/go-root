a8ade39d63b833544d8e97220bd446944fa55b43
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-23 08:59:26 +0100

    Example 8: string format for integer values

---
+++ article_09/08_string_format_int.go
@@ -0,0 +1,19 @@
+// Seriál "Programovací jazyk Go"
+//
+// Devátá část
+//
+// Demonstrační příklad číslo 8:
+//    Základní funkce pro formátování řetězců: FormatInt.
+
+package main
+
+import (
+	. "strconv"
+)
+
+func main() {
+	value := int64(0xcafebabe)
+	for base := 2; base < 36; base++ {
+		println(base, FormatInt(value, base))
+	}
+}
