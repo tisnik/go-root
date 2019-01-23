3a6bca1fd0b7a82e4f90eee2f83a5c79b424436e
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-23 08:59:26 +0100

    Example 13: GoDS library - array list

---
+++ article_09/13_gods_arraylist.go
@@ -0,0 +1,40 @@
+// Seriál "Programovací jazyk Go"
+//
+// Devátá část
+//
+// Demonstrační příklad číslo 13:
+//    Datová struktura arraylist z knihovny GoDS.
+
+package main
+
+import (
+	"fmt"
+	"github.com/emirpasic/gods/lists/arraylist"
+)
+
+func printList(list *arraylist.List) {
+	iterator := list.Iterator()
+	for iterator.Next() {
+		index, value := iterator.Index(), iterator.Value()
+		fmt.Printf("item #%d == %s\n", index, value)
+	}
+	fmt.Println()
+}
+
+func main() {
+	list := arraylist.New()
+	list.Add("a")
+	list.Add("c", "b")
+
+	printList(list)
+
+	list.Swap(0, 1)
+	list.Swap(1, 2)
+	printList(list)
+
+	list.Remove(2)
+	printList(list)
+
+	list.Remove(1)
+	printList(list)
+}
