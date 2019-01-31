a5f37f65d30d458a733f2f3dd8cbe5b82620d36b
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-31 09:13:58 +0100

    Example 12: basic file operations

---
+++ article_10/12_file_operations.go
@@ -0,0 +1,16 @@
+// Seriál "Programovací jazyk Go"
+//
+// Desátá část
+//
+// Demonstrační příklad číslo 12:
+//    Základní operace se soubory.
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+}
