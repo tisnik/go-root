66a4dec4232ed14c9682438299a05c5a0c28fa8e
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-11 12:28:36 +0100

    Example 13: HTTP server with state

---
+++ article_11/13_http_server_with_state.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+var counter int
+
+func mainEndpoint(writer http.ResponseWriter, request *http.Request) {
+	io.WriteString(writer, "Hello world!\n")
+}
+
+func counterEndpoint(writer http.ResponseWriter, request *http.Request) {
+	counter++
+	fmt.Fprintf(writer, "Counter: %d\n", counter)
+}
+
+func main() {
+	http.HandleFunc("/", mainEndpoint)
+	http.HandleFunc("/counter", counterEndpoint)
+	http.ListenAndServe(":8000", nil)
+}
