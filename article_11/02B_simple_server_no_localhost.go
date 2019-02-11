0a4e46b6b3e0fa57d45cfb826ed0afc96a22dcd8
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-11 16:09:03 +0100

    No localhost specified for server

---
+++ article_11/02B_simple_server_no_localhost.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net"
+)
+
+func main() {
+	cnt := byte(0)
+
+	l, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		println("Can't open the port!")
+	}
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		defer conn.Close()
+		if err != nil {
+			println("Connection refused!")
+		} else {
+			var b = []byte{cnt}
+			cnt++
+			conn.Write(b)
+		}
+	}
+}
