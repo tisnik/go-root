ed4b36dcd48d4d03c2105105d3f9ec272c67afd8
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-11 12:28:36 +0100

    Example 5: text client

---
+++ article_11/05_text_client.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:1234")
+	if err != nil {
+		println("Connection refused!")
+	} else {
+		fmt.Fprintf(conn, "Hello")
+		status, err := bufio.NewReader(conn).ReadString('\n')
+		println(status, err)
+		if err != nil {
+			println("No response!")
+		} else { 
+			println(status)
+		}
+	}
+}
