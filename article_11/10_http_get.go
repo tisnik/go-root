486041d34eb38a65994d0c5af4703a16164b7afd
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-11 12:28:36 +0100

    Example 10: implementation of HTTP GET method

---
+++ article_11/10_http_get.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	response, err := http.Get("http://httpbin.org/uuid")
+	if err != nil {
+		println("Connection refused")
+	}
+	defer response.Body.Close()
+
+	fmt.Printf("Status: %s\n", response.Status)
+	fmt.Printf("Content length: %d\n", response.ContentLength)
+
+	raw_body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		println("Response body can't be read")
+	} else {
+		body := string(raw_body)
+		println(body)
+	}
+}
