df1813bbb0fca79ba99bc12719b14921745418f5
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-11 12:28:36 +0100

    Example 3: slow socket server

---
+++ article_11/03_slow_server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net"
+	"time"
+)
+
+func main() {
+	cnt := byte(0)
+
+	l, err := net.Listen("tcp", "localhost:1234")
+	if err != nil {
+		println("Can't open the port!")
+	}
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		time.Sleep(2 * time.Second)
+		defer conn.Close()
+		if err != nil {
+			println("Connection refused!")
+		} else {
+			var b = []byte{cnt}
+			cnt++
+			conn.Write(b)
+		}
+	}
+}
