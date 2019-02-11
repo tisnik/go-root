cbeed880320a42bb3b75ac0bc0e6690a35e479bb
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-11 12:28:36 +0100

    Example 8: parse IP address

---
+++ article_11/08_parse_ip.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func parseIP(address string) {
+	ip := net.ParseIP(address)
+	if ip == nil {
+		println("ParseIP failure")
+	} else {
+		fmt.Printf("%v\n", ip)
+	}
+}
+
+func main() {
+	parseIP("127.0.0.1")
+	parseIP("1000::68")
+	parseIP("fe80::224:d7ff:fe83:1f28")
+}
