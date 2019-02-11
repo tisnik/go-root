a6f3f7890972e97291d9afbf8df88881e487db00
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-11 12:28:36 +0100

    Example 4: server that supports multiple connections

---
+++ article_11/04_multi_connection_server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net"
+)
+
+func main() {
+	cnt := byte(0)
+
+	l, err := net.Listen("tcp", "localhost:1234")
+	if err != nil {
+		println("Can't open the port!")
+	}
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			println("Connection refused!")
+		} else {
+			println("Connected")
+			go func(c net.Conn) {
+				var b = []byte{cnt}
+				cnt++
+				c.Write(b)
+				c.Close()
+			}(conn)
+		}
+	}
+}
