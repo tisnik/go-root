6546939a61102e5eefad6b8a7ff6b35f31e49f67
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-11 12:28:36 +0100

    Example 1: simple socket client

---
+++ article_11/01_simple_client.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:1234")
+	if err != nil {
+		println("Connection refused!")
+	} else {
+		var b [1]byte
+		n, err := conn.Read(b[:])
+		if err != nil {
+			println("No response!")
+		} else {
+			if n == 1 {
+				fmt.Printf("Received %d byte: %v\n", n, b)
+			} else {
+				fmt.Printf("Received %d bytes: %v\n", n, b)
+			}
+		}
+	}
+}
