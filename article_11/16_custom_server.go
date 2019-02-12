6b65aaa0cbd914ca355bfd0026e3e3d25a3174d0
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-12 13:24:15 +0100

    Example 16: custom HTTP server

---
+++ article_11/16_custom_server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"sync"
+)
+
+var counter int
+var mutex = &sync.Mutex{}
+
+func mainEndpoint(writer http.ResponseWriter, request *http.Request) {
+	io.WriteString(writer, "Hello world!\n")
+}
+
+func counterEndpoint(writer http.ResponseWriter, request *http.Request) {
+	mutex.Lock()
+	counter++
+	fmt.Fprintf(writer, "Counter: %d\n", counter)
+	mutex.Unlock()
+}
+
+func filesEndpoint(writer http.ResponseWriter, request *http.Request) {
+	url := request.URL.Path[len("/files/"):]
+	println("Serving file from URL: " + url)
+	http.ServeFile(writer, request, url)
+}
+
+func main() {
+	http.HandleFunc("/", mainEndpoint)
+	http.HandleFunc("/counter", counterEndpoint)
+	http.HandleFunc("/files/", filesEndpoint)
+	http.ListenAndServe(":8000", nil)
+}
