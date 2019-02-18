79b07c8e40165d1507d1ee05ac3b135b0de20f90
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-18 08:38:38 +0100

    Example 11: HTTPS server

---
+++ article_12/11_https_server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func mainEndpoint(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/plain")
+	io.WriteString(writer, "Hello world!\n")
+}
+
+func main() {
+	http.HandleFunc("/", mainEndpoint)
+	err := http.ListenAndServeTLS(":4443", "server.crt", "server.key", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
