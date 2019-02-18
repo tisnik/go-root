ad1e64524f9c040d2de3e66a66d4652e53f1c9d9
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-18 08:38:38 +0100

    Example 3: specify Content-Type in HTTP response

---
+++ article_12/03_specify_content_type.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+)
+
+func endpointHtml(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/html")
+	response := "<body><h1>Hello world!</h1></body>\n"
+	writer.Write([]byte(response))
+}
+
+func endpointText(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/plain")
+	response := "Hello world!\n"
+	writer.Write([]byte(response))
+}
+
+func endpointAsm(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/x-asm")
+	response := "START: brk\n"
+	writer.Write([]byte(response))
+}
+
+func main() {
+	http.HandleFunc("/html", endpointHtml)
+	http.HandleFunc("/text", endpointText)
+	http.HandleFunc("/asm", endpointAsm)
+	http.ListenAndServe(":8000", nil)
+}
