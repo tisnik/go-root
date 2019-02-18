13e6fd27e90d39722de6044e9af3c1f923ea1295
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-18 08:38:38 +0100

    Example 9: better factorial web app

---
+++ article_12/09_factorial_compute.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
+)
+
+type FactorialPageDynContent struct {
+	N      int64
+	Result int64
+}
+
+func Factorial(n int64) int64 {
+	switch {
+	case n < 0:
+		return 1
+	case n == 0:
+		return 1
+	default:
+		return n * Factorial(n-1)
+	}
+}
+
+func mainEndpoint(writer http.ResponseWriter, request *http.Request) {
+	err := request.ParseForm()
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	n, err := strconv.ParseInt(request.FormValue("n"), 10, 64)
+	if err != nil {
+		n = 0
+	}
+
+	t, err := template.ParseFiles("factorial_compute.html")
+	if err != nil {
+		writer.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(writer, "Not found!")
+		return
+	}
+
+	dynData := FactorialPageDynContent{N: n, Result: Factorial(n)}
+	err = t.Execute(writer, dynData)
+	if err != nil {
+		println("Error executing template")
+	}
+}
+
+func main() {
+	http.HandleFunc("/", mainEndpoint)
+	http.ListenAndServe(":8000", nil)
+}
