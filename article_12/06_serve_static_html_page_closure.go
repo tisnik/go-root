5f63e047e0b1cadeed719f6326912357a69000b7
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-18 08:38:38 +0100

    Example 6: usage of closures

---
+++ article_12/06_serve_static_html_page_closure.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func sendStaticPage(writer http.ResponseWriter, filename string) {
+	body, err := ioutil.ReadFile(filename)
+	if err == nil {
+		fmt.Fprint(writer, string(body))
+	} else {
+		writer.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(writer, "Not found!")
+	}
+}
+
+func staticPage(filename string) func(writer http.ResponseWriter, request *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		sendStaticPage(writer, filename)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", staticPage("index.html"))
+	http.ListenAndServe(":8000", nil)
+}
