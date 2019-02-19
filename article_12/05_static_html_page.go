8bdb8d5a5dd4ba6d17106fcfd3da70773f6ee561
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-19 08:19:49 +0100

    Rest of examples

---
+++ article_12/05_static_html_page.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func mainEndpoint(writer http.ResponseWriter, request *http.Request) {
+	body, err := ioutil.ReadFile("index.html")
+	if err == nil {
+		fmt.Fprint(writer, string(body))
+	} else {
+		writer.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(writer, "Not found!")
+	}
+}
+
+func missingEndpoint(writer http.ResponseWriter, request *http.Request) {
+	body, err := ioutil.ReadFile("missing.html")
+	if err == nil {
+		fmt.Fprint(writer, string(body))
+	} else {
+		writer.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(writer, "Not found!")
+	}
+}
+
+func main() {
+	http.HandleFunc("/", mainEndpoint)
+	http.HandleFunc("/missing", missingPageEndpoint)
+	http.ListenAndServe(":8000", nil)
+}
