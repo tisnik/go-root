af25691e693c54b17d401dc43a83bc8e252accd3
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-22 08:09:14 +0100

    Example 5: structures marshalling

---
+++ article_13/05_json_marshal_struct.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type User1 struct {
+	id      uint32
+	name    string
+	surname string
+}
+
+type User2 struct {
+	Id      uint32
+	Name    string
+	Surname string
+}
+
+func main() {
+	user1 := User1{
+		1,
+		"Pepek",
+		"Vyskoč"}
+
+	user2 := User2{
+		1,
+		"Pepek",
+		"Vyskoč"}
+
+	user1_json, _ := json.Marshal(user1)
+	fmt.Println(string(user1_json))
+
+	user2_json, _ := json.Marshal(user2)
+	fmt.Println(string(user2_json))
+}
