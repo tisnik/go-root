b17b4f196f0d0fc488350a55f64cd808bbbe48c1
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-22 08:09:14 +0100

    Example 9: complex maps

---
+++ article_13/09_json_marshal_complex_map.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Identifiers struct {
+	UID     uint32
+	GID     uint32
+}
+
+type User struct {
+	Name    string
+	Surname string
+	Sign    []byte
+	Enabled bool
+	Ids     Identifiers
+}
+
+func main() {
+	mapOfUsers := make(map[string]User)
+
+	mapOfUsers["user-id-1"] = User{
+		Ids:     Identifiers{1, 1},
+		Name:    "Pepek",
+		Surname: "Vyskoč",
+		Enabled: true,
+		Sign:    []byte{0,0,0,0}}
+
+	mapOfUsers["user-id-2"] = User{
+		Ids:     Identifiers{2, 1},
+		Name:    "Josef",
+		Surname: "Vyskočil",
+		Enabled: false,
+		Sign:    []byte{42, 10, 0, 255}}
+
+	mapOfUsers["user-id-3"] = User{
+		Ids:     Identifiers{3, 1},
+		Name:    "Varel",
+		Surname: "Frištenský"}
+
+	mapOfUsers_json, _ := json.Marshal(mapOfUsers)
+	fmt.Println(string(mapOfUsers_json))
+}
