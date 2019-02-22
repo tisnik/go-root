fa3b1542432c40b80189681d6902e4a9701f7fad
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-22 08:09:14 +0100

    Example 6: array of structures marshalling

---
+++ article_13/06_json_mashal_array_of_struct.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type User struct {
+	Id      uint32
+	Name    string
+	Surname string
+}
+
+func main() {
+	var users = [3]User{
+		User{
+			Id:      1,
+			Name:    "Pepek",
+			Surname: "Vyskoč"},
+		User{
+			Id:      2,
+			Name:    "Pepek",
+			Surname: "Vyskoč"},
+		User{
+			Id:      3,
+			Name:    "Josef",
+			Surname: "Vyskočil"},
+	}
+
+	users_json, _ := json.Marshal(users)
+	fmt.Println(string(users_json))
+}
