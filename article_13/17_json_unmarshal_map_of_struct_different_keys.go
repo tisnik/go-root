4217174aac4fbe23b92cb0e5409b24a5bb5e1d56
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-25 09:03:45 +0100

    Example 17: map of struct unmarshalling

---
+++ article_13/17_json_unmarshal_map_of_struct_different_keys.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+type User struct {
+	Id      uint32 `json:"user-id"`
+	Name    string `json:"user-name"`
+	Surname string
+}
+
+func main() {
+	input_json_as_bytes, err := ioutil.ReadFile("users_map_different_keys.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Input (bytes):")
+	fmt.Println(input_json_as_bytes)
+
+	fmt.Println("\nInput (string):")
+	fmt.Println(string(input_json_as_bytes))
+
+	m1 := map[string]User{}
+	json.Unmarshal(input_json_as_bytes, &m1)
+
+	fmt.Println("\nOutput:")
+	fmt.Println(m1)
+
+	fmt.Println("\nUsers:")
+	for key, user := range m1 {
+		fmt.Printf("%s\t%d\t%s\t%s\n", key, user.Id, user.Name, user.Surname)
+	}
+}
