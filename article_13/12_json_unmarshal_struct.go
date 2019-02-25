099c9fe063a5da02149b4b7bd5ba619f99359fd3
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-25 09:02:19 +0100

    Example 12: struct unmarshalling

---
+++ article_13/12_json_unmarshal_struct.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type User struct {
+	Id      uint32
+	Name    string
+	Surname string
+}
+
+func main() {
+	input_json := `{
+    "Id":1,
+    "Name":"Pepek",
+    "Surname":"Vyskoč"
+}`
+	fmt.Println("Input:")
+	fmt.Println(input_json)
+
+	bytes := []byte(input_json)
+	var user User
+	json.Unmarshal(bytes, &user)
+
+	fmt.Println("\nOutput:")
+	fmt.Println(user)
+
+	fmt.Println("\nFields:")
+	fmt.Printf("ID:      %d\n", user.Id)
+	fmt.Printf("Name:    %s\n", user.Name)
+	fmt.Printf("Surname: %s\n", user.Surname)
+}
