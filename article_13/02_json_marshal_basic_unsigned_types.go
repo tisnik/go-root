61cf1405151b6fc07ff2ce7a750b131500247c7f
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-25 08:58:14 +0100

    Updated example 2: byte marshalling

--- article_13/02_json_marshal_basic_unsigned_types.go
+++ article_13/02_json_marshal_basic_unsigned_types.go
@@ -6,10 +6,14 @@ import (
 )
 
 func main() {
+	var b8 byte = 0x42
 	var a uint8 = 10
 	var b uint16 = 1000
 	var c uint32 = 10000
 	var d uint32 = 1000000
+
+	b8_json, _ := json.Marshal(b8)
+	fmt.Println(string(b8_json))
 
 	a_json, _ := json.Marshal(a)
 	fmt.Println(string(a_json))
