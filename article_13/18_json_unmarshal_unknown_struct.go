a2e500232e2d63bb24ddb2d0fdcc2a9768f1a468
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-25 09:04:13 +0100

    Example 18: unknown struct unmarshalling

---
+++ article_13/18_json_unmarshal_unknown_struct.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	input_json_as_bytes, err := ioutil.ReadFile("users_map_different_keys.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Input (bytes):")
+	fmt.Println(input_json_as_bytes)
+
+	fmt.Println("\nInput (string):")
+	fmt.Println(string(input_json_as_bytes))
+
+	m1 := map[string]interface{}{}
+	json.Unmarshal(input_json_as_bytes, &m1)
+
+	fmt.Println("\nOutput:")
+	fmt.Println(m1)
+
+	fmt.Println("\nUsers:")
+	for key, user := range m1 {
+		fmt.Printf("%s\t%s\n", key, user)
+	}
+}
