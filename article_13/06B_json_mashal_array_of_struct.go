8e7ee7a6dec0e5279bd0305ef77145e8f1534ff1
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-26 08:43:03 +0100

    Marshalling array of struct, second variant

---
+++ article_13/06B_json_mashal_array_of_struct.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type User struct {
+	id      uint32
+	name    string
+	surname string
+}
+
+func main() {
+	var users = [3]User{
+		User{
+			id:      1,
+			name:    "Pepek",
+			surname: "Vyskoč"},
+		User{
+			id:      2,
+			name:    "Pepek",
+			surname: "Vyskoč"},
+		User{
+			id:      3,
+			name:    "Josef",
+			surname: "Vyskočil"},
+	}
+
+	users_json, _ := json.Marshal(users)
+	fmt.Println(string(users_json))
+}
