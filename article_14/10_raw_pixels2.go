4ec7e09d4fbde9d549f1f3ec881c930c3278d62d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-04 09:15:47 +0100

    Example 10: přímý přístup k jednotlivým pixelům; druhá varianta

---
+++ article_14/10_raw_pixels2.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"image"
+	"image/png"
+	"os"
+)
+
+const width = 256
+const height = 256
+
+func main() {
+	img := image.NewNRGBA(image.Rect(0, 0, width, height))
+
+	outfile, err := os.Create("10.png")
+	if err != nil {
+		panic(err)
+	}
+	defer outfile.Close()
+
+	for y := 0; y < height; y++ {
+		scanline := img.Pix[img.Stride*y:]
+		i := 0
+		for x := 0; x < width; x++ {
+			scanline[i] = 0
+			i++
+			scanline[i] = byte(y)
+			i++
+			scanline[i] = 255
+			i++
+			scanline[i] = byte(x)
+			i++
+		}
+	}
+	png.Encode(outfile, img)
+}
