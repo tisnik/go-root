3f6d40b6365e6d4e3f29e47da1cd8869be2c423b
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-04 09:15:47 +0100

    Example 15: základní vlastnosti barvového prostoru CMYK

---
+++ article_14/15_cmyk_images.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"os"
+)
+
+const width = 512
+const height = 512
+
+func main() {
+	img := image.NewCMYK(image.Rect(0, 0, width, height))
+
+	outfile, err := os.Create("15.png")
+	if err != nil {
+		panic(err)
+	}
+	defer outfile.Close()
+
+	for x := 0; x < 256; x++ {
+		for y := 0; y < 256; y++ {
+			c := color.CMYK{byte(x), 0, 0, 0}
+			img.SetCMYK(x, y, c)
+
+			c = color.CMYK{0, byte(x), 0, 0}
+			img.SetCMYK(x+256, y, c)
+
+			c = color.CMYK{0, 0, byte(x), 0}
+			img.SetCMYK(x, y+256, c)
+
+			c = color.CMYK{0, 0, 0, byte(x)}
+			img.SetCMYK(x+256, y+256, c)
+		}
+	}
+	png.Encode(outfile, img)
+}
