5f3463bd5ed6efa30f5605f5a8ec2a59d2078ecd
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-04 09:15:47 +0100

    Example 2: alternativní způsob vytvoření obdélníku pro určení rozměrů obrázku

---
+++ article_14/02_empty_image_rectangle.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+        "image"
+        "image/png"
+        "os"
+)
+
+const width = 256
+const height = 256
+
+func main() {
+        rect := image.Rectangle{image.Point{0, 0},
+                                image.Point{width, height}}
+
+        img := image.NewNRGBA(rect)
+
+        outfile, err := os.Create("02.png")
+        if err != nil {
+                panic(err)
+        }
+        defer outfile.Close()
+        png.Encode(outfile, img)
+}
