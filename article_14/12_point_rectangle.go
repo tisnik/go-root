6d2385bc2c7fd843a703e4516809d76396703076
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-04 09:15:47 +0100

    Example 12: datové struktury Point a Rectangle

---
+++ article_14/12_point_rectangle.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"image"
+)
+
+func main() {
+	point1 := image.Pt(10, 10)
+	point2 := image.Point{10, 10}
+
+	rectangle1 := image.Rect(0, 0, 320, 240)
+	rectangle2 := image.Rectangle{image.Point{0, 0}, image.Point{320, 240}}
+
+	println(point1.String())
+	println(point2.String())
+
+	println(rectangle1.String())
+	println(rectangle2.String())
+}
