b4c76eda584c96aef10b62acef099e04a395bebd
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-04 09:15:47 +0100

    Example 7: vylepšená varianta předchozího příkladu

---
+++ article_14/07_filled_image_alpha_correct.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"os"
+)
+
+const width = 256
+const height = 256
+
+func main() {
+	img := image.NewNRGBA(image.Rect(0, 0, width, height))
+
+	outfile, err := os.Create("07.png")
+	if err != nil {
+		panic(err)
+	}
+	defer outfile.Close()
+
+	for x := 0; x < width; x++ {
+		alpha := byte(x)
+		c := color.NRGBA{0, 255, 0, alpha}
+		for y := 0; y < height; y++ {
+			img.Set(x, y, c)
+		}
+	}
+	png.Encode(outfile, img)
+}
