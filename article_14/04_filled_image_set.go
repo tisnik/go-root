d5d9bc442eb77d96ea56363f89b0a6a9367919a1
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-04 09:15:47 +0100

    Example 4: rastrový obrázek vyplněný konstantní barvou, použití metody Set

---
+++ article_14/04_filled_image_set.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"os"
+)
+
+const width = 256
+const height = 256
+
+func main() {
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	outfile, err := os.Create("04.png")
+	if err != nil {
+		panic(err)
+	}
+	defer outfile.Close()
+
+	c := color.RGBA{0, 255, 0, 255}
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			img.Set(x, y, c)
+		}
+	}
+	png.Encode(outfile, img)
+}
