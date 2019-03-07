5206515071d71793ab0343cc62ac1ba1514a5b8e
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-07 08:33:45 +0100

    Example 4: publisher and subscriber in one app, two-channel approach

---
+++ queues/push-pull-one-app-2/publisher_subscriber.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-stomp/stomp"
+	"time"
+)
+
+const serverAddr = "localhost:61613"
+const queueName = "/queue/go_test"
+
+const messageCount = 10
+
+var stopProducer = make(chan bool)
+var stopConsumer = make(chan bool)
+
+func sendMessages(stop chan bool) {
+	defer func() {
+		stop <- true
+	}()
+
+	conn, err := stomp.Dial("tcp", serverAddr, nil)
+	if err != nil {
+		println("Cannot connect to server", err.Error())
+		return
+	} else {
+		println("Publisher part connected to server", serverAddr)
+	}
+	defer conn.Disconnect()
+
+	time.Sleep(5 * time.Second)
+
+	for i := 1; i <= messageCount; i++ {
+		text := fmt.Sprintf("Message #%d", i)
+		err = conn.Send(queueName, "text/plain", []byte(text), nil)
+		if err != nil {
+			println("Failed to send to server", err)
+			return
+		} else {
+			println("Message sent")
+		}
+	}
+	println("Sending EXIT message")
+	err = conn.Send(queueName, "text/plain", []byte("EXIT"), nil)
+	if err != nil {
+		println("Failed to send EXIT message to server", err)
+		return
+	} else {
+		println("Message sent")
+	}
+	println("Publisher finished")
+}
+
+func receiveMessages(subscribed chan bool, stop chan bool) {
+	defer func() {
+		stop <- true
+	}()
+
+	conn, err := stomp.Dial("tcp", serverAddr, nil)
+
+	if err != nil {
+		println("Cannot connect to server", err.Error())
+		return
+	} else {
+		println("Subscriber part connected to server", serverAddr)
+	}
+
+	defer conn.Disconnect()
+
+	time.Sleep(5 * time.Second)
+
+	sub, err := conn.Subscribe(queueName, stomp.AckAuto)
+	if err != nil {
+		println("Cannot subscribe to", queueName, err.Error())
+		return
+	}
+	close(subscribed)
+
+	for {
+		msg := <-sub.C
+		text := string(msg.Body)
+		if text != "EXIT" {
+			println("Received message", text)
+		} else {
+			println("Received EXIT command")
+			break
+		}
+	}
+	println("Subscriber finished")
+}
+
+func main() {
+	go sendMessages(stopProducer)
+	subscribed := make(chan bool)
+	go receiveMessages(subscribed, stopConsumer)
+
+	<-subscribed
+
+	<-stopProducer
+	<-stopConsumer
+}
