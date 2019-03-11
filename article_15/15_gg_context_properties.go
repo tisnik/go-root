a23ab8cf58ad4eb672db0fdb1a36bd65952e4d82
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-11 08:59:19 +0100

    Example 15: Context properties

---
+++ article_15/15_gg_context_properties.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/fogleman/gg"
+
+func main() {
+	const width = 320
+	const height = 240
+
+	dc := gg.NewContext(width, height)
+
+	dc.DrawCircle(width/2, height/2, 100)
+
+	dc.SetRGB(0.2, 0.0, 0.8)
+	dc.Fill()
+
+	dc.SavePNG("15.png")
+}
