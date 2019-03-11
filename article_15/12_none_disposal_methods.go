3f10b124c5740ae2903ae22a25b4fd491c19c741
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-11 08:59:19 +0100

    Example 12: Disposal method - none

---
+++ article_15/12_none_disposal_methods.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/draw"
+	"image/gif"
+	"os"
+)
+
+const BoardSize = 8
+
+func CreateImage(width int, height int, imageIndex int) *image.Paletted {
+	var palette = []color.Color{
+		color.RGBA{150, 150, 150, 255},
+		color.RGBA{250, 0, 0, 255},
+	}
+
+	if imageIndex == 0 {
+		img := image.NewPaletted(image.Rect(0, 0, width, height), palette)
+
+		r := image.Rect(0, 0, width, height)
+		draw.Draw(img, r, &image.Uniform{palette[0]}, image.ZP, draw.Src)
+
+		r2 := image.Rect(0, 0, 50, height)
+		draw.Draw(img, r2, &image.Uniform{palette[1]}, image.ZP, draw.Src)
+
+		return img
+	} else {
+		img := image.NewPaletted(image.Rect(imageIndex*60, 0, imageIndex*60+50, height), palette)
+
+		r2 := image.Rect(imageIndex*60, 0, imageIndex*60+50, height)
+		draw.Draw(img, r2, &image.Uniform{palette[1]}, image.ZP, draw.Src)
+
+		return img
+	}
+
+}
+
+func main() {
+	var images []*image.Paletted
+	var delays []int
+	var disposals []byte
+
+	for i := 0; i < 6; i++ {
+		img := CreateImage(350, 50, i)
+		images = append(images, img)
+		delays = append(delays, 100)
+		disposals = append(disposals, gif.DisposalNone)
+	}
+
+	outfile, err := os.Create("12.gif")
+	if err != nil {
+		panic(err)
+	}
+	defer outfile.Close()
+
+	gif.EncodeAll(outfile, &gif.GIF{
+		Image:    images,
+		Delay:    delays,
+		Disposal: disposals,
+	})
+}
