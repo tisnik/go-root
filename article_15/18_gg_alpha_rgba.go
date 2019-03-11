fdc616e0aa2e5708349605501c55e1d2a0ac6b7f
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-11 08:59:19 +0100

    Example 18: Set RGBA color (with alpha)

---
+++ article_15/18_gg_alpha_rgba.go
@@ -0,0 +1,27 @@
+package main
+
+import "github.com/fogleman/gg"
+
+func main() {
+	const width = 320
+	const height = 240
+
+	dc := gg.NewContext(width, height)
+
+	dc.DrawRectangle(0, 0, width, height)
+	dc.SetRGB(1.0, 1.0, 1.0)
+	dc.Fill()
+
+	for i := 0; i < 256; i += 8 {
+		// průhlednost je hodnota v rozsahu 0.0 až 1.0
+		alpha := float64(i) / 256.0
+		dc.SetRGBA(0.0, 0.0, 0.0, alpha)
+
+		x := float64(i + 32)
+		dc.DrawLine(x, 20, x, height-20)
+
+		dc.Stroke()
+	}
+
+	dc.SavePNG("18.png")
+}
