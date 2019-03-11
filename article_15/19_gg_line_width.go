6710b61a6ee731dc14f250d74100c38cfe793ea7
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-11 08:59:19 +0100

    Example 19: Set line width

---
+++ article_15/19_gg_line_width.go
@@ -0,0 +1,28 @@
+package main
+
+import "github.com/fogleman/gg"
+
+func main() {
+	const width = 320
+	const height = 240
+
+	dc := gg.NewContext(width, height)
+
+	dc.DrawRectangle(0, 0, width, height)
+	dc.SetRGB(1.0, 1.0, 1.0)
+	dc.Fill()
+
+	dc.SetRGBA(0.0, 0.0, 0.0, 1)
+
+	for i := 0; i < 256; i += 16 {
+		width := float64(i) / 20
+		dc.SetLineWidth(width)
+
+		x := float64(i + 32)
+		dc.DrawLine(x, 20, x, height-20)
+
+		dc.Stroke()
+	}
+
+	dc.SavePNG("19.png")
+}
