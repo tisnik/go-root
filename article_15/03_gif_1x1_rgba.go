9dcc356dad1c9d5a07fa78f12f7416c47e2b0bf3
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-11 08:59:19 +0100

    Example 3: Export small RGBA image 1x1 pixel into GIF

---
+++ article_15/03_gif_1x1_rgba.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"os"
+)
+
+func CreateImage(width int, height int) *image.RGBA {
+	c := color.RGBA{255, 0, 0, 255}
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	img.Set(0, 0, c)
+
+	return img
+}
+
+func main() {
+	img := CreateImage(1, 1)
+
+	outfile, err := os.Create("03.gif")
+	if err != nil {
+		panic(err)
+	}
+	defer outfile.Close()
+
+	err = gif.Encode(outfile, img, nil)
+	if err != nil {
+		panic(err)
+	}
+}
