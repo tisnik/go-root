f9485771818ff7c5ba56d74ba4d1ce77fe02e2d3
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-11 08:59:19 +0100

    Example 4: Export small paletted image 1x1 pixel into GIF

---
+++ article_15/04_gif_1x1_palette.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"os"
+)
+
+func CreateImage(width int, height int) *image.Paletted {
+	var palette = []color.Color{
+		color.RGBA{255, 128, 128, 255},
+		color.RGBA{255, 255, 255, 255},
+	}
+
+	c := color.RGBA{255, 128, 128, 255}
+	img := image.NewPaletted(image.Rect(0, 0, width, height), palette)
+	img.Set(0, 0, c)
+
+	return img
+}
+
+func main() {
+	img := CreateImage(1, 1)
+
+	outfile, err := os.Create("04.gif")
+	if err != nil {
+		panic(err)
+	}
+	defer outfile.Close()
+
+	err = gif.Encode(outfile, img, nil)
+	if err != nil {
+		panic(err)
+	}
+}
