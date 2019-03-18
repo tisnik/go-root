65d2c5fd4744554abc60d6481ddce98483fa0bdf
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-18 08:33:49 +0100

    Example 1: clear screen by Fill method

---
+++ article_16/01_gg_clear_screen.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/fogleman/gg"
+
+func main() {
+	const width = 320
+	const height = 240
+
+	dc := gg.NewContext(width, height)
+
+	dc.DrawRectangle(0, 0, width, height)
+	dc.SetRGB(1.0, 1.0, 1.0)
+	dc.Fill()
+
+	dc.SavePNG("01.png")
+}
