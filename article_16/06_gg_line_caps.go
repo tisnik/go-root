3fffbcb61440769e223f7ea0cc4fe1efb36d86f1
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-18 08:33:49 +0100

    Example 6: changing line caps

---
+++ article_16/06_gg_line_caps.go
@@ -0,0 +1,60 @@
+package main
+
+import "github.com/fogleman/gg"
+
+func main() {
+	const width = 320
+	const height = 240
+
+	dc := gg.NewContext(width, height)
+
+	dc.DrawRectangle(0, 0, width, height)
+	dc.SetRGB(1.0, 1.0, 1.0)
+	dc.Fill()
+
+	dc.SetRGBA(0.0, 0.0, 0.0, 1.0)
+
+	dc.SetLineWidth(10.0)
+
+	for i := 0; i < 256; i += 16 {
+		x := float64(i + 32)
+
+		width := float64(i) / 20
+		dc.SetLineWidth(width)
+
+		dc.SetLineCapRound()
+		dc.DrawLine(x, 20, x, 75)
+		dc.Stroke()
+
+		dc.SetLineCapButt()
+		dc.DrawLine(x, 92, x, height-92)
+		dc.Stroke()
+
+		dc.SetLineCapSquare()
+		dc.DrawLine(x, height-75, x, height-20)
+		dc.Stroke()
+	}
+
+	dc.SetRGBA(1.0, 0.0, 0.0, 1.0)
+	dc.SetLineWidth(1.0)
+
+	dc.DrawLine(32, 20, width-32, 20)
+	dc.Stroke()
+
+	dc.DrawLine(32, 75, width-32, 75)
+	dc.Stroke()
+
+	dc.DrawLine(32, 92, width-32, 92)
+	dc.Stroke()
+
+	dc.DrawLine(32, height-92, width-32, height-92)
+	dc.Stroke()
+
+	dc.DrawLine(32, height-75, width-32, height-75)
+	dc.Stroke()
+
+	dc.DrawLine(32, height-20, width-32, height-20)
+	dc.Stroke()
+
+	dc.SavePNG("06.png")
+}
