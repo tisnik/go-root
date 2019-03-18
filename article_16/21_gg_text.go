987f5529d883bc9668ada534d3f231c7397fc545
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-18 08:33:49 +0100

    Example 21: simple text rendering

---
+++ article_16/21_gg_text.go
@@ -0,0 +1,23 @@
+package main
+
+import "github.com/fogleman/gg"
+
+func main() {
+	const width = 320
+	const height = 240
+
+	dc := gg.NewContext(width, height)
+
+	dc.DrawRectangle(0, 0, width, height)
+	dc.SetRGB(1.0, 1.0, 1.0)
+	dc.Fill()
+
+	dc.SetRGB(0.0, 0.0, 0.0)
+	if err := dc.LoadFontFace("luxisr.ttf", 36); err != nil {
+		println("Cannot load font")
+		panic(err)
+	}
+	dc.DrawString("Hello, world!", 0, height)
+
+	dc.SavePNG("21.png")
+}
