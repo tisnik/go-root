64b9fcfed1653022e9933f73291a39ab6337a2eb
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-18 08:33:49 +0100

    Example 23: centered text rendering

---
+++ article_16/23_gg_text_centered.go
@@ -0,0 +1,31 @@
+package main
+
+import "github.com/fogleman/gg"
+
+func main() {
+	const width = 320
+	const height = 240
+
+	dc := gg.NewContext(width, height)
+
+	dc.DrawRectangle(0, 0, width, height)
+	dc.SetRGB(1.0, 1.0, 1.0)
+	dc.Fill()
+
+	dc.SetRGB(0.0, 0.0, 0.0)
+	if err := dc.LoadFontFace("luxisr.ttf", 24); err != nil {
+		println("Cannot load font")
+		panic(err)
+	}
+
+	var h float64 = 20
+	weight := -1.0
+
+	for i := 0; i < 11; i++ {
+		dc.DrawStringAnchored("Hello, world!", width/2, h, weight, 0.5)
+		h += 20
+		weight += 0.2
+	}
+
+	dc.SavePNG("23.png")
+}
