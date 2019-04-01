753256194449092f4b762b07a835dbe44756af10
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-01 09:50:06 +0200

    Example 9: basic usage of 'Convey' package

---
+++ article_18/09_factorial_convey/behaviour_test.go
@@ -0,0 +1,48 @@
+package factorial
+
+import (
+	"github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestFactorialForZero(t *testing.T) {
+	convey.Convey("0! should be equal 1", t, func() {
+		convey.So(Factorial(0), convey.ShouldEqual, 1)
+	})
+}
+
+func TestFactorialForOne(t *testing.T) {
+	convey.Convey("1! should be equal 1", t, func() {
+		convey.So(Factorial(1), convey.ShouldEqual, 1)
+	})
+}
+
+func TestFactorialForSmallNumber(t *testing.T) {
+	convey.Convey("5! should be between 1 and 10000", t, func() {
+		convey.So(Factorial(5), convey.ShouldBeBetween, 1, 10000)
+	})
+}
+
+func TestFactorialForSmallNumberNegative(t *testing.T) {
+	convey.Convey("20! should be between 1 and 10000", t, func() {
+		convey.So(Factorial(20), convey.ShouldBeBetween, 1, 10000)
+	})
+}
+
+func TestFactorialForTen(t *testing.T) {
+	convey.Convey("10! should be equal to 3628800", t, func() {
+		convey.So(Factorial(10), convey.ShouldEqual, 3628800)
+	})
+}
+
+func TestFactorialForBigNumber(t *testing.T) {
+	convey.Convey("20! should be greater than zero", t, func() {
+		convey.So(Factorial(20), convey.ShouldBeGreaterThan, 0)
+	})
+}
+
+func TestFactorialForEvenBiggerNumber(t *testing.T) {
+	convey.Convey("30! should be greater than zero", t, func() {
+		convey.So(Factorial(30), convey.ShouldBeGreaterThan, 0)
+	})
+}
