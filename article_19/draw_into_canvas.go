7c8cd014834d2b12d96a0d0df6910dc1066947e2
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-03 07:20:46 +0200

    4th example: draw into 2D canvas

---
+++ article_19/draw_into_canvas.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"syscall/js"
+)
+
+func main() {
+	const CanvasWidth = 256
+	const CanvasHeight = 256
+
+	println("started")
+
+	window := js.Global()
+	document := window.Get("document")
+
+	canvas := document.Call("createElement", "canvas")
+	canvas.Set("height", CanvasWidth)
+	canvas.Set("width", CanvasHeight)
+	document.Get("body").Call("appendChild", canvas)
+
+	context2d := canvas.Call("getContext", "2d")
+	context2d.Set("fillStyle", "#c0c0c0")
+	context2d.Call("fillRect", 0, 0, CanvasWidth, CanvasHeight)
+
+	context2d.Set("fillStyle", "yellow")
+	context2d.Call("fillRect", 10, 10, CanvasWidth-20, CanvasHeight-20)
+
+	println("finished")
+}
