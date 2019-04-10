ceb71cb393cfbc49c536fd0afcf71a7dde8309a1
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-10 08:12:56 +0200

    Example 4: fully prompt

---
+++ article_20/04_prompt.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/c-bata/go-prompt"
+
+func executor(t string) {
+	println(t)
+}
+
+func completer(in prompt.Document) []prompt.Suggest {
+	return []prompt.Suggest{}
+}
+
+func main() {
+	p := prompt.New(executor, completer)
+	p.Run()
+}
