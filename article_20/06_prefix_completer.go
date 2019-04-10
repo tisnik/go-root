e6f84b13d48873fefc4e32dfacff7adcbcf9f4d1
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-10 08:12:56 +0200

    Example 6: prefix-based completer

---
+++ article_20/06_prefix_completer.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/c-bata/go-prompt"
+	"os"
+)
+
+func executor(t string) {
+	switch t {
+	case "exit":
+		fallthrough
+	case "quit":
+		println("Quitting")
+		os.Exit(0)
+	case "help":
+		println("HELP:\nexit\nquit")
+	default:
+		println("Nothing happens")
+	}
+}
+
+func completer(in prompt.Document) []prompt.Suggest {
+	s := []prompt.Suggest{
+		{Text: "help"},
+		{Text: "exit"},
+		{Text: "quit"},
+	}
+	return prompt.FilterHasPrefix(s, in.GetWordBeforeCursor(), true)
+}
+
+func main() {
+	p := prompt.New(executor, completer)
+	p.Run()
+}
