7965e0a6dc222bf4a5473a6fe30e2066df9eedbb
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-10 08:12:56 +0200

    Example 1: standard Scanln function

---
+++ article_20/01_scanln.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var login string
+	var password string
+
+	print("Login: ")
+	n, err := fmt.Scanln(&login)
+	if n != 1 || err != nil {
+		println("Error reading login")
+	}
+
+	print("Password: ")
+	n, err = fmt.Scanln(&password)
+	if n != 1 || err != nil {
+		println("Error reading password")
+	}
+
+	println(login)
+	println(password)
+}
