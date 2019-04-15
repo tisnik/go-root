89f8d14287dd7205d7bd4e8da8999d7781870731
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-15 12:39:22 +0200

    Example 4: usage of Duration for two jobs

---
+++ article_21/clockwerk/jobs4.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/onatm/clockwerk"
+	"time"
+)
+
+type Task1 struct{}
+type Task2 struct{}
+
+func (t Task1) Run() {
+	println("task/job #1 called", time.Now().String())
+}
+
+func (t Task2) Run() {
+	println("task/job #2 called", time.Now().String())
+}
+
+func main() {
+	c := make(chan bool)
+
+	var task1 Task1
+	var task2 Task2
+
+	duration1, _ := time.ParseDuration("2s")
+	duration2, _ := time.ParseDuration("3s")
+
+	scheduler := clockwerk.New()
+	scheduler.Every(duration1).Do(task1)
+	scheduler.Every(duration2).Do(task2)
+	scheduler.Start()
+
+	<-c
+}
