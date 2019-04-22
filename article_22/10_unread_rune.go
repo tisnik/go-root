3b7c530b08f3eeb0a371cb12a98edb1ec0ae360d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-22 20:26:50 +0200

    Example 10: method UnreadRune

---
+++ article_22/10_unread_rune.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+func main() {
+	reader := strings.NewReader("* ěščř ½µ§я¤ *")
+	cnt := 0
+
+	for {
+		c, size, err := reader.ReadRune()
+		if err == io.EOF {
+			fmt.Println("\nEOF detected")
+			break
+		}
+		if err == nil {
+			fmt.Printf("%c %d\n", c, size)
+		} else {
+			fmt.Printf("\nerror %v", err)
+			break
+		}
+		cnt++
+		if cnt == 5 || cnt == 10 || cnt == 14 || cnt == 15 {
+			reader.UnreadRune()
+		}
+	}
+}
