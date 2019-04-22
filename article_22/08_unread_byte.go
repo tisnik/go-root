81509dd249bec956688fe36192f59bfcb045c38f
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-22 20:26:50 +0200

    Example 8: method UnreadByte

---
+++ article_22/08_unread_byte.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	reader := strings.NewReader("Hello world!")
+	cnt := 0
+
+	for {
+		b, err := reader.ReadByte()
+		if err == nil {
+			fmt.Printf("%c", b)
+		} else {
+			fmt.Printf("\nerror %v", err)
+			break
+		}
+		cnt++
+		if cnt == 5 || cnt == 10 || cnt == 14 || cnt == 15 {
+			reader.UnreadByte()
+		}
+	}
+}
