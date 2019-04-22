bb6a86aa6ac45ce0f9648fca6248d907406f3545
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-22 20:26:50 +0200

    Example 4: read runes from Unicode string

---
+++ article_22/04_read_rune_from_unicode.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+func main() {
+	reader := strings.NewReader("* ěščř ½µ§я¤ *")
+	for {
+		c, size, err := reader.ReadRune()
+		if err == io.EOF {
+			fmt.Println("\nEOF detected")
+			break
+		}
+		if err == nil {
+			fmt.Printf("%c %d\n", c, size)
+		} else {
+			fmt.Printf("\nerror %v", err)
+			break
+		}
+	}
+}
