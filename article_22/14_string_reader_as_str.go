d34a9c0eb32ac6c92bb59ddd6a35abdcc68861a0
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-22 20:26:50 +0200

    Example 14: read input as string

---
+++ article_22/14_string_reader_as_str.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+const input_string = "Hello world!"
+const buffer_size = 4
+
+func main() {
+	r := strings.NewReader(input_string)
+
+	buffer := make([]byte, buffer_size)
+
+	for {
+		read, err := r.Read(buffer)
+
+		if read > 0 {
+			fmt.Printf("read %d bytes\n", read)
+			fmt.Println(string(buffer[:read]))
+		}
+
+		if err == io.EOF {
+			fmt.Println("reached end of file")
+			break
+		}
+
+		if err != nil {
+			fmt.Printf("other error %v\n", err)
+			break
+		}
+	}
+}
