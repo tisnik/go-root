c905e0a56eb575c4c33f880bb81470b20e63788d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-22 20:26:50 +0200

    Example 6: simple string buffer

---
+++ article_22/06_write_byte_read_rune.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func main() {
+	buffer := bytes.Buffer{}
+	buffer.WriteByte(65)
+
+	c, size, err := buffer.ReadRune()
+	fmt.Printf("%c %d %v\n", c, size, err)
+
+	buffer.WriteByte(0xc4)
+	buffer.WriteByte(0x9b)
+
+	c, size, err = buffer.ReadRune()
+	fmt.Printf("%c %d %v\n", c, size, err)
+
+	c, size, err = buffer.ReadRune()
+	fmt.Printf("%c %d %v\n", c, size, err)
+}
