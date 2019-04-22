753de1a66872d3dba825afd6e0996085137567b5
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-22 20:26:50 +0200

    Example 9: unread more bytes from standard input

---
+++ article_22/09_unread_more_bytes.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	reader := strings.NewReader("Hello world!")
+	cnt := 0
+
+	for {
+		b, err := reader.ReadByte()
+		if err == nil {
+			fmt.Printf("%c", b)
+		} else {
+			fmt.Printf("\nerror %v", err)
+			break
+		}
+		cnt++
+		if cnt == 6 {
+			for i := 0; i <= 6; i++ {
+				reader.UnreadByte()
+			}
+		}
+	}
+}
