c35a599a8e859435abb9ef2ee10ab00c9624b156
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-29 08:31:31 +0200

    Example 6: usage of Seek() to absolute position (from start of file)

---
+++ article_23/06_reader_seeker_absolute.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+const input_string = "*** Hello world! ***"
+const buffer_size = 6
+
+func main() {
+	reader := strings.NewReader(input_string)
+
+	buffer := make([]byte, buffer_size)
+	reader.Seek(4, io.SeekStart)
+
+	for {
+		read, err := reader.Read(buffer)
+
+		if read > 0 {
+			fmt.Printf("read %d bytes translated into '%s'\n", read, buffer[:read])
+		}
+
+		if err == io.EOF {
+			fmt.Println("reached end of file")
+			break
+		}
+
+		if err != nil {
+			fmt.Printf("other error %v\n", err)
+			break
+		}
+	}
+}
