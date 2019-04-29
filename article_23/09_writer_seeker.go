02ea5d0b52208fc40ba0fc6bbb5edf2086ad8ec8
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-29 08:31:31 +0200

    Example 9: usage of Seek() during writing data

---
+++ article_23/09_writer_seeker.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+const filename = "small.bin"
+
+func writeMark(writer io.Writer) {
+	buffer := []byte("**")
+	written, err := writer.Write(buffer)
+
+	if written > 0 {
+		fmt.Printf("written %d bytes\n", written)
+	}
+
+	if err != nil {
+		fmt.Printf("I/O error %v\n", err)
+	}
+}
+
+func main() {
+	writer, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer writer.Close()
+
+	writeMark(writer)
+	writer.Seek(100, io.SeekCurrent)
+	writeMark(writer)
+	writer.Seek(100, io.SeekCurrent)
+	writeMark(writer)
+}
