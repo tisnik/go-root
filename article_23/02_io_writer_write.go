041ae188a688c815b9c1a6e31fc417bdf9ad99f2
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-29 08:31:31 +0200

    Example 2: usage of Writer to write into file

---
+++ article_23/02_io_writer_write.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+const filename = "test_output.txt"
+const message = "Hello world!"
+
+func main() {
+	writer, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer writer.Close()
+
+	buffer := []byte(message)
+
+	written, err := writer.Write(buffer)
+
+	if written > 0 {
+		fmt.Printf("written %d bytes\n", written)
+	}
+
+	if err != nil {
+		fmt.Printf("I/O error %v\n", err)
+	}
+}
