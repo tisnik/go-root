df42a80a6763fe139331d5d20e09268b4c26724d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-29 08:31:31 +0200

    Example 18: Reading GZIPped data via memory-based pipe

---
+++ article_23/18_gzip_writer_pipe.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"compress/gzip"
+	"fmt"
+	"io"
+)
+
+const message = "Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello "
+
+func main() {
+	reader, writer := io.Pipe()
+
+	gzipWriter := gzip.NewWriter(writer)
+
+	go func() {
+		buffer := []byte(message)
+		written, err := gzipWriter.Write(buffer)
+
+		if written >= 0 {
+			fmt.Printf("written %d bytes\n", written)
+		}
+
+		if err != nil {
+			fmt.Printf("I/O error %v\n", err)
+		}
+
+		gzipWriter.Close()
+		writer.Close()
+	}()
+
+	buffer := make([]byte, 100)
+
+	for {
+		read, err := reader.Read(buffer)
+
+		if read > 0 {
+			fmt.Printf("read %d bytes:", read)
+			for i := 0; i < read; i++ {
+				fmt.Printf(" %02x", buffer[i])
+			}
+			fmt.Println()
+		}
+
+		if err == io.EOF {
+			fmt.Println("reached end of file")
+			break
+		}
+
+		if err != nil {
+			fmt.Printf("other error %v\n", err)
+			break
+		}
+
+	}
+}
