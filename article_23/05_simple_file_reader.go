e734ff0f8fdc64fdddd053ef97e8a3e67c0d36f7
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-29 08:31:31 +0200

    example 5: simple file reader

---
+++ article_23/05_simple_file_reader.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+const input_string = "*** Hello world! ***"
+const buffer_size = 6
+
+func main() {
+	reader := strings.NewReader(input_string)
+
+	buffer := make([]byte, buffer_size)
+
+	for {
+		read, err := reader.Read(buffer)
+
+		if read > 0 {
+			fmt.Printf("read %d bytes translated into '%s'\n", read, buffer[:read])
+		}
+
+		if err == io.EOF {
+			fmt.Println("reached end of file")
+			break
+		}
+
+		if err != nil {
+			fmt.Printf("other error %v\n", err)
+			break
+		}
+	}
+}
