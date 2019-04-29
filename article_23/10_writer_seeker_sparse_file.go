99bb405c5a1f3753bae7dcfd5a507951084822d0
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-29 08:31:31 +0200

    Example 10: creation of sparse binary file

---
+++ article_23/10_writer_seeker_sparse_file.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+const filename = "huge.bin"
+
+func writeMark(writer io.Writer) {
+	buffer := []byte("**")
+	written, err := writer.Write(buffer)
+
+	if written > 0 {
+		fmt.Printf("written %d bytes\n", written)
+	}
+
+	if err != nil {
+		fmt.Printf("I/O error %v\n", err)
+	}
+}
+
+func main() {
+	writer, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer writer.Close()
+
+	writeMark(writer)
+	writer.Seek(1000000, io.SeekCurrent)
+	writeMark(writer)
+	writer.Seek(1000000, io.SeekCurrent)
+	writeMark(writer)
+}
