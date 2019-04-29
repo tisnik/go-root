e0f6db2ab674b452ef336570f434e91fe321a64e
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-29 08:31:31 +0200

    Example 15: read bytes using bufio package

---
+++ article_23/15_bufio_reader_read_bytes.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+const filename = "test_input_no_eoln.txt"
+const buffer_size = 16
+
+func main() {
+	reader, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer reader.Close()
+
+	bufferedReader := bufio.NewReader(reader)
+
+	for {
+		array, err := bufferedReader.ReadBytes('\n')
+
+		fmt.Printf("read array of bytes with size %d bytes: %v\n", len(array), array)
+
+		if err == io.EOF {
+			fmt.Println("reached end of file")
+			break
+		}
+
+		if err != nil {
+			fmt.Printf("other error %v\n", err)
+			break
+		}
+	}
+}
