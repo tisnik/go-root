8ce1cedde2979a6ae63c065221cfb4c5917cd466
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-29 08:31:31 +0200

    Example 11: MultiWriter usage

---
+++ article_23/11_multi_writer.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+const filename1 = "test_output_1.txt"
+const filename2 = "test_output_2.txt"
+const message = "Hello world!"
+
+func main() {
+	writer1, err := os.Create(filename1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer writer1.Close()
+
+	writer2, err := os.Create(filename2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer writer2.Close()
+
+	writer := io.MultiWriter(writer1, writer2)
+
+	buffer := []byte(message)
+
+	written, err := writer.Write(buffer)
+
+	if written > 0 {
+		fmt.Printf("written %d bytes\n", written)
+	}
+
+	if err != nil {
+		fmt.Printf("I/O error %v\n", err)
+	}
+}
