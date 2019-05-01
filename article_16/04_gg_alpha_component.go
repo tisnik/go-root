24cc916309e980c18bca4cee94912b2f0958e7af
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-01 17:59:43 +0200

    Added comments into example #4

--- article_16/04_gg_alpha_component.go
+++ article_16/04_gg_alpha_component.go
@@ -1,3 +1,10 @@
+// Seriál "Programovací jazyk Go"
+//
+// Šestnáctá část
+//
+// Demonstrační příklad číslo 4:
+//     Alfa kanál v barvovém prostoru RGBA.
+
 package main
 
 import "github.com/fogleman/gg"
