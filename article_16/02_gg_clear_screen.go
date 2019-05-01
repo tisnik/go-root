c71d0ef682ee0fe40456beb588ab640c18a9f414
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-01 17:59:43 +0200

    Added comments into example #2

--- article_16/02_gg_clear_screen.go
+++ article_16/02_gg_clear_screen.go
@@ -1,3 +1,10 @@
+// Seriál "Programovací jazyk Go"
+//
+// Šestnáctá část
+//
+// Demonstrační příklad číslo 2:
+//     Vymazání obrazovky (druhá, lepší varianta).
+
 package main
 
 import "github.com/fogleman/gg"
