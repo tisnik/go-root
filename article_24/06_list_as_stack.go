622f96781bac81bca2e481947b078dd1f9b0b065
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-06 09:19:38 +0200

    Example 6: Usage of list as stack

---
+++ article_24/06_list_as_stack.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Stack list.List
+
+func printStack(l *list.List) {
+	for e := l.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
+}
+
+func push(l *list.List, number int) {
+	l.PushBack(number)
+}
+
+func pop(l *list.List) int {
+	tos := l.Back()
+	l.Remove(tos)
+	return tos.Value.(int)
+}
+
+func main() {
+	expression := "1 2 + 2 3 * 8 + *"
+	terms := strings.Split(expression, " ")
+	stack := list.New()
+
+	for _, term := range terms {
+		switch term {
+		case "+":
+			operand1 := pop(stack)
+			operand2 := pop(stack)
+			push(stack, operand1+operand2)
+		case "-":
+			operand1 := pop(stack)
+			operand2 := pop(stack)
+			push(stack, operand2-operand1)
+		case "*":
+			operand1 := pop(stack)
+			operand2 := pop(stack)
+			push(stack, operand1*operand2)
+		case "/":
+			operand1 := pop(stack)
+			operand2 := pop(stack)
+			push(stack, operand2/operand1)
+		default:
+			number, err := strconv.Atoi(term)
+			if err == nil {
+				push(stack, number)
+			}
+		}
+	}
+	printStack(stack)
+}
