ddd53ec1ae2c484c5ef0a644fe76330a2e5e1e03
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-06 09:19:38 +0200

    Example 3: Usage of Flush()

---
+++ article_24/03_flushing_server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+func handler(writer http.ResponseWriter, request *http.Request) {
+	println("handler started")
+	for i := 0; i < 50; i++ {
+		writer.Write([]byte("Hello world!\n"))
+		print(".")
+		if f, ok := writer.(http.Flusher); ok {
+			f.Flush()
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+	println("\nhandler finished")
+}
+
+func main() {
+	http.HandleFunc("/data", handler)
+	http.ListenAndServe(":8080", nil)
+}
