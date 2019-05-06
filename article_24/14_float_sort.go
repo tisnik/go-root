72f65470ca2d7510a95330f1b4e83c58309ddb97
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-06 09:19:38 +0200

    Example 14: Sort array/slice of floats

---
+++ article_24/14_float_sort.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"sort"
+)
+
+func printArray(prefix string, numbers []float64) {
+	var state string
+	if sort.Float64sAreSorted(numbers) {
+		state = "sorted"
+	} else {
+		state = "unsorted"
+	}
+	fmt.Printf("%s variant of %s array: %v\n", prefix, state, numbers)
+}
+
+func main() {
+	numbers := make([]float64, 20)
+
+	for i := 0; i < len(numbers); i++ {
+		numbers[i] = math.Sin(float64(i) * math.Pi / float64(len(numbers)))
+	}
+	printArray("1st", numbers)
+
+	sort.Float64s(numbers)
+	printArray("2nd", numbers)
+}
