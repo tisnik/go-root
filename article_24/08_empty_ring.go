06ca32c9a185c907f618b218451f073dd199d4a2
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-06 09:19:38 +0200

    Example 8: Ring data container

---
+++ article_24/08_empty_ring.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"container/ring"
+	"fmt"
+)
+
+func printRing(r *ring.Ring) {
+	length := r.Len()
+	for i := 0; i < length; i++ {
+		fmt.Println(i, r.Value)
+		r = r.Next()
+	}
+}
+
+func main() {
+	r := ring.New(10)
+	printRing(r)
+}
