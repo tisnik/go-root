878cf2215252e448d4761bf3bbaff6a926582c73
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-06 09:19:38 +0200

    Example 9: Filled ring items

---
+++ article_24/09_filled_ring.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"container/ring"
+	"fmt"
+)
+
+func printRing(r *ring.Ring) {
+	length := r.Len()
+	for i := 0; i < length*2; i++ {
+		fmt.Println(i, r.Value)
+		r = r.Next()
+	}
+}
+
+func main() {
+	r := ring.New(3)
+	r.Value = "foo"
+	r = r.Next()
+	r.Value = "bar"
+	r = r.Next()
+	r.Value = "baz"
+	printRing(r)
+}
