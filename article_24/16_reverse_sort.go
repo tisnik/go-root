4f1c59bb828a0035a4e3d50eda537748e208b743
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-06 09:19:38 +0200

    Example 16: Reverse sorted array/slice

---
+++ article_24/16_reverse_sort.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+)
+
+func printArray(prefix string, numbers []int) {
+	var state string
+	if sort.IntsAreSorted(numbers) {
+		state = "sorted"
+	} else {
+		state = "unsorted"
+	}
+	fmt.Printf("%s variant of %s array: %v\n", prefix, state, numbers)
+}
+
+func main() {
+	numbers := make([]int, 20)
+
+	for i := 0; i < len(numbers); i++ {
+		numbers[i] = rand.Int() % 10
+	}
+	printArray("1st", numbers)
+
+	sort.Sort(sort.Reverse(sort.IntSlice(numbers)))
+	printArray("2nd", numbers)
+}
