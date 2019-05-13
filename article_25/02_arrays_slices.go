66cac212e5968239d53ce28dff568136c1da9d32
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-13 08:19:37 +0200

    Example #2: array and slice operations

---
+++ article_25/02_arrays_slices.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a1 [20]int
+
+	for i := 0; i < len(a1); i++ {
+		a1[i] = i + 1
+	}
+
+	var slice1 []int = a1[:]
+	var slice2 []int = a1[5:15]
+	slice3 := slice2[5:]
+
+	fmt.Println(a1)
+	fmt.Println(slice1)
+	fmt.Println(slice2)
+	fmt.Println(slice3)
+}
