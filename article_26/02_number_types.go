aef1b036eb4da7a2a1fb3d785c44a4cd18131b82
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-17 08:54:57 +0200

    Example #2: numeric types 'zero' value

---
+++ article_26/02_number_types.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+	var i1 int8
+	var i2 int32
+	var u1 uint8
+	var u2 uint32
+
+	var f1 float32
+	var f2 float64
+	var c1 complex64
+	var c2 complex128
+
+	println(i1)
+	println(i2)
+	println(u1)
+	println(u2)
+
+	println(f1)
+	println(f2)
+	println(c1)
+	println(c2)
+}
