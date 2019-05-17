f50b991d5793bec2995b11509b0fcd18fa1b8ba6
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-17 08:54:57 +0200

    Example #4: other types 'zero' value

---
+++ article_26/04_other_types.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	var p *int
+	var s []int
+	var m map[string]int
+	var c chan int
+	var f func()
+	var i interface{}
+
+	println(p)
+	println(s)
+	println(m)
+	println(c)
+	println(f)
+	println(i)
+}
