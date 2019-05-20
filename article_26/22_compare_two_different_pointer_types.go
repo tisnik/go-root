f8e3b17a3334c7d6f75e1ddcc426b3a1b76789e5
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-20 08:43:51 +0200

    Example 22: compare two different pointer types

---
+++ article_26/22_compare_two_different_pointer_types.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i1 *int
+
+	fmt.Printf("%v %v\n", i1, i1 == nil)
+
+        var i2 *int32
+	fmt.Printf("%v %v\n", i2, i2 == nil)
+
+	fmt.Printf("%v\n", i1 == i2)
+}
