1f3db929115cc250da9eece06a0affc1ab5c5b8a
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-21 08:21:12 +0200

    Example 12B: appeding into nil slice

---
+++ article_26/12B_nil_slice_append.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s0 []int
+	s1 := []int{}
+	s2 := make([]int, 0)
+	s3 := make([]int, 10)
+
+	fmt.Println(s0)
+	fmt.Println(s1)
+	fmt.Println(s2)
+	fmt.Println(s3)
+
+	fmt.Println()
+
+	s0 = append(s0, 1, 2, 3)
+	s1 = append(s1, 1, 2, 3)
+	s2 = append(s2, 1, 2, 3)
+	s3 = append(s3, 1, 2, 3)
+
+	fmt.Println(s0)
+	fmt.Println(s1)
+	fmt.Println(s2)
+	fmt.Println(s3)
+}
