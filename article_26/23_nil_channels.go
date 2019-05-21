c2e6cc40f69123c73b03176941fa44230dfb4d2d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-21 08:21:29 +0200

    Example #23: nil channels

---
+++ article_26/23_nil_channels.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var c1 chan int = nil
+	var c2 chan int
+
+	fmt.Printf("%v %v\n", c1, c1 == nil)
+	fmt.Printf("%v %v\n", c2, c2 == nil)
+	fmt.Printf("%v\n", c1 ==  c2)
+}
