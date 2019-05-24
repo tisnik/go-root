bec846e89a1a579cb8acf249c65bc5b54b64b376
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-24 09:06:57 +0200

    Example #3: DoFile function to call Lua

---
+++ article_27/03_do_file.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/yuin/gopher-lua"
+	"log"
+)
+
+const LuaSource = "hello.lua"
+
+func main() {
+	luaVM := lua.NewState()
+	log.Println("Lua VM has been initialized")
+
+	defer func() {
+		luaVM.Close()
+		log.Println("Lua VM has been closed")
+	}()
+
+	err := luaVM.DoFile(LuaSource)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
