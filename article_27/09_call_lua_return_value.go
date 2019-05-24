dcc2c6e214f9bfd15de33dba55cb4942ae193a1d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-24 09:06:58 +0200

    Example #9: Call Lua function with two parameters and return value

---
+++ article_27/09_call_lua_return_value.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/yuin/gopher-lua"
+	"log"
+)
+
+const LuaSource = "add.lua"
+
+func main() {
+	luaVM := lua.NewState()
+	log.Println("Lua VM has been initialized")
+
+	defer func() {
+		luaVM.Close()
+		log.Println("Lua VM has been closed")
+	}()
+
+	err := luaVM.DoFile(LuaSource)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = luaVM.CallByParam(lua.P{
+		Fn:      luaVM.GetGlobal("add"),
+		NRet:    1,
+		Protect: true,
+	}, lua.LNumber(1), lua.LNumber(2))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ret := luaVM.Get(-1)
+	luaVM.Pop(1)
+	println("Type", ret.Type())
+	println("Value", ret.String())
+}
