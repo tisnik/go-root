4c36568ad8ba103d784490d7b2ee50d6e012079c
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-24 09:06:57 +0200

    Example #5: Two Lua virtual machines in separated goroutines

---
+++ article_27/05_two_vms_in_goroutines.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"github.com/yuin/gopher-lua"
+	"log"
+)
+
+const LuaSource1 = "l1.lua"
+const LuaSource2 = "l2.lua"
+
+func callLuaVM1(c chan bool) {
+	defer func() { c <- true }()
+
+	luaVM1 := lua.NewState()
+	log.Println("Lua VM1 has been initialized")
+
+	defer func() {
+		luaVM1.Close()
+		log.Println("Lua VM1 has been closed")
+	}()
+
+	err := luaVM1.DoFile(LuaSource1)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func callLuaVM2(c chan bool) {
+	defer func() { c <- true }()
+
+	luaVM2 := lua.NewState()
+	log.Println("Lua VM2 has been initialized")
+
+	defer func() {
+		luaVM2.Close()
+		log.Println("Lua VM2 has been closed")
+	}()
+
+	err := luaVM2.DoFile(LuaSource2)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	c1 := make(chan bool)
+	c2 := make(chan bool)
+
+	go callLuaVM1(c1)
+	go callLuaVM2(c2)
+
+	<-c1
+	<-c2
+}
