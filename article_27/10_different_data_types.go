ec7d6775f50ef3045972404bfe403c8691f53097
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-28 08:39:13 +0200

    Example #10: calling Lua function that returns different data type

---
+++ article_27/10_different_data_types.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/yuin/gopher-lua"
+	"log"
+)
+
+const LuaSource = "concatenate.lua"
+
+func main() {
+	luaVM := lua.NewState()
+	log.Println("Lua VM has been initialized")
+
+	defer func() {
+		luaVM.Close()
+		log.Println("Lua VM has been closed")
+	}()
+
+	err := luaVM.DoFile(LuaSource)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = luaVM.CallByParam(lua.P{
+		Fn:      luaVM.GetGlobal("concatenate"),
+		NRet:    1,
+		Protect: true,
+	}, lua.LString("foo"), lua.LString("bar"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ret := luaVM.Get(-1)
+	luaVM.Pop(1)
+	println("Type", ret.Type())
+	println("Value", ret.String())
+}
