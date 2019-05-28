26e47e6198227dbb5dd88b15639ad40feab51be7
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-28 08:39:13 +0200

    Example #12: boolean values conversion Lua->Go

---
+++ article_27/12_boolean.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/yuin/gopher-lua"
+	"log"
+)
+
+const LuaSource = "compare.lua"
+
+func compareNumbers(a int, b int) {
+	luaVM := lua.NewState()
+	log.Println("Lua VM has been initialized")
+
+	defer func() {
+		luaVM.Close()
+		log.Println("Lua VM has been closed")
+	}()
+
+	err := luaVM.DoFile(LuaSource)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = luaVM.CallByParam(lua.P{
+		Fn:      luaVM.GetGlobal("compare"),
+		NRet:    1,
+		Protect: true,
+	}, lua.LNumber(a), lua.LNumber(b))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ret := luaVM.Get(-1)
+	luaVM.Pop(1)
+	println("Type", ret.Type())
+	println("Value", ret.String())
+	println("Result", ret == lua.LTrue)
+}
+
+func main() {
+	compareNumbers(1, 2)
+	compareNumbers(1, 1)
+}
