a31127ab2dcce6f8c594506d120ed9981f98f7f5
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-28 08:39:13 +0200

    Example #15: calling Go from Lua, incorrect approach

---
+++ article_27/15_go_from_lua.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/yuin/gopher-lua"
+	"log"
+)
+
+const LuaSource = "go_from_lua.lua"
+
+func Hello(L *lua.LState) int {
+	log.Println("Hello from Go!")
+	return 0
+}
+
+func main() {
+	luaVM := lua.NewState()
+	log.Println("Lua VM has been initialized")
+
+	defer func() {
+		luaVM.Close()
+		log.Println("Lua VM has been closed")
+	}()
+
+	err := luaVM.DoFile(LuaSource)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = luaVM.CallByParam(lua.P{
+		Fn:      luaVM.GetGlobal("call_go"),
+		NRet:    0,
+		Protect: true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
