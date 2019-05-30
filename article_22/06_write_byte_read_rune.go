6d509a19cb70e3a96c58998aa549339a8c56b5f9
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-30 08:14:46 +0200

    Added comments to the example #6

--- article_22/06_write_byte_read_rune.go
+++ article_22/06_write_byte_read_rune.go
@@ -1,3 +1,10 @@
+// Seriál "Programovací jazyk Go"
+//
+// Dvacátá druhá část část
+//
+// Demonstrační příklad číslo 6:
+//     Zápis bajtů du bufferu, přečtení ve formě run.
+
 package main
 
 import (
