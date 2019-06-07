5715af2df26781bfa7ad4fba900734452cce9672
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-06-07 11:39:44 +0200

    Updated example #12

--- article_28/12_add_drop.go
+++ article_28/12_add_drop.go
@@ -12,8 +12,10 @@ func main() {
 	stream1 := koazee.StreamOf(values)
 	stream2 := stream1.Add(6)
 	stream3 := stream1.Drop(3)
+	stream4 := stream1.Drop(10)
 
 	fmt.Printf("stream1:  %v\n", stream1.Out().Val())
 	fmt.Printf("stream2:  %v\n", stream2.Out().Val())
 	fmt.Printf("stream3:  %v\n", stream3.Out().Val())
+	fmt.Printf("stream4:  %v\n", stream4.Out().Val())
 }
