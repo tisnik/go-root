4302fb6d788075cd944d51872e6ca12035377db6
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-06-11 08:29:38 +0200

    Example #4: Mandelbrot renderer

---
+++ article_29/mandelbrot/mandelbrot.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"mandelbrot/renderer"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) < 4 {
+		println("usage: ./mandelbrot width height maxiter")
+		os.Exit(1)
+	}
+
+	width, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Printf("Improper width parameter: '%s'\n", os.Args[1])
+		os.Exit(1)
+	}
+
+	height, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Printf("Improper height parameter: '%s'\n", os.Args[2])
+		os.Exit(1)
+	}
+
+	maxiter, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		fmt.Printf("Improper maxiter parameter: '%s'\n", os.Args[3])
+		os.Exit(1)
+	}
+
+	renderer.Start(width, height, maxiter)
+}
---
+++ article_29/mandelbrot/renderer/mandelbrot.go
@@ -0,0 +1,82 @@
+package renderer
+
+import (
+	"image"
+	"image/png"
+	"log"
+	"os"
+)
+
+func writeImage(width uint, height uint, pixels []byte) {
+	img := image.NewNRGBA(image.Rect(0, 0, int(width), int(height)))
+	pixel := 0
+
+	for y := 0; y < int(height); y++ {
+		offset := img.PixOffset(0, y)
+		for x := uint(0); x < width; x++ {
+			img.Pix[offset] = pixels[pixel]
+			img.Pix[offset+1] = pixels[pixel+1]
+			img.Pix[offset+2] = pixels[pixel+2]
+			img.Pix[offset+3] = 0xff
+			pixel += 3
+			offset += 4
+		}
+	}
+
+	outputFile, err := os.Create("mandelbrot.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outputFile.Close()
+
+	png.Encode(outputFile, img)
+}
+
+func iterCount(cx float64, cy float64, maxiter uint) uint {
+	var zx float64 = 0.0
+	var zy float64 = 0.0
+	var i uint = 0
+	for i < maxiter {
+		zx2 := zx * zx
+		zy2 := zy * zy
+		if zx2+zy2 > 4.0 {
+			break
+		}
+		zy = 2.0*zx*zy + cy
+		zx = zx2 - zy2 + cx
+		i++
+	}
+	return i
+}
+
+func calcMandelbrot(width uint, height uint, maxiter uint, palette [][3]byte, image []byte, cy float64, done chan bool) {
+	var cx float64 = -2.0
+	for x := uint(0); x < width; x++ {
+		i := iterCount(cx, cy, maxiter)
+		color := palette[i]
+		image[3*x] = color[0]
+		image[3*x+1] = color[1]
+		image[3*x+2] = color[2]
+		cx += 3.0 / float64(width)
+	}
+	done <- true
+}
+
+func Start(width int, height int, maxiter int) {
+	done := make(chan bool, height)
+
+	pixels := make([]byte, width*height*3)
+	offset := 0
+	delta := width * 3
+
+	var cy float64 = -1.5
+	for y := 0; y < height; y++ {
+		go calcMandelbrot(uint(width), uint(height), uint(maxiter), mandmap[:], pixels[offset:offset+delta], cy, done)
+		offset += delta
+		cy += 3.0 / float64(height)
+	}
+	for i := 0; i < height; i++ {
+		<-done
+	}
+	writeImage(uint(width), uint(height), pixels)
+}
