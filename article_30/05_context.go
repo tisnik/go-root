8eae5b5572952d5e8fe3eb1e079e8bc1e984568f
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-06-17 07:40:55 +0200

    Example #5: Setting the context

---
+++ article_30/05_context.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"runtime/trace"
+)
+
+func perform_login() {
+	log.Println("login")
+}
+
+func perform_logout() {
+	log.Println("logout")
+}
+
+func transaction(typ string) {
+	log.Printf("transaction '%s'\n", typ)
+}
+
+func main() {
+	f, err := os.Create("trace2.trace")
+	if err != nil {
+		log.Fatalf("failed to create trace output file: %v", err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Fatalf("failed to close trace file: %v", err)
+		}
+	}()
+
+	if err := trace.Start(f); err != nil {
+		log.Fatalf("failed to start trace: %v", err)
+	}
+	defer trace.Stop()
+
+	ctx := context.Background()
+	ctx, task := trace.NewTask(ctx, "transactionTask")
+
+	perform_login()
+	transaction("A")
+	transaction("B")
+	perform_logout()
+
+	task.End()
+}
