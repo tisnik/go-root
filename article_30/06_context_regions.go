ab746061a982d9e3b1c3d76b9b474daf5bae06d8
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-06-17 07:40:55 +0200

    Example #6: Context regions usage

---
+++ article_30/06_context_regions.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"runtime/trace"
+)
+
+func perform_login() {
+	log.Println("login")
+}
+
+func perform_logout() {
+	log.Println("logout")
+}
+
+func transaction(typ string) {
+	log.Printf("transaction '%s'\n", typ)
+}
+
+func main() {
+	f, err := os.Create("trace3.trace")
+	if err != nil {
+		log.Fatalf("failed to create trace output file: %v", err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Fatalf("failed to close trace file: %v", err)
+		}
+	}()
+
+	if err := trace.Start(f); err != nil {
+		log.Fatalf("failed to start trace: %v", err)
+	}
+	defer trace.Stop()
+
+	ctx := context.Background()
+	ctx, task := trace.NewTask(ctx, "transactionTask")
+
+	region1 := trace.StartRegion(ctx, "login")
+	perform_login()
+	region1.End()
+
+	region2 := trace.StartRegion(ctx, "transactions")
+	transaction("A")
+	transaction("B")
+	region2.End()
+
+	region3 := trace.StartRegion(ctx, "logout")
+	perform_logout()
+	region3.End()
+
+	task.End()
+}
