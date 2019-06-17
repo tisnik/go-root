45442d5d3d26a08cf66951d8736dc1996098ae40
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-06-17 07:40:55 +0200

    Example #4: Basic usage of tracing

---
+++ article_30/04_basic_tracing.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"os"
+	"runtime/trace"
+)
+
+func perform_login() {
+	log.Println("login")
+}
+
+func perform_logout() {
+	log.Println("logout")
+}
+
+func transaction(typ string) {
+	log.Printf("transaction '%s'\n", typ)
+}
+
+func main() {
+	f, err := os.Create("trace1.trace")
+	if err != nil {
+		log.Fatalf("failed to create trace output file: %v", err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Fatalf("failed to close trace file: %v", err)
+		}
+	}()
+
+	if err := trace.Start(f); err != nil {
+		log.Fatalf("failed to start trace: %v", err)
+	}
+	defer trace.Stop()
+
+	perform_login()
+	transaction("A")
+	transaction("B")
+	perform_logout()
+}
