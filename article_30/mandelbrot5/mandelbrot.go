1102cac42846bcba1504dd1e2669776abf33fe61
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-06-17 07:40:55 +0200

    Example #11: Even better tracing in Mandelbrot set renderer

---
+++ article_30/mandelbrot5/mandelbrot.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"mandelbrot5/renderer"
+	"os"
+	"runtime/trace"
+	"strconv"
+)
+
+func main() {
+	f, err := os.Create("mandelbrot5.trace")
+	if err != nil {
+		log.Fatalf("failed to create trace output file: %v", err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Fatalf("failed to close trace file: %v", err)
+		}
+	}()
+
+	if err := trace.Start(f); err != nil {
+		log.Fatalf("failed to start trace: %v", err)
+	}
+	defer trace.Stop()
+
+	if len(os.Args) < 4 {
+		println("usage: ./mandelbrot width height maxiter")
+		os.Exit(1)
+	}
+
+	width, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Printf("Improper width parameter: '%s'\n", os.Args[1])
+		os.Exit(1)
+	}
+
+	height, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Printf("Improper height parameter: '%s'\n", os.Args[2])
+		os.Exit(1)
+	}
+
+	maxiter, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		fmt.Printf("Improper maxiter parameter: '%s'\n", os.Args[3])
+		os.Exit(1)
+	}
+
+	ctx := context.Background()
+	ctx, task := trace.NewTask(ctx, "renderFractal")
+	defer task.End()
+	renderer.Start(ctx, width, height, maxiter)
+}
