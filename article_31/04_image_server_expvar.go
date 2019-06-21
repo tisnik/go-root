e09ca9bca6302d7cc9a1ac166f37ff26fd018d0b
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-06-21 08:01:39 +0200

    Example 4: image server with exported variable(s)

---
+++ article_31/04_image_server_expvar.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"expvar"
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"io"
+	"math/rand"
+	"net/http"
+)
+
+var counter = expvar.NewInt("counter")
+
+func indexPageHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/html")
+	response := `
+<body>
+    <h1>Enterprise image renderer</h1>
+    <img src="/image" width=256 height=256 />
+</body>`
+	io.WriteString(writer, response)
+}
+
+func calculateColor() color.RGBA {
+	return color.RGBA{uint8(rand.Intn(255)),
+		uint8(rand.Intn(255)),
+		uint8(rand.Intn(255)), 255}
+}
+
+func imageHandler(writer http.ResponseWriter, request *http.Request) {
+	const ImageWidth = 256
+	const ImageHeight = 256
+	outputimage := image.NewRGBA(image.Rectangle{image.Point{0, 0},
+		image.Point{ImageWidth, ImageHeight}})
+
+	for y := 0; y < ImageHeight; y++ {
+		for x := 0; x < ImageWidth; x++ {
+			c := calculateColor()
+			outputimage.Set(x, y, c)
+		}
+	}
+	png.Encode(writer, outputimage)
+	counter.Add(1)
+}
+
+func counterHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/plain")
+	fmt.Fprintf(writer, "Counter: %d\n", counter.Value())
+}
+
+func main() {
+	http.HandleFunc("/", indexPageHandler)
+	http.HandleFunc("/image", imageHandler)
+	http.HandleFunc("/counter", counterHandler)
+	http.ListenAndServe(":8080", nil)
+}
