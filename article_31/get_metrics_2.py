3d6af8d8e1bfcd51e2526a7c2ead845bae9c0c55
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-06-24 08:00:04 +0200

    Second script to retrieve metrics from HTTP server

---
+++ article_31/get_metrics_2.py
@@ -0,0 +1,28 @@
+import requests
+import os
+import csv
+
+filename = "mandelbrot.csv"
+
+response = requests.get("http://localhost:8080/debug/vars")
+
+assert response.status_code == 200, "Chyba při čtení metrik: neočekávaný HTTP kód odpovědi"
+
+payload = response.json()
+
+mode = None
+write_header = False
+
+if os.path.exists(filename):
+    mode = "a"
+else:
+    mode = "w"
+    write_header = True
+
+with open(filename, mode) as csv_file:
+    writer = csv.writer(csv_file, delimiter=',')
+    if write_header:
+        writer.writerow(("Počet překreslení", "Čas výpočtu", "Obarvených pixelů"))
+    writer.writerow((payload["renderingCounter"],
+                     payload["humanReadableDuration"],
+                     payload["pixelsColored"]))
