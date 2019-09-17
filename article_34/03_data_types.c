1b098abc9444ef9ddd25f04b2be08da6d5f627c2
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-09-17 18:34:31 +0200

    Updated with long double

--- article_34/03_data_types.c
+++ article_34/03_data_types.c
@@ -2,13 +2,14 @@
 
 int main(void)
 {
-    printf("sizeof char      = %lu byte(s)\n", sizeof(char));
-    printf("sizeof short     = %lu byte(s)\n", sizeof(short));
-    printf("sizeof int       = %lu byte(s)\n", sizeof(int));
-    printf("sizeof long      = %lu byte(s)\n", sizeof(long));
-    printf("sizeof long long = %lu byte(s)\n", sizeof(long long));
-    printf("sizeof float     = %lu byte(s)\n", sizeof(float));
-    printf("sizeof double    = %lu byte(s)\n", sizeof(double));
+    printf("sizeof char        = %lu byte(s)\n", sizeof(char));
+    printf("sizeof short       = %lu byte(s)\n", sizeof(short));
+    printf("sizeof int         = %lu byte(s)\n", sizeof(int));
+    printf("sizeof long        = %lu byte(s)\n", sizeof(long));
+    printf("sizeof long long   = %lu byte(s)\n", sizeof(long long));
+    printf("sizeof float       = %lu byte(s)\n", sizeof(float));
+    printf("sizeof double      = %lu byte(s)\n", sizeof(double));
+    printf("sizeof long double = %lu byte(s)\n", sizeof(long double));
     return 0;
 }
 
