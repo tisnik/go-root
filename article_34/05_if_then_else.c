887bc6cf317c1228f708b0485204355d3c14e0ac
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-09-17 15:17:04 +0200

    Example #5: if-then-else control structure, C variant

---
+++ article_34/05_if_then_else.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 10;
+
+    if (x > 0) {
+        puts("x is positive number");
+    }
+    else {
+        puts("x is negative number or zero");
+    }
+
+    return 0;
+}
