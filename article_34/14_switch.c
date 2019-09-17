986ea84d33ac4a519fff897d209713d71ecb753d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-09-17 15:17:04 +0200

    Example #14: switch structure, C variant

---
+++ article_34/14_switch.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+const char *classify(int x) {
+	switch (x) {
+	case 0:
+		return "nula";
+        case 2:
+        case 4:
+        case 6:
+        case 8:
+		return "sudé číslo";
+	case 1:
+	case 3:
+	case 5:
+	case 7:
+	case 9:
+		return "liché číslo";
+	default:
+		return "?";
+	}
+}
+
+int main(void) {
+    int x;
+    for (x = 0; x <= 10; x++) {
+        printf("%d: %s\n", x, classify(x));
+    }
+    return 0;
+}
