0e80fed31353e54e0de68887c9c7fc4cd040fee6
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-09-17 15:17:04 +0200

    Example #14: switch structure, Go variant

---
+++ article_34/14_switch.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func classify(x int) string {
+	switch x {
+	case 0:
+		return "nula"
+	case 2, 4, 6, 8:
+		return "sudé číslo"
+	case 1, 3, 5, 7, 9:
+		return "liché číslo"
+	default:
+		return "?"
+	}
+}
+
+func main() {
+	for x := 0; x <= 10; x++ {
+		fmt.Printf("%d: %s\n", x, classify(x))
+	}
+}
