d6fadb8aa12d04493c96ed79a062ad54d1d4dcfe
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-09-18 21:00:43 +0200

    Enumeration in C

---
+++ article_34/21_enum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+enum {
+    Pondeli,
+    Utery,
+    Streda,
+    Ctvrtek,
+    Patek,
+    Sobota,
+    Nedele
+};
+
+int main(void) {
+    printf("%d\n", Pondeli);
+    printf("%d\n", Streda);
+    printf("%d\n", Patek);
+    return 0;
+}
