f6fa7be76d803571881ba643a7e8ce66f6d388b7
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-09-18 11:58:43 +0200

    Example #19 and #20: constants

---
+++ article_34/19_constants.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+const Pi float64 = 3.1415927
+const E = 2.71828
+
+const z0 int = 0
+const z1 = 0
+
+const z2 = z0 + z1
+
+func main() {
+	fmt.Printf("Pi = %f\n", Pi)
+	fmt.Printf("e = %f\n", E)
+
+	fmt.Printf("z0 = %d\n", z0)
+	fmt.Printf("z1 = %d\n", z1)
+
+	fmt.Printf("z2 = %d\n", z2)
+}
---
+++ article_34/20_constants.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+const (
+	Pi float64 = 3.1415927
+	E          = 2.71828
+
+	z0 int = 0
+	z1     = 0
+	z2     = z0 + z1
+)
+
+func main() {
+	fmt.Printf("Pi = %f\n", Pi)
+	fmt.Printf("e = %f\n", E)
+
+	fmt.Printf("z0 = %d\n", z0)
+	fmt.Printf("z1 = %d\n", z1)
+
+	fmt.Printf("z2 = %d\n", z2)
+}
