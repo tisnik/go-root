0119e5420f9123d8c5637d6300ba37c14d01caa1
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-09-24 12:47:59 +0200

    Example #2: boolean type checks

---
+++ article_35/02_boolean_type_checks.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a bool = true
+	var b bool = false
+	var c bool = 0
+	var d bool = ""
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
