2a2b0b1d7dc7f6e8be4817b382cd8f7794592888
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-09-24 14:04:02 +0200

    Example 8: string concatenation

---
+++ article_35/08_string_concatenation.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+    const char *s1 = "Hello ";
+    const char *s2 = "world!";
+
+    size_t length = strlen(s1) + strlen(s2) + 1;
+    char *s3 = (char *)calloc(length, sizeof(char*));
+
+    if (s3) {
+        strcpy(s3, s1);
+        strcat(s3, s2);
+
+        puts(s3);
+        free(s3);
+    }
+    else {
+        perror("calloc() failed");
+    }
+
+    return 0;
+}
---
+++ article_35/08_string_concatenation.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	s1 := "Hello "
+	s2 := "world!"
+
+	s := s1 + s2
+	fmt.Println(s)
+}
