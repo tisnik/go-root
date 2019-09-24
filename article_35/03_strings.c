bd66785c69f75427135b84aa142f0ae574658fb0
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-09-24 12:59:43 +0200

    Example 3: strings

---
+++ article_35/03_strings.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    char *s1 = "www.root.cz";
+    char *s2 = "";
+    char *s3 = "Hello\nworld!\n";
+    char *s4 = "[шщэюя]";
+
+    puts(s1);
+    puts(s2);
+    puts(s3);
+    puts(s4);
+
+    return 0;
+}
---
+++ article_35/03_strings.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s1 string = "www.root.cz"
+	var s2 string = ""
+	var s3 string = "Hello\nworld!\n"
+	var s4 string = "шщэюя"
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+	fmt.Println(s3)
+	fmt.Println(s4)
+}
