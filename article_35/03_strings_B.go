08793f1e766b327f90ae5187815762c18ba82baf
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-09-25 07:54:59 +0200

    Example #3B: strings

---
+++ article_35/03_strings_B.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	s1 := "www.root.cz"
+	s2 := ""
+	s3 := "Hello\nworld!\n"
+	s4 := "шщэюя"
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+	fmt.Println(s3)
+	fmt.Println(s4)
+}
