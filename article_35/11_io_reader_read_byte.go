311d49a33515eba1079f6f66ea661f30286ca398
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-09-25 08:41:38 +0200

    Example #11: read byte from file

---
+++ article_35/11_io_reader_read_byte.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+const filename = "test_input.txt"
+
+func main() {
+	fin, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fin.Close()
+
+	buffer := make([]byte, 1)
+
+	for {
+		read, err := fin.Read(buffer)
+
+		if read > 0 {
+			fmt.Printf("byte from file\n")
+			fmt.Println(buffer[0])
+		}
+
+		if err == io.EOF {
+			fmt.Println("reached end of file")
+			break
+		}
+
+		if err != nil {
+			fmt.Printf("other error %v\n", err)
+			break
+		}
+	}
+}
