71ded8d6e0af66565c5ee2be4c9008b8a1f32f67
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-02 07:59:13 +0200

    Example #18: double C.free() call

---
+++ article_36/18_double_free.go
@@ -0,0 +1,13 @@
+package main
+
+// #include <stdio.h>
+// #include <stdlib.h>
+import "C"
+import "unsafe"
+
+func main() {
+	cs := C.CString("Hello world!")
+	C.puts(cs)
+	C.free(unsafe.Pointer(cs))
+	C.free(unsafe.Pointer(cs))
+}
