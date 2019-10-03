cd1e584ae14b229448b4ce732231bc36b2a95185
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-03 15:11:21 +0200

    Updated comments in example #8

--- article_26/08_true_false_vars.go
+++ article_26/08_true_false_vars.go
@@ -1,3 +1,10 @@
+// Seriál "Programovací jazyk Go"
+//
+// Dvacátá šestá část
+//
+// Demonstrační příklad číslo 8:
+//    true a false mohou být korektní jméno konstanty.
+
 package main
 
 import "fmt"
