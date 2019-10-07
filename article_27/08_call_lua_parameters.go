8a9b9451716bf9d92154b0795aec7d3ae371aa5e
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-07 14:17:01 +0200

    Updated comments in example #8

--- article_27/08_call_lua_parameters.go
+++ article_27/08_call_lua_parameters.go
@@ -1,3 +1,11 @@
+// Seriál "Programovací jazyk Go"
+//
+// Dvacátá sedmá část
+//
+// Demonstrační příklad číslo 8:
+//    Zavolání Lua funkce z Go s předáním parametrů.
+//    
+
 package main
 
 import (
