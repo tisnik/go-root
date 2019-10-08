e9f224bad850bb4e9c46a8525265020fa01d378a
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-08 14:38:09 +0200

    Example #4: automatic counter registration

---
+++ article_37/04_automatic_registration.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+	"os"
+	"time"
+)
+
+var counter = promauto.NewCounter(prometheus.CounterOpts{
+	Name: "number_of_ticks",
+	Help: "The total number of ticks since the app is started",
+})
+
+func tick() {
+	for {
+		counter.Inc()
+		time.Sleep(time.Second)
+	}
+}
+
+func recordMetrics() {
+	fmt.Println("Starting recording metrics")
+	go tick()
+}
+
+func main() {
+	recordMetrics()
+	fmt.Println("Initializing HTTP server")
+	http.Handle("/metrics", promhttp.Handler())
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+}
