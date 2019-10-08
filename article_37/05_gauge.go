70a8606667f10f68413ab3f371e643f012f66850
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-08 14:38:09 +0200

    Example #5: gauge

---
+++ article_37/05_gauge.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+	"os"
+	"runtime"
+	"time"
+)
+
+var goroutines = promauto.NewGauge(prometheus.GaugeOpts{
+	Name: "number_of_goroutines",
+	Help: "Number of goroutines in the process",
+})
+
+func tick() {
+	for {
+		goroutines.Set(float64(runtime.NumGoroutine()))
+		time.Sleep(time.Second)
+	}
+}
+
+func recordMetrics() {
+	fmt.Println("Starting recording metrics")
+	go tick()
+}
+
+func main() {
+	recordMetrics()
+	fmt.Println("Initializing HTTP server")
+	http.Handle("/metrics", promhttp.Handler())
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+}
