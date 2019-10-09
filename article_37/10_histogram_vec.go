5b49db2bf2ab0b1e0240ef19c661c170cf7a391d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-09 09:11:33 +0200

    Example #10: histogram vec

---
+++ article_37/10_histogram_vec.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+var pageRequests = promauto.NewCounterVec(prometheus.CounterOpts{
+	Name: "page_requests",
+	Help: "The total number page/URL requests",
+}, []string{"url"})
+
+var histogram = promauto.NewHistogramVec(prometheus.HistogramOpts{
+	Name:    "response_time",
+	Help:    "Response time",
+	Buckets: prometheus.LinearBuckets(0, 5, 20),
+}, []string{"url"})
+
+func countEndpoint(request *http.Request, start time.Time) {
+	url := request.URL.String()
+	fmt.Printf("Request URL: %s\n", url)
+	duration := time.Since(start)
+	fmt.Printf("Time to serve the page: %s\n", duration)
+
+	// uprava citacu stranek
+	pageRequests.With(prometheus.Labels{"url": url}).Inc()
+
+	// uprava histogramu
+	histogram.With(prometheus.Labels{"url": url}).Observe(float64(duration.Microseconds()))
+}
+
+func mainEndpoint(writer http.ResponseWriter, request *http.Request) {
+	start := time.Now()
+	io.WriteString(writer, "Hello world!\n")
+	countEndpoint(request, start)
+}
+
+func fooEndpoint(writer http.ResponseWriter, request *http.Request) {
+	start := time.Now()
+	countEndpoint(request, start)
+	io.WriteString(writer, "FOO!\n")
+}
+
+func barEndpoint(writer http.ResponseWriter, request *http.Request) {
+	start := time.Now()
+	io.WriteString(writer, "BAR!\n")
+	countEndpoint(request, start)
+}
+
+func main() {
+	fmt.Println("Initializing HTTP server")
+
+	http.HandleFunc("/", mainEndpoint)
+	http.HandleFunc("/foo", fooEndpoint)
+	http.HandleFunc("/bar", barEndpoint)
+	http.Handle("/metrics", promhttp.Handler())
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+}
