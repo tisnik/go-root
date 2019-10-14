53871a2eefaa70771214e21d63a932f4b997baab
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-14 08:28:55 +0200

    Example #3: method specification, the first variant

---
+++ article_38/03_method_specification.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+)
+
+const ADDRESS = ":8080"
+
+var counter int
+var mutex = &sync.Mutex{}
+
+func mainEndpoint(writer http.ResponseWriter, request *http.Request) {
+	io.WriteString(writer, "Hello world!\n")
+}
+
+func counterEndpoint(writer http.ResponseWriter, request *http.Request) {
+	mutex.Lock()
+	counter++
+	fmt.Fprintf(writer, "Counter: %d\n", counter)
+	mutex.Unlock()
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/", mainEndpoint).Methods("GET")
+	router.HandleFunc("/counter", counterEndpoint).Methods("GET")
+
+	log.Println("Starting HTTP server at address", ADDRESS)
+	err := http.ListenAndServe(ADDRESS, router)
+	if err != nil {
+		log.Fatal("Unable to initialize HTTP server", err)
+		os.Exit(2)
+	}
+}
