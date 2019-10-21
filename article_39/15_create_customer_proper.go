df1cfbe58fedd6133281eea885aba9180dedd422
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-21 17:32:55 +0200

    Example #15: creating new customer using GORM - proper variant

---
+++ article_39/15_create_customer_proper.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+)
+
+type Customer struct {
+	Id      int `gorm:"Column:ID;PRIMARY_KEY"`
+	Name    string
+	Surname string
+	Address string
+	Country string
+	Phone   string
+}
+
+func main() {
+	dbDriver := flag.String("dbdriver", "sqlite3", "database driver specification")
+	storageSpecification := flag.String("storage", "./test.db", "storage specification")
+	flag.Parse()
+
+	db, err := gorm.Open(*dbDriver, *storageSpecification)
+	if err != nil {
+		log.Fatal("failed to connect database")
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&Customer{})
+
+	customer := Customer{Name: "Franta", Surname: "Vomáčka", Address: "Horní dolní", Country: "CR", Phone: "603 123 456"}
+	db.Create(&customer)
+
+	var customers []Customer
+	db.Find(&customers)
+	for _, customer := range customers {
+		fmt.Printf("%2d %-10s %-10s %-12s %-12s %s\n", customer.Id, customer.Name, customer.Surname, customer.Address, customer.Country, customer.Phone)
+	}
+}
