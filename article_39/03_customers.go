c497c05886090b7180d1953705f77c1ed512cfc0
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-21 17:32:55 +0200

    Example #3: reading all customers

---
+++ article_39/03_customers.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+)
+
+func listOfCustomers(connections *sql.DB) {
+	rows, err := connections.Query("SELECT id, name, surname, address, country, phone FROM customers ORDER BY id")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var name string
+		var surname string
+		var address string
+		var country string
+		var phone string
+
+		if err := rows.Scan(&id, &name, &surname, &address, &country, &phone); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%2d %-10s %-10s %-12s %-12s %s\n", id, name, surname, address, country, phone)
+	}
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	dbDriver := flag.String("dbdriver", "sqlite3", "database driver specification")
+	storageSpecification := flag.String("storage", "./test.db", "storage specification")
+	flag.Parse()
+
+	connections, err := sql.Open(*dbDriver, *storageSpecification)
+	if err != nil {
+		log.Fatal("Can not connect to data storage", err)
+	}
+	defer connections.Close()
+
+	log.Printf("Connected to database %v", connections)
+	listOfCustomers(connections)
+}
