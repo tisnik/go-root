bece8b8b04b209ee51dac991384cf63556f4385c
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-21 17:32:55 +0200

    Example #12: basic usage of GORM

---
+++ article_39/12_gorm_basics.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+)
+
+type Customer struct {
+	Id      int
+	Name    string
+	Surname string
+	Address string
+	Country string
+	Phone   string
+}
+
+func main() {
+	dbDriver := flag.String("dbdriver", "sqlite3", "database driver specification")
+	storageSpecification := flag.String("storage", "./test.db", "storage specification")
+	flag.Parse()
+
+	db, err := gorm.Open(*dbDriver, *storageSpecification)
+	if err != nil {
+		log.Fatal("failed to connect database")
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&Customer{})
+
+	var customers []Customer
+	db.Find(&customers)
+	for _, customer := range customers {
+		fmt.Printf("%2d %-10s %-10s %-12s %-12s %s\n", customer.Id, customer.Name, customer.Surname, customer.Address, customer.Country, customer.Phone)
+	}
+}
