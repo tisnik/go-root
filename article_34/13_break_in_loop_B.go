731585b14c8cf87db36eb3092becf18ef5cf2b98
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-24 10:29:37 +0200

    Added comments into example #13B

--- article_34/13_break_in_loop_B.go
+++ article_34/13_break_in_loop_B.go
@@ -1,9 +1,16 @@
+// Seriál "Programovací jazyk Go"
+//
+// Třicátá čtvrtá část
+//
+// Demonstrační příklad číslo 13:
+//    Nekonečná smyčka v jazyce Go.
+
 package main
 
 import "fmt"
 
 func main() {
-	for x := 1; true;  { /* endless loop */
+	for x := 1; true; { /* endless loop */
 		fmt.Printf("%d\n", x)
 		x++
 		if x > 10 {
