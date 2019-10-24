aae96f768c54f4869a95832f1f0f799cffdb708a
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-24 10:29:37 +0200

    Added comments into example #11

--- article_34/11_for_loop.go
+++ article_34/11_for_loop.go
@@ -1,9 +1,16 @@
+// Seriál "Programovací jazyk Go"
+//
+// Třicátá čtvrtá část
+//
+// Demonstrační příklad číslo 11:
+//    Implementace programové smyčky typu "for" v jazyce Go.
+
 package main
 
 import "fmt"
 
 func main() {
-	for x := 1; x <= 10000; x<<=1 {
+	for x := 1; x <= 10000; x <<= 1 {
 		fmt.Printf("%d\n", x)
 	}
 }
