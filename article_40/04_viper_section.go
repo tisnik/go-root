acd14a477e3a0a03adfdb449adf0d48892ac822e
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-30 09:43:25 +0100

    Example #4

---
+++ article_40/04_viper_section.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/spf13/viper"
+	"log"
+)
+
+func main() {
+	log.Println("Reading configuration")
+
+	viper.SetConfigName("config2")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Fatal error in config file: %s \n", err)
+	}
+	log.Println("Done")
+
+	serviceConfig := viper.Sub("service")
+	url := serviceConfig.GetString("url")
+	port := serviceConfig.GetInt("port")
+
+	log.Printf("Starting the service at address %s:%d\n", url, port)
+}
