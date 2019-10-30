0e20d95eeaedb93ff54c62d0ad3228f1d0594b0b
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-30 09:43:25 +0100

    Example #2

---
+++ article_40/02_viper_read_toml.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/spf13/viper"
+	"log"
+)
+
+func main() {
+	log.Println("Reading configuration")
+
+	viper.SetConfigName("config1")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Fatal error in config file: %s \n", err)
+	}
+	log.Println("Done")
+
+	url := viper.GetString("url")
+	port := viper.GetInt("port")
+
+	log.Printf("Starting the service at address %s:%d\n", url, port)
+}
