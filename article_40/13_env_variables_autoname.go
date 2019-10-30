c049820a60785ef54688ae218319c496ed8fbef6
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-30 13:21:59 +0100

    Example #1š

---
+++ article_40/13_env_variables_autoname.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/spf13/viper"
+	"log"
+)
+
+func main() {
+	log.Println("Reading configuration")
+
+	viper.SetConfigName("config9")
+	viper.AddConfigPath(".")
+
+	viper.BindEnv("editor")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Fatal error in config file: %s \n", err)
+	}
+	log.Println("Done")
+
+	editor := viper.GetString("editor")
+
+	log.Printf("Selected editor: %s\n", editor)
+}
