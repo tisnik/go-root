0a5922f982c5ba7e95540ff4635f39823199d10e
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-30 09:43:25 +0100

    Example #1

---
+++ article_40/01_viper_init.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/spf13/viper"
+	"log"
+)
+
+func main() {
+	log.Println("Reading configuration")
+
+	viper.SetConfigName("config1")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Fatal error in config file: %s \n", err)
+	}
+	log.Println("Done")
+}
