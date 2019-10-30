546782ff126b1ee9103154de2964954af5ece7f1
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-30 09:43:25 +0100

    Example #3

---
+++ article_40/03_viper_check.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/spf13/viper"
+	"log"
+)
+
+func main() {
+	log.Println("Reading configuration")
+
+	viper.SetConfigName("config1")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Fatal error in config file: %s \n", err)
+	}
+	log.Println("Done")
+
+	if !viper.IsSet("url") {
+		log.Fatalf("URL is not specified in config file")
+	}
+
+	if !viper.IsSet("port") {
+		log.Fatalf("port is not specified in config file")
+	}
+
+	url := viper.GetString("url")
+	port := viper.GetInt("port")
+
+	log.Printf("Starting the service at address %s:%d\n", url, port)
+}
