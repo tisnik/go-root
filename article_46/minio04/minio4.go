c910ab703e1a601df3b4a0c2cb4c9623c6ffefd4
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-12-16 09:25:47 +0100

    Minio SDK usage #4

---
+++ article_46/minio04/minio4.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/minio/minio-go/v6"
+	"log"
+)
+
+func listBuckets(minioClient *minio.Client) {
+	fmt.Println("List of buckets:")
+
+	buckets, err := minioClient.ListBuckets()
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+	for i, bucket := range buckets {
+		fmt.Printf("%d\t%+v\n", i, bucket)
+	}
+}
+
+func listObjects(minioClient *minio.Client, bucket string) {
+	fmt.Println("List of objects for bucket:", bucket)
+
+	done := make(chan struct{})
+	defer close(done)
+
+	objects := minioClient.ListObjects(bucket, "", false, done)
+	for object := range objects {
+		if object.Err != nil {
+			log.Println(object.Err)
+			return
+		}
+		fmt.Println(object)
+	}
+}
+
+func main() {
+	var endpoint = flag.String("endpoint", "127.0.0.1:9000", "MinIO service endpoint")
+	var accessKeyID = flag.String("accessKeyID", "", "Access key ID for MinIO")
+	var secretAccessKey = flag.String("secretAccessKey", "", "Secret access key for MinIO")
+	var useSSL = flag.Bool("useSSL", false, "Use SSL for communication with MinIO")
+	flag.Parse()
+
+	// initialize minio client object
+	minioClient, err := minio.New(*endpoint, *accessKeyID, *secretAccessKey, *useSSL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// everything seems to be ok
+	log.Printf("%#v\n", minioClient)
+
+	listBuckets(minioClient)
+	listObjects(minioClient, "foo")
+}
