0c21ff05f3ec8760148a47d3ac0724e455c18669
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-12-16 09:25:47 +0100

    Minio SDK usage #6

---
+++ article_46/minio06/minio6.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"github.com/minio/minio-go/v6"
+	"log"
+)
+
+func main() {
+	var endpoint = flag.String("endpoint", "127.0.0.1:9000", "MinIO service endpoint")
+	var accessKeyID = flag.String("accessKeyID", "", "Access key ID for MinIO")
+	var secretAccessKey = flag.String("secretAccessKey", "", "Secret access key for MinIO")
+	var useSSL = flag.Bool("useSSL", false, "Use SSL for communication with MinIO")
+	flag.Parse()
+
+	// initialize minio client object
+	minioClient, err := minio.New(*endpoint, *accessKeyID, *secretAccessKey, *useSSL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// everything seems to be ok
+	log.Printf("%#v\n", minioClient)
+
+	err = minioClient.FGetObject("foo", "logos.jpg", "logos.jpg", minio.GetObjectOptions{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
