e8efd5e7b4136ed61d5f550174c60b685665659b
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-12-16 09:25:47 +0100

    Minio SDK usage #7

---
+++ article_46/minio07/minio7.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"github.com/minio/minio-go/v6"
+	"log"
+)
+
+func printObject(minioClient *minio.Client, bucket string, objectName string) {
+	object, err := minioClient.GetObject(bucket, objectName, minio.GetObjectOptions{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer object.Close()
+
+	scanner := bufio.NewScanner(object)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func main() {
+	var endpoint = flag.String("endpoint", "127.0.0.1:9000", "MinIO service endpoint")
+	var accessKeyID = flag.String("accessKeyID", "", "Access key ID for MinIO")
+	var secretAccessKey = flag.String("secretAccessKey", "", "Secret access key for MinIO")
+	var useSSL = flag.Bool("useSSL", false, "Use SSL for communication with MinIO")
+	flag.Parse()
+
+	// initialize minio client object
+	minioClient, err := minio.New(*endpoint, *accessKeyID, *secretAccessKey, *useSSL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// everything seems to be ok
+	log.Printf("%#v\n", minioClient)
+
+	printObject(minioClient, "foo", "t.go")
+}
