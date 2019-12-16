aaf9aca52773ced004fea90015365d294b5f67d7
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-12-16 09:25:47 +0100

    Minio SDK usage #1

---
+++ article_46/minio01/minio1.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/minio/minio-go/v6"
+	"log"
+)
+
+func main() {
+	endpoint := "127.0.0.1:9000"
+
+	// it is needed to change the following two keys
+	accessKeyID := "3V8WMANF061SGOIVR7AA"
+	secretAccessKey := "AHTM6+74n1Z8DZRZ4V7o83QcnYRnTEVblVb8sIlE"
+
+	useSSL := true
+
+	// initialize minio client object
+	minioClient, err := minio.New(endpoint, accessKeyID, secretAccessKey, useSSL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// everything seems to be ok
+	log.Printf("%#v\n", minioClient)
+}
