27cd1639e0a166417932e02f177f2f3344148a73
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-12-18 10:14:40 +0100

    Minio example client written in Python #3

---
+++ article_47/python/minio3.py
@@ -0,0 +1,30 @@
+import argparse
+
+from minio import Minio
+from minio.error import ResponseError
+
+
+def list_buckets(minioClient):
+    buckets = minioClient.list_buckets()
+    for bucket in buckets:
+        print(bucket.name, bucket.creation_date)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--endpoint", default="127.0.0.1:9000",
+                    help="MinIO service endpoint")
+parser.add_argument("--accessKeyID", default="",
+                    help="Access key ID for MinIO")
+parser.add_argument("--secretAccessKey", default="",
+                    help="Secret access key for MinIO")
+parser.add_argument("--enable-ssl", dest="useSSL", action="store_true",
+                    help="Use SSL for communication with MinIO")
+parser.add_argument("--disable-ssl", dest="useSSL", action="store_false",
+                    help="Don't SSL for communication with MinIO")
+args = parser.parse_args()
+
+minioClient = Minio(args.endpoint,
+                    access_key=args.accessKeyID,
+                    secret_key=args.secretAccessKey,
+                    secure=args.useSSL)
+list_buckets(minioClient)
