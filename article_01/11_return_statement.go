bbc71fa8c3e1ec669f67830ddf08777380805e66
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-01-02 08:52:29 +0100

    Added link to article where this example is used

--- article_01/11_return_statement.go
+++ article_01/11_return_statement.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
-// První část
+// První část:
+//    Go: minimalistický a překvapivě výkonný programovací jazyk
+//    https://www.root.cz/clanky/go-minimalisticky-a-prekvapive-vykonny-programovaci-jazyk/
 //
 // Demonstrační příklad číslo 11:
 //    Deklarace uživatelské funkce s návratovou hodnotou
