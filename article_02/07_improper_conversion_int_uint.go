91f51579177cbc4d1cdbc536dadd0bfdfed0d41d
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-01-03 08:52:00 +0100

    Added link to article where this example is used

--- article_02/07_improper_conversion_int_uint.go
+++ article_02/07_improper_conversion_int_uint.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
-// Druhá část
+// Druhá část:
+//    Datové typy v programovacím jazyku Go
+//    https://www.root.cz/clanky/datove-typy-v-programovacim-jazyku-go/
 //
 // Demonstrační příklad číslo 7:
 //    Přiřazení uint8 -> int8 a naopak
