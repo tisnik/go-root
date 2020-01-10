0e46ee1320624bb1afa1799facbaf62ff8c6d703
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-01-10 09:09:45 +0100

    Plotly example #3

---
+++ article_51/plotly03.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func dataHandler(writer http.ResponseWriter, reader *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	fmt.Fprintf(writer, `
+{
+    "x": [1, 2, 3, 4, 5],
+    "y": [1, 2, 4, 8, 16]
+}
+	`)
+}
+
+func startHttpServer(address string) {
+	log.Printf("Starting server on address %s", address)
+	http.Handle("/", http.FileServer(http.Dir("plotly03/")))
+	http.HandleFunc("/data", dataHandler)
+	http.ListenAndServe(address, nil)
+}
+
+func main() {
+	startHttpServer(":8080")
+}
