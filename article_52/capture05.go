76a106e3dd1a52386cfec2c40b4073bfd6039d9f
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-01-15 10:00:14 +0100

    Error output is not captured

---
+++ article_52/capture05.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"sync"
+)
+
+func CaptureStandardOutput(function func()) (string, error) {
+	// backup of the real stdout
+	stdout := os.Stdout
+
+	// temporary replacement for stdout
+	reader, writer, err := os.Pipe()
+	if err != nil {
+		return "", err
+	}
+
+	// temporarily replace real Stdout by the mocked one
+	defer func() {
+		os.Stdout = stdout
+	}()
+	os.Stdout = writer
+
+	// channel with captured standard output
+	captured := make(chan string)
+
+	// synchronization object
+	wg := new(sync.WaitGroup)
+	// we are going to wait for one goroutine only
+	wg.Add(1)
+
+	go func() {
+		var buf bytes.Buffer
+		// goroutine is started -> inform main one via WaitGroup object
+		wg.Done()
+		io.Copy(&buf, reader)
+		captured <- buf.String()
+	}()
+	// wait for goroutine to start
+	wg.Wait()
+	// provided function that (probably) prints something to standard output
+	function()
+	writer.Close()
+	return <-captured, nil
+}
+
+func main() {
+	str, err := CaptureStandardOutput(func() { fmt.Fprintln(os.Stderr, "Error output again") })
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Last line of captured output:")
+	fmt.Println("-------------------------------")
+	fmt.Println(str)
+	fmt.Println("-------------------------------")
+}
