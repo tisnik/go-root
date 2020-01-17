19e4ff039eff517ad3c623db8ebf83e03ddc4e75
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-01-17 08:33:00 +0100

    Fixed issue #57

--- article_04/01_B_unique_names.go
+++ article_04/01_B_unique_names.go
@@ -12,10 +12,12 @@ import (
 	"math"
 )
 
+// Point je uživatelsky definovaná datová struktura
 type Point struct {
 	x1, y1 float64
 }
 
+// Line je uživatelsky definovaná datová struktura
 type Line struct {
 	x1, y1 float64
 	x2, y2 float64
@@ -25,10 +27,12 @@ func length() float64 {
 	return 0
 }
 
+// Metoda definovaná pro strukturu Point
 func (point Point) length() float64 {
 	return 0
 }
 
+// Metoda definovaná pro strukturu Line
 func (line Line) length() float64 {
 	return math.Hypot(line.x1-line.x2, line.y1-line.y2)
 }
