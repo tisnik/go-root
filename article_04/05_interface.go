661da30374629cb7ef9377570f65b5d30f0adfeb
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-01-17 08:38:07 +0100

    Fixed issues #63, #64, and #65

--- article_04/05_interface.go
+++ article_04/05_interface.go
@@ -7,13 +7,19 @@
 
 package main
 
+// Shape je uživatelsky definovaná datová struktura
+// představující geometrický tvar
 type Shape interface {
 }
 
+// OpenShape je uživatelsky definovaná datová struktura
+// představující otevřené tvary (úsečka, oblouk, křivka)
 type OpenShape interface {
 	length() float64
 }
 
+// ClosedShape je uživatelsky definovaná datová struktura
+// představující uzavřené tvary (obdélník, kružnice, elipsa)
 type ClosedShape interface {
 	area() float64
 	perimeter() float64
