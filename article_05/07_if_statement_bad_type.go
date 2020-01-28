15125ba36bc02a4533783b3000d82e17b6f858e8
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-01-28 08:16:48 +0100

    Added link to article where this example is used

--- article_05/07_if_statement_bad_type.go
+++ article_05/07_if_statement_bad_type.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Pátá část
+//    Konstrukce pro řízení běhu programu v jazyce Go
+//    https://www.root.cz/clanky/konstrukce-pro-rizeni-behu-programu-v-jazyce-go/
 //
 // Demonstrační příklad číslo 7:
 //    Řídicí konstrukce if a hodnoty, které nejsou typu boolean.
