17790414852ab0f34f685aa2fec67f3f8ac42d2d
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-01-29 08:52:45 +0100

    Asm in Go: implementation of Add functions for various bit sizes

---
+++ article_54/asm04.go
@@ -0,0 +1,24 @@
+package main
+
+func Add8(x int8, y int8, z int8) int8 {
+	return x + y + z
+}
+
+func Add16(x int16, y int16, z int16) int16 {
+	return x + y + z
+}
+
+func Add32(x int32, y int32, z int32) int32 {
+	return x + y + z
+}
+
+func Add64(x int64, y int64, z int64) int64 {
+	return x + y + z
+}
+
+func main() {
+	println(Add8(1, 2, 3))
+	println(Add16(1, 2, 3))
+	println(Add32(1, 2, 3))
+	println(Add64(1, 2, 3))
+}
