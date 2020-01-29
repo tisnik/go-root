d0ef45866c8262b2fa8ecfe520ce1aa15d2304de
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-01-29 08:51:08 +0100

    Added link to article where this example is used

--- article_05/13_variables_in_switch.go
+++ article_05/13_variables_in_switch.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Pátá část
+//    Konstrukce pro řízení běhu programu v jazyce Go
+//    https://www.root.cz/clanky/konstrukce-pro-rizeni-behu-programu-v-jazyce-go/
 //
 // Demonstrační příklad číslo 13:
 //    Řídicí konstrukce switch s vyhodnocovaným výrazem.
