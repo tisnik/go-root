0a322d07ff0111dcf553cf9ab69be4fa92949994
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-01-30 08:52:59 +0100

    Added link to article where this example is used

--- article_05/21_for_loop_with_condition.go
+++ article_05/21_for_loop_with_condition.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Pátá část
+//    Konstrukce pro řízení běhu programu v jazyce Go
+//    https://www.root.cz/clanky/konstrukce-pro-rizeni-behu-programu-v-jazyce-go/
 //
 // Demonstrační příklad číslo 21:
 //    Základní forma programové smyčky for s podmínkou.
