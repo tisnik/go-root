b91bf02f2e0c29a6fe532789015f144302d28305
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-02-05 08:38:58 +0100

    No-op filter

---
+++ article_55/01_no_op_filter.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"github.com/disintegration/gift"
+	"image"
+	"image/png"
+	"log"
+	"os"
+)
+
+const SourceImageFileName = "Lenna.png"
+const DestinationImageFileName = "01.png"
+
+func loadImage(filename string) (image.Image, error) {
+	infile, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer infile.Close()
+
+	src, _, err := image.Decode(infile)
+	if err != nil {
+		return nil, err
+	}
+	return src, nil
+}
+
+func saveImage(filename string, img image.Image) error {
+	outfile, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer outfile.Close()
+
+	png.Encode(outfile, img)
+	return nil
+}
+
+func main() {
+	sourceImage, err := loadImage(SourceImageFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	g := gift.New()
+
+	destinationImage := image.NewRGBA(g.Bounds(sourceImage.Bounds()))
+	g.Draw(destinationImage, sourceImage)
+
+	err = saveImage(DestinationImageFileName, destinationImage)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
