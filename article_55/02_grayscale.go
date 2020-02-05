8c2c590b33f5f4c2edac57b8e683f44cc96810d7
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-02-05 08:38:59 +0100

    Grayscale filter

---
+++ article_55/02_grayscale.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"github.com/disintegration/gift"
+	"image"
+	"image/png"
+	"log"
+	"os"
+)
+
+const SourceImageFileName = "Lenna.png"
+const DestinationImageFileName1 = "02_grayscale.png"
+const DestinationImageFileName2 = "02_sepia.png"
+
+func loadImage(filename string) (image.Image, error) {
+	infile, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer infile.Close()
+
+	src, _, err := image.Decode(infile)
+	if err != nil {
+		return nil, err
+	}
+	return src, nil
+}
+
+func saveImage(filename string, img image.Image) error {
+	outfile, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer outfile.Close()
+
+	png.Encode(outfile, img)
+	return nil
+}
+
+func main() {
+	sourceImage, err := loadImage(SourceImageFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	g := gift.New(
+		gift.Grayscale())
+
+	destinationImage := image.NewRGBA(g.Bounds(sourceImage.Bounds()))
+	g.Draw(destinationImage, sourceImage)
+
+	err = saveImage(DestinationImageFileName1, destinationImage)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	g = gift.New(
+		gift.Sepia(50.0))
+
+	destinationImage = image.NewRGBA(g.Bounds(sourceImage.Bounds()))
+	g.Draw(destinationImage, sourceImage)
+
+	err = saveImage(DestinationImageFileName2, destinationImage)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
