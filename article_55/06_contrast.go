6025c3dcddd4de8ed8a7bbafee59f826a0a5e0fb
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-02-05 08:38:59 +0100

    Contrast change filter

---
+++ article_55/06_contrast.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"github.com/disintegration/gift"
+	"image"
+	"image/png"
+	"log"
+	"os"
+)
+
+const SourceImageFileName = "Lenna.png"
+const DestinationImageFileNameTemplate = "06_contrast_%d.png"
+
+func loadImage(filename string) (image.Image, error) {
+	infile, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer infile.Close()
+
+	src, _, err := image.Decode(infile)
+	if err != nil {
+		return nil, err
+	}
+	return src, nil
+}
+
+func saveImage(filename string, img image.Image) error {
+	outfile, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer outfile.Close()
+
+	png.Encode(outfile, img)
+	return nil
+}
+
+func main() {
+	sourceImage, err := loadImage(SourceImageFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for contrast := -50; contrast <= 50; contrast += 20 {
+		g := gift.New(
+			gift.Contrast(float32(contrast)))
+
+		destinationImage := image.NewRGBA(g.Bounds(sourceImage.Bounds()))
+		g.Draw(destinationImage, sourceImage)
+
+		filename := fmt.Sprintf(DestinationImageFileNameTemplate, contrast)
+		err = saveImage(filename, destinationImage)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
