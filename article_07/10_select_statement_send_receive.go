25522aaf5d29aef000f9d18d5b0e4577fea5e43f
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-02-12 08:06:22 +0100

    Added link to article where this example is used

--- article_07/10_select_statement_send_receive.go
+++ article_07/10_select_statement_send_receive.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Sedmá část
+//    Programovací jazyk Go: dokončení popisu vlastností samotného jazyka
+//    https://www.root.cz/clanky/programovaci-jazyk-go-dokonceni-popisu-vlastnosti-samotneho-jazyka/
 //
 // Demonstrační příklad číslo 10:
 //    Použití příkazu select pro zápis do kanálu i pro čtení z kanálu.
