55b922cb0650287ceef843dbf5e599f5b8e88588
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-02-12 08:23:11 +0100

    Usage of go-memset package

---
+++ article_56/06_empty_image_go_memset/empty_image.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	memset "github.com/tmthrgd/go-memset"
+	"image"
+	"image/png"
+	"log"
+	"os"
+)
+
+const DestinationImageFileName = "empty.png"
+
+func saveImage(filename string, img image.Image) error {
+	outfile, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer outfile.Close()
+
+	png.Encode(outfile, img)
+	return nil
+}
+
+func fillPixels(pixels []uint8) {
+	memset.Memset(pixels, 0xff)
+}
+
+func main() {
+	destinationImage := image.NewRGBA(image.Rect(0, 0, 256, 256))
+
+	fillPixels(destinationImage.Pix)
+
+	err := saveImage(DestinationImageFileName, destinationImage)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
