e33b75c4c16778f80dec6d5af80c239493269f60
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-02-12 08:22:06 +0100

    Image filling, Go version

---
+++ article_56/01_empty_image_go/empty_image.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"image"
+	"image/png"
+	"log"
+	"os"
+)
+
+const DestinationImageFileName = "empty.png"
+
+func saveImage(filename string, img image.Image) error {
+	outfile, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer outfile.Close()
+
+	png.Encode(outfile, img)
+	return nil
+}
+
+func fillPixels(pixels []uint8) {
+	for i := 0; i < len(pixels); i++ {
+		pixels[i] = 255
+	}
+}
+
+func main() {
+	destinationImage := image.NewRGBA(image.Rect(0, 0, 256, 256))
+
+	fillPixels(destinationImage.Pix)
+
+	err := saveImage(DestinationImageFileName, destinationImage)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
