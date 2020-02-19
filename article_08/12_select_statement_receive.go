55c618b4472a273692c480efab2ac31c9a1f7459
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-02-19 11:17:18 +0100

    Added link to article where this example is used

--- article_08/12_select_statement_receive.go
+++ article_08/12_select_statement_receive.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Osmá část
+//     Tvorba balíčků a pokročilejší operace s kanály v jazyce Go
+//     https://www.root.cz/clanky/tvorba-balicku-a-pokrocilejsi-operace-s-kanaly-v-jazyce-go/
 //
 // Demonstrační příklad číslo 12:
 //    Použití příkazu select pro čtení z kanálů s timeoutem.
