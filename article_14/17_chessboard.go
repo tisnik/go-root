0879ce5c903a916b7288f1f1180aa99f8b3a9a84
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-02-20 12:15:46 +0100

    Fixed issue #11

--- article_14/17_chessboard.go
+++ article_14/17_chessboard.go
@@ -32,26 +32,26 @@ func main() {
 	palette[0] = color.RGBA{150, 205, 50, 255}
 	palette[1] = color.RGBA{0, 100, 0, 255}
 
-	index_color := 0
-	board_size := 8
-	hor_block := int(width / board_size)
-	ver_block := int(height / board_size)
+	indexColor := 0
+	boardSize := 8
+	horizontalBlock := int(width / boardSize)
+	verticalBlock := int(height / boardSize)
 
-	x_from := 0
-	x_to := hor_block
-	for x := 0; x < board_size; x++ {
-		y_from := 0
-		y_to := ver_block
-		for y := 0; y < board_size; y++ {
-			r := image.Rect(x_from, y_from, x_to, y_to)
-			draw.Draw(img, r, &image.Uniform{palette[index_color]}, image.ZP, draw.Src)
-			y_from = y_to
-			y_to += ver_block
-			index_color = 1 - index_color
+	xFrom := 0
+	xTo := horizontalBlock
+	for x := 0; x < boardSize; x++ {
+		yFrom := 0
+		yTo := verticalBlock
+		for y := 0; y < boardSize; y++ {
+			r := image.Rect(xFrom, yFrom, xTo, yTo)
+			draw.Draw(img, r, &image.Uniform{palette[indexColor]}, image.ZP, draw.Src)
+			yFrom = yTo
+			yTo += verticalBlock
+			indexColor = 1 - indexColor
 		}
-		x_from = x_to
-		x_to += hor_block
-		index_color = 1 - index_color
+		xFrom = xTo
+		xTo += horizontalBlock
+		indexColor = 1 - indexColor
 	}
 	png.Encode(outfile, img)
 }
