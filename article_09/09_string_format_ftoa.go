6ca700955f64c2429baddd1d79f1271205597214
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-02-20 08:32:49 +0100

    Fixed issue #129: 09_string_format_ftoa.go:12:2: should not use dot imports

--- article_09/09_string_format_ftoa.go
+++ article_09/09_string_format_ftoa.go
@@ -9,32 +9,32 @@ package main
 
 import (
 	"math"
-	. "strconv"
+	"strconv"
 )
 
 func main() {
 	value := math.Pi
-	println(FormatFloat(value, 'f', 5, 64))
-	println(FormatFloat(value, 'f', 10, 64))
-	println(FormatFloat(value, 'e', 10, 64))
-	println(FormatFloat(value, 'g', 10, 64))
-	println(FormatFloat(value, 'b', 10, 64))
+	println(strconv.FormatFloat(value, 'f', 5, 64))
+	println(strconv.FormatFloat(value, 'f', 10, 64))
+	println(strconv.FormatFloat(value, 'e', 10, 64))
+	println(strconv.FormatFloat(value, 'g', 10, 64))
+	println(strconv.FormatFloat(value, 'b', 10, 64))
 
 	println()
 
 	value = 1e20
-	println(FormatFloat(value, 'f', 5, 64))
-	println(FormatFloat(value, 'f', 10, 64))
-	println(FormatFloat(value, 'e', 10, 64))
-	println(FormatFloat(value, 'g', 10, 64))
-	println(FormatFloat(value, 'b', 10, 64))
+	println(strconv.FormatFloat(value, 'f', 5, 64))
+	println(strconv.FormatFloat(value, 'f', 10, 64))
+	println(strconv.FormatFloat(value, 'e', 10, 64))
+	println(strconv.FormatFloat(value, 'g', 10, 64))
+	println(strconv.FormatFloat(value, 'b', 10, 64))
 
 	println()
 
 	value = 0
-	println(FormatFloat(value, 'f', 5, 64))
-	println(FormatFloat(value, 'f', 10, 64))
-	println(FormatFloat(value, 'e', 10, 64))
-	println(FormatFloat(value, 'g', 10, 64))
-	println(FormatFloat(value, 'b', 5, 64))
+	println(strconv.FormatFloat(value, 'f', 5, 64))
+	println(strconv.FormatFloat(value, 'f', 10, 64))
+	println(strconv.FormatFloat(value, 'e', 10, 64))
+	println(strconv.FormatFloat(value, 'g', 10, 64))
+	println(strconv.FormatFloat(value, 'b', 5, 64))
 }
