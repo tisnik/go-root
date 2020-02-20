3230e3afe9b4f9d27ef9b05750985e3e2fd94956
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-02-20 08:32:01 +0100

    Fixed issue #128: 08_string_format_int.go:11:2: should not use dot imports

--- article_09/08_string_format_int.go
+++ article_09/08_string_format_int.go
@@ -8,12 +8,12 @@
 package main
 
 import (
-	. "strconv"
+	"strconv"
 )
 
 func main() {
 	value := int64(0xcafebabe)
 	for base := 2; base < 36; base++ {
-		println(base, FormatInt(value, base))
+		println(base, strconv.FormatInt(value, base))
 	}
 }
