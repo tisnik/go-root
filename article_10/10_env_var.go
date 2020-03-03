cf9c1aec34cfd46dce10e14255284eaf185a652e
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-03-03 08:00:58 +0100

    Added link to article where this example is used

--- article_10/10_env_var.go
+++ article_10/10_env_var.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Desátá část
+//     Užitečné balíčky pro každodenní použití Go (2), porovnání výkonnosti Go s céčkem
+//     https://www.root.cz/clanky/uzitecne-balicky-pro-kazdodenni-pouziti-go-2-porovnani-vykonnosti-go-s-ceckem/
 //
 // Demonstrační příklad číslo 10:
 //    Přístup k proměnným prostředí.
