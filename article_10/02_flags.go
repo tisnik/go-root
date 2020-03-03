0e7d0d1206737d99e805326c84eac072e430c422
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-03-03 08:00:58 +0100

    Added link to article where this example is used

--- article_10/02_flags.go
+++ article_10/02_flags.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Desátá část
+//     Užitečné balíčky pro každodenní použití Go (2), porovnání výkonnosti Go s céčkem
+//     https://www.root.cz/clanky/uzitecne-balicky-pro-kazdodenni-pouziti-go-2-porovnani-vykonnosti-go-s-ceckem/
 //
 // Demonstrační příklad číslo 2:
 //    Vylepšené přečtení argumentů předaných na příkazovém řádku.
