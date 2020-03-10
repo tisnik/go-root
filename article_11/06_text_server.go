8e178269f5ef291a0da4cc3f86ecaf4dd703fdc7
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-03-10 08:47:31 +0100

    Added link to article where this example is used

--- article_11/06_text_server.go
+++ article_11/06_text_server.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Jedenáctá část
+//     Vývoj síťových aplikací v programovacím jazyku Go
+//     https://www.root.cz/clanky/vyvoj-sitovych-aplikaci-v-programovacim-jazyku-go/
 //
 // Demonstrační příklad číslo 6:
 //     Server posílající klientovi textová data
