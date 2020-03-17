53567b0401746365596cdebaa1d4e92d0bc9ca6f
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-03-17 08:50:10 +0100

    Added link to article where this example is used

--- article_12/08_factorial_prepare.go
+++ article_12/08_factorial_prepare.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Dvanáctá část
+//     Vývoj síťových aplikací v programovacím jazyku Go (pokračování)
+//     https://www.root.cz/clanky/vyvoj-sitovych-aplikaci-v-programovacim-jazyku-go-pokracovani/
 //
 // Demonstrační příklad číslo 8:
 //    Webová aplikace pro výpočet faktoriálu (příprava)
