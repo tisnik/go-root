57ce7af9bdab03933367aeff429f6e6b01a59e64
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-03-25 11:23:54 +0100

    Mimicking C example #3

--- article_61/go/test3.go
+++ article_61/go/test3.go
@@ -42,7 +42,13 @@ func main() {
 
 	primarySurface.FillRect(nil, sdl.MapRGB(convertedImage.Format, 192, 255, 192))
 
-	err = convertedImage.Blit(nil, primarySurface, nil)
+	var destRect sdl.Rect
+	destRect.X = 160
+	destRect.Y = 120
+	destRect.W = 250
+	destRect.H = 250
+
+	err = convertedImage.BlitScaled(nil, primarySurface, &destRect)
 	if err != nil {
 		panic(err)
 	}
