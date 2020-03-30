525ece4d3614d1aa11b001df991005ecdc6fe7c9
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-03-30 08:15:29 +0200

    Example #15: putpixel function

---
+++ article_62/c/test15.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+#define WIDTH  640
+#define HEIGHT 480
+
+void putpixel(SDL_Surface *surface, int x, int y, unsigned char r, unsigned char g, unsigned char b)
+{
+    if (x>=0 && x< surface->w && y>=0 && y < surface->h) {
+        if (surface->format->BitsPerPixel == 24) {
+            Uint8 *pixel = (Uint8 *)surface->pixels;
+            pixel += x*3;
+            pixel += y*surface->pitch;
+            *pixel++ = b;
+            *pixel++ = g;
+            *pixel   = r;
+        }
+        if (surface->format->BitsPerPixel == 32) {
+            Uint8 *pixel = (Uint8 *)surface->pixels;
+            pixel += x*4;
+            pixel += y*surface->pitch;
+            *pixel++ = b;
+            *pixel++ = g;
+            *pixel   = r;
+        }
+    }
+}
+
+int main(int argc, char** args) {
+    SDL_Surface *primarySurface = NULL;
+    SDL_Window  *window = NULL;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        puts("Error initializing SDL");
+        puts(SDL_GetError());
+        return 1;
+    } 
+
+    window = SDL_CreateWindow("SDL2 example #11", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+
+    if (!window) {
+        puts("Error creating window");
+        puts(SDL_GetError());
+        return 1;
+    }
+
+    primarySurface = SDL_GetWindowSurface(window);
+    printf("Must lock: %d\n", SDL_MUSTLOCK(primarySurface));
+
+    if (!primarySurface) {
+        puts("Error getting surface");
+        puts(SDL_GetError());
+        return 1;
+    }
+
+    {
+        int x, y;
+        unsigned char  r, g, b;
+
+        SDL_LockSurface(primarySurface);
+        for (y=0; y<primarySurface->h; y++) {
+            for (x=0; x<primarySurface->w; x++) {
+                r = 255 * x / primarySurface->w;
+                g = 128;
+                b = 255 * y / primarySurface->h;
+                putpixel(primarySurface, x, y, r, g, b);
+            }
+        }
+        SDL_UnlockSurface(primarySurface);
+    }
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Delay(5000);
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+    
+    return 0;
+}
