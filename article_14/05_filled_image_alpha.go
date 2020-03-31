74fa1258d777c348d424d97b4350ab18f3b75dcc
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-03-31 09:38:19 +0200

    Added link to article where this example is used

--- article_14/05_filled_image_alpha.go
+++ article_14/05_filled_image_alpha.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Čtrnáctá část
+//     Programovací jazyk Go a počítačová grafika (úvod)
+//     https://www.root.cz/clanky/programovaci-jazyk-go-a-pocitacova-grafika-uvod/
 //
 // Demonstrační příklad číslo 5:
 //     Obrázek, v němž mají pixely různou průhlednost (nekorektní varianta)
