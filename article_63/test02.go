74279bcd96a462f24a56e504b4d7d1a621cbc126
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-04-01 09:48:08 +0200

    More structured source

---
+++ article_63/test02.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"github.com/veandco/go-sdl2/img"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const (
+	width  = 640
+	height = 480
+)
+
+var (
+	window         *sdl.Window
+	primarySurface *sdl.Surface
+	image          *sdl.Surface
+)
+
+func initialize() {
+	err := sdl.Init(sdl.INIT_VIDEO)
+	if err != nil {
+		panic(err)
+	}
+
+	window, err = sdl.CreateWindow("SDL2 example #10", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		width, height, sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+
+	primarySurface, err = window.GetSurface()
+	if err != nil {
+		panic(err)
+	}
+
+	image, err = img.Load("globe.png")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func finalize() {
+	window.Destroy()
+	sdl.Quit()
+}
+
+func redraw() {
+	primarySurface.FillRect(nil, sdl.MapRGB(primarySurface.Format, 192, 255, 192))
+
+	dstRect := sdl.Rect{
+		X: width/2 - image.W/2,
+		Y: height/2 - image.H/2,
+		W: 0,
+		H: 0,
+	}
+
+	err := image.Blit(nil, primarySurface, &dstRect)
+	if err != nil {
+		panic(err)
+	}
+
+	window.UpdateSurface()
+}
+
+func main() {
+	initialize()
+	defer finalize()
+
+	sdl.Delay(10)
+	redraw()
+	sdl.Delay(5000)
+
+	finalize()
+}
