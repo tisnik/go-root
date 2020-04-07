3f10749fa2a6b9e2e4aa4de779664d7f8210c5ea
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-04-07 08:50:48 +0200

    Added link to article where this example is used

--- article_15/19_gg_line_width.go
+++ article_15/19_gg_line_width.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Patnáctá část
+//     Programovací jazyk Go a grafika: tvorba animovaných GIFů, grafická knihovna GG
+//     https://www.root.cz/clanky/programovaci-jazyk-go-a-grafika-tvorba-animovanych-gifu-graficka-knihovna-gg/
 //
 // Demonstrační příklad číslo 19:
 //     Specifikace šířky vykreslovaných cest
