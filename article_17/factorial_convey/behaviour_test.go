59306389f4bfdcb26dd73ccd6d70a82d90c0ede6
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-04-20 08:08:57 +0200

    Added link to article where this example is used

--- article_17/factorial_convey/behaviour_test.go
+++ article_17/factorial_convey/behaviour_test.go
@@ -1,3 +1,9 @@
+// Seriál "Programovací jazyk Go"
+//
+// Sedmnáctá část
+//     Testování aplikací naprogramovaných v jazyce Go
+//     https://www.root.cz/clanky/testovani-aplikaci-naprogramovanych-v-jazyce-go/
+//
 package factorial
 
 import (
