a3043aa065b54fdc0c1b4cc75425f75108bf679a
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-04-21 08:02:11 +0200

    Added link to article where this example is used

--- article_17/tests03/add_test.go
+++ article_17/tests03/add_test.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Sedmnáctá část
+//     Testování aplikací naprogramovaných v jazyce Go
+//     https://www.root.cz/clanky/testovani-aplikaci-naprogramovanych-v-jazyce-go/
 //
 // Demonstrační příklad číslo 3:
 //     Implementace jednotkových testů.
