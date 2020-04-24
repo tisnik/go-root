c7fc618e4c579f9d15392b80d07164a63ece0e04
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-04-24 11:53:33 +0200

    Simple no-op raster filter

---
+++ article_56/02_no_op_filter.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"image"
+	"image/png"
+	"log"
+	"os"
+)
+
+const SourceImageFileName = "gopher.png"
+const DestinationImageFileName = "gopher_copied.png"
+
+func loadImage(filename string) (image.Image, error) {
+	infile, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer infile.Close()
+
+	src, _, err := image.Decode(infile)
+	if err != nil {
+		return nil, err
+	}
+	return src, nil
+}
+
+func saveImage(filename string, img image.Image) error {
+	outfile, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer outfile.Close()
+
+	png.Encode(outfile, img)
+	return nil
+}
+
+func copyPixels(src []uint8, dst []uint8) {
+	for i := 0; i < len(src); i++ {
+		dst[i] = src[i]
+	}
+}
+
+func main() {
+	s, err := loadImage(SourceImageFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var sourceImage *image.RGBA = s.(*image.RGBA)
+	destinationImage := image.NewRGBA(sourceImage.Bounds())
+
+	copyPixels(sourceImage.Pix, destinationImage.Pix)
+
+	err = saveImage(DestinationImageFileName, destinationImage)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
