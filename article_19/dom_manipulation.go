3fff5f76308dd475125c5485dc12d5d6ee5b28d9
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-05-04 08:54:50 +0200

    Added comments

--- article_19/dom_manipulation.go
+++ article_19/dom_manipulation.go
@@ -1,3 +1,12 @@
+// Seriál "Programovací jazyk Go"
+//
+// Devatenáctá část
+//     Využití WebAssembly z programovacího jazyka Go
+//     https://www.root.cz/clanky/vyuziti-webassembly-z-programovaciho-jazyka-go/
+//
+// Demonstrační příklad:
+//     Manipulace s DOMem z jazyka Go.
+
 package main
 
 import (
