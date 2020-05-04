7fb618515894a6d0774bd2b7e146dcd7bfa36aa9
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-05-04 08:54:50 +0200

    Added comments

--- article_19/hello.go
+++ article_19/hello.go
@@ -1,3 +1,12 @@
+// Seriál "Programovací jazyk Go"
+//
+// Devatenáctá část
+//     Využití WebAssembly z programovacího jazyka Go
+//     https://www.root.cz/clanky/vyuziti-webassembly-z-programovaciho-jazyka-go/
+//
+// Demonstrační příklad:
+//     Typický program typu "Hello world"
+
 package main
 
 import "fmt"
