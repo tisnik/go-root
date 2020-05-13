38ed2f734bd233a0ec43681d134db2baf01ea77f
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-05-13 09:43:09 +0200

    Added comments and link to article where this example is used

--- article_20/05_basic_completer.go
+++ article_20/05_basic_completer.go
@@ -1,10 +1,12 @@
 // Seriál "Programovací jazyk Go"
 //
 // Dvacátá část
+//     Jazyk Go a aplikace s vlastním příkazovým řádkem
+//     https://www.root.cz/clanky/jazyk-go-a-aplikace-s-vlastnim-prikazovym-radkem/
 //
 // Demonstrační příklad číslo 5:
 //     Go-prompt: vstup s nápovědou.
-//     
+//
 
 package main
 
