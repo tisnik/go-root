f1be9dcc8b1f425adcfce23ee9cb5b79af6b7980
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-05-14 09:29:02 +0200

    Fixed issue #218

--- article_13/06_json_mashal_array_of_struct.go
+++ article_13/06_json_mashal_array_of_struct.go
@@ -15,7 +15,7 @@ import (
 )
 
 type User struct {
-	Id      uint32
+	ID      uint32
 	Name    string
 	Surname string
 }
@@ -23,15 +23,15 @@ type User struct {
 func main() {
 	var users = [3]User{
 		User{
-			Id:      1,
+			ID:      1,
 			Name:    "Pepek",
 			Surname: "Vyskoč"},
 		User{
-			Id:      2,
+			ID:      2,
 			Name:    "Pepek",
 			Surname: "Vyskoč"},
 		User{
-			Id:      3,
+			ID:      3,
 			Name:    "Josef",
 			Surname: "Vyskočil"},
 	}
