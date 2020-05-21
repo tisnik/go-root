a26746fdd49fb1636ea196130729ce65d781b93a
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-05-21 08:10:20 +0200

    Added comments and link to article where this example is used

--- article_21/duration/duration5.go
+++ article_21/duration/duration5.go
@@ -1,3 +1,12 @@
+// Seriál "Programovací jazyk Go"
+//
+// Dvacátá první část
+//     Knihovny pro Go umožňující naplánování a spouštění periodických úloh
+//     https://www.root.cz/clanky/knihovny-pro-go-umoznujici-naplanovani-a-spousteni-periodickych-uloh/
+//
+// Demonstrační příklad číslo 5:
+//     Práce s objektem typu Duration.
+
 package main
 
 import (
