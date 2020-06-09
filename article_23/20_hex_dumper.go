b8e23eb57bf9f5f4f335188977fc014c5744a0aa
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-09 08:27:10 +0200

    Added comments and link to article where this example is used

--- article_23/20_hex_dumper.go
+++ article_23/20_hex_dumper.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Dvacátá třetí část
+//     Pokročilejší použití vstupně-výstupních funkcí standardní knihovny jazyka Go
+//     https://www.root.cz/clanky/pokrocilejsi-pouziti-vstupne-vystupnich-funkci-standardni-knihovny-jazyka-go/
 //
 // Demonstrační příklad číslo 20:
 //     Zobrazení libovolných binárních dat ve formě hexadecimálního výpisu.
