cc4161103e38c45d98499d1c113368fa4416eb2d
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-30 08:13:32 +0200

    Added comments and link to article where this example is used

--- article_26/18_two_interfaces.go
+++ article_26/18_two_interfaces.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Dvacátá šestá část
+//    Problematika nulových hodnot v Go, aneb proč nil != nil
+//    https://www.root.cz/clanky/problematika-nulovych-hodnot-v-go-aneb-proc-nil-nil/
 //
 // Demonstrační příklad číslo 18:
 //    Prázdné rozhraní a rozhraní s jednou metodou
