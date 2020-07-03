b79a83a34b20b33471084a30c22d8d223a584eae
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-03 18:37:58 +0200

    Added comments and link to article where this example is used

--- article_27/02B_do_string_3.go
+++ article_27/02B_do_string_3.go
@@ -1,3 +1,12 @@
+// Seriál "Programovací jazyk Go"
+//
+// Dvacátá sedmá část
+//    Skriptovací jazyk Lua v aplikacích naprogramovaných v Go
+//    https://www.root.cz/clanky/skriptovaci-jazyk-lua-v-aplikacich-naprogramovanych-v-go/
+//
+// Demonstrační příklad číslo 2:
+//    Poslání skriptu v řetězci do Lua VM.
+
 package main
 
 import (
