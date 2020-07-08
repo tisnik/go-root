79a20dbd6d47a784d67299b5e3a700327bbd99fb
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-08 08:19:32 +0200

    Added header into 20_reduce.go

--- article_28/20_reduce.go
+++ article_28/20_reduce.go
@@ -1,3 +1,12 @@
+// Seriál "Programovací jazyk Go"
+//
+// Dvacátá osmá část
+//    Datové struktury s líným vyhodnocováním v programovacím jazyce Go
+//    https://www.root.cz/clanky/datove-struktury-s-linym-vyhodnocovanim-v-programovacim-jazyce-go/
+//
+// Demonstrační příklad číslo 20:
+//    Metoda Reduce().
+
 package main
 
 import (
