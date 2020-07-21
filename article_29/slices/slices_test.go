aa0259d053eb58e02f3b8f5eeee83b921f43c48b
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-21 08:22:47 +0200

    Added comments and link to article where this example is used

--- article_29/slices/slices_test.go
+++ article_29/slices/slices_test.go
@@ -1,3 +1,9 @@
+// Seriál "Programovací jazyk Go"
+//
+// Dvacátá devátá část
+//    Trasování a profilování aplikací naprogramovaných v Go
+//    https://www.root.cz/clanky/trasovani-a-profilovani-aplikaci-naprogramovanych-v-go/
+
 package slices_test
 
 import (
