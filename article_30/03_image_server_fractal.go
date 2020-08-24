d70513d7029d50b2fbd0f1984244e7c620e9a0b5
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-24 08:41:04 +0200

    Added comments and link to article where this example is used

--- article_30/03_image_server_fractal.go
+++ article_30/03_image_server_fractal.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Třicátá část
+//    Trasování a profilování aplikací naprogramovaných v Go (dokončení
+//    https://www.root.cz/clanky/trasovani-a-profilovani-aplikaci-naprogramovanych-v-go-dokonceni/
 //
 // Demonstrační příklad číslo 3:
 //    HTTP server generující rastrový obrázek s fraktálem.
