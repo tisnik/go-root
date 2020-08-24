000ea089597ba27a0ed44764785c1da914c9e77c
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-24 08:41:04 +0200

    Added comments and link to article where this example is used

--- article_30/02_image_server_with_pprof.go
+++ article_30/02_image_server_with_pprof.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Třicátá část
+//    Trasování a profilování aplikací naprogramovaných v Go (dokončení
+//    https://www.root.cz/clanky/trasovani-a-profilovani-aplikaci-naprogramovanych-v-go-dokonceni/
 //
 // Demonstrační příklad číslo 2:
 //     HTTP server nabízející i pprof metriky.
