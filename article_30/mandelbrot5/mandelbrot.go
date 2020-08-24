ac1ae8bd0fcc3b308b7cb6308df2d9213504a0b0
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-24 08:41:04 +0200

    Added comments and link to article where this example is used

--- article_30/mandelbrot5/mandelbrot.go
+++ article_30/mandelbrot5/mandelbrot.go
@@ -1,3 +1,9 @@
+// Seriál "Programovací jazyk Go"
+//
+// Třicátá část
+//    Trasování a profilování aplikací naprogramovaných v Go (dokončení
+//    https://www.root.cz/clanky/trasovani-a-profilovani-aplikaci-naprogramovanych-v-go-dokonceni/
+
 package main
 
 import (
