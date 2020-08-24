d3db0398a629ac253c14c0582dc0745d1353335c
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-24 08:41:04 +0200

    Added comments and link to article where this example is used

--- article_30/07_region_hierarchy.go
+++ article_30/07_region_hierarchy.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Třicátá část
+//    Trasování a profilování aplikací naprogramovaných v Go (dokončení
+//    https://www.root.cz/clanky/trasovani-a-profilovani-aplikaci-naprogramovanych-v-go-dokonceni/
 //
 // Demonstrační příklad číslo 7:
 //    Hierarchie regionů v souboru s trasovacími informacemi.
