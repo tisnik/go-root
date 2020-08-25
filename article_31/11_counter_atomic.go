1fc7a693c0825c56fe3d8f1af92570ca2a1017ad
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-25 09:29:41 +0200

    Added comments and link to article where this example is used

--- article_31/11_counter_atomic.go
+++ article_31/11_counter_atomic.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Třicátá první část
+//    Sledování vybraných metrik služeb naprogramovaných v jazyku Go
+//    https://www.root.cz/clanky/sledovani-vybranych-metrik-sluzeb-naprogramovanych-v-jazyku-go/
 //
 // Demonstrační příklad číslo 11:
 //    Čítač atomicky zvyšovaný ve více vláknech.
