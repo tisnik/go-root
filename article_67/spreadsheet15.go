ca91feaffcc2cb29c0ae8339b7c9d0e29b45d26f
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-26 17:38:44 +0200

    Example 15: merging cells

---
+++ article_67/spreadsheet15.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/tealeg/xlsx/v3"
+)
+
+// jméno generovaného souboru
+const spreadsheetName = "test15.xlsx"
+
+func main() {
+	// konstrukce struktury typu File
+	worksheet := xlsx.NewFile()
+
+	fmt.Println(worksheet)
+
+	// přidání listu do sešitu
+	sheet, err := worksheet.AddSheet("Tabulka1")
+	if err != nil {
+		panic(err)
+	}
+	defer sheet.Close()
+
+	fmt.Println(sheet)
+
+	// přidání řádku do tabulky
+	row := sheet.AddRow()
+
+	// přidání první buňky na řádek
+	cell := row.AddCell()
+
+	// naplnění buňky hodnotou
+	cell.SetString("1x1")
+
+	// přidání druhé buňky na řádek
+	cell = row.AddCell()
+
+	// naplnění buňky hodnotou
+	cell.SetString("2x1")
+
+	// a spojení s další buňkou
+	cell.Merge(1, 0)
+
+	// přidání třetí buňky na řádek
+	cell = row.AddCell()
+
+	// naplnění buňky hodnotou
+	cell.SetString("1x1")
+
+	// pokus o uložení sešitu
+	err = worksheet.Save(spreadsheetName)
+	if err != nil {
+		panic(err)
+	}
+}
