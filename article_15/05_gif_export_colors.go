10eee9b0c831c4fa6db758dc90be438830057929
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-26 09:19:13 +0200

    Fixed issue #398

--- article_15/05_gif_export_colors.go
+++ article_15/05_gif_export_colors.go
@@ -34,12 +34,12 @@ func CreateChessboard(width int, height int, board_size int) *image.RGBA {
 	xFrom := 0
 	xTo := hor_block
 	for x := 0; x < board_size; x++ {
-		y_from := 0
+		yFrom := 0
 		yTo := ver_block
 		for y := 0; y < board_size; y++ {
-			r := image.Rect(xFrom, y_from, xTo, yTo)
+			r := image.Rect(xFrom, yFrom, xTo, yTo)
 			draw.Draw(img, r, &image.Uniform{palette[index_color]}, image.ZP, draw.Src)
-			y_from = yTo
+			yFrom = yTo
 			yTo += ver_block
 			index_color = 1 - index_color
 		}
