7b5e5a36d286b17d81916f4b8475b37b6139f25b
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-21 07:45:10 +0200

    Added comments and link to article where this example is used

--- article_34/01_empty_main.c
+++ article_34/01_empty_main.c
@@ -1,3 +1,11 @@
+/*
+ Seriál "Programovací jazyk Go"
+
+ Třicátá čtvrtá část
+    Programovací jazyk Go pro skalní céčkaře
+    https://www.root.cz/clanky/programovaci-jazyk-go-pro-skalni-ceckare/
+*/
+
 int main(void)
 {
     return 0;
