55feb8fb9d8c4473ec2beda99cb22a802a76b99e
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-21 07:44:08 +0200

    Added comments and link to article where this example is used

--- article_34/09_do_while_loop.go
+++ article_34/09_do_while_loop.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Třicátá čtvrtá část
+//    Programovací jazyk Go pro skalní céčkaře
+//    https://www.root.cz/clanky/programovaci-jazyk-go-pro-skalni-ceckare/
 //
 // Demonstrační příklad číslo 9:
 //    Implementace programové smyčky typu "do-while" v jazyce Go.
