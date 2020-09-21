995f1742e0c25e3278d4f5f43405c388618ab16f
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-21 07:44:08 +0200

    Added comments and link to article where this example is used

--- article_34/21_iota.go
+++ article_34/21_iota.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Třicátá čtvrtá část
+//    Programovací jazyk Go pro skalní céčkaře
+//    https://www.root.cz/clanky/programovaci-jazyk-go-pro-skalni-ceckare/
 //
 // Demonstrační příklad číslo 21:
 //    Deklarace konstant v Go.
