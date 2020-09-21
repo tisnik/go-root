56be136b47de8c68c0f317d25d7bb6399f868e91
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-21 07:44:08 +0200

    Added comments and link to article where this example is used

--- article_34/10_for_loop.go
+++ article_34/10_for_loop.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Třicátá čtvrtá část
+//    Programovací jazyk Go pro skalní céčkaře
+//    https://www.root.cz/clanky/programovaci-jazyk-go-pro-skalni-ceckare/
 //
 // Demonstrační příklad číslo 10:
 //    Implementace programové smyčky typu "for" v jazyce Go.
