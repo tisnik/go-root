974cc52bdc293998f152432c4c134a1471c1da5c
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-21 07:44:08 +0200

    Added comments and link to article where this example is used

--- article_34/17_return_statement.go
+++ article_34/17_return_statement.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Třicátá čtvrtá část
+//    Programovací jazyk Go pro skalní céčkaře
+//    https://www.root.cz/clanky/programovaci-jazyk-go-pro-skalni-ceckare/
 //
 // Demonstrační příklad číslo 17:
 //    Příkaz return pro ukončení funkce.
