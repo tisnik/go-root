76c8c3f2331fe3816ac0917528b3d8b9e733e64a
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-22 08:14:51 +0200

    Added comments and link to article where this example is used

--- article_35/07_string_comparison_C.go
+++ article_35/07_string_comparison_C.go
@@ -1,6 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //
 // Třicátá pátá část
+//    Programovací jazyk Go pro skalní céčkaře (2.část)
+//    https://www.root.cz/clanky/programovaci-jazyk-go-pro-skalni-ceckare-2-cast/
 //
 // Demonstrační příklad číslo 7C:
 //     Porovnávání řetězců v Go s podporou češtiny.
