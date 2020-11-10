93874c7cd391ecb7519d672508ac3bf10ada2d81
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-10 08:08:53 +0100

    Added comments and link to article where this example is used

--- article_36/03_missing_include.go
+++ article_36/03_missing_include.go
@@ -1,3 +1,9 @@
+// Seriál "Programovací jazyk Go"
+//
+// Třicátá šestá část
+//    Kooperace mezi kódem psaným v Go a C: cgo
+//    https://www.root.cz/clanky/kooperace-mezi-kodem-psanym-v-go-a-c-cgo/
+
 package main
 
 import "C"
