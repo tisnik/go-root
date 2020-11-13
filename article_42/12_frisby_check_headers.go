daf03a36915fe6bebfd048319df7222a5a46498c
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-13 09:22:46 +0100

    Added comments and link to article where this example is used

--- article_42/12_frisby_check_headers.go
+++ article_42/12_frisby_check_headers.go
@@ -1,3 +1,9 @@
+// Seriál "Programovací jazyk Go"
+//
+// Čtyřicátá druhá část
+//    Testování aplikací psaných v Go s využitím knihoven Goblin a Frisby
+//    https://www.root.cz/clanky/testovani-aplikaci-psanych-v-go-s-vyuzitim-knihoven-goblin-a-frisby/
+
 package main
 
 import "github.com/verdverm/frisby"
