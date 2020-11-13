07e368ae78183c158912f091fcd96ba0401a8abb
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-13 09:22:51 +0100

    Added comments and link to article where this example is used

--- article_43/gexpect/06_python.go
+++ article_43/gexpect/06_python.go
@@ -1,3 +1,9 @@
+// Seriál "Programovací jazyk Go"
+//
+// Čtyřicátá třetí část
+//    Použití Go pro automatizaci práce s aplikacemi s interaktivním příkazovým řádkem
+//    https://www.root.cz/clanky/pouziti-go-pro-automatizaci-prace-s-aplikacemi-s-interaktivnim-prikazovym-radkem/
+
 package main
 
 import (
