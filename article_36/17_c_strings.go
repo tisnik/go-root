8fa31f7e0a4c21bfba08de247ce97362c51f06e6
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-08 09:58:08 +0100

    Fixed issue #561

--- article_36/17_c_strings.go
+++ article_36/17_c_strings.go
@@ -14,8 +14,8 @@ import "fmt"
 func main() {
 	fmt.Print("? ")
 	s := C.calloc(20, 1)
-	char_ptr := (*C.char)(s)
-	C.fgets(char_ptr, 20, C.stdin)
-	fmt.Printf("result=%v\n", C.GoString(char_ptr))
+	charPtr := (*C.char)(s)
+	C.fgets(charPtr, 20, C.stdin)
+	fmt.Printf("result=%v\n", C.GoString(charPtr))
 	C.free(s)
 }
