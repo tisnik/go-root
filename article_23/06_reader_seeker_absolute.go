3887e1ff4d2de9ee996d004e926189f3b89afab9
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-15 08:42:23 +0100

    Fixed issue #575

--- article_23/06_reader_seeker_absolute.go
+++ article_23/06_reader_seeker_absolute.go
@@ -15,11 +15,11 @@ import (
 	"strings"
 )
 
-const input_string = "*** Hello world! ***"
+const inputString = "*** Hello world! ***"
 const buffer_size = 6
 
 func main() {
-	reader := strings.NewReader(input_string)
+	reader := strings.NewReader(inputString)
 
 	buffer := make([]byte, buffer_size)
 	reader.Seek(4, io.SeekStart)
