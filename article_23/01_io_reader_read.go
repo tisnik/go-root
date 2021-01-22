fd641d2871b39b879302d7a33064e916ca057b48
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-22 12:15:31 +0100

    Fixed issue #571

--- article_23/01_io_reader_read.go
+++ article_23/01_io_reader_read.go
@@ -17,7 +17,7 @@ import (
 )
 
 const filename = "test_input.txt"
-const buffer_size = 16
+const bufferSize = 16
 
 func main() {
 	reader, err := os.Open(filename)
@@ -26,7 +26,7 @@ func main() {
 	}
 	defer reader.Close()
 
-	buffer := make([]byte, buffer_size)
+	buffer := make([]byte, bufferSize)
 
 	for {
 		read, err := reader.Read(buffer)
