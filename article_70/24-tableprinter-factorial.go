4d32afa7ca3915f4dab623ebb7fae92fa25146a8
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-01 10:29:31 +0100

    Factorial table prepared by tableprinter

---
+++ article_70/24-tableprinter-factorial.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"os"
+
+	"github.com/lensesio/tableprinter"
+)
+
+type Record struct {
+	N int   `header:"n"`
+	F int64 `header:"n!"`
+}
+
+// Factorial computes factorial for given n that might be positive integer
+func Factorial(n int64) int64 {
+	switch {
+	case n < 0:
+		return 1
+	case n == 0:
+		return 1
+	default:
+		return n * Factorial(n-1)
+	}
+}
+
+const MaxN = 17
+
+func main() {
+	data := make([]Record, 0)
+
+	for n := 0; n <= MaxN; n++ {
+		data = append(data, Record{n, Factorial(int64(n))})
+	}
+
+	table := tableprinter.New(os.Stdout)
+	table.BorderTop, table.BorderBottom, table.BorderLeft, table.BorderRight = true, true, true, true
+	table.CenterSeparator = "│"
+	table.ColumnSeparator = "│"
+	table.RowSeparator = "─"
+
+	table.Print(data)
+}
