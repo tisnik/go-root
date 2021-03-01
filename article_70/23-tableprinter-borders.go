a9a52102e2e8cd9444eb60b8a9668314109a813a
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-01 10:29:31 +0100

    Tableprinter table with border

---
+++ article_70/23-tableprinter-borders.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+
+	"github.com/lensesio/tableprinter"
+)
+
+type Record struct {
+	Rank  string `header:"rank"`
+	Title string `header:"title"`
+	Value int    `header:"value"`
+}
+
+func main() {
+	data := []Record{
+		{"A", "The Good", 500},
+		{"B", "The Very very Bad Man", 288},
+		{"C", "The Ugly", 120},
+		{"D", "The Gopher", 800},
+	}
+
+	table := tableprinter.New(os.Stdout)
+	table.BorderTop, table.BorderBottom, table.BorderLeft, table.BorderRight = true, true, true, true
+	table.CenterSeparator = "│"
+	table.ColumnSeparator = "│"
+	table.RowSeparator = "─"
+
+	table.Print(data)
+}
