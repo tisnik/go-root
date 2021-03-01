5988c4b5e9e51b0f2a51eb25f3c974db461dc525
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-01 10:29:31 +0100

    Setting padding in tabwriter

---
+++ article_70/04-tabwriter-padding.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"text/tabwriter"
+)
+
+const (
+	MinWidth         = 0
+	TabWidth         = 0
+	PaddingCharacter = ' '
+	Flags            = 0
+)
+
+func main() {
+	for padding := 0; padding < 10; padding++ {
+		fmt.Printf("padding = %d\n", padding)
+		w := tabwriter.NewWriter(os.Stdout, MinWidth, TabWidth, padding, PaddingCharacter, Flags)
+		fmt.Fprintln(w, "1\t1\t1")
+		fmt.Fprintln(w, "22\t22\t22")
+		fmt.Fprintln(w, "333\t333\t333")
+		fmt.Fprintln(w, "4444\t4444\t4444")
+		w.Flush()
+		fmt.Println()
+	}
+}
