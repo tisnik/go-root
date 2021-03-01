a03a952141ba769504a0fedd02d251963c468c04
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-01 10:29:31 +0100

    Setting min width in tabwriter

---
+++ article_70/03-tabwriter-min-width.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"text/tabwriter"
+)
+
+const (
+	TabWidth         = 0
+	Padding          = 1
+	PaddingCharacter = ' '
+	Flags            = 0
+)
+
+func main() {
+	for minWidth := 0; minWidth < 10; minWidth++ {
+		fmt.Printf("Min width = %d\n", minWidth)
+		w := tabwriter.NewWriter(os.Stdout, minWidth, TabWidth, Padding, PaddingCharacter, Flags)
+		fmt.Fprintln(w, "1\t1\t1")
+		fmt.Fprintln(w, "22\t22\t22")
+		fmt.Fprintln(w, "333\t333\t333")
+		fmt.Fprintln(w, "4444\t4444\t4444")
+		w.Flush()
+		fmt.Println()
+	}
+}
