868b8209ac40a582e5097653f1f4b5d161ee5373
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-08 09:14:32 +0100

    Automatically merge the same cells in a table

---
+++ article_71/07-tablewriter-merge-cells.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/olekukonko/tablewriter"
+)
+
+func main() {
+	table, err := tablewriter.NewCSV(os.Stdout, "tiobe.csv", true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	table.SetAutoMergeCells(true)
+	table.SetRowLine(true)
+	table.Render()
+}
