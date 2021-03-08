31f8362392f718ee2648450246fa4d8ca729de11
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-08 09:14:32 +0100

    Right align data in a table

---
+++ article_71/04-B-tablewriter-right-align.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/olekukonko/tablewriter"
+)
+
+func main() {
+	table, err := tablewriter.NewCSV(os.Stdout, "tiobe.csv", true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	table.SetAlignment(tablewriter.ALIGN_RIGHT)
+	table.Render()
+}
