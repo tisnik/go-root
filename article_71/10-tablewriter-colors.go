90a0275fd55f75531658c4023c2b96a3f04181a5
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-08 09:14:32 +0100

    Colors in table cells

---
+++ article_71/10-tablewriter-colors.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/olekukonko/tablewriter"
+)
+
+func main() {
+	table, err := tablewriter.NewCSV(os.Stdout, "tiobe.csv", true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	table.SetBorder(false)
+
+	table.SetHeaderColor(
+		tablewriter.Colors{tablewriter.Bold, tablewriter.BgGreenColor},
+		tablewriter.Colors{tablewriter.FgHiRedColor, tablewriter.Bold, tablewriter.BgBlackColor},
+		tablewriter.Colors{tablewriter.BgRedColor, tablewriter.FgWhiteColor},
+		tablewriter.Colors{tablewriter.BgCyanColor, tablewriter.FgWhiteColor},
+		tablewriter.Colors{tablewriter.Bold, tablewriter.BgWhiteColor})
+
+	table.Render()
+}
