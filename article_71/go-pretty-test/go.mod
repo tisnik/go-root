b10c246a308c610624f8f1135cdb8f548c79dd34
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-08 09:14:32 +0100

    Project that use go-pretty package

---
+++ article_71/go-pretty-test/go.mod
@@ -0,0 +1,9 @@
+module go-pretty-test
+
+go 1.14
+
+require (
+	github.com/go-openapi/strfmt v0.20.0 // indirect
+	github.com/jedib0t/go-pretty v4.3.0+incompatible
+	github.com/jedib0t/go-pretty/v6 v6.1.0
+)
---
+++ article_71/go-pretty-test/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/jedib0t/go-pretty/v6/list"
+	"github.com/jedib0t/go-pretty/v6/progress"
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
+)
+
+func main() {
+}
