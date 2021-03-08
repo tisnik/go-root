0540d64a29de6b2e68fdb5a700bb5857b5426b04
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-08 09:14:32 +0100

    Simple table rendered by go-pretty

---
+++ article_71/12-go-pretty-simple-table.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os"
+
+	"github.com/jedib0t/go-pretty/table"
+)
+
+// Role represents user role in some IS
+type Role struct {
+	name       string
+	surname    string
+	popularity int
+}
+
+func main() {
+	roles := []Role{
+		Role{"Eliška", "Najbrtová", 4},
+		Role{"Jenny", "Suk", 3},
+		Role{"Anička", "Šafářová", 1},
+		Role{"Sváťa", "Pulec", 3},
+		Role{"Blažej", "Motyčka", 8},
+		Role{"Eda", "Wasserfall", 3},
+		Role{"Přemysl", "Hájek", 10},
+	}
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"#", "First Name", "Last Name", "Popularity"})
+
+	for i, role := range roles {
+		t.AppendRow(table.Row{i, role.name, role.surname, role.popularity})
+	}
+	t.Render()
+}
