547ac21f6f2d39faef1e58d65c7e1408c50b59ab
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-10 08:50:42 +0100

    Added link to the whole serie into 02_slow_server.go

--- article_24/02_slow_server.go
+++ article_24/02_slow_server.go
@@ -1,4 +1,5 @@
 // Seriál "Programovací jazyk Go"
+//    https://www.root.cz/serialy/programovaci-jazyk-go/
 //
 // Dvacátá čtvrtá část
 //     Kontejnery v základní knihovně programovacího jazyka Go
