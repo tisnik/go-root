d72562b31d52b30c763a1e8d0ad18097950717f0
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-17 08:35:25 +0100

    First version of unit tests

---
+++ article_72/db_operations_test.go
@@ -0,0 +1,157 @@
+package main_test
+
+import (
+	"testing"
+
+	main "db-test"
+
+	"github.com/DATA-DOG/go-sqlmock"
+)
+
+func TestSelect1(t *testing.T) {
+	connection, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer connection.Close()
+
+	rows := sqlmock.NewRows([]string{})
+
+	mock.ExpectQuery("SELECT id, name, surname FROM persons").WillReturnRows(rows)
+
+	err = main.DisplayAllRecords(connection)
+	if err != nil {
+		t.Errorf("error was not expected while updating stats: %s", err)
+	}
+
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestSelect2(t *testing.T) {
+	connection, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer connection.Close()
+
+	rows := sqlmock.NewRows([]string{"id", "name", "surname"})
+	rows.AddRow(1, "foo", "bar")
+	rows.AddRow(2, "x", "y")
+	rows.AddRow(3, "a", "b")
+
+	mock.ExpectQuery("SELECT id, name, surname FROM persons").WillReturnRows(rows)
+
+	err = main.DisplayAllRecords(connection)
+	if err != nil {
+		t.Errorf("error was not expected while updating stats: %s", err)
+	}
+
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestSelect3(t *testing.T) {
+	connection, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer connection.Close()
+
+	rows := sqlmock.NewRows([]string{"id", "name", "surname"})
+	rows.AddRow(1, "foo", "bar")
+	rows.AddRow(2, "x", "y")
+	rows.AddRow(3, "a", "b")
+
+	mock.ExpectQuery("SELECT id, name, surname FROM persons").WillReturnRows(rows)
+
+	results, err := main.ReadAllRecords(connection)
+	if err != nil {
+		t.Errorf("error was not expected while updating stats: %s", err)
+	}
+
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+
+	if len(results) != 3 {
+		t.Errorf("different number of results read from database: %d instead of 3", len(results))
+		return
+	}
+
+	expected := main.Record{1, "foo", "bar"}
+	if results[0] != expected {
+		t.Errorf("first result is different: %+v versus %+v", results[0], expected)
+	}
+}
+
+func TestSelect4(t *testing.T) {
+	connection, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer connection.Close()
+
+	rows := sqlmock.NewRows([]string{"id", "name", "surname"})
+	rows.AddRow(2, "Eda", "Vodopád")
+
+	mock.ExpectQuery("SELECT id, name, surname FROM persons WHERE name=\\$1").WillReturnRows(rows)
+
+	results, err := main.ReadRecordsWithName(connection, "Eda")
+	if err != nil {
+		t.Errorf("error was not expected while updating stats: %s", err)
+	}
+
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+
+	if len(results) != 1 {
+		t.Errorf("different number of results read from database: %d instead of 1", len(results))
+		return
+	}
+
+	expected := main.Record{2, "Eda", "Vodopád"}
+	if results[0] != expected {
+		t.Errorf("first result is different: %+v versus %+v", results[0], expected)
+	}
+}
+
+func TestSelect5(t *testing.T) {
+	connection, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer connection.Close()
+
+	rows := sqlmock.NewRows([]string{"id", "name", "surname"})
+	rows.AddRow(2, "Eda", "Vodopád")
+
+	mock.ExpectQuery("SELECT id, name, surname FROM persons WHERE name=.*").WillReturnRows(rows)
+
+	results, err := main.ReadRecordsWithName(connection, "Eda")
+	if err != nil {
+		t.Errorf("error was not expected while updating stats: %s", err)
+	}
+
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+
+	if len(results) != 1 {
+		t.Errorf("different number of results read from database: %d instead of 1", len(results))
+		return
+	}
+
+	expected := main.Record{2, "Eda", "Vodopád"}
+	if results[0] != expected {
+		t.Errorf("first result is different: %+v versus %+v", results[0], expected)
+	}
+}
