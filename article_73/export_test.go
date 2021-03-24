966cc5349138ac2858006aa56623f88d3421f0bc
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-24 08:28:32 +0100

    Exported functions to be tested

---
+++ article_73/export_test.go
@@ -0,0 +1,11 @@
+package main
+
+var (
+	DisplayAllRecords                 = displayAllRecords
+	ReadAllRecords                    = readAllRecords
+	ReadRecordsWithName               = readRecordsWithName
+	InsertRecord                      = insertRecord
+	DeleteByName                      = deleteByName
+	DeleteByNameInTransaction         = deleteByNameInTransaction
+	DeleteByNameInTransactionNoCommit = deleteByNameInTransactionNoCommit
+)
