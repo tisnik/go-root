a0ab6f8f3cd61595c5f6e189e3904ac9d34b0497
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-04-07 11:19:48 +0200

    Added link to the whole serie of articles into 08_image_server_fractal.go

--- article_31/08_image_server_fractal.go
+++ article_31/08_image_server_fractal.go
@@ -1,4 +1,5 @@
 // Seriál "Programovací jazyk Go"
+//    https://www.root.cz/serialy/programovaci-jazyk-go/
 //
 // Třicátá první část
 //    Sledování vybraných metrik služeb naprogramovaných v jazyku Go
