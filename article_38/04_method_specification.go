3206e84be84ff60a44c1b7db4272968e2f8afc12
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-05-03 08:07:36 +0200

    Added link to the whole serie of articles into 04_method_specification.go

--- article_38/04_method_specification.go
+++ article_38/04_method_specification.go
@@ -1,4 +1,5 @@
 // Seriál "Programovací jazyk Go"
+//    https://www.root.cz/serialy/programovaci-jazyk-go/
 //
 // Třicátá osmá část
 //    Tvorba webových aplikací v Go s využitím projektu Gorilla web toolkit
