0f1d0a1955b01532928e7456debb41943cce287a
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-05-10 08:40:02 +0200

    Added comments and link to article where this example is used

--- article_46/minio07/minio7.go
+++ article_46/minio07/minio7.go
@@ -1,4 +1,5 @@
 // Seriál "Programovací jazyk Go"
+//    https://www.root.cz/serialy/programovaci-jazyk-go/
 //
 // Čtyřicátá šestá část
 //     Projekt MinIO: jedna z nejužitečnějších aplikací naprogramovaných v Go
