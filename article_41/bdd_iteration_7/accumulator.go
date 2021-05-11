c8457626b5be0cd7041eddd9a88940b7b77d181f
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-05-11 07:46:39 +0200

    Added comments and link to article where this example is used

--- article_41/bdd_iteration_7/accumulator.go
+++ article_41/bdd_iteration_7/accumulator.go
@@ -1,3 +1,10 @@
+// Seriál "Programovací jazyk Go"
+//    https://www.root.cz/serialy/programovaci-jazyk-go/
+//
+// Čtyřicátá první část
+//    Tvorba BDD testů s využitím jazyka Go a nástroje godog
+//    https://www.root.cz/clanky/tvorba-bdd-testu-s-vyuzitim-jazyka-go-a-nastroje-godog/
+
 package accumulator
 
 import (
