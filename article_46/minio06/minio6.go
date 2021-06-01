3aa5ab0b7956ac88305b88fbc31edf77c0bb19c9
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-06-01 08:00:52 +0200

    Fixed link to article in minio6.go

--- article_46/minio06/minio6.go
+++ article_46/minio06/minio6.go
@@ -3,7 +3,7 @@
 //
 // Čtyřicátá šestá část
 //     Projekt MinIO: jedna z nejužitečnějších aplikací naprogramovaných v Go
-//     https://www.root.cz/clanky/projekt-minio-jedna-z-nejuzitecnejsich-aplikaci-naprogramovanych-v-go/#k19
+//     https://www.root.cz/clanky/projekt-minio-jedna-z-nejuzitecnejsich-aplikaci-naprogramovanych-v-go/
 //
 // Demonstrační příklad číslo 6:
 //     Uložení objektu z bucketu do lokálního souboru
