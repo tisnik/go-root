3f24aefa28fbdc01c90b992034b99c973b2130b4
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-06-14 08:12:21 +0200

    Added header and link to the article

--- article_54/asm06.go
+++ article_54/asm06.go
@@ -1,3 +1,10 @@
+// Seriál "Programovací jazyk Go"
+//    https://www.root.cz/serialy/programovaci-jazyk-go/
+//
+// Padesátá čtvrtá část
+//    Programovací jazyk Go a assembler (2.část)
+//    https://www.root.cz/clanky/programovaci-jazyk-go-a-assembler-2-cast/
+
 package main
 
 func AddFloat32(x float32, y float32, z float32) float32 {
