3fb17246882be54613ac17e53d41cc9b9c0c5e14
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-06-14 08:11:30 +0200

    Added header and link to the article

--- article_53/add06/add.go
+++ article_53/add06/add.go
@@ -1,3 +1,10 @@
+// Seriál "Programovací jazyk Go"
+//    https://www.root.cz/serialy/programovaci-jazyk-go/
+//
+// Padesátá třetí část
+//    Programovací jazyk Go a assembler
+//    https://www.root.cz/clanky/programovaci-jazyk-go-a-assembler/
+
 package main
 
 func add(x int64, y int64) int64
