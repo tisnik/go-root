9fc35c9e091f8cf82b2af69337b7e672e6d6b67a
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-06-14 08:12:21 +0200

    Added header and link to the article

--- article_54/asm08.go
+++ article_54/asm08.go
@@ -1,3 +1,10 @@
+// Seriál "Programovací jazyk Go"
+//    https://www.root.cz/serialy/programovaci-jazyk-go/
+//
+// Padesátá čtvrtá část
+//    Programovací jazyk Go a assembler (2.část)
+//    https://www.root.cz/clanky/programovaci-jazyk-go-a-assembler-2-cast/
+
 package main
 
 func Sign(value int) int {
