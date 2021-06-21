afc377bc5422c91601fcc7fc6ce7bc7709e664a6
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-06-21 07:58:31 +0200

    Added link to article and the whole series of articles

--- article_57/14_xml_marshal_array_of_struct_4.go
+++ article_57/14_xml_marshal_array_of_struct_4.go
@@ -1,3 +1,10 @@
+// Seriál "Programovací jazyk Go"
+//    https://www.root.cz/serialy/programovaci-jazyk-go/
+//
+// Padesátá sedmá část
+//    Serializace a deserializace datových struktur v programovacím jazyce Go
+//    https://www.root.cz/clanky/serializace-a-deserializace-datovych-struktur-v-programovacim-jazyce-go/
+
 package main
 
 import (
