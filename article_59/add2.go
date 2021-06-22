aa16ef9591cf897294185ea15f6d0e6f2a19687f
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-06-22 07:49:18 +0200

    Added standard header with links

--- article_59/add2.go
+++ article_59/add2.go
@@ -1,3 +1,10 @@
+// Seriál "Programovací jazyk Go"
+//    https://www.root.cz/serialy/programovaci-jazyk-go/
+//
+// Padesátá osmá část
+//    Generické datové typy v jazyce Go?
+//    https://www.root.cz/clanky/genericke-datove-typy-v-jazyce-go/
+
 package main
 
 import "fmt"
