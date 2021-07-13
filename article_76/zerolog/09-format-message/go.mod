6f47e1ca9f0a741f289b119df030e81f778d8343
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-13 08:23:23 +0200

    Zerolog example #9: formatting messages

---
+++ article_76/zerolog/09-format-message/go.mod
@@ -0,0 +1,5 @@
+module 09-format-message
+
+go 1.13
+
+require github.com/rs/zerolog v1.23.0
---
+++ article_76/zerolog/09-format-message/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"net/http"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+const Address = "localhost"
+const Port = 8080
+const Enabled = true
+
+func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	log.Info().Msg("Started")
+
+	log.Info().
+		Str("address", Address).
+		Int("port", Port).
+		Bool("enabled", Enabled).Msg("Server settings")
+
+	if Enabled {
+		http.HandleFunc("/", handler)
+		where := fmt.Sprintf("%s:%d", Address, Port)
+		err := http.ListenAndServe(where, nil)
+		if err != nil {
+			log.Error().Err(err).Msgf("Initialize server on address %s:%d", Address, Port)
+		}
+	}
+	log.Info().Msg("Finished")
+}
+
+func handler(writer http.ResponseWriter, request *http.Request) {
+	io.WriteString(writer, "Hello world!")
+}
