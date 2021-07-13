fe0b901c1fc6cfffd2f574f7a1d42f3531231975
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-13 08:23:12 +0200

    Zerolog example #8: logging without specifying levels

---
+++ article_76/zerolog/08-no-level/go.mod
@@ -0,0 +1,5 @@
+module 08-no-level
+
+go 1.13
+
+require github.com/rs/zerolog v1.23.0
---
+++ article_76/zerolog/08-no-level/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+const Address = "localhost"
+const Port = 8080
+const Enabled = true
+
+func main() {
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	log.Info().Msg("Started")
+
+	log.Log().
+		Str("level", "trace").
+		Str("address", Address).
+		Int("port", Port).
+		Bool("enabled", Enabled).Msg("Server settings")
+
+	log.Info().Msg("Finished")
+}
