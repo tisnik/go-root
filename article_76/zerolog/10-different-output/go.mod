f831e2a2d8c5687b5390e76756fb8cdbcb47b199
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-13 08:23:37 +0200

    Zerolog example #10: setting different output formats

---
+++ article_76/zerolog/10-different-output/go.mod
@@ -0,0 +1,5 @@
+module 10-different-output
+
+go 1.13
+
+require github.com/rs/zerolog v1.23.0
---
+++ article_76/zerolog/10-different-output/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+const Address = "localhost"
+const Port = 8080
+const Enabled = true
+
+func main() {
+	var rawData []byte = []byte("ěščřžýáíé")
+
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	log.Info().Msg("Started")
+
+	log.Trace().
+		Str("level", "trace").
+		Str("address", Address).
+		Int("port", Port).
+		Hex("raw data", rawData).
+		Bool("enabled", Enabled).Msg("Server settings")
+
+	log.Debug().
+		Str("level", "debug").
+		Str("address", Address).
+		Int("port", Port).
+		Hex("raw data", rawData).
+		Bool("enabled", Enabled).Msg("Server settings")
+
+	log.Info().
+		Str("level", "info").
+		Str("address", Address).
+		Int("port", Port).
+		Hex("raw data", rawData).
+		Bool("enabled", Enabled).Msg("Server settings")
+
+	log.Warn().
+		Str("level", "warn").
+		Str("address", Address).
+		Int("port", Port).
+		Hex("raw data", rawData).
+		Bool("enabled", Enabled).Msg("Server settings")
+
+	log.Error().
+		Str("level", "error").
+		Str("address", Address).
+		Int("port", Port).
+		Hex("raw data", rawData).
+		Bool("enabled", Enabled).Msg("Server settings")
+
+	log.Fatal().
+		Str("level", "fatal").
+		Str("address", Address).
+		Int("port", Port).
+		Hex("raw data", rawData).
+		Bool("enabled", Enabled).Msg("Server settings")
+
+	log.Panic().
+		Str("level", "panic").
+		Str("address", Address).
+		Int("port", Port).
+		Hex("raw data", rawData).
+		Bool("enabled", Enabled).Msg("Server settings")
+
+	log.Info().Msg("Finished")
+}
