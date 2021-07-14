16fd26b478b492d7b445cfd72d947acfdef3024e
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-14 09:16:35 +0200

    Aurora example #2: disable colors

---
+++ article_76/aurora/02-disable-colors/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/logrusorgru/aurora"
+)
+
+func main() {
+	var colorizer aurora.Aurora
+	colorizer = aurora.NewAurora(false)
+
+	fmt.Println(colorizer.Red("Test"))
+	fmt.Println(colorizer.Green("Test"))
+	fmt.Println(colorizer.Blue("Test"))
+	fmt.Println(colorizer.Cyan("Test"))
+	fmt.Println(colorizer.Magenta("Test"))
+	fmt.Println(colorizer.Yellow("Test"))
+}
