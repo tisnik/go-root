96d059252f772ce4d13cf036914efef9d1ec1c96
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-14 09:16:35 +0200

    Aurora example #8: using grayscale background colors

---
+++ article_76/aurora/08-grayscale-background/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/logrusorgru/aurora"
+)
+
+var colorizer aurora.Aurora
+
+func init() {
+	var colors = flag.Bool("colors", false, "enable or disable colors")
+	flag.Parse()
+
+	colorizer = aurora.NewAurora(*colors)
+}
+
+func main() {
+	for i := uint8(0); i < 24; i++ {
+		message := fmt.Sprintf("Grayscale %d", i)
+		fmt.Println(colorizer.BgGray(i, message))
+	}
+}
