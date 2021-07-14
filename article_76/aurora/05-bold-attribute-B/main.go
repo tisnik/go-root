5d962dc71a54e38963f8de43d3d4cb6a80af5333
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-14 09:16:35 +0200

    Aurora example #5: output using bold attribute

---
+++ article_76/aurora/05-bold-attribute-B/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/logrusorgru/aurora"
+)
+
+var colorizer aurora.Aurora
+
+func init() {
+	var colors = flag.Bool("colors", false, "enable or disable colors")
+	flag.Parse()
+
+	colorizer = aurora.NewAurora(*colors)
+}
+
+func main() {
+	fmt.Println(colorizer.Red("Test"))
+	fmt.Println(colorizer.Green("Test"))
+	fmt.Println(colorizer.Blue("Test"))
+	fmt.Println(colorizer.Cyan("Test"))
+	fmt.Println(colorizer.Magenta("Test"))
+	fmt.Println(colorizer.Yellow("Test"))
+
+	fmt.Println()
+
+	fmt.Println(colorizer.Red("Test").Bold())
+	fmt.Println(colorizer.Green("Test").Bold())
+	fmt.Println(colorizer.Blue("Test").Bold())
+	fmt.Println(colorizer.Cyan("Test").Bold())
+	fmt.Println(colorizer.Magenta("Test").Bold())
+	fmt.Println(colorizer.Yellow("Test").Bold())
+}
