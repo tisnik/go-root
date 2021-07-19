47f53b01b771e39153bb348e3509a3400dbd81aa
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-19 07:40:57 +0200

    Basic usage of cfmt library

---
+++ article_77/cfmt-mindgrammer/01-basic-usage.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/mingrammer/cfmt"
+)
+
+func main() {
+	cfmt.Success("Success message")
+	cfmt.Info("Info message")
+	cfmt.Warning("Warning message")
+	cfmt.Error("Error message")
+}
