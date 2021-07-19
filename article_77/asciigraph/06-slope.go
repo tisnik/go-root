81ba06942214cf7968a4f23b9e442269eb48fd78
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-19 07:38:49 +0200

    Changing slope

---
+++ article_77/asciigraph/06-slope.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/guptarohit/asciigraph"
+)
+
+const SAMPLES = 64
+
+func displayGraph(modulus int, slope int) {
+	var values [SAMPLES]float64
+
+	for i := 0; i < SAMPLES; i++ {
+		values[i] = float64(i / slope % modulus)
+	}
+	graph := asciigraph.Plot(values[:])
+
+	fmt.Println(graph)
+}
+
+func main() {
+	const modulus = 16
+	for slope := 1; slope <= 5; slope++ {
+		displayGraph(modulus, slope)
+		fmt.Println("\n")
+	}
+}
