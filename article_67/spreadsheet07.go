6bdd7f8002cd88b86c8729cd6352e143486933e2
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-21 08:22:39 +0200

    Added/updated comments in header

--- article_67/spreadsheet07.go
+++ article_67/spreadsheet07.go
@@ -1,4 +1,5 @@
 // Seriál "Programovací jazyk Go"
+//    https://www.root.cz/serialy/programovaci-jazyk-go/
 //
 // Šedesátá sedmá část
 //    Tvorba sešitů pro tabulkové procesory v programovacím jazyku Go – formát xlsx
