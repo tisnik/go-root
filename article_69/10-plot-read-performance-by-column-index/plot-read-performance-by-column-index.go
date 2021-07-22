6fedb42eb9b436208e1a164b57d2c0c11ae08a27
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-22 08:19:40 +0200

    Added link to the whole serie of articles

--- article_69/10-plot-read-performance-by-column-index/plot-read-performance-by-column-index.go
+++ article_69/10-plot-read-performance-by-column-index/plot-read-performance-by-column-index.go
@@ -1,4 +1,5 @@
 // Seriál "Programovací jazyk Go"
+//    https://www.root.cz/serialy/programovaci-jazyk-go/
 //
 // Šedesátá devátá část
 //    Komunikace se sloupcovými databázemi z jazyka Go: Parquet soubory (dokončení)
