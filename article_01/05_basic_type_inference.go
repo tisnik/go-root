a05999e6f6f5508001732cbfb58b92b6a813448e
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-02 08:37:11 +0200

    Added link to generated documentation in literate programming style

--- article_01/05_basic_type_inference.go
+++ article_01/05_basic_type_inference.go
@@ -20,3 +20,17 @@ func main() {
 	c := true
 	fmt.Println(c)
 }
+
+
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	fmt.Println(a)
+	b := "hello"
+	fmt.Println(b)
+	c := true
+	fmt.Println(c)
+}
