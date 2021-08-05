f38ec48fb13c4e82aea3bfb66ca4e24f344268be
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-05 09:13:05 +0200

    Added link to generated documentation in literate programming style

--- article_02/17_raw_strings.go
+++ article_02/17_raw_strings.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 17
 //    Datový typ řetězec
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_02/17_raw_strings.html
 
 package main
 
