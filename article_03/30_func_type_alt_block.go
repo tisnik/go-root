adbb0ceba41d42fcb0a088fcc42f99321ce09f67
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-09 08:11:09 +0200

    Added link to generated documentation in literate programming style

--- article_03/30_func_type_alt_block.go
+++ article_03/30_func_type_alt_block.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 30:
 //    Datový typ "funkce s návratovou hodnotou"
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_03/30_func_type_alt_block.html
 
 package main
 
