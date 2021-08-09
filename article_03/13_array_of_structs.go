7eb7d5cb2d99a8758869b9c1de066f1a9979d26c
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-09 08:11:09 +0200

    Added link to generated documentation in literate programming style

--- article_03/13_array_of_structs.go
+++ article_03/13_array_of_structs.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 13:
 //    Pole struktur.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_03/13_array_of_structs.html
 
 package main
 
