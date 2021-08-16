91b1515ff9675499bfae52d6cedc1845a24e7d87
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-16 08:51:21 +0200

    Added link to generated documentation in literate programming style

--- article_05/19_simplest_for_loop.go
+++ article_05/19_simplest_for_loop.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 19:
 //    Základní forma programové smyčky for.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_05/19_simplest_for_loop.html
 
 package main
 
