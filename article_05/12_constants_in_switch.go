51738cc10365d48f8a02fefe29947c38a301c6d0
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-16 08:51:21 +0200

    Added link to generated documentation in literate programming style

--- article_05/12_constants_in_switch.go
+++ article_05/12_constants_in_switch.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 12:
 //    Řídicí konstrukce switch s vyhodnocovaným výrazem.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_05/12_constants_in_switch.html
 
 package main
 
