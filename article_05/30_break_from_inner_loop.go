c5771dec7028f3b46115dbda44583ca06199596a
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-16 08:51:21 +0200

    Added link to generated documentation in literate programming style

--- article_05/30_break_from_inner_loop.go
+++ article_05/30_break_from_inner_loop.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 30:
 //    Dvojice vnořených smyček for a příkaz break.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_05/30_break_from_inner_loop.html
 
 package main
 
