380f6f56bfcbb3ceb81d80f884b19e8f54803305
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-16 08:51:21 +0200

    Added link to generated documentation in literate programming style

--- article_05/20_basic_for_loop.go
+++ article_05/20_basic_for_loop.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 20:
 //    Základní forma programové smyčky for s podmínkou.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_05/20_basic_for_loop.html
 
 package main
 
