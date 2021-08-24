0886221f9662b8f40dbe2396459db1c9a7ecb23a
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-24 18:42:38 +0200

    Added link to generated documentation in literate programming style

--- article_06/05_goto_bad_usage_B.go
+++ article_06/05_goto_bad_usage_B.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 5:
 //    Špatné použití příkazu goto.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_06/05_goto_bad_usage_B.html
 
 package main
 
