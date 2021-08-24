55c6e1c075c9d4ac843e6da79a78141a3ebe3377
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-24 18:42:38 +0200

    Added link to generated documentation in literate programming style

--- article_06/19_unary_operators.go
+++ article_06/19_unary_operators.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 19:
 //    Všechny unární operátory jazyka Go.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_06/19_unary_operators.html
 
 package main
 
