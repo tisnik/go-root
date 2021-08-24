e0870aab77d80454d54c4062459645e66bd0decc
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-24 18:42:38 +0200

    Added link to generated documentation in literate programming style

--- article_06/07_defer.go
+++ article_06/07_defer.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 7:
 //    Použití příkazu defer.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_06/07_defer.html
 
 package main
 
