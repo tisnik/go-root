8fa6836bb61907bec73b543b9f3105273c86ea2d
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-01 08:12:37 +0200

    Added link to generated documentation in literate programming style

--- article_07/13_iota.go
+++ article_07/13_iota.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 13:
 //    Konstanty a identifikátor iota.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_07/13_iota.html
 
 package main
 
