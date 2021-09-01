b8c02c824639469e5c1fd6e0d0320c5ed838a50e
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-01 08:20:10 +0200

    Added link to generated documentation in literate programming style

--- article_08/02_enum_with_iota_type_check.go
+++ article_08/02_enum_with_iota_type_check.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 2:
 //    Náhrada za enum: běžné konstanty a identifikátor iota.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_07/02_enum_with_iota_type_check.html
 
 package main
 
