ce5e1d1e1e85ac003dde7f42335e19f7ff0a3bff
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-01 08:12:37 +0200

    Added link to generated documentation in literate programming style

--- article_07/04_inc_dec_bad_usage.go
+++ article_07/04_inc_dec_bad_usage.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 4:
 //    Nekorektní použití operátorů ++ a --.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_07/04_inc_dec_bad_usage.html
 
 package main
 
