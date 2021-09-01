b14b442d229ea8b5cf326397a12acea2970da115
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-01 08:12:37 +0200

    Added link to generated documentation in literate programming style

--- article_07/01_bit_operators.go
+++ article_07/01_bit_operators.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 1:
 //    Bitové operátory.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_07/01_bit_operators.html
 
 package main
 
