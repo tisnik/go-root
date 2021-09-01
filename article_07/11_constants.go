d22f6833efe1e419fd49343f3e3b6145ea378517
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-01 08:12:37 +0200

    Added link to generated documentation in literate programming style

--- article_07/11_constants.go
+++ article_07/11_constants.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 11:
 //    Konstanty.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_07/11_constants.html
 
 package main
 
