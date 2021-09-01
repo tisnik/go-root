1e3736a7d851a7901235ac3ce233f00dbffc0010
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-01 08:12:37 +0200

    Added link to generated documentation in literate programming style

--- article_07/18_variadic_function_improper_usage.go
+++ article_07/18_variadic_function_improper_usage.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 18:
 //    Variadické funkce.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_07/18_variadic_function_improper_usage.html
 
 package main
 
