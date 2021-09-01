17ac5c68d1650d7002e6f0a55fb5a102b9d6c5c3
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-01 08:20:10 +0200

    Added link to generated documentation in literate programming style

--- article_08/13_select_statement_receive.go
+++ article_08/13_select_statement_receive.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 13:
 //    Použití příkazu select pro čtení z kanálů s timeoutem.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_07/13_select_statement_receive.html
 
 package main
 
