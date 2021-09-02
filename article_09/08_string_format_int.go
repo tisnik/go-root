88033938e314cccc0b8cb2a73acc48ea14d5c594
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-02 07:47:43 +0200

    Added link to generated documentation in literate programming style

--- article_09/08_string_format_int.go
+++ article_09/08_string_format_int.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 8:
 //    Základní funkce pro formátování řetězců: FormatInt.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_09/08_string_format_int.html
 
 package main
 
