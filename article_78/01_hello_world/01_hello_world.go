d25bbb457f1d2d4417f6d7563254cb902e84fb33
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-15 08:27:59 +0200

    gval example #1

---
+++ article_78/01_hello_world/01_hello_world.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/PaesslerAG/gval"
+)
+
+func main() {
+	// parametry předávané vyhodnocovanému výrazu
+	parameters := map[string]interface{}{"name": "World"}
+
+	// vyhodnocení výrazu
+	value, err := gval.Evaluate(`"Hello " + name + "!"`, parameters)
+	if err != nil {
+		// kód pro zpracování chyby při vyhodnocování výrazu
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// výpis výsledku výrazu
+	fmt.Print(value)
+}
---
+++ article_78/01_hello_world/go.mod
@@ -0,0 +1,5 @@
+module 01_hello_world
+
+go 1.14
+
+require github.com/PaesslerAG/gval v1.1.1
