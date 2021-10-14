e2151ace17baa90610d823e0fa8a866aae5f25df
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-14 07:46:00 +0200

    Added link to generated documentation in literate programming style

--- article_10/04_bigint.go
+++ article_10/04_bigint.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 4:
 //    Použití balíčku big, typ Int.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_10/04_bigint.html
 
 package main
 
