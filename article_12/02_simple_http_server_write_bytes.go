1e8ce6eedbb95c65d4c5a8e2956ca1b7bfbda2bd
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-14 07:58:24 +0200

    Added link to the whole serie into On branch master

--- article_12/02_simple_http_server_write_bytes.go
+++ article_12/02_simple_http_server_write_bytes.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 2:
 //    Alternativní způsob vytvoření sekvence bajtů z řetězce
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_12/02_simple_http_server_write_bytes.html
 
 package main
 
