4d545c93056eef8797282229623123022e55b835
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-14 07:44:58 +0200

    Added link to generated documentation in literate programming style

--- article_11/16_custom_server.go
+++ article_11/16_custom_server.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 16:
 //     Kombinace předchozích možností
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_11/16_custom_server.html
 
 package main
 
