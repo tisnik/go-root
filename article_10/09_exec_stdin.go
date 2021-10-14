940f17a0048b14e3caa5e09df7989fe0d5a85cad
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-14 07:46:00 +0200

    Added link to generated documentation in literate programming style

--- article_10/09_exec_stdin.go
+++ article_10/09_exec_stdin.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 9:
 //    Použití balíčku exec, spuštění externí utility s předáním dat.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_10/09_exec_stdin.html
 
 package main
 
