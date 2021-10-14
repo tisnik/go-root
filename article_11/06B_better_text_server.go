5eb50c89fda92b54284089a4824dc7b1242aa0ab
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-14 07:44:58 +0200

    Added link to generated documentation in literate programming style

--- article_11/06B_better_text_server.go
+++ article_11/06B_better_text_server.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 6B:
 //     	Vylepšený server posílající klientovi textová data
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_11/06B_better_text_server.html
 
 package main
 
