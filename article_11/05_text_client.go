09b3d23e03e8d5192a683c7a2ab93ec7c9c9d42c
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-14 07:44:58 +0200

    Added link to generated documentation in literate programming style

--- article_11/05_text_client.go
+++ article_11/05_text_client.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 5:
 //     	Jednoduchý klient akceptující celý textový řádek
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_11/05_text_client.html
 
 package main
 
