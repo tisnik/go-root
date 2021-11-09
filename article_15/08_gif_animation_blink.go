4611ee03e5801b6a92913178eb6cce55e9dfd3de
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-09 09:02:58 +0100

    Added link to generated documentation in literate programming style

--- article_15/08_gif_animation_blink.go
+++ article_15/08_gif_animation_blink.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 8:
 //     Jednoduchá animace se dvěma snímky o velikosti 32×32 pixelů
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_15/08_gif_animation_blink.html
 
 package main
 
