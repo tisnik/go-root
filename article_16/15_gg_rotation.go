86557d1592a685b1c6a5366c0551b5818b7ecf84
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-09 09:02:53 +0100

    Added link to generated documentation in literate programming style

--- article_16/15_gg_rotation.go
+++ article_16/15_gg_rotation.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 15:
 //     Rotace vykreslovaných objektů.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_16/15_gg_rotation.html
 
 package main
 
