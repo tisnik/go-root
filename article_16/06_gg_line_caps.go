8992e5fade4e619f58cfab9b85148f00cbcfcc82
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-09 09:02:53 +0100

    Added link to generated documentation in literate programming style

--- article_16/06_gg_line_caps.go
+++ article_16/06_gg_line_caps.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 6:
 //     Nastavení stylu konců úseček.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_16/06_gg_line_caps.html
 
 package main
 
