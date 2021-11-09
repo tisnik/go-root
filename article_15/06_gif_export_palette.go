108acfdf353e3cc0c55f32c8d8938091433d7694
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-09 09:02:58 +0100

    Added link to generated documentation in literate programming style

--- article_15/06_gif_export_palette.go
+++ article_15/06_gif_export_palette.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 6:
 //     Šachovnice vykreslená s dvoubarevnou paletou
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_15/06_gif_export_palette.html
 
 package main
 
