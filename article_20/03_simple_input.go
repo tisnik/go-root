1dc697392e8f4c7fec59c2c726d4523f92f18ea8
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-12 08:07:44 +0100

    Added link to generated documentation in literate programming style

--- article_20/03_simple_input.go
+++ article_20/03_simple_input.go
@@ -7,6 +7,10 @@
 //
 // Demonstrační příklad číslo 3:
 //     Go-prompt: vstup dat.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_20/03_simple_input.html
+//
 
 package main
 
