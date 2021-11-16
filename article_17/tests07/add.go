95226a591da1f94f80539c5c4e3a6493512b5fdb
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-16 08:36:40 +0100

    Added link to generated documentation in literate programming style

--- article_17/tests07/add.go
+++ article_17/tests07/add.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 7:
 //     Testovaný balíček.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_17/test07/add.html
 
 package main
 
