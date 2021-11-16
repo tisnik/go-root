6b86fc0c47287a79f96097ab1d125eaf855a15ce
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-16 08:36:40 +0100

    Added link to generated documentation in literate programming style

--- article_17/tests08/add_fast_test.go
+++ article_17/tests08/add_fast_test.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 8:
 //     Implementace jednotkových testů.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_17/test08/add_fast_test.html
 
 // +build fast
 
