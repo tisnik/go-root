7b5c4b4a41911d7442d7c8bfb05dc509257d8222
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-17 11:23:37 +0100

    Added link to generated documentation in literate programming style

--- article_18/06_factorial_assertions/factorial_test.go
+++ article_18/06_factorial_assertions/factorial_test.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 6:
 //     Testy pro balíček.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_18/06_factorial_assertions/factorial_test.html
 
 package factorial_test
 
