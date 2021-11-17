18f622d8e99ff7597779a118daf9986ef7227022
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-17 11:23:37 +0100

    Added link to generated documentation in literate programming style

--- article_18/03_factorial_oglematchers2/factorial_test.go
+++ article_18/03_factorial_oglematchers2/factorial_test.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 3:
 //     Testy pro balíček.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_18/03_factorial_oglematchers2/factorial_test.html
 
 package factorial_test
 
