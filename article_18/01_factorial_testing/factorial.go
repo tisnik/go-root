602d29a6d0f3c2fce2b6a122f910d43b3109191e
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-17 11:23:37 +0100

    Added link to generated documentation in literate programming style

--- article_18/01_factorial_testing/factorial.go
+++ article_18/01_factorial_testing/factorial.go
@@ -7,6 +7,9 @@
 //
 // Demonstrační příklad číslo 1:
 //     Testovaný balíček.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_18/01_factorial_testing/factorial.html
 
 package factorial
 
