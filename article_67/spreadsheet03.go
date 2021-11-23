0a94d3d83c0c164dcbedd6b139c0ae9d58fb2375
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-23 18:09:33 +0100

    Added link to generated documentation in literate programming style

--- article_67/spreadsheet03.go
+++ article_67/spreadsheet03.go
@@ -4,6 +4,9 @@
 // Šedesátá sedmá část
 //    Tvorba sešitů pro tabulkové procesory v programovacím jazyku Go – formát xlsx
 //    https://www.root.cz/clanky/tvorba-sesitu-pro-tabulkove-procesory-v-programovacim-jazyku-go-format-xlsx/
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_67/spreadsheet03.html
 
 package main
 
