447d78fd4a9d3d4096baa54799b43ff9f005ace4
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-12-03 10:42:14 +0100

    Golang template, example 8

---
+++ article_79/template08.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+const (
+	templateName   = "test"
+	templateFormat = `Jméno {{.Name}} {{.Surname}}
+Popularita {{.Popularity}}
+
+`
+)
+
+type Role struct {
+	Name       string
+	Surname    string
+	Popularity int
+}
+
+func main() {
+	// vytvoření nové šablony
+	tmpl := template.Must(template.New(templateName).Parse(templateFormat))
+
+	// tyto hodnoty budou použity při aplikaci šablony
+	roles := []Role{
+		Role{"Eliška", "Najbrtová", 4},
+		Role{"Jenny", "Suk", 3},
+		Role{"Anička", "Šafářová", 1},
+		Role{"Sváťa", "Pulec", 3},
+		Role{"Blažej", "Motyčka", 8},
+		Role{"Eda", "Wasserfall", 3},
+		Role{"Přemysl", "Hájek", 10},
+	}
+
+	// aplikace šablony - přepis hodnot
+	for _, role := range roles {
+		err := tmpl.Execute(os.Stdout, role)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
