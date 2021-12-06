c6412266f49e5ac1620f1dd4b907482f00020891
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-12-06 09:27:14 +0100

    Go templates: 15th example

---
+++ article_79/template15.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+const (
+	templateFilename = "template15.txt"
+)
+
+type Role struct {
+	Name       string
+	Surname    string
+	Popularity int
+}
+
+func main() {
+	// vytvoření nové šablony
+	tmpl := template.Must(template.ParseFiles(templateFilename))
+
+	// tyto hodnoty budou použity při aplikaci šablony
+	roles := []Role{
+		Role{"Eliška", "Najbrtová", 4},
+		Role{"Jenny", "Suk", 3},
+		Role{"Anička", "Šafářová", 1},
+		Role{"Sváťa", "Pulec", 3},
+		Role{"Blažej", "Motyčka", 8},
+		Role{"Eda", "Wasserfall", 3},
+		Role{"Přemysl", "Hájek", 10},
+	}
+
+	// aplikace šablony - přepis hodnot
+	err := tmpl.Execute(os.Stdout, roles)
+	if err != nil {
+		panic(err)
+	}
+}
