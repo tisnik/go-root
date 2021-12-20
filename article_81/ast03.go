21fcf7f97941fedf0e9109fbe3fe7348e02ba717
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-12-20 10:06:35 +0100

    Parsing source code into AST, example #3

---
+++ article_81/ast03.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"go/ast"
+	"go/parser"
+	"go/token"
+)
+
+// zdrojový kód, který se má naparsovat
+const source = `
+package main
+
+var answer int = 42
+`
+
+// funkce volaná při průchodu AST
+func inspectCallback(n ast.Node) bool {
+	// dosáhli jsme koncového uzlu?
+	if n == nil {
+		return false
+	}
+
+	// tisk typu uzlu
+	fmt.Printf("%T\n", n)
+	return true
+}
+
+func main() {
+	// struktura reprezentující množinu zdrojových kódů
+	fileSet := token.NewFileSet()
+
+	// konstrukce parseru a parsing zdrojového kódu
+	f, err := parser.ParseFile(fileSet, "", source, parser.AllErrors)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// zahájení průchodu abstraktním syntaktickým stromem
+	ast.Inspect(f, inspectCallback)
+}
