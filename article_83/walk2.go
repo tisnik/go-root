7bf6fea5ed3d22aa98a131743ca0b87bbb9f723a
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-12-27 09:39:39 +0100

    Walking through AST, example #2

---
+++ article_83/walk2.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"go/ast"
+	"go/parser"
+)
+
+// výraz, který se má naparsovat
+const source = `
+1 + 2 * 3 + x + y * z - 1
+`
+
+func walk(node ast.Node) {
+	// dosáhli jsme koncového uzlu?
+	if node == nil {
+		return
+	}
+
+	var s string
+
+	// převod uzlu do tisknutelné podoby a rekurzivní průchod poduzly
+	switch x := node.(type) {
+	case *ast.BasicLit:
+		s = x.Value
+	case *ast.Ident:
+		s = x.Name
+	case *ast.UnaryExpr:
+		s = x.Op.String()
+		walk(x.X)
+	case *ast.BinaryExpr:
+		walk(x.X)
+		s = x.Op.String()
+		walk(x.Y)
+	}
+
+	// tisk obsahu uzlu
+	if s != "" {
+		fmt.Printf("%s ", s)
+	} else {
+		fmt.Printf("%T ", node)
+	}
+}
+
+func main() {
+	// konstrukce parseru a parsing zdrojového kódu
+	node, err := parser.ParseExpr(source)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// zahájení průchodu abstraktním syntaktickým stromem
+	walk(node)
+}
