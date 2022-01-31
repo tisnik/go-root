ba943df9cf1e2652c3aa55f7f79682d1ae6c9f92
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-31 11:40:42 +0100

    HTTP server providing static HTML page(s)

---
+++ article_88/static_pages.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func sendStaticPage(writer http.ResponseWriter, filename string) {
+	log.Printf("Sending static file %s", filename)
+
+	body, err := ioutil.ReadFile(filename)
+	if err == nil {
+		fmt.Fprint(writer, string(body))
+	} else {
+		writer.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(writer, "Not found!")
+	}
+}
+
+func staticPage(filename string) func(writer http.ResponseWriter, request *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		sendStaticPage(writer, filename)
+	}
+}
+
+func main() {
+	const address = ":8080"
+
+	log.Printf("Starting server on address %s", address)
+
+	http.HandleFunc("/", staticPage("index.html"))
+	http.HandleFunc("/missing", staticPage("missing.html"))
+	http.ListenAndServe(address, nil)
+}
