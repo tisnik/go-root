d808d4b2fd36d5e512022edff0b4e96ffdafdecd
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-31 11:40:42 +0100

    HTML templating, example #2

---
+++ article_88/html_template02.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+const (
+	templateFilename = "html_template02.htm"
+)
+
+// datový typ, jehož prvky budou vypisovány v šabloně
+type Role struct {
+	Name       string
+	Surname    string
+	Popularity int
+}
+
+func main() {
+	// vytvoření nové šablony
+	tmpl := template.Must(template.ParseFiles(templateFilename))
+
+	// tyto hodnoty budou použity při aplikaci šablony
+	roles := []Role{
+		Role{"Eliška", "Najbrtová", 4},
+		Role{"Jenny", "Suk", 3},
+		Role{"Anička", "Šafářová", 0},
+		Role{"Sváťa", "Pulec", 3},
+		Role{"Blažej", "Motyčka", 8},
+		Role{"Eda", "Wasserfall", 0},
+		Role{"Přemysl", "Hájek", 10},
+	}
+
+	// aplikace šablony - přepis hodnot
+	err := tmpl.Execute(os.Stdout, roles)
+	if err != nil {
+		panic(err)
+	}
+}
