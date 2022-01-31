fcd00b83bc9606b3c19782a692d39232d9efa075
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-31 11:40:42 +0100

    HTTP server providing HTML page(s) based on template, templates are reloaded when changed

---
+++ article_88/template_pages3/go.mod
@@ -0,0 +1,8 @@
+module template-pages-3
+
+go 1.17
+
+require (
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+)
---
+++ article_88/template_pages3/template_pages3.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+const templateFilename = "./html_template06.htm"
+
+// datový typ, jehož prvky budou vypisovány v šabloně
+type Role struct {
+	Name       string
+	Surname    string
+	Popularity int
+}
+
+func sendStaticPage(writer http.ResponseWriter, filename string) {
+	log.Printf("Sending static file %s", filename)
+
+	body, err := ioutil.ReadFile(filename)
+	if err == nil {
+		fmt.Fprint(writer, string(body))
+	} else {
+		writer.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(writer, "Not found!")
+	}
+}
+
+func staticPage(filename string) func(writer http.ResponseWriter, request *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		sendStaticPage(writer, filename)
+	}
+}
+
+func rolesHandler(tmpl *template.Template, roles []Role, changed chan string) func(writer http.ResponseWriter, request *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		select {
+		case filename := <-changed:
+			log.Printf("Have to reload template from file %s", filename)
+			tmpl = loadTemplate(filename)
+		default:
+			log.Print("Using old template")
+		}
+
+		log.Printf("Application template for %d data records", len(roles))
+		// aplikace šablony - přepis hodnot
+		err := tmpl.Execute(writer, roles)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error executing template: %v", err)
+			return
+		}
+	}
+}
+
+func loadTemplate(templateFilename string) *template.Template {
+	log.Printf("Constructing template from file %s", templateFilename)
+
+	// vytvoření nové šablony
+	tmpl, err := template.ParseFiles(templateFilename)
+	if err != nil {
+		log.Fatalf("Template can't be constructed: %v", err)
+		return nil
+	}
+
+	return tmpl
+}
+
+func startWatcher(directory string, filename string, changed chan string) {
+	log.Print("Starting watcher")
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	log.Printf("Watching directory %s", directory)
+
+	err = watcher.Add(directory)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return
+			}
+			log.Println("event:", event)
+			if event.Op&fsnotify.Write == fsnotify.Write {
+				log.Println("modified file:", event.Name)
+				if filename == event.Name {
+					log.Println("Template change detected")
+					changed <- filename
+				}
+			}
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return
+			}
+			log.Println("error:", err)
+		}
+	}
+}
+
+func main() {
+	// tyto hodnoty budou použity při aplikaci šablony
+	roles := []Role{
+		Role{"Eliška", "Najbrtová", 4},
+		Role{"Jenny", "Suk", 3},
+		Role{"Anička", "Šafářová", 0},
+		Role{"Sváťa", "Pulec", 3},
+		Role{"Blažej", "Motyčka", 8},
+		Role{"Eda", "Wasserfall", 0},
+		Role{"Přemysl", "Hájek", 10},
+	}
+
+	changed := make(chan string)
+	go startWatcher(".", templateFilename, changed)
+
+	tmpl := loadTemplate(templateFilename)
+
+	const address = ":8080"
+
+	log.Printf("Starting server on address %s", address)
+
+	http.HandleFunc("/", staticPage("index.html"))
+	http.HandleFunc("/missing", staticPage("missing.html"))
+	http.HandleFunc("/roles", rolesHandler(tmpl, roles, changed))
+	http.ListenAndServe(address, nil)
+}
