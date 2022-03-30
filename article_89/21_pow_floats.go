caa178f20906d9ddb3657d7d752e8d10cdd275b6
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-30 09:24:34 +0200

    Generics in Go: example #21

---
+++ article_89/21_pow_floats.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func pow32(x float32, y float32) float32 {
+	return float32(math.Pow(float64(x), float64(y)))
+}
+
+func pow64(x float64, y float64) float64 {
+	return math.Pow(x, y)
+}
+
+func main() {
+	for x := float32(0.0); x < 5.0; x += 0.5 {
+		fmt.Println(pow32(x, 2))
+	}
+
+	fmt.Println()
+
+	for x := 0.0; x < 5.0; x += 0.5 {
+		fmt.Println(pow64(x, 2))
+	}
+}
