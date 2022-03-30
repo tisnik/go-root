79ebd8b266202e3362095a576d4f2e696ccf3c91
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-30 09:24:34 +0200

    Generics in Go: example #26

---
+++ article_89/26_structs.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Person struct {
+	name    string
+	surname string
+}
+
+type Employee struct {
+	id      uint
+	name    string
+	surname string
+}
+
+func (person *Person) getName() string {
+	return person.name
+}
+
+func (employee *Employee) getName() string {
+	return employee.name
+}
+
+func main() {
+	var p Person = Person{
+		name:    "Pepek",
+		surname: "Vyskoč",
+	}
+
+	fmt.Println(p.getName())
+
+	var e Employee = Employee{
+		name:    "Eda",
+		surname: "Wasserfall",
+	}
+
+	fmt.Println(e.getName())
+}
