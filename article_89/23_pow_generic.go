86158212cef3617ada82926b3bd8142bf5062651
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-30 09:24:34 +0200

    Generics in Go: example #23

---
+++ article_89/23_pow_generic.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type floats interface {
+	float32 | float64
+}
+
+type numeric interface {
+	float32 | float64 | int
+}
+
+func pow[T floats, U numeric](x T, y U) T {
+	return T(math.Pow(float64(x), float64(y)))
+}
+
+func main() {
+	for x := float32(0.0); x < 5.0; x += 0.5 {
+		fmt.Println(pow(x, 2))
+	}
+
+	fmt.Println()
+
+	for x := 0.0; x < 5.0; x += 0.5 {
+		fmt.Println(pow(x, 2))
+	}
+}
