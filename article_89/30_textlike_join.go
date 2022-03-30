2d5c8d0a44d952ce04e21f070475326d58adef38
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-30 09:24:34 +0200

    Generics in Go: example #30

---
+++ article_89/30_textlike_join.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type TextLike interface {
+	String() string
+}
+
+type Employee struct {
+	name    string
+	surname string
+}
+
+func (employee Employee) String() string {
+	return employee.name + " " + employee.surname
+}
+
+func join(items ...TextLike) (result string) {
+	for _, value := range items {
+		result += value.String()
+		result += ","
+	}
+	return result
+}
+
+func main() {
+	var e1 Employee = Employee{
+		name:    "Pepek",
+		surname: "Vyskoč",
+	}
+
+	var e2 Employee = Employee{
+		name:    "Eda",
+		surname: "Wasserfall",
+	}
+
+	fmt.Println(join(e1, e2))
+}
