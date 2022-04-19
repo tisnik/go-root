e38ecdf2bc41c2400ad9f67564f33bcf7f60ac41
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-04-19 12:47:09 +0200

    Goflow example #2

---
+++ article_90/flow2/flow2.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"github.com/trustmaster/goflow"
+)
+
+// Jediný typ uzlu v síti
+type Printer struct {
+	// vstupní port
+	Message <-chan string
+}
+
+// Zpracování vstupních dat posílaných přes kanál
+func (c *Printer) Process() {
+	for message := range c.Message {
+		fmt.Println(message)
+	}
+}
+
+// Definice grafu sítě s aplikací
+func NewFlowApp() *goflow.Graph {
+	// konstrukce grafu
+	n := goflow.NewGraph()
+
+	// přidání uzlu s procesem do grafu
+	n.Add("printer", new(Printer))
+
+	// přidání uzlu s procesem do grafu
+	n.Add("printer2", new(Printer))
+
+	// propojení uzlu s procesem
+	n.MapInPort("Input", "printer", "Message")
+
+	// propojení uzlu s procesem
+	n.MapInPort("Input", "printer2", "Message")
+
+	// výsledný graf
+	return n
+}
+
+func main() {
+	// vytvoření sítě
+	net := NewFlowApp()
+
+	// konstrukce kanálu
+	channel := make(chan string)
+	net.SetInPort("Input", channel)
+
+	// spuštění sítě
+	// (kanál bude použit pro čekání na její ukončení)
+	wait := goflow.Run(net)
+
+	// poslání několika zpráv do sítě
+	channel <- "Foo"
+	channel <- "Bar"
+	channel <- "Baz"
+
+	// žádost o ukončení činnosti sítě
+	close(channel)
+
+	// čekání na ukončení činnosti sítě
+	<-wait
+}
---
+++ article_90/flow2/go.mod
@@ -0,0 +1,5 @@
+module flow2
+
+go 1.18
+
+require github.com/trustmaster/goflow v0.0.0-20210928125717-b7d4fd465ab2 // indirect
