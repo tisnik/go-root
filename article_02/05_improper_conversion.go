e6c4f537ea01919aae24c4f32209d60f226f11c6
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-23 07:58:11 +0200

    Added link to list of examples

--- article_02/05_improper_conversion.go
+++ article_02/05_improper_conversion.go
@@ -5,6 +5,9 @@
 //    Datové typy v programovacím jazyku Go
 //    https://www.root.cz/clanky/datove-typy-v-programovacim-jazyku-go/
 //
+// Seznam příkladů z druhé části:
+//    https://github.com/tisnik/go-root/blob/master/article_02/README.md
+//
 // Demonstrační příklad číslo 5:
 //    Přiřazení uint8 -> uint16
 //
