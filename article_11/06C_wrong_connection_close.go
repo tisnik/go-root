864ff3e9d7b278c13e73e7011f2fa6d003a27707
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-01 09:01:15 +0200

    Added links to list of examples

--- article_11/06C_wrong_connection_close.go
+++ article_11/06C_wrong_connection_close.go
@@ -5,6 +5,9 @@
 //     Vývoj síťových aplikací v programovacím jazyku Go
 //     https://www.root.cz/clanky/vyvoj-sitovych-aplikaci-v-programovacim-jazyku-go/
 //
+// Seznam příkladů z jedenácté části:
+//    https://github.com/tisnik/go-root/blob/master/article_11/README.md
+//
 // Demonstrační příklad číslo 6C:
 //     Připojení je nutné ukončit v gorutině, ne mimo ni
 //
