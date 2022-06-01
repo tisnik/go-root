242f6da70995a72cacab4768a8cb8c738e7f6f9a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-01 09:01:15 +0200

    Added links to list of examples

--- article_11/02B_simple_server_no_localhost.go
+++ article_11/02B_simple_server_no_localhost.go
@@ -5,6 +5,9 @@
 //     Vývoj síťových aplikací v programovacím jazyku Go
 //     https://www.root.cz/clanky/vyvoj-sitovych-aplikaci-v-programovacim-jazyku-go/
 //
+// Seznam příkladů z jedenácté části:
+//    https://github.com/tisnik/go-root/blob/master/article_11/README.md
+//
 // Demonstrační příklad číslo 2B:
 //     Úprava adresy v předchozím příkladu
 //
