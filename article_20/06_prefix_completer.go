2160f2ddb5a30dfde54ea4ebd021817f073580e0
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-03 07:17:19 +0200

    Added links to list of examples

--- article_20/06_prefix_completer.go
+++ article_20/06_prefix_completer.go
@@ -5,6 +5,9 @@
 //     Jazyk Go a aplikace s vlastním příkazovým řádkem
 //     https://www.root.cz/clanky/jazyk-go-a-aplikace-s-vlastnim-prikazovym-radkem/
 //
+// Seznam příkladů z dvacáté části:
+//    https://github.com/tisnik/go-root/blob/master/article_20/README.md
+//
 // Demonstrační příklad číslo 6:
 //     Go-prompt: vstup s nápovědou, reakce na zadaný příkaz.
 //
