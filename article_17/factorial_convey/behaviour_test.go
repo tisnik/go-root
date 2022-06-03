85eeec62e2d49094d17195bb0f7eb83d676d5bad
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-03 07:17:18 +0200

    Added links to list of examples

--- article_17/factorial_convey/behaviour_test.go
+++ article_17/factorial_convey/behaviour_test.go
@@ -5,6 +5,9 @@
 //     Testování aplikací naprogramovaných v jazyce Go
 //     https://www.root.cz/clanky/testovani-aplikaci-naprogramovanych-v-jazyce-go/
 //
+// Seznam příkladů ze sedmnácté části:
+//    https://github.com/tisnik/go-root/blob/master/article_17/README.md
+//
 // Dokumentace ve stylu "literate programming":
 //    https://tisnik.github.io/go-root/article_17/factorial_convey/behaviour_test.html
 //
