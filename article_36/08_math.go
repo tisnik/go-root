5f2183d50eeee71be544f2aa77e5cedf2f48384c
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-10 08:12:31 +0200

    Added links to list of examples

--- article_36/08_math.go
+++ article_36/08_math.go
@@ -4,6 +4,9 @@
 // Třicátá šestá část
 //    Kooperace mezi kódem psaným v Go a C: cgo
 //    https://www.root.cz/clanky/kooperace-mezi-kodem-psanym-v-go-a-c-cgo/
+//
+// Seznam příkladů ze třicáté šesté části:
+//    https://github.com/tisnik/go-root/blob/master/article_36/README.md
 
 package main
 
