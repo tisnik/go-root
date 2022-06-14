ca2a56efe8e0e2b9047bad1ffd0e2361fee8e272
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-14 07:54:13 +0200

    Added links to list of examples

--- article_41/bdd_iteration_5B/accumulator_test.go
+++ article_41/bdd_iteration_5B/accumulator_test.go
@@ -4,6 +4,9 @@
 // Čtyřicátá první část
 //    Tvorba BDD testů s využitím jazyka Go a nástroje godog
 //    https://www.root.cz/clanky/tvorba-bdd-testu-s-vyuzitim-jazyka-go-a-nastroje-godog/
+//
+// Seznam příkladů ze čtyřicáté první části:
+//    https://github.com/tisnik/go-root/blob/master/article_41/README.md
 
 package accumulator
 
