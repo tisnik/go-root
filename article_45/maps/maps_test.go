2a51e661e0a30986c174c5cd5d780ddaeda9e702
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-16 08:06:28 +0200

    Added links to list of examples

--- article_45/maps/maps_test.go
+++ article_45/maps/maps_test.go
@@ -1,3 +1,13 @@
+// Seriál "Programovací jazyk Go"
+//    https://www.root.cz/serialy/programovaci-jazyk-go/
+//
+// Čtyřicátá pátá část
+//     Testování Go aplikací s využitím knihovny GΩmega a frameworku Ginkgo
+//     https://www.root.cz/clanky/testovani-go-aplikaci-s-vyuzitim-knihovny-gomega-mega-a-frameworku-ginkgo/
+//
+// Seznam příkladů ze čtyřicáté páté části:
+//    https://github.com/tisnik/go-root/blob/master/article_45/README.md
+
 package main
 
 import (
