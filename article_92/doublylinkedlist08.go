e2053eeed43410f6531d1ba3e23630d854c75aca
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-21 07:15:38 +0200

    Go18DS data structures: doubly linked list

---
+++ article_92/doublylinkedlist08.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/daichi-m/go18ds/lists/doublylinkedlist"
+)
+
+func main() {
+	l := doublylinkedlist.New[string]()
+	fmt.Println(l)
+
+	l.Add("a")
+	fmt.Println(l)
+
+	l.Add("b", "c")
+	fmt.Println(l)
+
+	for i := 0; i < 5; i++ {
+		l.Remove(0)
+		fmt.Println(l)
+	}
+}
