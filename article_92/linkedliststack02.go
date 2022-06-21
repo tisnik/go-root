098b66df8e57defd3432e1d47eba97f5e55833c3
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-21 07:15:38 +0200

    Go18DS data structures: linked list stack

---
+++ article_92/linkedliststack02.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/daichi-m/go18ds/stacks/linkedliststack"
+)
+
+func main() {
+	stack := linkedliststack.New[string]()
+	stack.Push("foo")
+	stack.Push("bar")
+	stack.Push("baz")
+
+	for {
+		value, exists := stack.Pop()
+		if exists {
+			fmt.Println(value)
+		} else {
+			break
+		}
+	}
+}
