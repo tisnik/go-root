c553a2f9e75350129460c4009646433fa7547f69
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-21 07:15:38 +0200

    Go18DS data structures: stack for RPN

---
+++ article_92/stack_rpn_B.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+
+	"strconv"
+	"strings"
+
+	"github.com/daichi-m/go18ds/stacks/arraystack"
+)
+
+func printStack(s *arraystack.Stack[int]) {
+	it := s.Iterator()
+	for it.Next() {
+		value := it.Value()
+		fmt.Printf("%3d ", value)
+	}
+	fmt.Println()
+}
+
+func main() {
+	expression := "1 2 + 2 3 * 8 + *"
+	terms := strings.Split(expression, " ")
+	stack := arraystack.New[int]()
+
+	for _, term := range terms {
+		switch term {
+		case "+":
+			operand1, _ := stack.Pop()
+			operand2, _ := stack.Pop()
+			stack.Push(operand1 + operand2)
+			print("+ :\t")
+			printStack(stack)
+		case "-":
+			operand1, _ := stack.Pop()
+			operand2, _ := stack.Pop()
+			stack.Push(operand2 - operand1)
+			print("- :\t")
+			printStack(stack)
+		case "*":
+			operand1, _ := stack.Pop()
+			operand2, _ := stack.Pop()
+			stack.Push(operand1 * operand2)
+			print("* :\t")
+			printStack(stack)
+		case "/":
+			operand1, _ := stack.Pop()
+			operand2, _ := stack.Pop()
+			stack.Push(operand2 / operand1)
+			print("/ :\t")
+			printStack(stack)
+		default:
+			number, err := strconv.Atoi(term)
+			if err == nil {
+				stack.Push(number)
+			}
+			fmt.Printf("%-2d:\t", number)
+			printStack(stack)
+		}
+	}
+	print("Result: ")
+	printStack(stack)
+}
