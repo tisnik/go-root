df3808ba38b45be2deef2c850d57bbd23c2a67df
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-21 07:15:37 +0200

    Go18DS data structures: array stack

---
+++ article_92/arraystack02.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/daichi-m/go18ds/stacks/arraystack"
+)
+
+func main() {
+	stack := arraystack.New[string]()
+	stack.Push("foo")
+	stack.Push("bar")
+	stack.Push("baz")
+
+	for {
+		value, exists := stack.Pop()
+		if exists {
+			fmt.Println(value)
+		} else {
+			break
+		}
+	}
+}
