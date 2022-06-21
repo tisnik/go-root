137eebf16da1e215cf239b969b25e74929d4da0c
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-21 07:15:37 +0200

    Go18DS data structures: array list

---
+++ article_92/arraylist08.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/daichi-m/go18ds/lists/arraylist"
+)
+
+func main() {
+	l := arraylist.New[string]()
+	fmt.Println(l)
+
+	l.Add("a")
+	fmt.Println(l)
+
+	l.Add("b", "c")
+	fmt.Println(l)
+
+	for i := 0; i < 5; i++ {
+		l.Remove(0)
+		fmt.Println(l)
+	}
+}
