05e17e58586fbaba03c4bb2f0abef76cb55e9f8a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-23 07:13:59 +0200

    Erasthotenes sieve

---
+++ article_92/erasthotenes.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/daichi-m/go18ds/sets/linkedhashset"
+)
+
+const maxPrime = 1000
+
+func printSet(set *linkedhashset.Set[int]) {
+	iterator := set.Iterator()
+	for iterator.Next() {
+		index, value := iterator.Index(), iterator.Value()
+		fmt.Printf("%3d ", value)
+		if index%10 == 9 {
+			fmt.Println()
+		}
+	}
+	fmt.Println()
+}
+
+func main() {
+	primes := linkedhashset.New[int]()
+	for n := 2; n < maxPrime; n++ {
+		primes.Add(n)
+	}
+
+	for n := 2; n < maxPrime/2; n++ {
+		if primes.Contains(n) {
+			for k := 2 * n; k < maxPrime; k += n {
+				primes.Remove(k)
+			}
+		}
+	}
+	printSet(primes)
+}
