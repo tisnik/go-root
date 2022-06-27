fa8d02c8d2050450807ea762595feafa3623d9a2
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-27 09:29:46 +0200

    RB-tree, example #1

---
+++ article_93/rb-tree01.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	treeImpl "github.com/daichi-m/go18ds/trees/redblacktree"
+)
+
+func main() {
+	tree := treeImpl.NewWithIntComparator[string]()
+	fmt.Println(tree)
+
+	tree.Put(1, "G")
+	fmt.Println(tree)
+
+	tree.Put(2, "a")
+	tree.Put(3, "b")
+	fmt.Println(tree)
+
+	tree.Put(4, "a")
+	tree.Put(5, "b")
+	fmt.Println(tree)
+
+	tree.Put(6, "a")
+	tree.Put(7, "b")
+	fmt.Println(tree)
+
+	tree.Put(8, "a")
+	tree.Put(9, "b")
+	fmt.Println(tree)
+}
