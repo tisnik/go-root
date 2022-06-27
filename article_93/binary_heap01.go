019f55f8c6019b2bba7cb6ccbcc5d9a1edf404bf
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-27 09:29:46 +0200

    Binary heap, example #1

---
+++ article_93/binary_heap01.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	heapImpl "github.com/daichi-m/go18ds/trees/binaryheap"
+)
+
+func main() {
+	heap := heapImpl.NewWithIntComparator()
+	fmt.Println(heap)
+
+	heap.Push(1)
+	fmt.Println(heap)
+
+	heap.Push(2)
+	heap.Push(3)
+	fmt.Println(heap)
+
+	heap.Push(4)
+	heap.Push(5)
+	fmt.Println(heap)
+
+	heap.Push(6)
+	heap.Push(7)
+	fmt.Println(heap)
+
+	heap.Push(8)
+	heap.Push(9)
+	fmt.Println(heap)
+}
