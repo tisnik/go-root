b166dd865a98b25f4038599a999b57545c7018e4
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-27 09:29:46 +0200

    B-tree, example #4

---
+++ article_93/btree04.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	treeImpl "github.com/daichi-m/go18ds/trees/btree"
+)
+
+func main() {
+	tree := treeImpl.NewWithIntComparator[string](3)
+
+	tree.Put(1, "a")
+	tree.Put(9, "i")
+	tree.Put(2, "b")
+	tree.Put(8, "h")
+	tree.Put(3, "c")
+	tree.Put(7, "g")
+	tree.Put(4, "d")
+	tree.Put(6, "f")
+	tree.Put(5, "e")
+
+	fmt.Println(tree)
+
+	it := tree.Iterator()
+	for it.Next() {
+		value := it.Value()
+		fmt.Printf("%3s \t %T\n", value, value)
+	}
+
+}
