dc8fd0239f0e3798295088f150727b1dcb52c278
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-01 09:58:29 +0200

    Added link to list of examples

--- article_68/02-parquet-reader/parquet-reader.go
+++ article_68/02-parquet-reader/parquet-reader.go
@@ -4,6 +4,9 @@
 // Šedesátá osmá část
 //    Komunikace se sloupcovými databázemi z jazyka Go: Parquet soubory
 //    https://www.root.cz/clanky/komunikace-se-sloupcovymi-databazemi-z-jazyka-go-parquet-soubory/
+//
+// Seznam příkladů z šedesáté osmé části:
+//    https://github.com/tisnik/go-root/blob/master/article_68/README.md
 
 // This tool is able to read all records stored in selected Parquet file.
 // Currently, only records with the structure `Record` is read correctly. Name
