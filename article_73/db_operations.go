6b1431c54303a08daf48e4b2936ee99f8971d306
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-04 19:41:09 +0200

    Link to list of examples

--- article_73/db_operations.go
+++ article_73/db_operations.go
@@ -1,3 +1,10 @@
+// Seriál "Programovací jazyk Go"
+//    https://www.root.cz/serialy/programovaci-jazyk-go/
+//
+// Sedmdesátá třetí část
+//    Jazyk Go prakticky: jednotkové testy kódu, který přistupuje k SQL databázím (dokončení)
+//    https://www.root.cz/clanky/jazyk-go-prakticky-jednotkove-testy-kodu-ktery-pristupuje-k-sql-databazim-dokonceni/
+
 package main
 
 import (
