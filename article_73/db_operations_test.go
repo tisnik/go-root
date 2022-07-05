7af72b2835583cb8872105b205cd9a7b1d318c8b
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-05 18:24:43 +0200

    Link to list of examples

--- article_73/db_operations_test.go
+++ article_73/db_operations_test.go
@@ -4,6 +4,9 @@
 // Sedmdesátá třetí část
 //    Jazyk Go prakticky: jednotkové testy kódu, který přistupuje k SQL databázím (dokončení)
 //    https://www.root.cz/clanky/jazyk-go-prakticky-jednotkove-testy-kodu-ktery-pristupuje-k-sql-databazim-dokonceni/
+//
+// Seznam příkladů ze sedmdesáté třetí části:
+//    https://github.com/tisnik/go-root/blob/master/article_73/README.md
 
 package main_test
 
