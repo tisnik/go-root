810d27a1dc0f09801ea718094a6fbeb99247171a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-06 19:02:10 +0200

    Tree map, example #4

---
+++ article_94/treemap04.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+
+	mapImpl "github.com/daichi-m/go18ds/maps/treemap"
+)
+
+func main() {
+	cz := []string{
+		"Leden",
+		"Únor",
+		"Březen",
+		"Duben",
+		"Květen",
+		"Červen",
+		"Červenec",
+		"Srpen",
+		"Září",
+		"Říjen",
+		"Listopad",
+		"Prosinec",
+	}
+
+	lat := []string{
+		"Ianuarius",
+		"Februarius",
+		"Martius",
+		"Aprilis",
+		"Maius",
+		"Iunius",
+		"Iulius",
+		"Augustus",
+		"September",
+		"October",
+		"November",
+		"December",
+	}
+
+	m := mapImpl.NewWithStringComparator[string]()
+	fmt.Println(m)
+
+	for i := 0; i < len(cz); i++ {
+		m.Put(cz[i], lat[i])
+		fmt.Println(m)
+	}
+
+	it := m.Iterator()
+	for it.Next() {
+		key, value := it.Key(), it.Value()
+		fmt.Printf("%-10s \t %-10s \t %T\n", key, value, value)
+	}
+}
