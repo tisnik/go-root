a3acf1a9718802e2829e5afdf837c1c028873099
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-06 19:02:09 +0200

    Tree bidi map, example #4

---
+++ article_94/treebidimap04.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	mapImpl "github.com/daichi-m/go18ds/maps/treebidimap"
+	"github.com/daichi-m/go18ds/utils"
+)
+
+func main() {
+	m := mapImpl.NewWith(utils.NumberComparator[int], utils.NumberComparator[int])
+
+	m.Put(1, 1)
+	m.Put(2, 2)
+	m.Put(3, 3)
+
+	fmt.Println("Size", m.Size())
+	fmt.Println("Keys", m.Keys())
+	fmt.Println("Values", m.Values())
+
+	fmt.Println()
+
+	m.Put(4, 1)
+
+	fmt.Println("Size", m.Size())
+	fmt.Println("Keys", m.Keys())
+	fmt.Println("Values", m.Values())
+}
