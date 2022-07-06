eb96915a34f64f7cb21c9e2ddf86c2452701bf6e
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-06 19:02:09 +0200

    Tree bidi map, example #3

---
+++ article_94/treebidimap03.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+
+	mapImpl "github.com/daichi-m/go18ds/maps/treebidimap"
+	"github.com/daichi-m/go18ds/utils"
+)
+
+func main() {
+	cz := []string{
+		"Leden",
+		"Únor",
+		"Březen",
+		"Duben",
+		"Květen",
+		"Červen",
+		"Červenec",
+		"Srpen",
+		"Září",
+		"Říjen",
+		"Listopad",
+		"Prosinec",
+	}
+
+	lat := []string{
+		"Ianuarius",
+		"Februarius",
+		"Martius",
+		"Aprilis",
+		"Maius",
+		"Iunius",
+		"Iulius",
+		"Augustus",
+		"September",
+		"October",
+		"November",
+		"December",
+	}
+
+	m := mapImpl.NewWith(utils.StringComparator, utils.StringComparator)
+
+	for i := 0; i < len(cz); i++ {
+		m.Put(cz[i], lat[i])
+	}
+
+	fmt.Println("Metadata")
+	fmt.Println("Empty?", m.Empty())
+	fmt.Println("Size", m.Size())
+
+	fmt.Println()
+	fmt.Println("Keys")
+	fmt.Printf("%T\n", m.Keys())
+	fmt.Println(m.Keys())
+
+	fmt.Println()
+	fmt.Println("Values")
+	fmt.Printf("%T\n", m.Values())
+	fmt.Println(m.Values())
+}
