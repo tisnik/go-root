4a30d1cf2007f2213e6963096c2e6ee87711c7e9
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-06 19:02:09 +0200

    Hash bidi map, example #2

---
+++ article_94/hashbidimap02.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+
+	mapImpl "github.com/daichi-m/go18ds/maps/hashbidimap"
+)
+
+func main() {
+	cz := []string{
+		"Leden",
+		"Únor",
+		"Březen",
+		"Duben",
+		"Květen",
+		"Červen",
+		"Červenec",
+		"Srpen",
+		"Září",
+		"Říjen",
+		"Listopad",
+		"Prosinec",
+	}
+
+	lat := []string{
+		"Ianuarius",
+		"Februarius",
+		"Martius",
+		"Aprilis",
+		"Maius",
+		"Iunius",
+		"Iulius",
+		"Augustus",
+		"September",
+		"October",
+		"November",
+		"December",
+	}
+
+	m := mapImpl.New[string, string]()
+	fmt.Println(m)
+
+	for i := 0; i < len(cz); i++ {
+		m.Put(cz[i], lat[i])
+		fmt.Println(m)
+	}
+
+	fmt.Println()
+	fmt.Println(m.Get("Leden"))
+	fmt.Println(m.Get("Srpenec"))
+
+	fmt.Println()
+	fmt.Println(m.GetKey("Ianuarius"))
+	fmt.Println(m.GetKey("xyzzy"))
+}
