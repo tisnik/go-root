ae26962b4c871a8c650ef37309eb3d4c8cd410c1
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-06 19:02:09 +0200

    Tree bidi map, example #1

---
+++ article_94/treebidimap01.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+
+	mapImpl "github.com/daichi-m/go18ds/maps/treebidimap"
+	"github.com/daichi-m/go18ds/utils"
+)
+
+func main() {
+	cz := []string{
+		"Leden",
+		"Únor",
+		"Březen",
+		"Duben",
+		"Květen",
+		"Červen",
+		"Červenec",
+		"Srpen",
+		"Září",
+		"Říjen",
+		"Listopad",
+		"Prosinec",
+	}
+
+	lat := []string{
+		"Ianuarius",
+		"Februarius",
+		"Martius",
+		"Aprilis",
+		"Maius",
+		"Iunius",
+		"Iulius",
+		"Augustus",
+		"September",
+		"October",
+		"November",
+		"December",
+	}
+
+	m := mapImpl.NewWith(utils.StringComparator, utils.StringComparator)
+	fmt.Println(m)
+
+	for i := 0; i < len(cz); i++ {
+		m.Put(cz[i], lat[i])
+		fmt.Println(m)
+	}
+}
