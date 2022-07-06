ec867b98da3b0cd8e8c853637640cd2030e5c16e
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-06 19:03:53 +0200

    Link to list of examples

--- article_74/diagram1.go
+++ article_74/diagram1.go
@@ -1,5 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //    https://www.root.cz/serialy/programovaci-jazyk-go/
+//
+// Seznam příkladů ze sedmdesáté čtvrté části:
+//    https://github.com/tisnik/go-root/blob/master/article_74/README.md
 
 package main
 
