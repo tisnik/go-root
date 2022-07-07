21fe0c85ca11cce337f73de6abd72c3d6f89d6c5
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-07 18:51:25 +0200

    Added link to list of examples

--- article_90/flow2/flow2.go
+++ article_90/flow2/flow2.go
@@ -1,5 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //    https://www.root.cz/serialy/programovaci-jazyk-go/
+//
+// Seznam příkladů z devadesáté části:
+//    https://github.com/tisnik/go-root/blob/master/article_90/README.md
 
 package main
 
