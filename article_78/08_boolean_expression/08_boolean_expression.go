d4dd8ad52e30d6be5f49aa893fce05db6033166b
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-07 18:47:00 +0200

    Added link to list of examples

--- article_78/08_boolean_expression/08_boolean_expression.go
+++ article_78/08_boolean_expression/08_boolean_expression.go
@@ -4,6 +4,9 @@
 // Sedmdesátá osmá část:
 //    Jazyk Go a vyhodnocování výrazů v době běhu aplikace
 //    https://www.root.cz/clanky/jazyk-go-a-vyhodnocovani-vyrazu-v-dobe-behu-aplikace/
+//
+// Seznam příkladů ze sedmdesáté osmé části:
+//    https://github.com/tisnik/go-root/blob/master/article_78/README.md
 
 package main
 
