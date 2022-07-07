b2779a697bba4687d8285b239ea797a3daf19803
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-07 18:42:15 +0200

    Added link to list of examples

--- article_77/asciigraph/06-slope.go
+++ article_77/asciigraph/06-slope.go
@@ -4,6 +4,9 @@
 // Sedmdesátá sedmá část:
 //    Jazyk Go a textový terminál ve funkci základního prvku uživatelského rozhraní (2.část)
 //    https://www.root.cz/clanky/jazyk-go-a-textovy-terminal-ve-funkci-zakladniho-prvku-uzivatelskeho-rozhrani-2-cast/
+//
+// Seznam příkladů ze sedmdesáté sedmé části:
+//    https://github.com/tisnik/go-root/blob/master/article_77/README.md
 
 package main
 
