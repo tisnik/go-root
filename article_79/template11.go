339bb23d2ed5974f98e04f815e92fe9214419bcc
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-07 18:48:54 +0200

    Added link to list of examples

--- article_79/template11.go
+++ article_79/template11.go
@@ -5,6 +5,9 @@
 //    Standardní šablonovací systém jazyka Go
 //    https://www.root.cz/clanky/standardni-sablonovaci-system-jazyka-go/
 //
+// Seznam příkladů ze sedmdesáté deváté části:
+//    https://github.com/tisnik/go-root/blob/master/article_79/README.md
+//
 // Dokumentace ve stylu "literate programming":
 //    https://tisnik.github.io/go-root/article_79/template11.html
 
