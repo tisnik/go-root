2a3af66a903a466e39f63b5776fb70b3351de1ac
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-20 11:04:54 +0200

    Link to serie about Go language

--- article_89/23_pow_generic.go
+++ article_89/23_pow_generic.go
@@ -1,5 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //    https://www.root.cz/serialy/programovaci-jazyk-go/
+//
+// Seznam příkladů z osmdesáté deváté části:
+//    https://github.com/tisnik/go-root/blob/master/article_89/README.md
 
 package main
 
