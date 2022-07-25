83bb0b90af65d8e20440d34de967892c8c992377
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-25 10:11:58 +0200

    Added link to README.md with list of articles

--- article_82/lexer4.go
+++ article_82/lexer4.go
@@ -1,5 +1,8 @@
 // Seriál "Programovací jazyk Go"
 //    https://www.root.cz/serialy/programovaci-jazyk-go/
+//
+// Seznam příkladů z osmdesáté druhé části:
+//    https://github.com/tisnik/go-root/blob/master/article_82/README.md
 
 package main
 
