3d77d1adda233762680a0c32c310dd38ae8fe6c2
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-25 10:11:38 +0200

    Generics and type checks in compilation time

---
+++ article_94/hashmap01error.go
@@ -0,0 +1,53 @@
+// Seriál "Programovací jazyk Go"
+//    https://www.root.cz/serialy/programovaci-jazyk-go/
+//
+// Seznam příkladů ze devadesáté čtvrté části:
+//    https://github.com/tisnik/go-root/blob/master/article_94/README.md
+
+package main
+
+import (
+	"fmt"
+
+	mapImpl "github.com/daichi-m/go18ds/maps/hashmap"
+)
+
+func main() {
+	cz := []string{
+		"Leden",
+		"Únor",
+		"Březen",
+		"Duben",
+		"Květen",
+		"Červen",
+		"Červenec",
+		"Srpen",
+		"Září",
+		"Říjen",
+		"Listopad",
+		"Prosinec",
+	}
+
+	lat := []string{
+		"Ianuarius",
+		"Februarius",
+		"Martius",
+		"Aprilis",
+		"Maius",
+		"Iunius",
+		"Iulius",
+		"Augustus",
+		"September",
+		"October",
+		"November",
+		"December",
+	}
+
+	m := mapImpl.New[string, int]()
+	fmt.Println(m)
+
+	for i := 0; i < len(cz); i++ {
+		m.Put(cz[i], lat[i])
+		fmt.Println(m)
+	}
+}
