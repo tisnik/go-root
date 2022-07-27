7a6038df4cfd271d109aa2f3574630e4ec734ca1
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-27 07:50:46 +0200

    Link to article

--- article_93/avl-tree02.go
+++ article_93/avl-tree02.go
@@ -1,6 +1,10 @@
 // Seriál "Programovací jazyk Go"
 //    https://www.root.cz/serialy/programovaci-jazyk-go/
 //
+// Devadesátá třetí část
+//    Knihovny s implementací generických datových typů pro programovací jazyk Go (2)
+//    https://www.root.cz/clanky/knihovny-s-implementaci-generickych-datovych-typu-pro-programovaci-jazyk-go-2/
+//
 // Seznam příkladů ze devadesáté třetí části:
 //    https://github.com/tisnik/go-root/blob/master/article_93/README.md
 
