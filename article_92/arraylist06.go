2d4c41911ba19cf8cd562ee52150ef9db7933bcf
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-09-09 08:31:34 +0200

    Added link to article where this example is used

--- article_92/arraylist06.go
+++ article_92/arraylist06.go
@@ -1,6 +1,10 @@
 // Seriál "Programovací jazyk Go"
 //    https://www.root.cz/serialy/programovaci-jazyk-go/
 //
+// Devadesátá druhá část
+//    Knihovny s implementací generických datových typů pro programovací jazyk Go (2)
+//    https://www.root.cz/clanky/knihovny-s-implementaci-generickych-datovych-typu-pro-programovaci-jazyk-go-2/
+//
 // Seznam příkladů z devadesáté druhé části:
 //    https://github.com/tisnik/go-root/blob/master/article_92/README.md
 
