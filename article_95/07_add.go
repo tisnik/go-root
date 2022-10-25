836a95633a8124449d1e4dae374a9709fb43ea12
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-10-25 10:34:16 +0200

    Add method, first usage with two input arrays

---
+++ article_95/07_add.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	narray "github.com/akualab/narray/na64"
+)
+
+func main() {
+	vector1 := narray.NewArray([]float64{1, 2, 3, 4, 5}, 5)
+	vector2 := narray.NewArray([]float64{1, 2, 3, 4, 5}, 5)
+
+	vector3 := narray.Add(nil, vector1, vector2)
+	fmt.Println(vector1)
+	fmt.Println(vector2)
+	fmt.Println(vector3)
+}
