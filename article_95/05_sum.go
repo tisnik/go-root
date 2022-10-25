ec9884cefc214d744516230ee47f43a14b00abe7
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-10-25 10:34:16 +0200

    Sum method

---
+++ article_95/05_sum.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	narray "github.com/akualab/narray/na64"
+)
+
+func main() {
+	vector1 := narray.NewArray([]float64{1, 2, 3, 4, 5}, 5)
+
+	sum := vector1.Sum()
+	fmt.Println(sum)
+}
