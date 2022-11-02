7f86c5f11e9402375122c37bf31120605cd864a3
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-11-02 07:47:39 +0100

    Deque rotate operation, example #1

---
+++ article_96/11_rotate.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gammazero/deque"
+)
+
+func main() {
+	var q deque.Deque[int]
+
+	for i := 1; i <= 10; i++ {
+		q.PushBack(100 + i)
+	}
+
+	q.Rotate(3)
+
+	for q.Len() > 0 {
+		fmt.Println(q.PopFront())
+	}
+}
