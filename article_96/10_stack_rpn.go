759a80ae85a81753f796797725854dae756b6a06
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-11-02 07:47:39 +0100

    RPN expression evaluator

---
+++ article_96/10_stack_rpn.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+
+	"strconv"
+	"strings"
+
+	"github.com/gammazero/deque"
+)
+
+func printStack(s *deque.Deque[int]) {
+	for i := 0; i < s.Len(); i++ {
+		fmt.Println(s.At(i))
+	}
+	fmt.Println()
+}
+
+func main() {
+	expression := "1 2 + 2 3 * 8 + *"
+	terms := strings.Split(expression, " ")
+	var stack deque.Deque[int]
+
+	for _, term := range terms {
+		switch term {
+		case "+":
+			operand1 := stack.PopFront()
+			operand2 := stack.PopFront()
+			stack.PushFront(operand1 + operand2)
+			print("+ :\t")
+			printStack(&stack)
+		case "-":
+			operand1 := stack.PopFront()
+			operand2 := stack.PopFront()
+			stack.PushFront(operand2 - operand1)
+			print("- :\t")
+			printStack(&stack)
+		case "*":
+			operand1 := stack.PopFront()
+			operand2 := stack.PopFront()
+			stack.PushFront(operand1 * operand2)
+			print("* :\t")
+			printStack(&stack)
+		case "/":
+			operand1 := stack.PopFront()
+			operand2 := stack.PopFront()
+			stack.PushFront(operand2 / operand1)
+			print("/ :\t")
+			printStack(&stack)
+		default:
+			number, err := strconv.Atoi(term)
+			if err == nil {
+				stack.PushFront(number)
+			}
+			fmt.Printf("%-2d:\t", number)
+			printStack(&stack)
+		}
+	}
+	print("Result: ")
+	printStack(&stack)
+}
