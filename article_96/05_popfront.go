44edce5f95089128dcd3d86edf8cfef859a259b1
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-11-02 07:47:39 +0100

    PopFront operation with proper check for empty deque

---
+++ article_96/05_popfront.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gammazero/deque"
+)
+
+func main() {
+	var q deque.Deque[int]
+
+	for i := 1; i <= 10; i++ {
+		q.PushBack(100 + i)
+		fmt.Println("Deque length:  ", q.Len())
+		fmt.Println("Deque capacity:", q.Cap())
+		fmt.Println("Deque value:   ", q)
+		fmt.Println()
+	}
+
+	fmt.Println()
+
+	for q.Len() > 0 {
+		fmt.Println("Pop value:     ", q.PopFront())
+		fmt.Println("Deque length:  ", q.Len())
+		fmt.Println("Deque capacity:", q.Cap())
+		fmt.Println("Deque value:   ", q)
+		fmt.Println()
+	}
+}
