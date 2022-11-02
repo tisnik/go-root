4eae962a058ca941f43d9c6cdcd51fed8680d825
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-11-02 07:47:39 +0100

    Deque usage as stack, example #1

---
+++ article_96/08_as_stack_1.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gammazero/deque"
+)
+
+func main() {
+	var q deque.Deque[int]
+
+	for i := 1; i <= 10; i++ {
+		q.PushFront(100 + i)
+	}
+
+	for q.Len() > 0 {
+		fmt.Println(q.PopFront())
+	}
+}
