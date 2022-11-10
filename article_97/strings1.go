1bf61aa8ce8b535f48bbe474c32bd73bbf229bdc
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-11-10 09:26:05 +0100

    Strings handling transpiled from C to Go

---
+++ article_97/strings1.go
@@ -0,0 +1,20 @@
+//
+//	Package - transpiled by c4go
+//
+//	If you have found any issues, please raise an issue at:
+//	https://github.com/Konstantin8105/c4go/
+//
+
+package main
+
+import "github.com/Konstantin8105/c4go/noarch"
+
+// main - transpiled function from  /root/strings.c:5
+func main() {
+	defer noarch.AtexitRun()
+	var s []byte = make([]byte, 100*uint32(1))
+	noarch.Strcpy(s, []byte("Hello \x00"))
+	noarch.Strcat(s, []byte("world!\x00"))
+	noarch.Puts(s)
+	_ = s
+}
