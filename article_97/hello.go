be599326081bcac176f7ff0357889edf172e878d
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-11-11 17:22:05 +0100

    Hello world program transpiled into Go

---
+++ article_97/hello.go
@@ -0,0 +1,16 @@
+//
+//      Package - transpiled by c4go
+//
+//      If you have found any issues, please raise an issue at:
+//      https://github.com/Konstantin8105/c4go/
+//
+
+package main
+
+import "github.com/Konstantin8105/c4go/noarch"
+
+// main - transpiled function from  /root/c4go-test/hello.c:3
+func main() {
+        noarch.Puts([]byte("Hello world!\x00"))
+        return
+}
