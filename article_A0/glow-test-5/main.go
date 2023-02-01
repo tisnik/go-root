89652f4bb891a80bba9cc99321d35343b5546942
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-02-01 07:39:11 +0100

    Glow library: example #5

---
+++ article_A0/glow-test-5/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+
+	_ "github.com/chrislusf/glow/driver"
+	"github.com/chrislusf/glow/flow"
+)
+
+func main() {
+	flag.Parse()
+
+	flow.
+		New().
+		Slice([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}).
+		Partition(3).
+		Map(func(value int) int {
+			return value * 2
+		}).
+		Reduce(func(x int, y int) int {
+			return x + y
+		}).
+		Map(func(x int) {
+			println("sum:", x)
+		}).
+		Run()
+}
