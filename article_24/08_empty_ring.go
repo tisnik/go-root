137a1c250cda1675bf8095789208488b9f5e0a77
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-03-16 09:03:38 +0100

    Added link to repository

--- article_24/08_empty_ring.go
+++ article_24/08_empty_ring.go
@@ -5,7 +5,10 @@
 //     Kontejnery v základní knihovně programovacího jazyka Go
 //     https://www.root.cz/clanky/kontejnery-v-zakladni-knihovne-programovaciho-jazyka-go/
 //
-// Seznam příkladů z dvacáté čtvrté části:
+// Repositář:
+//    https://github.com/tisnik/go-root/
+//
+// Seznam demonstračních příkladů z dvacáté čtvrté části:
 //    https://github.com/tisnik/go-root/blob/master/article_24/README.md
 //
 // Demonstrační příklad číslo 8:
