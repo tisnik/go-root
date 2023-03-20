58d449a563a8a458efde4972ac5e002b72dcb5ad
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-03-20 07:35:45 +0100

    Deferred call to close file with check if operation was successful

--- article_A6/write-npy1/write-npy1.go
+++ article_A6/write-npy1/write-npy1.go
@@ -12,16 +12,16 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer f.Close()
+	defer func() {
+		err := f.Close()
+		if err != nil {
+			log.Fatalf("error closing file: %v\n", err)
+		}
+	}()
 
 	m := []int8{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 	err = npyio.Write(f, m)
 	if err != nil {
 		log.Fatalf("error writing to file: %v\n", err)
-	}
-
-	err = f.Close()
-	if err != nil {
-		log.Fatalf("error closing file: %v\n", err)
 	}
 }
