a568626fb64ba9e9c02e8e64d947e882c020590f
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-04-13 08:11:53 +0200

    Reformatted

--- article_57/11_xml_marshal_array_of_struct_1.go
+++ article_57/11_xml_marshal_array_of_struct_1.go
@@ -24,15 +24,15 @@ type User struct {
 
 func main() {
 	var users = [3]User{
-		User{
+		{
 			Id:      1,
 			Name:    "Pepek",
 			Surname: "Vyskoč"},
-		User{
+		{
 			Id:      2,
 			Name:    "Pepek",
 			Surname: "Vyskoč"},
-		User{
+		{
 			Id:      3,
 			Name:    "Josef",
 			Surname: "Vyskočil"},
