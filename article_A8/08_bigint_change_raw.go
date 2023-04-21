33e0d2ad4cba663ce04598a2b2542483372c0916
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-04-21 14:30:44 +0200

    Changing bigint on lower level

---
+++ article_A8/08_bigint_change_raw.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func main() {
+	var x big.Int
+	x.SetInt64(0)
+
+	fmt.Println(x.Bytes())
+	fmt.Println(x.Text(10))
+	fmt.Println()
+
+	x.SetBytes([]byte{})
+	fmt.Println(x.Bytes())
+	fmt.Println(x.Text(10))
+	fmt.Println()
+
+	x.SetBytes([]byte{1, 0})
+	fmt.Println(x.Bytes())
+	fmt.Println(x.Text(10))
+	fmt.Println()
+
+	x.SetBytes([]byte{1, 0, 0})
+	fmt.Println(x.Bytes())
+	fmt.Println(x.Text(10))
+	fmt.Println()
+
+	x.SetBytes([]byte{1, 0, 0, 0})
+	fmt.Println(x.Bytes())
+	fmt.Println(x.Text(10))
+	fmt.Println()
+
+	x.SetBytes([]byte{1, 0, 0, 0, 0})
+	fmt.Println(x.Bytes())
+	fmt.Println(x.Text(10))
+	fmt.Println()
+
+	x.SetBytes([]byte{1, 0, 0, 0, 0, 0})
+	fmt.Println(x.Bytes())
+	fmt.Println(x.Text(10))
+	fmt.Println()
+
+	x.SetBytes([]byte{1, 0, 0, 0, 0, 0, 0})
+	fmt.Println(x.Bytes())
+	fmt.Println(x.Text(10))
+	fmt.Println()
+
+	x.SetBytes([]byte{1, 0, 0, 0, 0, 0, 0, 0})
+	fmt.Println(x.Bytes())
+	fmt.Println(x.Text(10))
+	fmt.Println()
+}
