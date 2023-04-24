df7f8092c925531e3a9e08b7476cdc3d2ffbafff
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-04-24 08:05:53 +0200

    More values to be identified

--- article_A8/06_bigint_as_bytes.go
+++ article_A8/06_bigint_as_bytes.go
@@ -7,7 +7,23 @@ import (
 
 func main() {
 	var x big.Int
-	x.SetInt64(100000000)
 
+	x.SetInt64(0)
+	fmt.Println(x.Bytes())
+
+	x.SetInt64(1)
+	fmt.Println(x.Bytes())
+
+	x.SetInt64(100)
+	fmt.Println(x.Bytes())
+
+	x.SetInt64(1000)
+	fmt.Println(x.Bytes())
+
+	x.SetInt64(100000000)
+	fmt.Println(x.Bytes())
+
+	x.SetInt64(100000000)
+	x.Mul(&x, &x)
 	fmt.Println(x.Bytes())
 }
