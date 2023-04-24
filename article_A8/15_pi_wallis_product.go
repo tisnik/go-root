cb440bff31697da399c49178371c6a54f1787d01
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-04-24 08:05:54 +0200

    Wallis product (rationals)

---
+++ article_A8/15_pi_wallis_product.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/big"
+)
+
+func main() {
+	var result big.Rat
+
+	result.SetFrac64(2, 1)
+
+	for n := int64(1); n < 40; n++ {
+		m := 4 * n * n
+
+		var item big.Rat
+		item.SetFrac64(m, m-1)
+		result.Mul(&result, &item)
+
+		f, _ := result.Float64()
+		absError := math.Pi - f
+		relError := 100.0 * absError / math.Pi
+		fmt.Println(f, "\t", absError, "\t", relError, "\t", result.String())
+	}
+}
