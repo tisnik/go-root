cda64bbacd97c35a56b53d34b6f20abff0f246e9
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-04-24 08:05:53 +0200

    More values to be changed

--- article_A8/07_bigint_change_raw.go
+++ article_A8/07_bigint_change_raw.go
@@ -10,18 +10,28 @@ func main() {
 	x.SetInt64(100000000000)
 	x.Mul(&x, &x)
 
+	fmt.Println(x.Text(10))
 	fmt.Println(x.Bits())
+	fmt.Println()
 
 	words := x.Bits()
 	words[0] = 1
 	words[1] = 0
 
-	fmt.Println(x.Bits())
 	fmt.Println(x.Text(10))
+	fmt.Println(x.Bits())
+	fmt.Println()
 
 	words[0] = 0
 	words[1] = 1
 
-	fmt.Println(x.Bits())
 	fmt.Println(x.Text(10))
+	fmt.Println(x.Bits())
+	fmt.Println()
+
+	words[0] = 1000
+	words[1] = 1000
+
+	fmt.Println(x.Text(10))
+	fmt.Println(x.Bits())
 }
