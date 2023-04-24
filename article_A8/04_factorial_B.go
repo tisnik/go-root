5fc99092c6688278679bdeddc427ea4a4f0f9bd8
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-04-24 08:05:53 +0200

    Factorial computation based on MulRange

---
+++ article_A8/04_factorial_B.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/big"
+)
+
+func factorial(n *big.Int) *big.Int {
+	one := big.NewInt(1)
+	if n.Cmp(big.NewInt(0)) <= 0 {
+		return one
+	}
+	return one.Mul(n, factorial(one.Sub(n, one)))
+}
+
+func main() {
+	for n := int64(1); n < 31; n++ {
+		f1 := factorial(big.NewInt(n))
+
+		var f2 big.Int
+		f2.MulRange(1, n)
+
+		fmt.Printf("%3d! = %s = %s\n", n, f1.Text(10), f2.Text(10))
+
+		if f1.Cmp(&f2) != 0 {
+			log.Panic("Different results detected!")
+		}
+	}
+}
