ae62a506feada273106482e7cde16fa50e9f70a8
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-04-24 08:05:54 +0200

    Converting rationals to integers

---
+++ article_A8/13_rational_to_int.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func main() {
+	var x big.Rat
+	var y big.Rat
+	var z big.Rat
+
+	x.SetFrac64(1, 2)
+	y.SetFrac64(1, 1)
+	z.SetFrac64(1, 1)
+
+	for i := 0; i < 10; i++ {
+		z.Mul(&z, &x)
+		z.Add(&z, &y)
+
+		numerator := z.Num()
+		denominator := z.Num()
+		fmt.Println(numerator, denominator)
+	}
+
+}
