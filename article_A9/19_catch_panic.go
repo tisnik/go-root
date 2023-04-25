33e8732c8ef5547b3cd789052ced3009718973ed
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-04-25 16:43:00 +0200

    Catch panic

---
+++ article_A9/19_catch_panic.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/big"
+)
+
+func main() {
+	x := big.NewFloat(1.0)
+	y := big.NewFloat(-1.0)
+	z := big.NewFloat(0.0)
+
+	var positiveInfinity big.Float
+	positiveInfinity.Quo(x, z)
+	fmt.Println(positiveInfinity.Text('f', 31))
+
+	var negativeInfinity big.Float
+	negativeInfinity.Quo(y, z)
+	fmt.Println(negativeInfinity.Text('f', 31))
+
+	defer func() {
+		if recover() != nil {
+			log.Fatal("Improper operation")
+		}
+	}()
+
+	var infMultiplyBy0 big.Float
+	infMultiplyBy0.Mul(&positiveInfinity, z)
+	fmt.Println(infMultiplyBy0.Text('f', 31))
+}
