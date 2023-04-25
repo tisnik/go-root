1f0b6bf3469c98fe9f4254cb5c9aac76755d118c
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-04-25 07:46:48 +0200

    Example #11: Bigfoat to float32 conversion

---
+++ article_A9/11_bigfloat_to_float32.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func main() {
+	x := big.NewFloat(2.0)
+	y := big.NewFloat(0.1)
+
+	for i := 0; i < 20; i++ {
+		value, accuracy := x.Float32()
+		fmt.Println(accuracy, value)
+
+		x.Mul(x, y)
+	}
+}
