e02a3e485d4850dc08e1d243de2d8fcaf66ca505
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-05-09 07:58:40 +0200

    Added link to repository

--- article_44/14_error_channel_test.go
+++ article_44/14_error_channel_test.go
@@ -5,7 +5,10 @@
 //    Použití Go pro automatizaci práce s aplikacemi s interaktivním příkazovým řádkem (dokončení)
 //    https://www.root.cz/clanky/pouziti-go-pro-automatizaci-prace-s-aplikacemi-s-interaktivnim-prikazovym-radkem-dokonceni/
 //
-// Seznam příkladů ze čtyřicáté čtvrté části:
+// Repositář:
+//    https://github.com/tisnik/go-root/
+//
+// Seznam demonstračních příkladů ze čtyřicáté čtvrté části:
 //    https://github.com/tisnik/go-root/blob/master/article_44/README.md
 
 package main
