ab399b27a423e2e875518fc171d7c713fa740298
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-05-11 07:34:06 +0200

    Fixed issue #454

--- article_15/06_gif_export_palette.go
+++ article_15/06_gif_export_palette.go
@@ -37,7 +37,7 @@ func CreateChessboard(width int, height int, boardSize int) *image.Paletted {
 
 	img := image.NewPaletted(image.Rect(0, 0, width, height), palette)
 
-	index_color := 0
+	indexColor := 0
 	hor_block := int(width / boardSize)
 	ver_block := int(height / boardSize)
 
@@ -48,14 +48,14 @@ func CreateChessboard(width int, height int, boardSize int) *image.Paletted {
 		y_to := ver_block
 		for y := 0; y < boardSize; y++ {
 			r := image.Rect(x_from, y_from, x_to, y_to)
-			draw.Draw(img, r, &image.Uniform{palette[index_color]}, image.ZP, draw.Src)
+			draw.Draw(img, r, &image.Uniform{palette[indexColor]}, image.ZP, draw.Src)
 			y_from = y_to
 			y_to += ver_block
-			index_color = 1 - index_color
+			indexColor = 1 - indexColor
 		}
 		x_from = x_to
 		x_to += hor_block
-		index_color = 1 - index_color
+		indexColor = 1 - indexColor
 	}
 	return img
 }
