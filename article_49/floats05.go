75aa4bcc7a6986a0bbeaf6e9405fa69473ef9253
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-06-05 07:52:43 +0200

    Added link to repository

--- article_49/floats05.go
+++ article_49/floats05.go
@@ -5,7 +5,10 @@
 //    Popis vybraných balíčků nabízených projektem Gonum
 //    https://www.root.cz/clanky/popis-vybranych-balicku-nabizenych-projektem-gonum/
 //
-// Seznam příkladů ze čtyřicáté deváté části:
+// Repositář:
+//    https://github.com/tisnik/go-root/
+//
+// Seznam demonstračních příkladů ze čtyřicáté deváté části:
 //    https://github.com/tisnik/go-root/blob/master/article_49/README.md
 //
 // Demonstrační příklad číslo 5:
