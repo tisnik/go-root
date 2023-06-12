389c313df3577b800c496ed9c5a5f1956787ea34
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-06-12 09:13:52 +0200

    Added link to repository

--- article_51/chart_js.go
+++ article_51/chart_js.go
@@ -5,7 +5,10 @@
 //    Tvorba grafů v jazyce Go: kreslení ve webovém klientu
 //    https://www.root.cz/clanky/tvorba-grafu-v-jazyce-go-kresleni-ve-webovem-klientu/
 //
-// Seznam příkladů z padesáté první části:
+// Repositář:
+//    https://github.com/tisnik/go-root/
+//
+// Seznam demonstračních příkladů z padesáté první části:
 //    https://github.com/tisnik/go-root/blob/master/article_51/README.md
 
 package main
