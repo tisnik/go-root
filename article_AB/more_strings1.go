4c1b4e6d2f1eacd22fb7eaaedf3707a90a7131cf
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-07-17 08:06:21 +0200

    Embed more text files, 1st variant

---
+++ article_AB/more_strings1.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"log"
+)
+
+//go:embed hello.txt
+//go:embed lorem_ipsum.txt
+var f embed.FS
+
+func main() {
+	data, err := f.ReadFile("lorem_ipsum.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// pouziti retezce
+	fmt.Println(string(data))
+}
