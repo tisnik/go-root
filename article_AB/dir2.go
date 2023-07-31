87d94ef1645665b084036e3167760882343ad7f4
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-07-31 08:54:12 +0200

    Virtual directory listing

---
+++ article_AB/dir2.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"log"
+)
+
+//go:embed a/*
+//go:embed b/*
+var f embed.FS
+
+func main() {
+	entries, err := f.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, entry := range entries {
+		fmt.Printf("%-25s  %s\n", entry.Name(), entry.Type())
+	}
+}
