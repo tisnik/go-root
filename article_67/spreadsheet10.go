2eed18919ce62223e38b773df18ffffc9338edb0
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-24 08:29:51 +0200

    Added link to repository

--- article_67/spreadsheet10.go
+++ article_67/spreadsheet10.go
@@ -5,7 +5,10 @@
 //    Tvorba sešitů pro tabulkové procesory v programovacím jazyku Go – formát xlsx
 //    https://www.root.cz/clanky/tvorba-sesitu-pro-tabulkove-procesory-v-programovacim-jazyku-go-format-xlsx/
 //
-// Seznam příkladů z šedesáté sedmé části:
+// Repositář:
+//    https://github.com/tisnik/go-root/
+//
+// Seznam demonstračních příkladů z šedesáté sedmé části:
 //    https://github.com/tisnik/go-root/blob/master/article_67/README.md
 //
 // Dokumentace ve stylu "literate programming":
