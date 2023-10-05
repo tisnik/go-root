f0dc724b3c8e4f6a94492d70c95740f41662f5fc
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-05 08:34:17 +0200

    Added link to repository

--- article_76/aurora/06-background-colors/main.go
+++ article_76/aurora/06-background-colors/main.go
@@ -5,6 +5,9 @@
 //    Jazyk Go a textový terminál ve funkci základního prvku uživatelského rozhraní
 //    https://www.root.cz/clanky/jazyk-go-a-textovy-terminal-ve-funkci-zakladniho-prvku-uzivatelskeho-rozhrani/
 //
+// Repositář:
+//    https://github.com/tisnik/go-root/
+//
 // Seznam příkladů ze sedmdesáté šesté části:
 //    https://github.com/tisnik/go-root/blob/master/article_76/README.md
 
