9c908d61ed049c4440b86bee70eb9135e68a3939
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-11-20 16:04:41 +0100

    Consumers PoC: 2nd version

--- consumers/consumers.go
+++ consumers/consumers.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"context"
-	"log"
 
 	"github.com/Shopify/sarama"
+	"github.com/rs/zerolog/log"
 )
 
 // Consumer represents any consumer of insights-rules messages
@@ -16,20 +16,11 @@ type Consumer interface {
 
 type KafkaConsumer struct {
 	ConsumerGroup                        sarama.ConsumerGroup
+	topic                                string
 	numberOfSuccessfullyConsumedMessages uint64
 	numberOfErrorsConsumingMessages      uint64
 	ready                                chan bool
 	cancel                               context.CancelFunc
-}
-
-func (kafkaConsumer KafkaConsumer) start() error {
-	log.Print("Kafka Consumer start")
-	return nil
-}
-
-func (kafkaConsumer KafkaConsumer) stop() error {
-	log.Print("Kafka Consumer stop")
-	return nil
 }
 
 type OCPRulesConsumer struct {
@@ -40,28 +31,169 @@ type DVOConsumer struct {
 	KafkaConsumer
 }
 
-func NewOCPRulesConsumer() OCPRulesConsumer {
-	log.Print("New OCP rules consumer")
-	consumer := OCPRulesConsumer{}
-	return consumer
+func (consumer *KafkaConsumer) Serve() {
+	ctx, cancel := context.WithCancel(context.Background())
+	consumer.cancel = cancel
+
+	go func() {
+		for {
+			// `Consume` should be called inside an infinite loop, when a
+			// server-side rebalance happens, the consumer session will need to be
+			// recreated to get the new claims
+			if err := consumer.ConsumerGroup.Consume(ctx, []string{consumer.topic}, consumer); err != nil {
+				log.Fatal().Err(err).Msg("unable to recreate kafka session")
+			}
+
+			// check if context was cancelled, signaling that the consumer should stop
+			if ctx.Err() != nil {
+				return
+			}
+
+			log.Info().Msg("created new kafka session")
+
+			consumer.ready = make(chan bool)
+		}
+	}()
+
+	// Await till the consumer has been set up
+	log.Info().Msg("waiting for consumer to become ready")
+	<-consumer.ready
+	log.Info().Msg("finished waiting for consumer to become ready")
+
+	// Actual processing is done in goroutine created by sarama (see ConsumeClaim below)
+	log.Info().Msg("started serving consumer")
+	<-ctx.Done()
+	log.Info().Msg("context cancelled, exiting")
+
+	cancel()
 }
 
-func NewDVOConsumer() DVOConsumer {
-	log.Print("New DVO consumer")
-	consumer := DVOConsumer{}
-	return consumer
+// Setup is run at the beginning of a new session, before ConsumeClaim
+func (consumer *KafkaConsumer) Setup(sarama.ConsumerGroupSession) error {
+	log.Info().Msg("new session has been setup")
+	// Mark the consumer as ready
+	close(consumer.ready)
+	return nil
+}
+
+// Cleanup is run at the end of a session, once all ConsumeClaim goroutines have exited
+func (consumer *KafkaConsumer) Cleanup(sarama.ConsumerGroupSession) error {
+	log.Info().Msg("new session has been finished")
+	return nil
+}
+
+// ConsumeClaim starts a consumer loop of ConsumerGroupClaim's Messages().
+func (consumer *KafkaConsumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	log.Info().
+		Int64("offset", claim.InitialOffset()).
+		Msg("starting messages loop")
+
+	for message := range claim.Messages() {
+		err := consumer.HandleMessage(message)
+		if err != nil {
+			// already handled in HandleMessage, just log
+			log.Error().Err(err).Msg("Problem while handling the message")
+		}
+		session.MarkMessage(message, "")
+	}
+
+	return nil
+}
+
+// Close method closes all resources used by consumer
+func (consumer *KafkaConsumer) Close() error {
+	if consumer.cancel != nil {
+		consumer.cancel()
+	}
+
+	if consumer.ConsumerGroup != nil {
+		if err := consumer.ConsumerGroup.Close(); err != nil {
+			log.Error().Err(err).Msg("unable to close consumer group")
+		}
+	}
+	return nil
+}
+
+// GetNumberOfSuccessfullyConsumedMessages returns number of consumed messages
+// since creating OCPRulesConsumer obj
+func (consumer *KafkaConsumer) GetNumberOfSuccessfullyConsumedMessages() uint64 {
+	return consumer.numberOfSuccessfullyConsumedMessages
+}
+
+// GetNumberOfErrorsConsumingMessages returns number of errors during consuming messages
+// since creating OCPRulesConsumer obj
+func (consumer *KafkaConsumer) GetNumberOfErrorsConsumingMessages() uint64 {
+	return consumer.numberOfErrorsConsumingMessages
+}
+
+func (consumer *KafkaConsumer) HandleMessage(msg *sarama.ConsumerMessage) error {
+	log.Info().Msg("KafkaConsumer: handle message")
+	return nil
+}
+
+func NewKafkaConsumer(address string, topic string, group string) (*KafkaConsumer, error) {
+	log.Print("New Kafka consumer")
+
+	consumerGroup, err := sarama.NewConsumerGroup([]string{address}, group, sarama.NewConfig())
+	if err != nil {
+		log.Error().Err(err).Msg("Unable to create consumer group")
+		return nil, err
+	}
+
+	consumer := &KafkaConsumer{
+		ConsumerGroup:                        consumerGroup,
+		topic:                                topic,
+		numberOfSuccessfullyConsumedMessages: 0,
+		numberOfErrorsConsumingMessages:      0,
+		ready:                                make(chan bool),
+	}
+	return consumer, nil
+}
+
+func NewOCPRulesConsumer(address string, topic string, group string) (*OCPRulesConsumer, error) {
+	log.Info().Msg("New OCP rules consumer")
+
+	kafkaConsumer, err := NewKafkaConsumer(address, topic, group)
+	if err != nil {
+		log.Error().Err(err).Msg("Unable to create Kafka consumer")
+		return nil, err
+	}
+
+	consumer := &OCPRulesConsumer{
+		KafkaConsumer: *kafkaConsumer,
+	}
+	return consumer, nil
+}
+
+func NewDVOConsumer(address string, topic string, group string) (*DVOConsumer, error) {
+	log.Info().Msg("New DVO consumer")
+
+	kafkaConsumer, err := NewKafkaConsumer(address, topic, group)
+	if err != nil {
+		log.Error().Err(err).Msg("Unable to create Kafka consumer")
+		return nil, err
+	}
+
+	consumer := &DVOConsumer{
+		KafkaConsumer: *kafkaConsumer,
+	}
+	return consumer, nil
+}
+
+func (consumer *OCPRulesConsumer) HandleMessage(msg *sarama.ConsumerMessage) error {
+	log.Info().Msg("OCPRulesConsumer: handle message")
+	return nil
 }
 
 func main() {
-	log.Println("Starting")
+	log.Info().Msg("Starting")
 
-	consumer1 := NewOCPRulesConsumer()
-	consumer1.start()
+	consumer1, _ := NewOCPRulesConsumer("localhost:9092", "topic1", "group1")
+	println(consumer1)
+	consumer1.Serve()
 
-	consumer2 := NewDVOConsumer()
-	consumer2.start()
+	consumer2, _ := NewDVOConsumer("localhost:9092", "topic2", "group2")
+	println(consumer2)
 
-	consumer1.stop()
-	consumer2.stop()
-	log.Println("Finishing")
+	log.Info().Msg("Finishing")
 }
--- consumers/go.mod
+++ consumers/go.mod
@@ -13,10 +13,14 @@ require (
 	github.com/hashicorp/go-uuid v1.0.2 // indirect
 	github.com/jcmturner/gofork v1.0.0 // indirect
 	github.com/klauspost/compress v1.11.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/pierrec/lz4 v2.5.2+incompatible // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20200313005456-10cdbea86bc0 // indirect
+	github.com/rs/zerolog v1.31.0 // indirect
 	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
 	gopkg.in/jcmturner/aescts.v1 v1.0.1 // indirect
 	gopkg.in/jcmturner/dnsutils.v1 v1.0.1 // indirect
 	gopkg.in/jcmturner/gokrb5.v7 v7.5.0 // indirect
