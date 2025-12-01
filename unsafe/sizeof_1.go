d01ee6649bb0ea22f11e0bccc32d56e11990971f
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-12-01 08:35:18 +0100

    Function unsafe.Sizeof(), example #1

---
+++ unsafe/sizeof_1.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	fmt.Printf("sizeof int8       = %d byte(s)\n", unsafe.Sizeof(int8(0)))
+	fmt.Printf("sizeof int16      = %d byte(s)\n", unsafe.Sizeof(int16(0)))
+	fmt.Printf("sizeof int32      = %d byte(s)\n", unsafe.Sizeof(int32(0)))
+	fmt.Printf("sizeof int64      = %d byte(s)\n", unsafe.Sizeof(int64(0)))
+	fmt.Printf("sizeof int        = %d byte(s)\n", unsafe.Sizeof(int(0)))
+	fmt.Printf("sizeof float32    = %d byte(s)\n", unsafe.Sizeof(float32(0)))
+	fmt.Printf("sizeof float64    = %d byte(s)\n", unsafe.Sizeof(float64(0)))
+	fmt.Printf("sizeof complex64  = %d byte(s)\n", unsafe.Sizeof(complex64(0)))
+	fmt.Printf("sizeof complex128 = %d byte(s)\n", unsafe.Sizeof(complex128(0)))
+	fmt.Printf("sizeof uintptr    = %d byte(s)\n", unsafe.Sizeof(uintptr(0)))
+}
