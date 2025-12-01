0c03cf1752d49e7a829e9432bed4bff42cb01a17
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-12-01 08:35:18 +0100

    Function unsafe.Alignof(), example #4

---
+++ unsafe/alignof_4.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+type Struct1 struct {
+	i int32
+	x byte
+}
+
+type Struct2 struct {
+	i int32
+	x byte
+	y byte
+}
+
+type Struct3 struct {
+	i int32
+	x byte
+	y byte
+	z byte
+}
+
+type Struct4 struct {
+	i int32
+	x byte
+	y byte
+	z byte
+	w byte
+}
+
+func main() {
+	fmt.Printf("alignof Struct1   = %d byte(s)\n", unsafe.Alignof(Struct1{}))
+	fmt.Printf("alignof Struct2   = %d byte(s)\n", unsafe.Alignof(Struct2{}))
+	fmt.Printf("alignof Struct3   = %d byte(s)\n", unsafe.Alignof(Struct3{}))
+	fmt.Printf("alignof Struct4   = %d byte(s)\n", unsafe.Alignof(Struct4{}))
+}
