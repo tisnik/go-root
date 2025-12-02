a99ffc42d8ca015cf3a4a9c529ea57f59a7a40f5
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-12-02 08:33:29 +0100

    Link to series of articles

--- article_96/06_as_queue_1.go
+++ article_96/06_as_queue_1.go
@@ -1,3 +1,9 @@
+// Seriál "Programovací jazyk Go"
+//    https://www.root.cz/serialy/programovaci-jazyk-go/
+//
+// Seznam příkladů z devadesáté šesté části:
+//    https://github.com/tisnik/go-root/blob/master/article_96/README.md
+
 package main
 
 import (
