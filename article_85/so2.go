6eaf2b53242d6355c90a6d3fa9edd5781272eb3d
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-12-03 11:06:16 +0100

    Link to series of articles

--- article_85/so2.go
+++ article_85/so2.go
@@ -1,3 +1,12 @@
+// Seriál "Programovací jazyk Go"
+//    https://www.root.cz/serialy/programovaci-jazyk-go/
+//
+// Repositář:
+//    https://github.com/tisnik/go-root/
+//
+// Seznam příkladů z osmdesáté páté části:
+//    https://github.com/tisnik/go-root/blob/master/article_85/README.md
+
 package main
 
 import "C"
