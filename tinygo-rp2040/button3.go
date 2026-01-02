6463c7990150bd1bd8cd2e106528a5cd8800b702
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-02 10:00:30 +0100

    Button press detection, direct light LED

---
+++ tinygo-rp2040/button3.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"machine"
+)
+
+const InputPin = machine.GP0
+const OutputPin = machine.GP2
+
+func main() {
+	led := machine.Pin(OutputPin)
+	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	button := machine.Pin(InputPin)
+	button.Configure(machine.PinConfig{Mode: machine.PinInput})
+
+	for {
+		pressed := button.Get()
+		led.Set(pressed)
+	}
+}
