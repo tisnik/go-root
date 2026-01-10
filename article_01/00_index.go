4020128ce173436368b8593dc694c51b326e65dd
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-10 09:18:43 +0100

    Index file

---
+++ article_01/00_index.go
@@ -0,0 +1,50 @@
+// Seznam demonstračních příkladů uložených v tomto adresáři:
+//
+// 01_hello_world.go:
+//    Program typu "Hello world!"
+//
+// 02_better_hello_world.go:
+//    Vylepšený program typu "Hello world!"
+//
+// 03_unused_imports.go:
+//    Nepoužitý importovaný modul "fmt"
+//
+// 04_hello_unicode.go:
+//    Použití Unicode ve zdrojovém kódu
+//
+// 05_basic_type_inference.go:
+//    Deklarace proměnné s její inicializací
+//
+// 06_variable_redeclaration.go:
+//    Pokus o opětovnou deklaraci proměnných
+//
+// 07_variable_reassign.go:
+//    Změna hodnoty již existující proměnné
+//
+// 08_no_true_dynamic_type.go:
+//    Špatný typ hodnoty přiřazený do proměnných
+//
+// 09_simple_function.go:
+//    Deklarace uživatelské funkce
+//
+// 10_function_with_params.go:
+//    Deklarace uživatelské funkce s jedním parametrem
+//
+// 11_return_statement.go:
+//    Deklarace uživatelské funkce s návratovou hodnotou
+//
+// 12_named_return_variable.go:
+//    Použití pojmenovaného návratového parametru
+//
+// 13_sum_function.go:
+//    Funkce se dvěma parametry
+//
+// 14_swap_function.go:
+//    Funkce se dvěma parametry a dvěma návratovými hodnotami
+//
+// 15_swap_function_B.go:
+//    Funkce se dvěma parametry a dvěma návratovými hodnotami
+//
+// 16_return_at_the_end_of_function.go:
+//    Chybný pokus o deklaraci proměnné za příkazem return
+//
