e4fede18637accfd399e2e35e5ad6527b26d95df
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-13 09:09:33 +0100

    Index file for examples for article #3

---
+++ article_03/00_index.go
@@ -0,0 +1,93 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_user_types.go:
+//    Uživatelsky definované typy.
+//
+// 02_type_func_params.go:
+//    Kontrola parametrů funkcí.
+//
+// 03_typed_array.go:
+//    Pole s prvky definovaného typu.
+//
+// 04_typed_array_B.go:
+//    Pole s prvky definovaného typu.
+//
+// 05_user_type_checks.go:
+//    Kontrola uživatelsky definovaných typů.
+//
+// 06_type_check_func.go:
+//    Kontrola parametrů funkcí.
+//
+// 07_typed_array_check.go:
+//    Pole s prvky definovaného typu + kontrola typů.
+//
+// 08_typed_array_check.go:
+//    Pole s prvky definovaného typu + kontrola typů.
+//
+// 09_struct.go:
+//    Uživatelsky definovaná struktura.
+//
+// 10_struct_init.go:
+//    Uživatelsky definovaná struktura a její inicializace.
+//
+// 11_better_struct_init.go:
+//    Uživatelsky definovaná struktura a její inicializace.
+//
+// 12_struct_comparison.go:
+//    Porovnání struktur.
+//
+// 13_array_of_structs.go:
+//    Pole struktur.
+//
+// 14_array_of_structs.go:
+//    Pole struktur.
+//
+// 15_uninitialized_map.go:
+//    Mapa bez inicializace.
+//
+// 16_initialized_map.go:
+//    Mapa s inicializací.
+//
+// 17_initialized_map.go:
+//    Mapa s inicializací.
+//
+// 18_map_and_struct.go:
+//    Mapa struktur.
+//
+// 19_map_and_struct_B.go:
+//    Mapa struktur.
+//
+// 20_reading_from_maps.go:
+//    Čtení hodnot z mapy.
+//
+// 21_delete_from_map.go:
+//    Odstranění položek z mapy.
+//
+// 22_basic_pointers.go:
+//    Základní práce s ukazateli.
+//
+// 23_pointer_to_struct.go:
+//    Ukazatel na strukturu.
+//
+// 24_pointer_to_struct_item.go:
+//    Ukazatel na položku struktury.
+//
+// 25_pointer_to_array_item.go:
+//    Ukazatel na prvek pole.
+//
+// 26_func_type.go:
+//    Datový typ "funkce"
+//
+// 27_func_type_2.go:
+//    Datový typ "funkce s parametry"
+//
+// 28_improper_func_type.go:
+//    Kontroly při překladu.
+//
+// 29_func_type_3.go:
+//    Datový typ "funkce s návratovou hodnotou"
+//
+// 30_func_type_alt_block.go:
+//    Datový typ "funkce s návratovou hodnotou"
+//
