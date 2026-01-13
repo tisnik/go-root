ffb12fd5d403cc1a96d18679e3b992438e9272a2
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-13 09:09:33 +0100

    Index file for examples for article #4

---
+++ article_04/00_index.go
@@ -0,0 +1,69 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_B_unique_names.go:
+//    Unikátní jména metod.
+//
+// 01_methods.go:
+//    Definice a zavolání metody navázané na strukturu Line.
+//
+// 02_methods_with_parameters.go:
+//    Metoda bez parametrů a metoda s parametry.
+//
+// 03_methods_ptr.go:
+//    Metody s parametry, předání struktury odkazem.
+//
+// 04_methods_ptr_better_solution.go:
+//    Metody s parametry, předání struktury odkazem, lepší řešení.
+//
+// 05_interface.go:
+//    Definice trojice jednoduchých rozhraní.
+//
+// 06_interface_implementation.go:
+//    Implementace jednoduchého rozhraní nazvaného OpenShape.
+//
+// 07_B_wrong_return_type.go:
+//    Implementace jednoduchého rozhraní nazvaného OpenShape metodou.
+//
+// 07_interface_implementation_with_method.go:
+//    Implementace jednoduchého rozhraní nazvaného OpenShape metodou.
+//
+// 08_more_implementations.go:
+//    Několikanásobná implementace rozhraní.
+//
+// 09_slice_of_interfaces.go:
+//    Řez s objekty implementující rozhraní.
+//
+// 10_slice_of_interfaces_as_param.go:
+//    Řez s objekty implementující rozhraní předané jako parametr.
+//
+// 11_multiple_interfaces.go:
+//    Typ implementující dvě rozhraní.
+//
+// 12_embedded_interface.go:
+//    Typ implementující dvě rozhraní.
+//
+// 13_simple_goroutine.go:
+//    Zavolání funkce v gorutině.
+//
+// 14_wait_for_goroutine.go:
+//    Zavolání funkce v gorutině, počkání na dokončení.
+//
+// 15_multiple_goroutines.go:
+//    Zavolání funkcí v gorutinách, počkání na dokončení.
+//
+// 16_goroutine_from_goroutine.go:
+//    Zavolání další gorutiny z jiné gorutiny.
+//
+// 17_channel.go:
+//    Kanál pro komunikaci a synchronizaci mezi gorutinami.
+//
+// 18_worker.go:
+//    Jednoduchá implementace workerů.
+//
+// 19_more_workers.go:
+//    Jednoduchá implementace workerů.
+//
+// 20_workers_deadlock.go:
+//    Deadlock.
+//
