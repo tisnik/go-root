db899df90791b2de296942e8441e9722dc3173ab
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-13 09:09:33 +0100

    Index file for examples for article #2

---
+++ article_02/00_index.go
@@ -0,0 +1,75 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_integer_signed_types.go:
+//    Celočíselné datové typy se znaménkem
+//
+// 02_integer_signed_types_checks.go:
+//    Kontrola celočíselných konstant
+//
+// 03_integer_unsigned_types.go:
+//    Celočíselné datové typy bez znaménka
+//
+// 04_integer_constants.go:
+//    Celočíselné konstanty reprezentované v různých číselných soustavách
+//
+// 05_improper_conversion.go:
+//    Přiřazení uint8 -> uint16
+//
+// 06_improper_conversion.go:
+//    Přiřazení uint16 -> uint8
+//
+// 07_improper_conversion_int_uint.go:
+//    Přiřazení uint8 -> int8 a naopak
+//
+// 08_explicit_conversions.go:
+//    Konverze mezi základními datovými typy
+//
+// 09_formatting_output.go:
+//    Základní formátování celočíselných hodnot na výstupu
+//
+// 10_formatting_output.go:
+//    Základní formátování celočíselných hodnot na výstupu
+//
+// 11_fp_types.go:
+//    Datové typy reprezentující hodnoty s plovoucí řádovou čárkou
+//
+// 12_fp_types_checks.go:
+//    Datové typy reprezentující hodnoty s plovoucí řádovou čárkou, kontrola rozsahu
+//
+// 13_complex_types.go:
+//    Datové typy reprezentující hodnoty s plovoucí řádovou čárkou
+//
+// 14_boolean_type.go:
+//    Datový typ Boolean
+//
+// 15_boolean_type_checks.go:
+//    Datový typ Boolean - kontroly při překladu
+//
+// 16_string_type.go:
+//    Datový typ řetězec
+//
+// 17_raw_strings.go:
+//    Datový typ řetězec
+//
+// 18_string_content.go:
+//    Datový typ řetězec a jeho obsah
+//
+// 19_local_variables.go:
+//    Lokální proměnné
+//
+// 20_arrays.go:
+//    Základní operace s poli
+//
+// 21_array_copy.go:
+//    Kopie polí
+//
+// 22_slices.go:
+//    Řezy polí
+//
+// 23_slice_copy.go:
+//    Kopie řezů polí
+//
+// 24_slice_from_slice.go:
+//    Kopie řezů polí
+//
