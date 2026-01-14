929e9a016517c045a71fb73f218f66c65962aae1
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-14 09:23:09 +0100

    Index file with list of all examples

---
+++ article_08/00_index.go
@@ -0,0 +1,72 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_enum_with_iota.go:
+//    Náhrada za enum: běžné konstanty a identifikátor iota.
+//
+// 02_enum_with_iota_type_check.go:
+//    Náhrada za enum: běžné konstanty a identifikátor iota.
+//
+// 03_enum_with_iota.go:
+//    Náhrada za enum: běžné konstanty a identifikátor iota.
+//
+// 04_enum_with_iota.go:
+//    Převod "výčtu" na řetězec.
+//
+// 05_enum_as_type.go:
+//    Výčet jako datový typ.
+//
+// 06_enum_as_type.go:
+//    Výčet jako datový typ: nemožnost použití operátoru ++.
+//
+// 07_channel_as_fifo.go:
+//    Kanál s definovanou kapacitou, který se chová jako FIFO.
+//
+// 08_channel_and_range.go:
+//    Kanál s definovanou kapacitou, příkaz range.
+//
+// 09_channel_and_range.go:
+//    Kanál s definovanou kapacitou, příkaz range.
+//
+// 10_read_from_closed_channel.go:
+//    Čtení ze zavřeného kanálu.
+//
+// 11_sync_with_channel.go:
+//    Kanál ve funkci synchronizační struktury.
+//
+// 12_select_statement_receive.go:
+//    Použití příkazu select pro čtení z kanálů s timeoutem.
+//
+// 13_select_statement_receive.go:
+//    Použití příkazu select pro čtení z kanálů s timeoutem.
+//
+// 14_select_statement_receive.go:
+//    Použití příkazu select pro čtení z kanálů s timeoutem + větev default.
+//
+// 15_alternate_import_alias.go:
+//    Alternativní způsob importu balíčku.
+//
+// 16_alternate_import_alias.go:
+//    Alternativní způsob importu balíčku.
+//
+// 17_alternate_import_dot.go:
+//    Alternativní způsob importu balíčku.
+//
+// 18_alternate_import_dot.go:
+//    Alternativní způsob importu balíčku.
+//
+// 19_use_package_hello1.go:
+//    Import vlastního balíčku.
+//
+// 20_use_package_hello2.go:
+//    Import vlastního balíčku.
+//
+// 21_use_package_hello3.go:
+//    Import vlastního balíčku.
+//
+// 22_package_init.go:
+//    Inicializace balíčku.
+//
+// 23_package_init.go:
+//    Inicializace balíčku.
+//
