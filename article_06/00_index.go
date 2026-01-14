a6119ffe3f15f0229ce0991d1ed53008594f4f7e
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-14 09:23:08 +0100

    Index file with list of all examples

---
+++ article_06/00_index.go
@@ -0,0 +1,87 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_goto.go:
+//    Použití příkazu goto.
+//
+// 02_goto_from_inner_loop.go:
+//    Dvojice vnořených smyček for a příkaz goto pro jejich opuštění.
+//
+// 03_goto_from_switch.go:
+//    Výskok z příkazu switch pomocí goto.
+//
+// 04_goto_bad_usage_A.go:
+//    Špatné použití příkazu goto.
+//
+// 05_goto_bad_usage_B.go:
+//    Špatné použití příkazu goto.
+//
+// 06_goto_bad_usage_C.go:
+//    Špatné použití příkazu goto.
+//
+// 06_goto_bad_usage_D.go:
+//    Špatné použití příkazu goto.
+//
+// 06_goto_local_labels.go:
+//    Lokální návěští.
+//
+// 07_defer.go:
+//    Použití příkazu defer.
+//
+// 08_defer_func.go:
+//    Použití příkazu defer.
+//
+// 09_defer_with_parameters.go:
+//    Použití příkazu defer, zavolání funkce s parametrem.
+//
+// 10_more_defers.go:
+//    Zjištění pořadí volání funkcí deklarovaných v defer().
+//
+// 11_defer_arguments_evaluation.go:
+//    Zjištění, kdy se vyhodnocují výraz pro volání funkce v defer().
+//
+// 12_defer_arguments_evaluation.go:
+//    Zjištění, kdy se vyhodnocují výraz pro volání funkce v defer().
+//
+// 13_defer_on_all_returns.go:
+//    Defer a příkaz return.
+//
+// 14_defer_practical_usage.go:
+//    Praktické použití konstrukce defer.
+//
+// 15_defer_practical_usage.go:
+//    Praktické použití konstrukce defer.
+//
+// 16_defer_return_values.go:
+//    Modifikace návratové hodnoty funkce v sekci defer.
+//
+// 17_panic.go:
+//    Praktické použití konstrukce defer a panic.
+//
+// 18_panic_recover.go:
+//    Praktické použití konstrukce defer, panic a recover.
+//
+// 19_unary_operators.go:
+//    Všechny unární operátory jazyka Go.
+//
+// 20_relational_operators.go:
+//    Relační operátory.
+//
+// 21_div_mod.go:
+//    Dělení a zbytek po dělení.
+//
+// 22_div_by_zero.go:
+//    Dělení a zbytek po dělení.
+//
+// 23_bit_shift_negative_shift.go:
+//    Bitové posuny s negativním druhým operandem.
+//
+// 24_bit_shifts.go:
+//    Bitové posuny.
+//
+// 25_bit_shift_assignment.go:
+//    Bitové posuny zkombinované s přiřazením.
+//
+// 26_bit_operators.go:
+//    Bitové operátory.
+//
