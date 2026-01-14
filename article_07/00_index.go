118789d04f8885de4357becc5b114b0822fb6429
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-14 09:23:09 +0100

    Index file with list of all examples

---
+++ article_07/00_index.go
@@ -0,0 +1,69 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_bit_operators.go:
+//    Bitové operátory.
+//
+// 02_boolean_operators.go:
+//    Logické operátory.
+//
+// 02A_boolean_operators_short_circuit.go:
+//    Logické operátory.
+//
+// 02B_boolean_operators_improper_usage.go:
+//    Nekorektní použití operátorů na pravdivostní hodnoty.
+//
+// 02C_boolean_operators_improper_usage.go:
+//    Logické operátory.
+//
+// 03_inc_dec.go:
+//    Operátory ++ a --.
+//
+// 04_inc_dec_bad_usage.go:
+//    Nekorektní použití operátorů ++ a --.
+//
+// 05_inc_dec_bad_usage.go:
+//    Nekorektní použití operátorů ++ a --.
+//
+// 06_select_statement_receive.go:
+//    Použití příkazu select pro čtení z kanálů.
+//
+// 07_select_statement_receive_default.go:
+//    Použití příkazu select pro čtení z kanálů.
+//
+// 08_select_statement_send.go:
+//    Použití příkazu select pro zápis do kanálu.
+//
+// 09_select_statement_send.go:
+//    Použití příkazu select pro zápis do kanálu.
+//
+// 09B_select_statement_send.go:
+//    Použití příkazu select pro zápis do kanálu.
+//
+// 10_select_statement_send_receive.go:
+//    Použití příkazu select pro zápis do kanálu i pro čtení z kanálu.
+//
+// 11_constants.go:
+//    Konstanty.
+//
+// 12_constants.go:
+//    Konstanty.
+//
+// 13_iota.go:
+//    Konstanty a identifikátor iota.
+//
+// 14_iota_better_usage.go:
+//    Konstanty a identifikátor iota.
+//
+// 15_iota_another_usage.go:
+//    Konstanty a identifikátor iota.
+//
+// 16_variadic_function.go:
+//    Variadické funkce.
+//
+// 17_variadic_function_improper_usage.go:
+//    Variadické funkce.
+//
+// 18_variadic_function_improper_usage.go:
+//    Variadické funkce.
+//
