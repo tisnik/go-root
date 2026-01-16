dc245b13565098266def1a8787427415b60e3f2f
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-16 09:11:57 +0100

    Index file

---
+++ article_10/00_index.go
@@ -0,0 +1,39 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_cmdline_params.go:
+//    Přečtení argumentů předaných na příkazovém řádku.
+//
+// 02_flags.go:
+//    Vylepšené přečtení argumentů předaných na příkazovém řádku.
+//
+// 03_flag_shorthands.go:
+//    Vylepšené přečtení argumentů předaných na příkazovém řádku.
+//
+// 04_bigint.go:
+//    Použití balíčku big, typ Int.
+//
+// 05_factorial.go:
+//    Použití balíčku big pro výpočet faktoriálu.
+//
+// 06_bigfloat.go:
+//    Použití balíčku big, typ Float.
+//
+// 07_exec.go:
+//    Použití balíčku exec, spuštění externí utility.
+//
+// 08_exec_stdin.go:
+//    Použití balíčku exec, spuštění externí utility s předáním dat.
+//
+// 09_exec_stdin.go:
+//    Použití balíčku exec, spuštění externí utility s předáním dat.
+//
+// 10_env_var.go:
+//    Přístup k proměnným prostředí.
+//
+// 11_process_info.go:
+//    Získání základních informací o procesu.
+//
+// 12_file_operations.go:
+//    Základní operace se soubory.
+//
