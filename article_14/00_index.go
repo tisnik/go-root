a7997e6f7fcefbb2cb8cf4a307c88e1649b84c8d
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-16 09:11:57 +0100

    Index file

---
+++ article_14/00_index.go
@@ -0,0 +1,60 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_empty_image.go:
+//     Vytvoření rastrového obrázku s výchozími hodnotami pixelů (zcela průhledné černé pixely)
+//
+// 02_empty_image_rectangle.go:
+//     Alternativní způsob vytvoření obdélníku pro určení rozměrů obrázku
+//
+// 03_filled_image_setrgba.go:
+//     Rastrový obrázek vyplněný konstantní barvou, použití metody SetRGBA
+//
+// 04_filled_image_set.go:
+//     Rastrový obrázek vyplněný konstantní barvou, použití metody Set
+//
+// 05_filled_image_alpha.go:
+//     Obrázek, v němž mají pixely různou průhlednost (nekorektní varianta)
+//
+// 06_filled_image_alpha_correct.go:
+//     Korektní varianta předchozího příkladu
+//
+// 07_filled_image_alpha_correct.go:
+//     Vylepšená varianta předchozího příkladu
+//
+// 08_image_internals.go:
+//     Interní struktura záznamu s informacemi o obrázku
+//
+// 09_raw_pixels.go:
+//     Přímý přístup k jednotlivým pixelům; první varianta
+//
+// 10_raw_pixels2.go:
+//     Přímý přístup k jednotlivým pixelům; druhá varianta
+//
+// 11_raw_pixels3.go:
+//     Přímý přístup k jednotlivým pixelům; třetí varianta
+//
+// 12_point_rectangle.go:
+//     Datové struktury Point a Rectangle
+//
+// 13_point_rectangle_operations.go:
+//     Základní operace s obdélníky a body
+//
+// 14_rgba_images.go:
+//     Základní vlastnosti barvového prostoru RGBA
+//
+// 15_cmyk_images.go:
+//     Základní vlastnosti barvového prostoru CMYK
+//
+// 16_cmyk_images.go:
+//     Základní vlastnosti barvového prostoru CMYK
+//
+// 17_chessboard.go:
+//     Využití balíčku draw pro vykreslení šachovnice (rastrové operace)
+//
+// 18_bresenham_algorithm.go:
+//     Implementace Bresenhamova algoritmu pro vykreslení úsečky
+//
+// 19_blending.go:
+//     Blending v Go (opět rastrové operace)
+//
