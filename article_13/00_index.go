0d6d0ebeafa40b82c258d044ef5db43dd760a44a
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-16 09:11:57 +0100

    Index file

---
+++ article_13/00_index.go
@@ -0,0 +1,72 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_json_marshal_basic_signed_types.go:
+//     Marshalling celých čísel se znaménkem do JSONu
+//
+// 02_json_marshal_basic_unsigned_types.go:
+//     Marshalling celých čísel bez znaménka do JSONu
+//
+// 03_json_marshal_basic_complex_types.go:
+//     Pokus o marshalling komplexních čísel do JSONu
+//
+// 03B_json_marshal_basic_complex_types_err.go:
+//     Pokus o marshalling komplexních čísel do JSONu, kontrola převodu
+//
+// 04_json_marshal_arrays.go:
+//     Marshalling jednorozměrných i dvourozměrných polí do JSONu
+//
+// 05_json_marshal_struct.go:
+//     Marshalling struktury/záznamu do JSONu
+//
+// 06_json_mashal_array_of_struct.go:
+//     Marshalling pole struktur/záznamů do JSONu
+//
+// 06B_json_mashal_array_of_struct.go:
+//     Marshalling pole struktur/záznamů do JSONu, privátní prvky struktury
+//
+// 07_json_marshal_map_of_structs.go:
+//     Marshalling map struktur/záznamů do JSONu
+//
+// 08_json_marshal_map_of_structs.go:
+//     Marshalling map struktur/záznamů do JSONu
+//
+// 09_json_marshal_complex_map.go:
+//     Marshalling map struktur/záznamů
+//
+// 10_json_different_keys.go:
+//     Změna názvů klíčů pri marshallingu
+//
+// 11_json_marshal_special_values.go:
+//     Pokus o marshalling speciálních hodnot
+//
+// 12_json_unmarshal_struct.go:
+//     Unmarshalling struktury z JSONu
+//
+// 13_json_unmarshal_struct.go:
+//     Unmarshalling struktury z JSONu
+//
+// 14_json_unmarshal_array.go:
+//     Unmarshalling pole z JSONu
+//
+// 15_json_unmarshal_array_of_struct.go:
+//     Unmarshalling pole struktur
+//
+// 16_json_unmarshal_map_of_struct.go:
+//     Unmarshalling mapy struktur
+//
+// 17_json_unmarshal_map_of_struct_different_keys.go:
+//     Unmarshalling mapy struktur, specifikace klíčů
+//
+// 18_json_unmarshal_unknown_struct.go:
+//     Pokus o unmarshalling struktury s obecnými daty
+//
+// 19_png_output.go:
+//     Export rastrového obrázku do formátu PNG
+//
+// 20_jpeg_output.go:
+//     Export rastrového obrázku do formátu JPEG
+//
+// 21_jpeg_output_low_quality.go:
+//     Export rastrového obrázku do formátu JPEG s ovlivněním kvality
+//
