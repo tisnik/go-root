9f6b1db003b313ebb0cf3b52cfb638242858309c
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-16 09:11:57 +0100

    Index file

---
+++ article_09/00_index.go
@@ -0,0 +1,60 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_levenshtein.go:
+//    Import nainstalovaného balíčku.
+//
+// 02_string_parsing_bool.go:
+//    Základní funkce pro parsing řetězců: ParseBool.
+//
+// 03_string_parsing_int.go:
+//    Základní funkce pro parsing řetězců: ParseInt.
+//
+// 04_string_parsing_uint.go:
+//    Základní funkce pro parsing řetězců: ParseUInt.
+//
+// 05_string_parsing_atoi.go:
+//    Základní funkce pro parsing řetězců: Atoi.
+//
+// 06_string_parsing_float.go:
+//    Základní funkce pro parsing řetězců: parseFloat.
+//
+// 07_string_format_itoa.go:
+//    Základní funkce pro formátování řetězců: Itoa.
+//
+// 08_string_format_int.go:
+//    Základní funkce pro formátování řetězců: FormatInt.
+//
+// 09_string_format_ftoa.go:
+//    Základní funkce pro formátování řetězců: FormatFloat.
+//
+// 10_string_format_sprintf.go:
+//    Základní funkce pro formátování řetězců: Sprintf.
+//
+// 11_list.go:
+//    Použití standardního balíčku "container/list"
+//
+// 12_list_as_stack.go:
+//    Použití obousměrně vázaného seznamu ve funkci zásobníku.
+//
+// 12B_print_stack_content.go:
+//    Použití obousměrně vázaného seznamu ve funkci zásobníku.
+//
+// 13_gods_arraylist.go:
+//    Datová struktura arraylist z knihovny GoDS.
+//
+// 14_gods_singlylinkedlist.go:
+//    Datová struktura singlylinkedlist z knihovny GoDS.
+//
+// 15_gods_stack_RPN.go:
+//    Použití zásobníku implementovaného polem.
+//
+// 15B_gods_stack_RPN.go:
+//    Použití zásobníku implementovaného seznamem.
+//
+// 16_gods_set.go:
+//    Použití množin z knihovny GoDS.
+//
+// 17_gods_rb-tree.go:
+//    Datová struktura Red-Black tree.
+//
