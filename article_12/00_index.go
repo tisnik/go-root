3c7cd8c83b5fff68f343056e3e8bb58832d4a1a6
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-16 09:11:57 +0100

    Index file

---
+++ article_12/00_index.go
@@ -0,0 +1,42 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_simple_http_server.go:
+//    Jednoduchý HTTP server
+//
+// 02_simple_http_server_write_bytes.go:
+//    Alternativní způsob vytvoření sekvence bajtů z řetězce
+//
+// 03_specify_content_type.go:
+//     HTTP server se specifikací MIME typu odpovědí
+//
+// 04_serve_static_html_page.go:
+//     Server, který vrací statické HTML stránky načtené ze souborů
+//
+// 04_static_html_page.go:
+//     Server, který vrací statické HTML stránky načtené ze souborů
+//
+// 05_serve_static_html_page.go:
+//     Úprava předchozího příkladu
+//
+// 05_static_html_page.go:
+//    Úprava předchozího příkladu
+//
+// 06_serve_static_html_page_closure.go:
+//    Využití uzávěrů pro zápis kratšího kódu
+//
+// 07_html_templates.go:
+//    Základní použití šablon
+//
+// 08_factorial_prepare.go:
+//    Webová aplikace pro výpočet faktoriálu (příprava)
+//
+// 09_factorial_compute.go:
+//    Druhá varianta webové aplikace pro výpočet faktoriálu
+//
+// 10_factorial_compute_const_template.go:
+//     Neměnná šablona stránky
+//
+// 11_https_server.go:
+//     Jednoduchý HTTPS server
+//
