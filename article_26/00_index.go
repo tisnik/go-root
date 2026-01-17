d5f7a9d5014d67dfae31ee12c82706120dfd8d01
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-17 09:02:14 +0100

    Index file for article #26

---
+++ article_26/00_index.go
@@ -0,0 +1,90 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_boolean_type.go:
+//    Datový typ boolean
+//
+// 02_number_types.go:
+//    Numerické datové typy
+//
+// 03_string_type.go:
+//    Datový typ řetězec
+//
+// 04_other_types.go:
+//    Ostatní datové typy
+//
+// 05_other_types_better_solution.go:
+//    Ostatní datové typy, lepší přístup
+//
+// 06_structs.go:
+//    Struktury (záznamy)
+//
+// 07_nil_as_variable.go:
+//    nil může být korektní jméno konstanty.
+//
+// 08_true_false_vars.go:
+//    true a false mohou být korektní jméno konstanty.
+//
+// 09_nil_value.go:
+//    Hodnota nil a její význam (bez typu)
+//
+// 10_nil_pointer.go:
+//    Ukazatel inicializovaný na nil
+//
+// 10B_nil_pointer.go:
+//    Ukazatel inicializovaný na nil
+//
+// 11_other_pointer.go:
+//    Ukazatel na celé číslo
+//
+// 12_nil_slice.go:
+//    Nulový řez
+//
+// 12B_nil_slice_append.go:
+//    Nulový řez a funkce append
+//
+// 13_nil_map.go:
+//    Nulová mapa
+//
+// 14_nil_map_is_really_nil.go:
+//    Nulová mapa
+//
+// 15_add_into_nil_map.go:
+//    Přidání prvků do nulové mapy
+//
+// 16_empty_map.go:
+//    Prázdná mapa a přidání prvků do prázdné mapy
+//
+// 17_nil_interface.go:
+//    Prázdné rozhraní
+//
+// 18_two_interfaces.go:
+//    Prázdné rozhraní a rozhraní s jednou metodou
+//
+// 19_nil_is_not_nil.go:
+//    nil se někdy nerovná nil
+//
+// 20_two_nil_pointers.go:
+//    Dva ukazatele s nulovou hodnotou
+//
+// 21_comparing_interface_to_pointer.go:
+//    Porovnání dvou ukazatelů
+//
+// 22_compare_two_different_pointer_types.go:
+//    Porovnání dvou ukazatelů různých typů
+//
+// 23_nil_channels.go:
+//    Nulový kanál
+//
+// 24_nil_channel_write.go:
+//    Zápis do nulového kanálu
+//
+// 25_nil_channel_read.go:
+//    Čtení z nulového kanálu
+//
+// 26_nil_and_normal_channel.go:
+//    Nulové i normální kanály
+//
+// 27_three_nil_interfaces.go:
+//    Tři nulová rozhraní
+//
