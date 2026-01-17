8886c6d073cd3a535f19c975bcc34f846fbff10a
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-17 09:02:14 +0100

    Index file for article #15

---
+++ article_15/00_index.go
@@ -0,0 +1,63 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_blending.go:
+//     Ukázka blendingu
+//
+// 02_gif_export_rgba.go:
+//     Export obrázku šachovnice do formátu GIF
+//
+// 03_gif_1x1_rgba.go:
+//     Export obrázku o rozlišení 1×1 pixel s 256 barvovou paletou
+//
+// 04_gif_1x1_palette.go:
+//     Export obrázku o rozlišení 1×1 pixel s dvoubarevnou paletou
+//
+// 05_gif_export_colors.go:
+//     Export se specifikací počtu barev v paletě
+//
+// 06_gif_export_palette.go:
+//     Šachovnice vykreslená s dvoubarevnou paletou
+//
+// 07_rgba_to_palette.go:
+//     Převod obrázku z prostoru RGBA do prostoru s paletou se snižováním počtu barev
+//
+// 08_gif_animation_blink.go:
+//     Jednoduchá animace se dvěma snímky o velikosti 32×32 pixelů
+//
+// 09_gif_animation.go:
+//     Složitější animace pohybující se šachovnice
+//
+// 10_default_disposal_methods.go:
+//     Výchozí hodnota metody použité při přepínání snímků
+//
+// 11_background_disposal_methods.go:
+//     Nastavení metody přepínání snímků: vykreslení pozadí
+//
+// 12_none_disposal_methods.go:
+//     Nastavení metody přepínání snímků: žádná modifikace snímku
+//
+// 13_progress_bar.go:
+//     Animace progress baru vytvořená jako animovaný GIF
+//
+// 14_gg_basic.go:
+//     Základní způsob použití knihovny GG
+//
+// 15_gg_context_properties.go:
+//     Použití kontextu a nastavení vlastností kreslení
+//
+// 16_gg_path_stroke.go:
+//     Vykreslení cesty
+//
+// 17_gg_filled_background.go:
+//     Vyplnění pozadí konstantní barvou
+//
+// 18_gg_alpha_rgba.go:
+//     Specifikace alfa kanálu (průhlednosti)
+//
+// 19_gg_line_width.go:
+//     Specifikace šířky vykreslovaných cest
+//
+// 20_gg_set_dash.go:
+//     Specifikace stylu vykreslovaných cest
+//
