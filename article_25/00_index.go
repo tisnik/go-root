38ae122cc4ab6634088f1157c9b6be8310be8ac8
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-17 09:02:14 +0100

    Index file for article #25

---
+++ article_25/00_index.go
@@ -0,0 +1,21 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_add.go:
+//     Jednoduchý program, který sečte dvojici celočíselných hodnot
+//
+// 02_arrays_slices.go:
+//     Základní operace s poli a s řezy
+//
+// 03_structs.go:
+//     Práce se strukturami (záznamy)
+//
+// 04_factorial.go:
+//     Rekurzivní výpočet faktoriálu
+//
+// 05_goroutines.go:
+//     Spuštění dvou gorutin
+//
+// 06_channels.go:
+//     Gorutiny a kanály
+//
