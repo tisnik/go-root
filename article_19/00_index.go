dd8427e38d40366f6bb1192bc88743b6305c0a3e
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-17 09:02:14 +0100

    Index file for article #19

---
+++ article_19/00_index.go
@@ -0,0 +1,21 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// dom_add_element.go:
+//     Manipulace s DOMem z jazyka Go.
+//
+// dom_manipulation.go:
+//     Manipulace s DOMem z jazyka Go.
+//
+// draw_into_canvas.go:
+//     Kreslení na Canvas.
+//
+// file_server.go:
+//     HTTP server vracející statický obsah
+//
+// hello.go:
+//     Typický program typu "Hello world"
+//
+// js_interop.go:
+//     Rozhraní mezi jazyky Go a JavaScript.
+//
