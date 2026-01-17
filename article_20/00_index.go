6038e5663d352f948af49eb64b6797092015ec56
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-17 09:02:14 +0100

    Index file for article #20

---
+++ article_20/00_index.go
@@ -0,0 +1,33 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_scanln.go:
+//     Načtení řetězců ze standardního vstupu.
+//
+// 02_input_via_reader.go:
+//     Použití metody Reader.ReadString pro načítání ze standardního vstupu.
+//
+// 03_simple_input.go:
+//     Go-prompt: vstup dat.
+//
+// 04_prompt.go:
+//     Go-prompt: vstup s nápovědou.
+//
+// 05_basic_completer.go:
+//     Go-prompt: vstup s nápovědou.
+//
+// 06_prefix_completer.go:
+//     Go-prompt: vstup s nápovědou, reakce na zadaný příkaz.
+//
+// 07_completion_description.go:
+//     Go-prompt: vylepšená nápověda.
+//
+// 08_basic_statements.go:
+//     Zápis příkazů Atari BASICu.
+//
+// 09_fuzzy_filter.go:
+//     Vylepšení zápisu příkazů Atari BASICu.
+//
+// 10_two_word_commands.go:
+//     Víceslovní příkazy.
+//
