01a46c61a0ff920671355594d957972a11414a52
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-17 09:02:14 +0100

    Index file for article #24

---
+++ article_24/00_index.go
@@ -0,0 +1,63 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_server.go:
+//     	Jednoduchý HTTP server posílající dynamicky generovaný obsah
+//
+// 02_slow_server.go:
+//     	Zpomalení generování jednotlivých bloků generovaného obsahu
+//
+// 03_flushing_server.go:
+//     	Využití metody Flush z rozhraní Flusher
+//
+// 04_close_detector.go:
+//     	Test, zda klient neukončil spojení
+//
+// 05_list.go:
+//     	Operace nad seznamem
+//
+// 06_list_as_stack.go:
+//     	Použití seznamu ve funkci zásobníku
+//
+// 07_print_stack_content.go:
+//     	Vylepšení předchozího příkladu
+//
+// 08_empty_ring.go:
+//     	Prázdná cyklická fronta
+//
+// 09_filled_ring.go:
+//     	Cyklická fronta s naplněnými prvky
+//
+// 10_ring_empty_items.go:
+//     	Cyklická fronta s některými prázdnými prvky
+//
+// 11_ring_do_iterator.go:
+//     	Iterace nad prvky cyklické fronty
+//
+// 12_ring_do_closure.go:
+//     	Vylepšená iterace nad prvky cyklické fronty
+//
+// 13_integer_sort.go:
+//     	Seřazení sekvence celých čísel
+//
+// 14_float_sort.go:
+//     	Seřazení sekvence čísel s plovoucí řádovou čárkou
+//
+// 15_string_sort.go:
+//     	Načtení textového souboru s jeho rozdělením na slova a seřazením
+//
+// 16_reverse_sort.go:
+//     	Opačně seřazení sekvence/řezu
+//
+// 17_int_slice.go:
+//     	Použití datového typu IntSlice
+//
+// 18_sort_by.go:
+//     	Seřazení sekvence uživatelských datových struktur
+//
+// 19_heap.go:
+//     	Základní způsob využití datového typu halda
+//
+// 20_heap_trace.go:
+//     	Úprava předchozího příkladu, aby zobrazoval prováděné operace
+//
