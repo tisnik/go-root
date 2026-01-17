f9b43dde644630ce5a4805ab5156cd6b09e510ef
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-17 09:02:14 +0100

    Index file for article #27

---
+++ article_27/00_index.go
@@ -0,0 +1,57 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_do_string_1.go:
+//    Vytvoření virtuálního stroje jazyka Lua, poslání skriptu v řetězci.
+//
+// 02_do_string_2.go:
+//    Poslání skriptu v řetězci do Lua VM.
+//
+// 02B_do_string_3.go:
+//    Poslání skriptu v řetězci do Lua VM.
+//
+// 03_do_file.go:
+//    Načtení externího skriptu do Lua VM.
+//
+// 04_two_vms.go:
+//    Vytvoření dvou samostatných Lua VM.
+//
+// 05_two_vms_in_goroutines.go:
+//    Spuštění dvou Lua VM, každé v jedné gorutině.
+//
+// 06_call_lua.go:
+//    Zavolání Lua funkce z Go.
+//
+// 07_call_lua_parameters.go:
+//    Zavolání Lua funkce z Go s předáním parametrů.
+//
+// 08_call_lua_parameters.go:
+//    Zavolání Lua funkce z Go s předáním parametrů.
+//
+// 09_call_lua_return_value.go:
+//    Zavolání Lua funkce z Go s předáním parametrů, přečtení výsledku.
+//
+// 10_different_data_types.go:
+//    Použití datových typů jazyka Lua.
+//
+// 11_conversion.go:
+//    Konverze datových typů jazyka Lua.
+//
+// 12_boolean.go:
+//    Pravdivostní hodnoty.
+//
+// 13_nil.go:
+//    Hodnota nil.
+//
+// 14_two_return_values.go:
+//    Funkce vracející dvě hodnoty.
+//
+// 15_go_from_lua.go:
+//    Volání Go funkcí z Lua.
+//
+// 16_go_from_lua_correct.go:
+//    Volání Go funkcí z Lua, korektní řešení.
+//
+// 17_go_from_lua_calc.go:
+//    Zjištění návratové hodnoty a jejího typu.
+//
