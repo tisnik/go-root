a875547c2fe10c08ac0e9bbabb80cdd00fadf1a2
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-17 09:02:14 +0100

    Index file for article #23

---
+++ article_23/00_index.go
@@ -0,0 +1,66 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_io_reader_read.go:
+//     	Čtení dat ze souboru s využitím metody Read z rozhraní Reader.
+//
+// 02_io_writer_write.go:
+//     Použití metody Write z rozhraní Writer pro zápis do souboru.
+//
+// 03_memory_pipe.go:
+//     Přenos dat s využitím roury (pipe) alokované v operační paměti.
+//
+// 04_memory_pipe_bytes_buffer.go:
+//     Vylepšení předchozího příkladu.
+//
+// 05_simple_file_reader.go:
+//     Čtení dat z řetězce po blocích.
+//
+// 06_reader_seeker_absolute.go:
+//     Použití operace Seek pro posun počítaný od začátku souboru.
+//
+// 07_reader_seeker_from_end.go:
+//     Použití operace Seek pro posun počítaný od konce souboru.
+//
+// 08_reader_seeker_relative.go:
+//     Použití operace Seek pro relativní posun v souboru.
+//
+// 09_writer_seeker.go:
+//     Použití operace Seek při zápisu do souboru.
+//
+// 10_writer_seeker_sparse_file.go:
+//     Vytvoření řídkého souboru.
+//
+// 11_multi_writer.go:
+//     Souběžný zápis do více souborů.
+//
+// 12_string_writer.go:
+//     Zápis řetězců (StringWriter).
+//
+// 13_bufio_reader_read_string.go:
+//     Čtení řetězců pomocí funkcí z balíčku bufio.
+//
+// 14_bufio_reader_other_file.go:
+//     Čtení řetězců pomocí funkcí z balíčku bufio.
+//
+// 15_bufio_reader_read_bytes.go:
+//     Čtení sekvence bajtů s využitím funkcí z balíčku bufio.
+//
+// 16_gzip_writer.go:
+//     Komprimace dat metodou Deflate.
+//
+// 17_gzip_writer_compression_level.go:
+//     Volba metody a úrovně komprimace.
+//
+// 18_gzip_writer_pipe.go:
+//     Přečtení zkomprimovaných dat zpět.
+//
+// 19_gzip_multiwriter.go:
+//     Souběžná komprimace dat více metodami.
+//
+// 20_hex_dumper.go:
+//     Zobrazení libovolných binárních dat ve formě hexadecimálního výpisu.
+//
+// 21_base_64_encoder.go:
+//     Zakódování binárních dat do tisknutelné podoby podle Base64.
+//
