c7669a2465e2017ab7de64d44d23dfb97efaa6de
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-17 09:02:14 +0100

    Index file for article #22

---
+++ article_22/00_index.go
@@ -0,0 +1,57 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_read_byte.go:
+//     Postupné načtení bajtů z řetězce.
+//
+// 02_better_read_byte.go:
+//     Postupné načtení bajtů z řetězce, lepší reakce na chyby.
+//
+// 03_read_byte_from_unicode.go:
+//     Postupné načtení bajtů z řetězce, ukázka použití Unicode.
+//
+// 04_read_rune_from_unicode.go:
+//     Načítání jednotlivých run (ne bajtů) z řetězce.
+//
+// 05_write_byte.go:
+//     Zápis bajtu do bufferu.
+//
+// 06_write_byte_read_rune.go:
+//     Zápis bajtů du bufferu, přečtení ve formě run.
+//
+// 07_write_rune.go:
+//     Zápis runy do bufferu.
+//
+// 08_unread_byte.go:
+//     Použití metody UnreadByte.
+//
+// 09_unread_more_bytes.go:
+//     Opakované použití metody UnreadByte.
+//
+// 10_unread_rune.go:
+//     Použití metody UnreadRune.
+//
+// 11_io_reader_read.go:
+//     Čtení dat ze souboru.
+//
+// 12_stdin_read.go:
+//     Čtení dat ze standardního vstupu.
+//
+// 13_string_reader_read.go:
+//     StringReader.
+//
+// 14_string_reader_as_str.go:
+//     Načítání ze standardního vstupu.
+//
+// 15_multi_reader.go:
+//     Multi reader se třemi vstupy.
+//
+// 16_io_writer_write.go:
+//     Zápis do souboru.
+//
+// 17_input_via_reader.go:
+//     Čtení ze standardního vstupu přes reader.
+//
+// 18_file_block_copy.go:
+//     Blokové přenosy dat při kopii souboru.
+//
