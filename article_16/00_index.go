6e51d1f720a4cbb69ab93231c6a2c6059189edbe
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-17 09:02:14 +0100

    Index file for article #16

---
+++ article_16/00_index.go
@@ -0,0 +1,72 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_gg_clear_screen.go:
+//     Vymazání obrazovky.
+//
+// 02_gg_clear_screen.go:
+//     Vymazání obrazovky (druhá, lepší varianta).
+//
+// 03_gg_rgba_color.go:
+//     Barvový prostor RGB a RGBA.
+//
+// 04_gg_alpha_component.go:
+//     Alfa kanál v barvovém prostoru RGBA.
+//
+// 05_gg_line_width.go:
+//     Nastavení šířky čáry.
+//
+// 06_gg_line_caps.go:
+//     Nastavení stylu konců úseček.
+//
+// 07_gg_set_dash.go:
+//     Nastavení stylu vykreslovaných úseček.
+//
+// 08_gg_simple_path.go:
+//     Práce s cestou (path).
+//
+// 09_gg_open_path.go:
+//     Otevřená cesta (open path).
+//
+// 10_gg_closed_path.go:
+//     Uzavřená cesta (closed path).
+//
+// 11_gg_quadratic_bezier.go:
+//     Kvadratická Bézierova křivka.
+//
+// 12_gg_quadratic_bezier_control_points.go:
+//     Kvadratická Bézierova křivka s řídicími body.
+//
+// 13_gg_cubic_bezier.go:
+//     Kubická Bézierova křivka.
+//
+// 14_gg_cubic_bezier_control_points.go:
+//     Kubická Bézierova křivka s řídicími body.
+//
+// 15_gg_rotation.go:
+//     Rotace vykreslovaných objektů.
+//
+// 16_gg_to_image.go:
+//     Rasterizace do zvoleného rastrového formátu.
+//
+// 17_gg_transformation_rotate.go:
+//     Transformace (rotace).
+//
+// 18_gg_transformation_rotate_about.go:
+//     Transformace (rotace okolo zvoleného bodu).
+//
+// 19_gg_scale_about.go:
+//     Transformace (změna měřítka okolo zvoleného bodu).
+//
+// 20_gg_translate.go:
+//     Transformace (posun).
+//
+// 21_gg_text.go:
+//     Vykreslení textu.
+//
+// 22_gg_text_centered.go:
+//     Vykreslení vycentrovaného textu.
+//
+// 23_gg_text_centered.go:
+//     Vykreslení vycentrovaného textu.
+//
