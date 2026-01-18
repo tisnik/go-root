82ab71b6eb2718a31552e0948670815cd8a73fb0
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-18 09:45:23 +0100

    Index file

---
+++ article_34/00_index.go
@@ -0,0 +1,69 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_empty_main.go:
+//    Kostra programu napsaného v Go.
+//
+// 02_hello_world.go:
+//    Klasický program typu Hello world.
+//
+// 03_data_types.go:
+//    Zjištění velikostí základních datových typů jazyka Go.
+//
+// 04_if_then.go:
+//     Podmínka typu "if-then" v jazyce Go.
+//
+// 05_if_then_else.go:
+//     Podmínka typu "if-then-else" v jazyce Go.
+//
+// 06_better_approach.go:
+//     Rozvětvení s využitím switch namísto podmínky typu "if-then-else".
+//
+// 06_nested_if.go:
+//    Vnořené podmínky typu "if".
+//
+// 07_while_loop.go:
+//    Implementace programové smyčky typu "while" v jazyce Go.
+//
+// 09_do_while_loop.go:
+//    Implementace programové smyčky typu "do-while" v jazyce Go.
+//
+// 10_for_loop.go:
+//    Implementace programové smyčky typu "for" v jazyce Go.
+//
+// 11_for_loop.go:
+//    Implementace programové smyčky typu "for" v jazyce Go.
+//
+// 12_for_loop.go:
+//    Implementace programové smyčky typu "for" v jazyce Go.
+//
+// 13_break_in_loop.go:
+//     Smyčky a příkaz break.
+//
+// 13_break_in_loop_B.go:
+//    Nekonečná smyčka v jazyce Go.
+//
+// 14_switch.go:
+//     Rozvětvení v jazyce Go.
+//
+// 15_simple_function.go:
+//    Deklarace jednoduché funkce.
+//
+// 16_function_with_params.go:
+//    Funkce s parametry.
+//
+// 17_return_statement.go:
+//    Příkaz return pro ukončení funkce.
+//
+// 18_variadic_function.go:
+//    Variadické funkce.
+//
+// 19_constants.go:
+//    Deklarace konstant v Go.
+//
+// 20_constants.go:
+//    Deklarace konstant v Go.
+//
+// 21_iota.go:
+//    Deklarace konstant v Go.
+//
