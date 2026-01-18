64446124407450bd5c7cd533d6e30f38dabc8cdc
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-18 09:44:46 +0100

    Index file

---
+++ article_31/00_index.go
@@ -0,0 +1,36 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_image_server.go:
+//    HTTP server, který vrací HTML stránku s generovaným obrázkem.
+//
+// 02_image_server_counter.go:
+//    Přidání jednoduchého čítače do HTTP serveru.
+//
+// 03_image_server_counter_mutex.go:
+//    Čítač měněný v mutexu.
+//
+// 04_image_server_counter_atom.go:
+//    Atomická změna čítače.
+//
+// 05_image_server_expvar.go:
+//    Exportované proměnné - čítač.
+//
+// 06_image_server_expvar_closure.go:
+//    Úprava předchozího příkladu používajícího uzávěry.
+//
+// 07_image_server_expvar_pprof.go:
+//    Použití pprof (profilovací a další metriky)
+//
+// 08_image_server_fractal.go:
+//    Další tři sledované metriky: čas výpočtu, počet vypočtených pixelů, čas v tisknutelné podobě.
+//
+// 09_counter.go:
+//    Čítač zvyšovaný ve více vláknech bez synchronizace.
+//
+// 10_counter_lock.go:
+//    Čítač zvyšovaný ve více vláknech se synchronizací (v mutextu).
+//
+// 11_counter_atomic.go:
+//    Čítač atomicky zvyšovaný ve více vláknech.
+//
