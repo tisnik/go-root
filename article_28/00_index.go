0b56020f23dd630eda82efe778c822d9a12cadf0
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-18 09:41:08 +0100

    Index file

---
+++ article_28/00_index.go
@@ -0,0 +1,78 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_new_stream.go:
+//    Vytvoření několika streamů.
+//
+// 02_to_stream.go:
+//    Konverze polí a řezů do streamů.
+//
+// 03_user_structs_to_stream.go:
+//    Struktury konvertované do streamů.
+//
+// 04_from_array.go:
+//    Vytvoření streamů z polí struktur.
+//
+// 05_new_stream_from_array.go:
+//    Nový stream vytvořený z pole.
+//
+// 06_basic_attributes.go:
+//    Základní atributy streamů.
+//
+// 07_contains.go:
+//    Metoda Contains().
+//
+// 08_index_of.go:
+//    Metoda IndexOf().
+//
+// 09_contains_user_structs.go:
+//    Uživatelské struktury a streamy.
+//
+// 10_index_of_user_struct.go:
+//    Metoda IndexOf() a stream se strukturami.
+//
+// 11_take.go:
+//    Metoda Take() a stream s celočíselnými hodnotami.
+//
+// 12_add_drop.go:
+//    Metody Add() a Drop().
+//
+// 13_filter.go:
+//    Metoda Filter().
+//
+// 13B_filter_anonymous.go:
+//    Metoda Filter() a stream s celočíselnými hodnotami.
+//
+// 14_map.go:
+//    Metoda Map().
+//
+// 14B_map_anonymous.go:
+//    Metoda Map() a stream s celočíselnými hodnotami.
+//
+// 15_for_each.go:
+//    Metoda ForEach().
+//
+// 16_sort.go:
+//    Metoda Sort().
+//
+// 16B_sort_anonymous.go:
+//    Metoda Sort() a stream s celočíselnými hodnotami.
+//
+// 17_reverse.go:
+//    Metoda Reverse().
+//
+// 18_combinations.go:
+//    Kombinace různých metod pro "streaming".
+//
+// 19_combinations_error.go:
+//    Kombinace různých metod pro "streaming", zpracování chyb.
+//
+// 20_reduce.go:
+//    Metoda Reduce().
+//
+// 20B_reduce_anonymous.go:
+//    Metoda Reduce() a stream s celočíselnými hodnotami.
+//
+// 21_max_value.go:
+//    Metoda Reduce().
+//
