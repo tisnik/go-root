6233c77c8e4179fb382fd41fa4e404f15920ce96
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-18 09:41:40 +0100

    Index file

---
+++ article_30/00_index.go
@@ -0,0 +1,24 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_image_server.go:
+//    HTTP server, který generuje rastrové obrázky.
+//
+// 02_image_server_with_pprof.go:
+//     HTTP server nabízející i pprof metriky.
+//
+// 03_image_server_fractal.go:
+//    HTTP server generující rastrový obrázek s fraktálem.
+//
+// 04_basic_tracing.go:
+//     Vytvoření souboru s trasovacími informacemi.
+//
+// 05_context.go:
+//    Zpracování kontextu v trasovacích informacích.
+//
+// 06_context_regions.go:
+//    Transakce a regiony v trasovacím souboru.
+//
+// 07_region_hierarchy.go:
+//    Hierarchie regionů v souboru s trasovacími informacemi.
+//
