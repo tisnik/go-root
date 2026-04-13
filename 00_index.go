7ae480b6080b2ff1285282384ea58d39fc599129
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-13 11:07:29 +0200

    Index file with examples for article #2

--- 00_index.go
+++ 00_index.go
@@ -3,10 +3,10 @@
 // ----------------------------------------------------
 //
 // 01_hello_world.go:
-//    Program typu "Hello world!"
+//    Program typu "Hello world!" naprogramovaný v jazyce Go.
 //
 // 02_better_hello_world.go:
-//    Vylepšený program typu "Hello world!"
+//    Vylepšený program typu "Hello world!" naprogramovaný v jazyce Go
 //
 // 03_unused_imports.go:
 //    Nepoužitý importovaný modul "fmt"
@@ -15,7 +15,7 @@
 //    Použití Unicode ve zdrojovém kódu
 //
 // 05_basic_type_inference.go:
-//    Deklarace proměnné s její inicializací
+//    Deklarace proměnné s její inicializací, typová inference
 //
 // 06_variable_redeclaration.go:
 //    Pokus o opětovnou deklaraci proměnných
@@ -27,7 +27,7 @@
 //    Špatný typ hodnoty přiřazený do proměnných
 //
 // 09_simple_function.go:
-//    Deklarace uživatelské funkce
+//    Deklarace uživatelské funkce bez parametrů a bez návratové hodnoty
 //
 // 10_function_with_params.go:
 //    Deklarace uživatelské funkce s jedním parametrem
@@ -36,7 +36,7 @@
 //    Deklarace uživatelské funkce s návratovou hodnotou
 //
 // 12_named_return_variable.go:
-//    Použití pojmenovaného návratového parametru
+//    Použití pojmenovaného návratového parametru v definici funkce
 //
 // 13_sum_function.go:
 //    Funkce se dvěma parametry
@@ -45,8 +45,88 @@
 //    Funkce se dvěma parametry a dvěma návratovými hodnotami
 //
 // 15_swap_function_B.go:
-//    Funkce se dvěma parametry a dvěma návratovými hodnotami
+//    Funkce se dvěma parametry a dvěma návratovými hodnotami, využití typové inference
 //
 // 16_return_at_the_end_of_function.go:
 //    Chybný pokus o deklaraci proměnné za příkazem return
 //
+
+// ----------------------------------------------------
+// Adresář: article_02
+// ----------------------------------------------------
+//
+// 01_integer_signed_types.go:
+//    Celočíselné datové typy se znaménkem
+//
+// 02_integer_signed_types_checks.go:
+//    Kontrola celočíselných konstant
+//
+// 03_integer_unsigned_types.go:
+//    Celočíselné datové typy bez znaménka
+//
+// 04_integer_constants.go:
+//    Celočíselné konstanty reprezentované v různých číselných soustavách
+//
+// 05_improper_conversion.go:
+//    Přiřazení uint8 -> uint16
+//
+// 06_improper_conversion.go:
+//    Přiřazení uint16 -> uint8
+//
+// 07_improper_conversion_int_uint.go:
+//    Přiřazení uint8 -> int8 a naopak
+//
+// 08_explicit_conversions.go:
+//    Konverze mezi základními datovými typy
+//
+// 09_formatting_output.go:
+//    Základní formátování celočíselných hodnot na výstupu
+//
+// 10_formatting_output.go:
+//    Základní formátování celočíselných hodnot na výstupu
+//
+// 11_fp_types.go:
+//    Datové typy reprezentující hodnoty s plovoucí řádovou čárkou
+//
+// 12_fp_types_checks.go:
+//    Datové typy reprezentující hodnoty s plovoucí řádovou čárkou, kontrola rozsahu
+//
+// 13_complex_types.go:
+//    Datové typy reprezentující hodnoty s plovoucí řádovou čárkou
+//
+// 14_boolean_type.go:
+//    Datový typ Boolean
+//
+// 15_boolean_type_checks.go:
+//    Datový typ Boolean - kontroly při překladu
+//
+// 16_string_type.go:
+//    Datový typ řetězec
+//
+// 17_raw_strings.go:
+//    Datový typ řetězec
+//
+// 18_string_content.go:
+//    Datový typ řetězec a jeho obsah
+//
+// 19_local_variables.go:
+//    Lokální proměnné
+//
+// 20_arrays.go:
+//    Základní operace s poli
+//
+// 21_array_copy.go:
+//    Kopie polí
+//
+// 22_slices.go:
+//    Řezy polí
+//
+// 23_slice_copy.go:
+//    Kopie řezů polí
+//
+// 24_slice_from_slice.go:
+//    Kopie řezů polí
+//
+
+
+
