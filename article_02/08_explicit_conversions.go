c65135ebe758734f05077b34d8d336f65308e3fd
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-14 08:58:52 +0200

    Updated description of this example

--- article_02/08_explicit_conversions.go
+++ article_02/08_explicit_conversions.go
@@ -12,7 +12,7 @@
 //    https://github.com/tisnik/go-root/blob/master/article_02/README.md
 //
 // Demonstrační příklad číslo 8
-//    Konverze mezi základními datovými typy
+//    Explicitní konverze mezi základními datovými typy
 //
 // Dokumentace ve stylu "literate programming":
 //    https://tisnik.github.io/go-root/article_02/08_explicit_conversions.html
