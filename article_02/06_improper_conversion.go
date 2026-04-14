558980c1ead9a6d518b4c7c0921b5b9fd025ef0b
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-14 08:58:52 +0200

    Updated description of this example

--- article_02/06_improper_conversion.go
+++ article_02/06_improper_conversion.go
@@ -12,7 +12,7 @@
 //    https://github.com/tisnik/go-root/blob/master/article_02/README.md
 //
 // Demonstrační příklad číslo 6:
-//    Přiřazení uint16 -> uint8
+//    Přiřazení uint16 -> uint8 ve zdrojovém kódu
 //
 // Dokumentace ve stylu "literate programming":
 //    https://tisnik.github.io/go-root/article_02/06_improper_conversion.html
