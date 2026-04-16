5a18271bc3e05eaf0e45e28384a7e91921a572d2
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-16 08:43:29 +0200

    Fixed description for examples from article #2

--- 00_index.go
+++ 00_index.go
@@ -56,76 +56,76 @@
 // ----------------------------------------------------
 //
 // 01_integer_signed_types.go:
-//    Všechny celočíselné datové typy se znaménkem (signed integers)
+//    Všechny celočíselné datové typy se znaménkem (signed integers).
 //
 // 02_integer_signed_types_checks.go:
-//    Kontrola celočíselných konstant překladačem
+//    Kontrola celočíselných konstant překladačem.
 //
 // 03_integer_unsigned_types.go:
-//    Celočíselné datové typy bez znaménka (unsigned integers)
+//    Celočíselné datové typy bez znaménka (unsigned integers).
 //
 // 04_integer_constants.go:
-//    Celočíselné konstanty reprezentované v různých číselných soustavách
+//    Celočíselné konstanty reprezentované v různých číselných soustavách.
 //
 // 05_improper_conversion.go:
-//    Přiřazení uint8 -> uint16 ve zdrojovém kódu
+//    Přiřazení uint8 -> uint16 ve zdrojovém kódu.
 //
 // 06_improper_conversion.go:
-//    Přiřazení uint16 -> uint8 ve zdrojovém kódu
+//    Přiřazení uint16 -> uint8 ve zdrojovém kódu.
 //
 // 07_improper_conversion_int_uint.go:
-//    Přiřazení uint8 -> int8 a naopak
+//    Přiřazení uint8 -> int8 a naopak.
 //
 // 08_explicit_conversions.go:
-//    Explicitní konverze mezi základními datovými typy
+//    Explicitní konverze mezi základními datovými typy.
 //
 // 09_formatting_output.go:
-//    Základní formátování celočíselných hodnot na výstupu
+//    Základní formátování celočíselných hodnot na výstupu.
 //
 // 10_formatting_output.go:
-//    Základní formátování celočíselných hodnot na výstupu
+//    Základní formátování celočíselných hodnot na výstupu.
 //
 // 11_fp_types.go:
-//    Datové typy reprezentující hodnoty s plovoucí řádovou čárkou
+//    Datové typy reprezentující hodnoty s plovoucí řádovou čárkou.
 //
 // 12_fp_types_checks.go:
-//    Datové typy reprezentující hodnoty s plovoucí řádovou čárkou, kontrola rozsahu
+//    Datové typy reprezentující hodnoty s plovoucí řádovou čárkou, kontrola rozsahu.
 //
 // 13_complex_types.go:
-//    Datové typy reprezentující hodnoty s plovoucí řádovou čárkou
+//    Datové typy reprezentující hodnoty s plovoucí řádovou čárkou.
 //
 // 14_boolean_type.go:
-//    Datový typ Boolean
+//    Datový typ Boolean.
 //
 // 15_boolean_type_checks.go:
-//    Datový typ Boolean - kontroly při překladu
+//    Datový typ Boolean - kontroly při překladu.
 //
 // 16_string_type.go:
-//    Datový typ řetězec
+//    Datový typ řetězec.
 //
 // 17_raw_strings.go:
-//    Datový typ řetězec
+//    Datový typ řetězec.
 //
 // 18_string_content.go:
-//    Datový typ řetězec a jeho obsah
+//    Datový typ řetězec a jeho obsah.
 //
 // 19_local_variables.go:
-//    Lokální proměnné
+//    Lokální proměnné.
 //
 // 20_arrays.go:
-//    Základní operace s poli
+//    Základní operace s poli.
 //
 // 21_array_copy.go:
-//    Kopie polí
+//    Kopie polí.
 //
 // 22_slices.go:
-//    Řezy polí
+//    Řezy polí.
 //
 // 23_slice_copy.go:
-//    Kopie řezů polí, modifikace obsahu pole i řezu
+//    Kopie řezů polí, modifikace obsahu pole i řezu.
 //
 // 24_slice_from_slice.go:
-//    Kopie řezů polí
+//    Kopie řezů polí.
 //
 
 // ----------------------------------------------------
