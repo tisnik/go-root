4a4ad8ae868b3bcdfbc9768449c561ffa3d785ab
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-17 08:36:20 +0200

    Index file with examples for article #5

--- 00_index.go
+++ 00_index.go
@@ -295,3 +295,113 @@
 // 20_workers_deadlock.go:
 //    Deadlock, který může nastat při volání gorutin a komunikace přes kanály.
 //
+
+// ----------------------------------------------------
+// Adresář: article_05
+// ----------------------------------------------------
+//
+// 01_main_function_only.go:
+//    Funkce main() jako jediný uživatelsky definovaný prvek aplikace.
+//
+// 02_return_statement.go:
+//    Příkaz return ve funkci bez návratové hodnoty.
+//
+// 03_return_statement_no_value.go:
+//    Příkaz return ve funkci bez návratové hodnoty, korektní použití.
+//
+// 04_return_statement_int_value.go:
+//    Příkaz return ve funkci s návratovou hodnotou, nekorektní použití.
+//
+// 05_return_statement_int_value.go:
+//    Příkaz return ve funkci s návratovou hodnotou, korektní použití.
+//
+// 06_if_statement.go:
+//    Řídicí konstrukce typu if.
+//
+// 07_if_statement_bad_type.go:
+//    Řídicí konstrukce if a hodnoty, které nejsou typu boolean.
+//
+// 08_if_statement_nil.go:
+//    Řídicí konstrukce if a hodnoty, které nejsou typu boolean.
+//
+// 09_if_else_construct.go:
+//    Řídicí konstrukce typu if-else.
+//
+// 10_bad_syntax.go:
+//    Příklady špatné syntaxe konstrukce typu if-else.
+//
+// 10B_statement_in_if.go:
+//    Příkaz zapsaný za klíčovým slovem if.
+//
+// 10C_statement_in_if_not_visible.go:
+//    Příkaz zapsaný za klíčovým slovem if, hodnota není viditelná za if.
+//
+// 11_switch_statement.go:
+//    Řídicí konstrukce typu switch bez vyhodnocovaného výrazu.
+//
+// 12_constants_in_switch.go:
+//    Řídicí konstrukce typu switch s vyhodnocovaným výrazem.
+//
+// 13_variables_in_switch.go:
+//    Řídicí konstrukce typu switch s vyhodnocovaným výrazem.
+//
+// 14_conditions_in_switch.go:
+//    Řídicí konstrukce typu switch s výrazy ve větvích case.
+//
+// 15_no_fallthrough_in_switch.go:
+//    Řídicí konstrukce switch - pracuje jinak než v C!
+//
+// 16_fallthrough_in_switch.go:
+//    Řídicí konstrukce switch - simulace chování C.
+//
+// 17_switch_combinations.go:
+//    Řídicí konstrukce switch: kombinace podmínek a fallthrough.
+//
+// 18_switch_on_string.go:
+//    Řídicí konstrukce switch s vyhodnocovaným výrazem typu string.
+//
+// 19_simplest_for_loop.go:
+//    Základní forma programové smyčky typu for.
+//
+// 20_basic_for_loop.go:
+//    Základní forma programové smyčky typu for s podmínkou.
+//
+// 21_for_loop_with_condition.go:
+//    Základní forma programové smyčky typu for s podmínkou.
+//
+// 22_c_like_loop.go:
+//    Programová smyčka for odvozená od C.
+//
+// 23_better_for.go:
+//    Zjednodušená forma programové smyčky for odvozené od C.
+//
+// 24_better_for_local_variable.go:
+//    Zjednodušená forma programové smyčky for odvozené od C.
+//
+// 25_for_range.go:
+//    Smyčka for s range.
+//
+// 25B_for_range_without_index.go:
+//    Varianta s nepoužitým indexem.
+//
+// 25C_for_range_without_index.go:
+//    Smyčka for s range.
+//
+// 26_for_range_map.go:
+//    For s range prováděným nad mapou.
+//
+// 27_break_statement.go:
+//    Příkaz break.
+//
+// 28_continue_statement.go:
+//    Příkaz continue.
+//
+// 29_nested_loops.go:
+//    Dvojice vnořených programových smyček for.
+//
+// 30_break_from_inner_loop.go:
+//    Dvojice vnořených smyček for a příkaz break.
+//
+// 31_continue_from_inner_loop.go:
+//    Dvojice vnořených smyček for a příkaz continue.
+//
