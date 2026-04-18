03cea6f36dc740c41376bd3f0c50095d90a40e4e
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-18 09:09:16 +0200

    Updated example description

--- article_05/13_variables_in_switch.go
+++ article_05/13_variables_in_switch.go
@@ -12,7 +12,7 @@
 //    https://github.com/tisnik/go-root/blob/master/article_05/README.md
 //
 // Demonstrační příklad číslo 13:
-//    Řídicí konstrukce switch s vyhodnocovaným výrazem.
+//    Řídicí konstrukce typu switch s vyhodnocovaným výrazem.
 //
 // Dokumentace ve stylu "literate programming":
 //    https://tisnik.github.io/go-root/article_05/13_variables_in_switch.html
