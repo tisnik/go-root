8d059c187ad271e79bb9dd4c093e8d4f3560a72a
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-19 09:16:39 +0200

    Updated description

--- article_06/06_goto_bad_usage_D.go
+++ article_06/06_goto_bad_usage_D.go
@@ -12,7 +12,7 @@
 //    https://github.com/tisnik/go-root/blob/master/article_06/README.md
 //
 // Demonstrační příklad číslo 6B:
-//    Špatné použití příkazu goto.
+//    Špatné použití příkazu goto - skok do jiné funkce.
 //
 // Dokumentace ve stylu "literate programming":
 //    https://tisnik.github.io/go-root/article_06/06_goto_bad_usage_D.html
