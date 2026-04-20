808c6e5ea23da7db4b792a3da4809780d534e427
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-20 09:33:15 +0200

    Added description

--- tinygo-rp2040/terminal2.go
+++ tinygo-rp2040/terminal2.go
@@ -1,3 +1,17 @@
+// Seriál "Programovací jazyk Go"
+//    https://www.root.cz/serialy/programovaci-jazyk-go/
+//
+// Repositář:
+//    https://github.com/tisnik/go-root/
+//
+// Demonstrační příklad použitý ve článcích:
+//    Využití TinyGo při programování Raspberry Pi Pico: od GPIO až k PWM
+//    https://www.root.cz/clanky/vyuziti-tinygo-pri-programovani-raspberry-pi-pico-od-gpio-az-k-pwm/
+//
+//    Využití TinyGo při programování Raspberry Pi Pico (2. část)
+//    https://www.root.cz/clanky/vyuziti-tinygo-pri-programovani-raspberry-pi-pico-2-cast/
+//
+
 package main
 
 import "fmt"
