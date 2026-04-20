feffa836fff7ab6e2f205c1680aa7aef8ea85026
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-20 09:32:53 +0200

    Index file with examples for article #6

--- 00_index.go
+++ 00_index.go
@@ -411,3 +411,94 @@
 // 31_continue_from_inner_loop.go:
 //    Dvojice vnořených smyček for a příkaz continue.
 //
+
+
+
+// ----------------------------------------------------
+// Adresář: article_06
+// ----------------------------------------------------
+//
+// 01_goto.go:
+//    Použití příkazu goto.
+//
+// 02_goto_from_inner_loop.go:
+//    Dvojice vnořených smyček for a příkaz goto pro jejich opuštění.
+//
+// 03_goto_from_switch.go:
+//    Výskok z příkazu switch pomocí goto.
+//
+// 04_goto_bad_usage_A.go:
+//    Špatné použití příkazu goto - přeskakuje se deklarace proměnné.
+//
+// 05_goto_bad_usage_B.go:
+//    Špatné použití příkazu goto - skok do programového bloku.
+//
+// 06_goto_bad_usage_C.go:
+//    Špatné použití příkazu goto - skok do programové smyčky.
+//
+// 06_goto_bad_usage_D.go:
+//    Špatné použití příkazu goto - skok do jiné funkce.
+//
+// 06_goto_local_labels.go:
+//    Lokální návěští použité v příkazu goto.
+//
+// 07_defer.go:
+//    Použití příkazu defer - odložené volání funkce.
+//
+// 08_defer_func.go:
+//    Použití příkazu defer - odložené volání anonymní funkce.
+//
+// 09_defer_with_parameters.go:
+//    Použití příkazu defer, odložené zavolání funkce s parametrem.
+//
+// 10_more_defers.go:
+//    Zjištění pořadí volání funkcí deklarovaných v defer().
+//
+// 11_defer_arguments_evaluation.go:
+//    Zjištění, kdy se vyhodnocují výraz pro volání funkce v defer().
+//
+// 12_defer_arguments_evaluation.go:
+//    Zjištění, kdy se vyhodnocují výraz pro volání funkce v defer().
+//
+// 13_defer_on_all_returns.go:
+//    Defer a příkaz return.
+//
+// 14_defer_practical_usage.go:
+//    Praktické použití konstrukce defer.
+//
+// 15_defer_practical_usage.go:
+//    Praktické použití konstrukce defer.
+//
+// 16_defer_return_values.go:
+//    Modifikace návratové hodnoty funkce v sekci defer.
+//
+// 17_panic.go:
+//    Praktické použití konstrukce defer a funkce panic.
+//
+// 18_panic_recover.go:
+//    Praktické použití konstrukce defer a funkcí panic i recover.
+//
+// 19_unary_operators.go:
+//    Všechny unární operátory jazyka Go.
+//
+// 20_relational_operators.go:
+//    Relační operátory.
+//
+// 21_div_mod.go:
+//    Dělení a zbytek po dělení.
+//
+// 22_div_by_zero.go:
+//    Dělení nulou a zbytek po dělení nulou.
+//
+// 23_bit_shift_negative_shift.go:
+//    Bitové posuny s negativním druhým operandem.
+//
+// 24_bit_shifts.go:
+//    Bitové posuny.
+//
+// 25_bit_shift_assignment.go:
+//    Bitové posuny zkombinované s přiřazením.
+//
+// 26_bit_operators.go:
+//    Bitové operátory se dvěma operandy.
+//
