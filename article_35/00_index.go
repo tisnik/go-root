b52c8ceb042e7cfbc72003dc3540dbafd8141444
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-21 09:56:57 +0200

    Index file

---
+++ article_35/00_index.go
@@ -0,0 +1,54 @@
+// 01_booleans.go:
+//     Pravdivostní hodnoty v jazyce Go.
+//
+// 02_boolean_type_checks.go:
+//     Převod různých hodnot na typ bool.
+//
+// 03_strings.go:
+//     Práce s řetězci v jazyce Go.
+//
+// 03_strings_B.go:
+//     Práce s řetězci v jazyce Go.
+//
+// 04_immutable_strings.go:
+//     Řetězce jsou v Go neměnitelné.
+//
+// 05_mutable_strings.go:
+//    Alternativa k měnitelným řetězcům.
+//
+// 06_mutable_utf8_strings.go:
+//     Přístup k bajtům řetězce v UTF-8.
+//
+// 06_mutable_utf8_strings_B.go:
+//     Přístup k bajtům řetězce v UTF-8.
+//
+// 07_string_comparison.go:
+//     Porovnávání řetězců v Go.
+//
+// 07_string_comparison_B.go:
+//     Porovnávání řetězců v Go bez podpory češtiny.
+//
+// 07_string_comparison_C.go:
+//     Porovnávání řetězců v Go s podporou češtiny.
+//
+// 08_string_concatenation.go:
+//     Spojení řetězců.
+//
+// 09_substring.go:
+//
+// 09_substring_B.go:
+//
+// 09_substring_C.go:
+//
+// 10_arrays.go:
+//
+// 10_arrays_C.go:
+//
+// 11_array_copy.go:
+//
+// 12_io_reader_read_byte.go:
+//
+// 13_io_reader_read_ascii_char.go:
+//
+// 14_io_reader_read_block.go:
+//
