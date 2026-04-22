8e2f895707df84e9e4da41c5ce6cb663735f3566
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-22 08:26:12 +0200

    Updated example description

--- article_07/11_constants.go
+++ article_07/11_constants.go
@@ -12,7 +12,7 @@
 //    https://github.com/tisnik/go-root/blob/master/article_07/README.md
 //
 // Demonstrační příklad číslo 11:
-//    Konstanty.
+//    Deklarace globálních konstant.
 //
 // Dokumentace ve stylu "literate programming":
 //    https://tisnik.github.io/go-root/article_07/11_constants.html
