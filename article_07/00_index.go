68b1f0f772fd922b938aa53bd09384de76b157c9
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-22 08:26:12 +0200

    Updated index file

--- article_07/00_index.go
+++ article_07/00_index.go
@@ -5,58 +5,58 @@
 //    Bitové operátory.
 //
 // 02_boolean_operators.go:
-//    Logické operátory.
+//    Logické operátory, korektní použití.
 //
 // 02A_boolean_operators_short_circuit.go:
-//    Logické operátory.
+//    Logické operátory, zkrácené vyhodnocování výrazů.
 //
 // 02B_boolean_operators_improper_usage.go:
 //    Nekorektní použití operátorů na pravdivostní hodnoty.
 //
 // 02C_boolean_operators_improper_usage.go:
-//    Logické operátory.
+//    Logické operátory, nekorektní použití.
 //
 // 03_inc_dec.go:
-//    Operátory ++ a --.
+//    Operátory ++ a -- (inkrementace a dekrementace).
 //
 // 04_inc_dec_bad_usage.go:
-//    Nekorektní použití operátorů ++ a --.
+//    Nekorektní použití operátorů ++ a -- (inkrementace a dekrementace).
 //
 // 05_inc_dec_bad_usage.go:
-//    Nekorektní použití operátorů ++ a --.
+//    Nekorektní použití operátorů ++ a -- (inkrementace a dekrementace).
 //
 // 06_select_statement_receive.go:
-//    Použití příkazu select pro čtení z kanálů.
+//    Použití příkazu select pro čtení hodnot z kanálů.
 //
 // 07_select_statement_receive_default.go:
-//    Použití příkazu select pro čtení z kanálů.
+//    Použití příkazu select pro čtení hodnot z kanálů.
 //
 // 08_select_statement_send.go:
-//    Použití příkazu select pro zápis do kanálu.
+//    Použití příkazu select pro zápis hodnot do kanálu.
 //
 // 09_select_statement_send.go:
-//    Použití příkazu select pro zápis do kanálu.
+//    Použití příkazu select pro zápis hodnot do kanálu.
 //
 // 09B_select_statement_send.go:
-//    Použití příkazu select pro zápis do kanálu.
+//    Použití příkazu select pro zápis hodnot do kanálu.
 //
 // 10_select_statement_send_receive.go:
-//    Použití příkazu select pro zápis do kanálu i pro čtení z kanálu.
+//    Použití příkazu select pro zápis hodnot do kanálu i pro čtení hodnot z kanálu.
 //
 // 11_constants.go:
-//    Konstanty.
+//    Deklarace globálních konstant.
 //
 // 12_constants.go:
-//    Konstanty.
+//    Deklarace globálních konstant.
 //
 // 13_iota.go:
 //    Konstanty a identifikátor iota.
 //
 // 14_iota_better_usage.go:
-//    Konstanty a identifikátor iota.
+//    Konstanty a identifikátor iota, lepší způsob využití.
 //
 // 15_iota_another_usage.go:
-//    Konstanty a identifikátor iota.
+//    Konstanty a identifikátor iota, komplikovanější příklady.
 //
 // 16_variadic_function.go:
 //    Variadické funkce.
