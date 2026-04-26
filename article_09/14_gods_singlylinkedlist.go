1ddd846f8f0878c34192600c3ebc1858b195b2f5
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-26 10:46:22 +0200

    Updated description

--- article_09/14_gods_singlylinkedlist.go
+++ article_09/14_gods_singlylinkedlist.go
@@ -12,6 +12,7 @@
 //    https://github.com/tisnik/go-root/blob/master/article_09/README.md
 //
 // Demonstrační příklad číslo 14:
+//    Použití balíčku "gods"
 //    Datová struktura singlylinkedlist z knihovny GoDS.
 //
 // Dokumentace ve stylu "literate programming":
