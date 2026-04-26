1133bee277d529a20591b12f340c5fe87863b69e
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-26 10:46:14 +0200

    Updated description

--- article_09/12_list_as_stack.go
+++ article_09/12_list_as_stack.go
@@ -12,7 +12,8 @@
 //    https://github.com/tisnik/go-root/blob/master/article_09/README.md
 //
 // Demonstrační příklad číslo 12:
-//    Použití obousměrně vázaného seznamu ve funkci zásobníku.
+//    Použití standardního balíčku "container/list"
+//    Obousměrně vázaný seznam ve funkci zásobníku.
 //
 // Dokumentace ve stylu "literate programming":
 //    https://tisnik.github.io/go-root/article_09/12_list_as_stack.html
