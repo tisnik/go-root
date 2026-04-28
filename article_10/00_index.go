a97c9f79c3c5713adf3a7647c6ddb4deadcc36f3
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-28 08:40:40 +0200

    Updated description

--- article_10/00_index.go
+++ article_10/00_index.go
@@ -2,38 +2,47 @@
 // ------------------------------------------
 //
 // 01_cmdline_params.go:
+//    Využití standardního balíčku "os".
 //    Přečtení argumentů předaných na příkazovém řádku.
 //
 // 02_flags.go:
+//    Využití standardního balíčku "flag".
 //    Vylepšené přečtení argumentů předaných na příkazovém řádku.
 //
 // 03_flag_shorthands.go:
+//    Využití standardního balíčku "flag".
 //    Vylepšené přečtení argumentů předaných na příkazovém řádku.
 //
 // 04_bigint.go:
-//    Použití balíčku big, typ Int.
+//    Použití balíčku big, konkrétně typu Int.
 //
 // 05_factorial.go:
-//    Použití balíčku big pro výpočet faktoriálu.
+//    Použití balíčku big a typu Int pro výpočet faktoriálu.
 //
 // 06_bigfloat.go:
-//    Použití balíčku big, typ Float.
+//    Použití balíčku big, konkrétně typu Float.
 //
 // 07_exec.go:
-//    Použití balíčku exec, spuštění externí utility.
+//    Použití balíčku os/exec.
+//    Spuštění externí utility.
 //
 // 08_exec_stdin.go:
-//    Použití balíčku exec, spuštění externí utility s předáním dat.
+//    Použití balíčku os/exec.
+//    Spuštění externí utility s předáním dat.
 //
 // 09_exec_stdin.go:
-//    Použití balíčku exec, spuštění externí utility s předáním dat.
+//    Použití balíčku os/exec.
+//    Spuštění externí utility s předáním dat.
 //
 // 10_env_var.go:
+//    Použití balíčku os.
 //    Přístup k proměnným prostředí.
 //
 // 11_process_info.go:
+//    Použití balíčku os.
 //    Získání základních informací o procesu.
 //
 // 12_file_operations.go:
+//    Použití balíčku os.
 //    Základní operace se soubory.
 //
