50cc77979ffdd435e08ad5a408006b60095a5dc6
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-29 08:44:52 +0200

    Index file with examples for article #10

--- 00_index.go
+++ 00_index.go
@@ -737,3 +737,52 @@
 
 
 
+// ----------------------------------------------------
+// Adresář: article_10
+// ----------------------------------------------------
+//
+// 01_cmdline_params.go:
+//    Využití standardního balíčku "os".
+//    Přečtení argumentů předaných na příkazovém řádku.
+//
+// 02_flags.go:
+//    Využití standardního balíčku "flag".
+//    Vylepšené přečtení argumentů předaných na příkazovém řádku.
+//
+// 03_flag_shorthands.go:
+//    Využití standardního balíčku "flag".
+//    Vylepšené přečtení argumentů předaných na příkazovém řádku.
+//
+// 04_bigint.go:
+//    Použití balíčku big, konkrétně typu Int.
+//
+// 05_factorial.go:
+//    Použití balíčku big a typu Int pro výpočet faktoriálu.
+//
+// 06_bigfloat.go:
+//    Použití balíčku big, konkrétně typu Float.
+//
+// 07_exec.go:
+//    Použití balíčku os/exec.
+//    Spuštění externí utility.
+//
+// 08_exec_stdin.go:
+//    Použití balíčku os/exec.
+//    Spuštění externí utility s předáním dat.
+//
+// 09_exec_stdin.go:
+//    Použití balíčku os/exec.
+//    Spuštění externí utility s předáním dat.
+//
+// 10_env_var.go:
+//    Použití balíčku os.
+//    Přístup k proměnným prostředí.
+//
+// 11_process_info.go:
+//    Použití balíčku os.
+//    Získání základních informací o procesu.
+//
+// 12_file_operations.go:
+//    Použití balíčku os.
+//    Základní operace se soubory.
+//
