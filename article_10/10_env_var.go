92c12b785e2c5bbd71a06c869b2ee0c85691143d
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-29 08:44:52 +0200

    Updated description

--- article_10/10_env_var.go
+++ article_10/10_env_var.go
@@ -12,6 +12,7 @@
 //    https://github.com/tisnik/go-root/blob/master/article_10/README.md
 //
 // Demonstrační příklad číslo 10:
+//    Použití balíčku os.
 //    Přístup k proměnným prostředí.
 //
 // Dokumentace ve stylu "literate programming":
