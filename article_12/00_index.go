7bcefb7dec0b34762b7b690523809e29980a9172
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-02 09:41:35 +0200

    Regenerated index file

--- article_12/00_index.go
+++ article_12/00_index.go
@@ -2,41 +2,61 @@
 // ------------------------------------------
 //
 // 01_simple_http_server.go:
-//    Jednoduchý HTTP server
+//    Standardní balíček "net/http".
+//    Jednoduchý HTTP server.
 //
 // 02_simple_http_server_write_bytes.go:
-//    Alternativní způsob vytvoření sekvence bajtů z řetězce
+//    Standardní balíček "net/http".
+//    Alternativní způsob vytvoření sekvence bajtů z řetězce.
 //
 // 03_specify_content_type.go:
-//     HTTP server se specifikací MIME typu odpovědí
+//    Standardní balíček "net/http".
+//    HTTP server se specifikací MIME typu odpovědí.
 //
 // 04_serve_static_html_page.go:
-//     Server, který vrací statické HTML stránky načtené ze souborů
+//    Standardní balíček "net/http".
+//    Server, který vrací statické HTML stránky načtené ze souborů.
 //
 // 04_static_html_page.go:
-//     Server, který vrací statické HTML stránky načtené ze souborů
+//    Standardní balíček "net/http".
+//    Server, který vrací statické HTML stránky načtené ze souborů.
 //
 // 05_serve_static_html_page.go:
-//     Úprava předchozího příkladu
+//    Standardní balíček "net/http".
+//    Server, který vrací statické HTML stránky načtené ze souborů.
+//    Úprava předchozího demonstračního příkladu.
 //
 // 05_static_html_page.go:
-//    Úprava předchozího příkladu
+//    Standardní balíček "net/http".
+//    Server, který vrací statické HTML stránky načtené ze souborů.
+//    Úprava předchozího příkladu.
 //
 // 06_serve_static_html_page_closure.go:
-//    Využití uzávěrů pro zápis kratšího kódu
+//    Standardní balíček "net/http".
+//    Využití uzávěrů pro zápis kratšího kódu.
 //
 // 07_html_templates.go:
-//    Základní použití šablon
+//    Standardní balíček "net/http".
+//    Standardní balíček "html/template".
+//    Základní použití šablon.
 //
 // 08_factorial_prepare.go:
-//    Webová aplikace pro výpočet faktoriálu (příprava)
+//    Standardní balíček "net/http".
+//    Standardní balíček "html/template".
+//    Webová aplikace pro výpočet faktoriálu (příprava).
 //
 // 09_factorial_compute.go:
-//    Druhá varianta webové aplikace pro výpočet faktoriálu
+//    Standardní balíček "net/http".
+//    Standardní balíček "html/template".
+//    Druhá varianta webové aplikace pro výpočet faktoriálu.
 //
 // 10_factorial_compute_const_template.go:
-//     Neměnná šablona stránky
+//    Standardní balíček "net/http".
+//    Standardní balíček "html/template".
+//    Neměnná šablona stránky.
 //
 // 11_https_server.go:
-//     Jednoduchý HTTPS server
+//    Standardní balíček "net/http".
+//    Standardní balíček "html/template".
+//    Jednoduchý HTTPS server.
 //
