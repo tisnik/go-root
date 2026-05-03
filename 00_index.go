dc5d4eda02a99f347ee4c5c9dea0d9d0d2f419a7
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-03 10:02:15 +0200

    Index file with examples for article #12

--- 00_index.go
+++ 00_index.go
@@ -873,3 +873,72 @@
 //    Standardní balíček "net".
 //    Kombinace předchozích možností
 //
+
+
+
+// ----------------------------------------------------
+// Adresář: article_12
+// ----------------------------------------------------
+//
+// 01_simple_http_server.go:
+//    Standardní balíček "net/http".
+//    Jednoduchý HTTP server.
+//
+// 02_simple_http_server_write_bytes.go:
+//    Standardní balíček "net/http".
+//    Alternativní způsob vytvoření sekvence bajtů z řetězce.
+//
+// 03_specify_content_type.go:
+//    Standardní balíček "net/http".
+//    HTTP server se specifikací MIME typu odpovědí.
+//
+// 04_serve_static_html_page.go:
+//    Standardní balíček "net/http".
+//    Server, který vrací statické HTML stránky načtené ze souborů.
+//
+// 04_static_html_page.go:
+//    Standardní balíček "net/http".
+//    Server, který vrací statické HTML stránky načtené ze souborů.
+//
+// 05_serve_static_html_page.go:
+//    Standardní balíček "net/http".
+//    Server, který vrací statické HTML stránky načtené ze souborů.
+//    Úprava předchozího demonstračního příkladu.
+//
+// 05_static_html_page.go:
+//    Standardní balíček "net/http".
+//    Server, který vrací statické HTML stránky načtené ze souborů.
+//    Úprava předchozího příkladu.
+//
+// 06_serve_static_html_page_closure.go:
+//    Standardní balíček "net/http".
+//    Využití uzávěrů pro zápis kratšího kódu.
+//
+// 07_html_templates.go:
+//    Standardní balíček "net/http".
+//    Standardní balíček "html/template".
+//    Základní použití šablon.
+//
+// 08_factorial_prepare.go:
+//    Standardní balíček "net/http".
+//    Standardní balíček "html/template".
+//    Webová aplikace pro výpočet faktoriálu (příprava).
+//
+// 09_factorial_compute.go:
+//    Standardní balíček "net/http".
+//    Standardní balíček "html/template".
+//    Druhá varianta webové aplikace pro výpočet faktoriálu.
+//
+// 10_factorial_compute_const_template.go:
+//    Standardní balíček "net/http".
+//    Standardní balíček "html/template".
+//    Neměnná šablona stránky.
+//
+// 11_https_server.go:
+//    Standardní balíček "net/http".
+//    Standardní balíček "html/template".
+//    Jednoduchý HTTPS server.
+//
+
+
+
