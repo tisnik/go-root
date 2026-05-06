747c48380200e911cb28c40adcfcabd64c2722e2
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-06 08:33:55 +0200

    Updated description

--- article_14/00_index.go
+++ article_14/00_index.go
@@ -2,59 +2,78 @@
 // ------------------------------------------
 //
 // 01_empty_image.go:
-//     Vytvoření rastrového obrázku s výchozími hodnotami pixelů (zcela průhledné černé pixely)
+//     Standardní balíčky image a image/png.
+//     Vytvoření rastrového obrázku s výchozími hodnotami pixelů (zcela průhledné černé pixely).
 //
 // 02_empty_image_rectangle.go:
-//     Alternativní způsob vytvoření obdélníku pro určení rozměrů obrázku
+//     Standardní balíčky image a image/png.
+//     Alternativní způsob vytvoření obdélníku pro určení rozměrů obrázku.
 //
 // 03_filled_image_setrgba.go:
-//     Rastrový obrázek vyplněný konstantní barvou, použití metody SetRGBA
+//     Standardní balíčky image, image/color a image/png.
+//     Rastrový obrázek vyplněný konstantní barvou, použití metody SetRGBA.
 //
 // 04_filled_image_set.go:
-//     Rastrový obrázek vyplněný konstantní barvou, použití metody Set
+//     Standardní balíčky image, image/color a image/png.
+//     Rastrový obrázek vyplněný konstantní barvou, použití metody Set.
 //
 // 05_filled_image_alpha.go:
-//     Obrázek, v němž mají pixely různou průhlednost (nekorektní varianta)
+//     Standardní balíčky image, image/color a image/png.
+//     Obrázek, v němž mají pixely různou průhlednost (nekorektní varianta).
 //
 // 06_filled_image_alpha_correct.go:
-//     Korektní varianta předchozího příkladu
+//     Standardní balíčky image, image/color a image/png.
+//     Korektní varianta předchozího příkladu.
 //
 // 07_filled_image_alpha_correct.go:
-//     Vylepšená varianta předchozího příkladu
+//     Standardní balíčky image, image/color a image/png.
+//     Vylepšená varianta předchozího příkladu.
 //
 // 08_image_internals.go:
-//     Interní struktura záznamu s informacemi o obrázku
+//     Standardní balíček image.
+//     Interní struktura záznamu s informacemi o obrázku.
 //
 // 09_raw_pixels.go:
-//     Přímý přístup k jednotlivým pixelům; první varianta
+//     Standardní balíčky image a image/png.
+//     Přímý přístup k jednotlivým pixelům; první varianta.
 //
 // 10_raw_pixels2.go:
-//     Přímý přístup k jednotlivým pixelům; druhá varianta
+//     Standardní balíčky image a image/png.
+//     Přímý přístup k jednotlivým pixelům; druhá varianta.
 //
 // 11_raw_pixels3.go:
-//     Přímý přístup k jednotlivým pixelům; třetí varianta
+//     Standardní balíčky image a image/png.
+//     Přímý přístup k jednotlivým pixelům; třetí varianta.
 //
 // 12_point_rectangle.go:
-//     Datové struktury Point a Rectangle
+//     Standardní balíček image.
+//     Datové struktury Point a Rectangle.
 //
 // 13_point_rectangle_operations.go:
-//     Základní operace s obdélníky a body
+//     Standardní balíček image.
+//     Základní operace s obdélníky a body.
 //
 // 14_rgba_images.go:
-//     Základní vlastnosti barvového prostoru RGBA
+//     Standardní balíčky image, image/color a image/png.
+//     Základní vlastnosti barvového prostoru RGBA.
 //
 // 15_cmyk_images.go:
-//     Základní vlastnosti barvového prostoru CMYK
+//     Standardní balíčky image, image/color a image/png.
+//     Základní vlastnosti barvového prostoru CMYK.
 //
 // 16_cmyk_images.go:
-//     Základní vlastnosti barvového prostoru CMYK
+//     Standardní balíčky image, image/color a image/png.
+//     Základní vlastnosti barvového prostoru CMYK.
 //
 // 17_chessboard.go:
-//     Využití balíčku draw pro vykreslení šachovnice (rastrové operace)
+//     Standardní balíčky image, image/color, image/draw a image/png.
+//     Využití balíčku draw pro vykreslení šachovnice (rastrové operace).
 //
 // 18_bresenham_algorithm.go:
-//     Implementace Bresenhamova algoritmu pro vykreslení úsečky
+//     Standardní balíčky image, image/color a image/png.
+//     Implementace Bresenhamova algoritmu pro vykreslení úsečky.
 //
 // 19_blending.go:
-//     Blending v Go (opět rastrové operace)
+//     Standardní balíčky image, image/color, image/draw a image/png.
+//     Blending v Go (opět rastrové operace).
 //
