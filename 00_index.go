ba3fc4067a5ee0651020970aebea87c9dfb95d08
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-08 09:57:56 +0200

    Index file with examples for article #15

--- 00_index.go
+++ 00_index.go
@@ -1121,3 +1121,90 @@
 //     Standardní balíčky image, image/color, image/draw a image/png.
 //     Blending v Go (opět rastrové operace).
 //
+
+
+
+// ----------------------------------------------------
+// Adresář: article_15
+// ----------------------------------------------------
+//
+// 01_blending.go:
+//     Standardní balíčky image, image/draw a image/png.
+//     Ukázka blendingu.
+//
+// 02_gif_export_rgba.go:
+//     Standardní balíčky image, image/draw a image/gif.
+//     Export obrázku šachovnice do formátu GIF.
+//
+// 03_gif_1x1_rgba.go:
+//     Standardní balíčky image a image/gif.
+//     Export obrázku o rozlišení 1×1 pixel s 256 barvovou paletou.
+//
+// 04_gif_1x1_palette.go:
+//     Standardní balíčky image a image/gif.
+//     Export obrázku o rozlišení 1×1 pixel s dvoubarevnou paletou.
+//
+// 05_gif_export_colors.go:
+//     Standardní balíčky image, image/draw a image/gif.
+//     Export se specifikací počtu barev v paletě.
+//
+// 06_gif_export_palette.go:
+//     Standardní balíčky image, image/draw a image/gif.
+//     Šachovnice vykreslená s dvoubarevnou paletou.
+//
+// 07_rgba_to_palette.go:
+//     Standardní balíčky image, image/color a image/gif.
+//     Převod obrázku z prostoru RGBA do prostoru s paletou se snižováním počtu barev.
+//
+// 08_gif_animation_blink.go:
+//     Standardní balíčky image, image/color a image/gif.
+//     Jednoduchá animace se dvěma snímky o velikosti 32×32 pixelů.
+//
+// 09_gif_animation.go:
+//     Standardní balíčky image, image/color a image/gif.
+//     Složitější animace pohybující se šachovnice.
+//
+// 10_default_disposal_methods.go:
+//     Standardní balíčky image, image/color a image/gif.
+//     Výchozí hodnota metody použité při přepínání snímků.
+//
+// 11_background_disposal_methods.go:
+//     Standardní balíčky image, image/color a image/gif.
+//     Nastavení metody přepínání snímků: vykreslení pozadí.
+//
+// 12_none_disposal_methods.go:
+//     Standardní balíčky image, image/color a image/gif.
+//     Nastavení metody přepínání snímků: žádná modifikace snímku.
+//
+// 13_progress_bar.go:
+//     Standardní balíčky image, image/color a image/gif.
+//     Animace progress baru vytvořená jako animovaný GIF.
+//
+// 14_gg_basic.go:
+//     Balíček gg.
+//     Základní způsob použití knihovny GG.
+//
+// 15_gg_context_properties.go:
+//     Balíček gg.
+//     Použití kontextu a nastavení vlastností kreslení.
+//
+// 16_gg_path_stroke.go:
+//     Balíček gg.
+//     Vykreslení cesty.
+//
+// 17_gg_filled_background.go:
+//     Balíček gg.
+//     Vyplnění pozadí konstantní barvou.
+//
+// 18_gg_alpha_rgba.go:
+//     Balíček gg.
+//     Specifikace alfa kanálu (průhlednosti).
+//
+// 19_gg_line_width.go:
+//     Balíče. gg.
+//     Specifikace šířky vykreslovaných cest.
+//
+// 20_gg_set_dash.go:
+//     Balíček gg.
+//     Specifikace stylu vykreslovaných cest.
+//
