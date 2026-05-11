fafcb97e5198f9f91490b002eb3d3c9be6c31c06
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-11 10:17:00 +0200

    Reformatted descriptions for examples from article #13

--- 00_index.go
+++ 00_index.go
@@ -947,96 +947,96 @@
 // ----------------------------------------------------
 //
 // 01_json_marshal_basic_signed_types.go:
-//     Standardní balíček "encoding/json".
-//     Marshalling celých čísel se znaménkem do JSONu.
+//    Standardní balíček "encoding/json".
+//    Marshalling celých čísel se znaménkem do JSONu.
 //
 // 02_json_marshal_basic_unsigned_types.go:
-//     Standardní balíček "encoding/json".
-//     Marshalling celých čísel bez znaménka do JSONu.
+//    Standardní balíček "encoding/json".
+//    Marshalling celých čísel bez znaménka do JSONu.
 //
 // 03_json_marshal_basic_complex_types.go:
-//     Standardní balíček "encoding/json".
-//     Pokus o marshalling komplexních čísel do JSONu.
+//    Standardní balíček "encoding/json".
+//    Pokus o marshalling komplexních čísel do JSONu.
 //
 // 03B_json_marshal_basic_complex_types_err.go:
-//     Standardní balíček "encoding/json".
-//     Pokus o marshalling komplexních čísel do JSONu, kontrola převodu.
+//    Standardní balíček "encoding/json".
+//    Pokus o marshalling komplexních čísel do JSONu, kontrola převodu.
 //
 // 04_json_marshal_arrays.go:
-//     Standardní balíček "encoding/json".
-//     Marshalling jednorozměrných i dvourozměrných polí do JSONu.
+//    Standardní balíček "encoding/json".
+//    Marshalling jednorozměrných i dvourozměrných polí do JSONu.
 //
 // 05_json_marshal_struct.go:
-//     Standardní balíček "encoding/json".
-//     Marshalling struktury/záznamu do JSONu.
+//    Standardní balíček "encoding/json".
+//    Marshalling struktury/záznamu do JSONu.
 //
 // 06_json_mashal_array_of_struct.go:
-//     Standardní balíček "encoding/json".
-//     Marshalling pole struktur/záznamů do JSONu.
+//    Standardní balíček "encoding/json".
+//    Marshalling pole struktur/záznamů do JSONu.
 //
 // 06B_json_mashal_array_of_struct.go:
-//     Standardní balíček "encoding/json".
-//     Marshalling pole struktur/záznamů do JSONu, privátní prvky struktury.
+//    Standardní balíček "encoding/json".
+//    Marshalling pole struktur/záznamů do JSONu, privátní prvky struktury.
 //
 // 07_json_marshal_map_of_structs.go:
-//     Standardní balíček "encoding/json".
-//     Marshalling map struktur/záznamů do JSONu.
+//    Standardní balíček "encoding/json".
+//    Marshalling map struktur/záznamů do JSONu.
 //
 // 08_json_marshal_map_of_structs.go:
-//     Standardní balíček "encoding/json".
-//     Marshalling map struktur/záznamů do JSONu.
+//    Standardní balíček "encoding/json".
+//    Marshalling map struktur/záznamů do JSONu.
 //
 // 09_json_marshal_complex_map.go:
-//     Standardní balíček "encoding/json".
-//     Marshalling map struktur/záznamů.
+//    Standardní balíček "encoding/json".
+//    Marshalling map struktur/záznamů.
 //
 // 10_json_different_keys.go:
-//     Standardní balíček "encoding/json".
-//     Změna názvů klíčů pri marshallingu.
+//    Standardní balíček "encoding/json".
+//    Změna názvů klíčů pri marshallingu.
 //
 // 11_json_marshal_special_values.go:
-//     Standardní balíček "encoding/json".
-//     Pokus o marshalling speciálních hodnot.
+//    Standardní balíček "encoding/json".
+//    Pokus o marshalling speciálních hodnot.
 //
 // 12_json_unmarshal_struct.go:
-//     Standardní balíček "encoding/json".
-//     Unmarshalling struktury z JSONu.
+//    Standardní balíček "encoding/json".
+//    Unmarshalling struktury z JSONu.
 //
 // 13_json_unmarshal_struct.go:
-//     Standardní balíček "encoding/json".
-//     Unmarshalling struktury z JSONu.
+//    Standardní balíček "encoding/json".
+//    Unmarshalling struktury z JSONu.
 //
 // 14_json_unmarshal_array.go:
-//     Standardní balíček "encoding/json".
-//     Unmarshalling pole z JSONu.
+//    Standardní balíček "encoding/json".
+//    Unmarshalling pole z JSONu.
 //
 // 15_json_unmarshal_array_of_struct.go:
-//     Standardní balíček "encoding/json".
-//     Unmarshalling pole struktur.
+//    Standardní balíček "encoding/json".
+//    Unmarshalling pole struktur.
 //
 // 16_json_unmarshal_map_of_struct.go:
-//     Standardní balíček "encoding/json".
-//     Unmarshalling mapy struktur.
+//    Standardní balíček "encoding/json".
+//    Unmarshalling mapy struktur.
 //
 // 17_json_unmarshal_map_of_struct_different_keys.go:
-//     Standardní balíček "encoding/json".
-//     Unmarshalling mapy struktur, specifikace klíčů.
+//    Standardní balíček "encoding/json".
+//    Unmarshalling mapy struktur, specifikace klíčů.
 //
 // 18_json_unmarshal_unknown_struct.go:
-//     Standardní balíček "encoding/json".
-//     Pokus o unmarshalling struktury s obecnými daty.
+//    Standardní balíček "encoding/json".
+//    Pokus o unmarshalling struktury s obecnými daty.
 //
 // 19_png_output.go:
-//     Standardní balíček "image" a "image/png.
-//     Export rastrového obrázku do formátu PNG.
+//    Standardní balíček "image" a "image/png.
+//    Export rastrového obrázku do formátu PNG.
 //
 // 20_jpeg_output.go:
-//     Standardní balíček "image" a "image/jpeg.
-//     Export rastrového obrázku do formátu JPEG.
+//    Standardní balíček "image" a "image/jpeg.
+//    Export rastrového obrázku do formátu JPEG.
 //
 // 21_jpeg_output_low_quality.go:
-//     Standardní balíček "image" a "image/jpeg.
-//     Export rastrového obrázku do formátu JPEG s ovlivněním kvality.
+//    Standardní balíček "image" a "image/jpeg.
+//    Export rastrového obrázku do formátu JPEG s ovlivněním kvality.
 //
 
 
