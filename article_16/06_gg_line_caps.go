282b42b5781f62adba8db952dd6019a31eb88725
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-11 10:15:36 +0200

    Updated example description

--- article_16/06_gg_line_caps.go
+++ article_16/06_gg_line_caps.go
@@ -12,7 +12,8 @@
 //    https://github.com/tisnik/go-root/blob/master/article_16/README.md
 //
 // Demonstrační příklad číslo 6:
-//     Nastavení stylu konců úseček.
+//    Balíček gg.
+//    Nastavení stylu konců úseček.
 //
 // Dokumentace ve stylu "literate programming":
 //    https://tisnik.github.io/go-root/article_16/06_gg_line_caps.html
