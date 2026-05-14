e9bdda20c94e91f060057ea33ea8cdaf34d65b0e
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-14 09:41:01 +0200

    Reformatted descriptions for examples from article #15

--- 00_index.go
+++ 00_index.go
@@ -1216,94 +1216,94 @@
 // ----------------------------------------------------
 //
 // 01_gg_clear_screen.go:
-//     Balíček gg.
-//     Vymazání obrazovky.
+//    Balíček gg.
+//    Vymazání obrazovky.
 //
 // 02_gg_clear_screen.go:
-//     Balíček gg.
-//     Vymazání obrazovky (druhá, lepší varianta).
+//    Balíček gg.
+//    Vymazání obrazovky (druhá, lepší varianta).
 //
 // 03_gg_rgba_color.go:
-//     Balíček gg.
-//     Barvový prostor RGB a RGBA.
+//    Balíček gg.
+//    Barvový prostor RGB a RGBA.
 //
 // 04_gg_alpha_component.go:
-//     Balíček gg.
-//     Alfa kanál v barvovém prostoru RGBA.
+//    Balíček gg.
+//    Alfa kanál v barvovém prostoru RGBA.
 //
 // 05_gg_line_width.go:
-//     Balíček gg.
-//     Nastavení šířky čáry.
+//    Balíček gg.
+//    Nastavení šířky čáry.
 //
 // 06_gg_line_caps.go:
-//     Balíček gg.
-//     Nastavení stylu konců úseček.
+//    Balíček gg.
+//    Nastavení stylu konců úseček.
 //
 // 07_gg_set_dash.go:
-//     Balíček gg.
-//     Nastavení stylu vykreslovaných úseček.
+//    Balíček gg.
+//    Nastavení stylu vykreslovaných úseček.
 //
 // 08_gg_simple_path.go:
-//     Balíček gg.
-//     Práce s cestou (path).
+//    Balíček gg.
+//    Práce s cestou (path).
 //
 // 09_gg_open_path.go:
-//     Balíček gg.
-//     Otevřená cesta (open path).
+//    Balíček gg.
+//    Otevřená cesta (open path).
 //
 // 10_gg_closed_path.go:
-//     Balíček gg.
-//     Uzavřená cesta (closed path).
+//    Balíček gg.
+//    Uzavřená cesta (closed path).
 //
 // 11_gg_quadratic_bezier.go:
-//     Balíček gg.
-//     Kvadratická Bézierova křivka.
+//    Balíček gg.
+//    Kvadratická Bézierova křivka.
 //
 // 12_gg_quadratic_bezier_control_points.go:
-//     Balíček gg.
-//     Kvadratická Bézierova křivka s řídicími body.
+//    Balíček gg.
+//    Kvadratická Bézierova křivka s řídicími body.
 //
 // 13_gg_cubic_bezier.go:
-//     Balíček gg.
-//     Kubická Bézierova křivka.
+//    Balíček gg.
+//    Kubická Bézierova křivka.
 //
 // 14_gg_cubic_bezier_control_points.go:
-//     Balíček gg.
-//     Kubická Bézierova křivka s řídicími body.
+//    Balíček gg.
+//    Kubická Bézierova křivka s řídicími body.
 //
 // 15_gg_rotation.go:
-//     Balíček gg.
-//     Rotace vykreslovaných objektů.
+//    Balíček gg.
+//    Rotace vykreslovaných objektů.
 //
 // 16_gg_to_image.go:
-//     Balíček gg.
-//     Rasterizace do zvoleného rastrového formátu.
+//    Balíček gg.
+//    Rasterizace do zvoleného rastrového formátu.
 //
 // 17_gg_transformation_rotate.go:
-//     Balíček gg.
-//     Transformace (rotace).
+//    Balíček gg.
+//    Transformace (rotace).
 //
 // 18_gg_transformation_rotate_about.go:
-//     Balíček gg.
-//     Transformace (rotace okolo zvoleného bodu).
+//    Balíček gg.
+//    Transformace (rotace okolo zvoleného bodu).
 //
 // 19_gg_scale_about.go:
-//     Balíček gg.
-//     Transformace (změna měřítka okolo zvoleného bodu).
+//    Balíček gg.
+//    Transformace (změna měřítka okolo zvoleného bodu).
 //
 // 20_gg_translate.go:
-//     Balíček gg.
-//     Transformace (posun).
+//    Balíček gg.
+//    Transformace (posun).
 //
 // 21_gg_text.go:
-//     Balíček gg.
-//     Vykreslení textu.
+//    Balíček gg.
+//    Vykreslení textu.
 //
 // 22_gg_text_centered.go:
-//     Balíček gg.
-//     Vykreslení vycentrovaného textu.
+//    Balíček gg.
+//    Vykreslení vycentrovaného textu.
 //
 // 23_gg_text_centered.go:
-//     Balíček gg.
-//     Vykreslení vycentrovaného textu.
+//    Balíček gg.
+//    Vykreslení vycentrovaného textu.
 //
