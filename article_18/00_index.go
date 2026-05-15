49036c7448f9695b5fea423312e5951af6c18115
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-15 09:08:47 +0200

    Created index file for all examples used in article #18

---
+++ article_18/00_index.go
@@ -0,0 +1,73 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// 01_factorial_testing/factorial.go:
+//    Testovaný balíček s výpočtem faktoriálu.
+//
+// 01_factorial_testing/factorial_test.go:
+//    Testy pro balíček s implementací výpočtu faktoriálu.
+//    Testy jsou založeny na standardním balíčku testing.
+//
+// 02_factorial_oglematchers/factorial.go:
+//    Testovaný balíček s výpočtem faktoriálu.
+//
+// 02_factorial_oglematchers/factorial_test.go:
+//    Testy pro balíček s implementací výpočtu faktoriálu.
+//    Testy využívají balíček jacobsa/oglematchers.
+//
+// 03_factorial_oglematchers2/factorial.go:
+//    Testovaný balíček s výpočtem faktoriálu.
+//
+// 03_factorial_oglematchers2/factorial_test.go:
+//    Testy pro balíček s implementací výpočtu faktoriálu.
+//    Testy využívají balíček jacobsa/oglematchers.
+//
+// 04_factorial_ogletest/factorial.go:
+//    Testovaný balíček s výpočtem faktoriálu.
+//
+// 04_factorial_ogletest/factorial_test.go:
+//    Testy pro balíček s implementací výpočtu faktoriálu.
+//    Testy využívají balíčky jacobsa/oglematchers a jacobsa/ogletest.
+//
+// 05_factorial_ogletest2/factorial.go:
+//    Testovaný balíček s výpočtem faktoriálu.
+//
+// 05_factorial_ogletest2/factorial_test.go:
+//    Testy pro balíček s implementací výpočtu faktoriálu.
+//    Testy využívají balíčky jacobsa/oglematchers a jacobsa/ogletest.
+//
+// 06_factorial_assertions/factorial.go:
+//    Testovaný balíček s výpočtem faktoriálu.
+//
+// 06_factorial_assertions/factorial_test.go:
+//    Testy pro balíček s implementací výpočtu faktoriálu.
+//    Testy využívají balíček smartystreets/assertions.
+//
+// 07_factorial_assertions2/factorial.go:
+//    Testovaný balíček s výpočtem faktoriálu.
+//
+// 07_factorial_assertions2/factorial_test.go:
+//    Testy pro balíček s implementací výpočtu faktoriálu.
+//    Testy využívají balíček smartystreets/assertions.
+//
+// 08_factorial_assertions3/factorial.go:
+//    Testovaný balíček s výpočtem faktoriálu.
+//
+// 08_factorial_assertions3/factorial_test.go:
+//    Testy pro balíček s implementací výpočtu faktoriálu.
+//    Testy využívají balíček smartystreets/assertions.
+//
+// 09_factorial_convey/behaviour_test.go:
+//    Testy pro balíček s implementací výpočtu faktoriálu.
+//    Testy využívají balíček smartystreets/goconvey/convey.
+//
+// 09_factorial_convey/factorial.go:
+//    Testovaný balíček s výpočtem faktoriálu.
+//
+// 10_factorial_convey2/behaviour_test.go:
+//    Testy pro balíček s implementací výpočtu faktoriálu.
+//    Testy využívají balíček smartystreets/goconvey/convey.
+//
+// 10_factorial_convey2/factorial.go:
+//    Testovaný balíček s výpočtem faktoriálu.
+//
