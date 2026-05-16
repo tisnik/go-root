2905d2b0c896e2e1a63451d530871ac493d9fbba
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-16 09:31:37 +0200

    Index file with all example from article #19

--- article_19/00_index.go
+++ article_19/00_index.go
@@ -2,20 +2,24 @@
 // ------------------------------------------
 //
 // dom_add_element.go:
-//     Manipulace s DOMem z jazyka Go.
+//    Standardní balíček syscall/js.
+//    Manipulace s DOMem z jazyka Go.
 //
 // dom_manipulation.go:
-//     Manipulace s DOMem z jazyka Go.
+//    Standardní balíček syscall/js.
+//    Manipulace s DOMem z jazyka Go.
 //
 // draw_into_canvas.go:
-//     Kreslení na Canvas.
+//    Standardní balíček syscall/js.
+//    Kreslení na Canvas.
 //
 // file_server.go:
-//     HTTP server vracející statický obsah
+//    HTTP server vracející statický obsah
 //
 // hello.go:
-//     Typický program typu "Hello world"
+//    Typický program typu "Hello world"
 //
 // js_interop.go:
-//     Rozhraní mezi jazyky Go a JavaScript.
+//    Standardní balíček syscall/js.
+//    Rozhraní mezi jazyky Go a JavaScript.
 //
