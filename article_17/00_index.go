011605248d86fc9b4171b6eb952874b1178fddfc
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-16 09:27:13 +0200

    Index file for all examples from article #17

---
+++ article_17/00_index.go
@@ -0,0 +1,75 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// factorial/factorial.go:
+//    Balíček s funkcí pro výpočet faktoriálu
+//
+// factorial/factorial_test.go:
+//    Jednotkové testy pro výpočet faktoriálu.
+//
+// factorial_convey/behaviour_test.go:
+//    BDD testy.
+//
+// factorial_convey/factorial.go:
+//    Balíček s funkcí pro výpočet faktoriálu
+//
+// mocking/kurzy.go:
+//    Přístup k tabulce kurzů a použití kurzů při výpočtu převodu měn.
+//
+// slice_test/slice_test.go:
+//    Test práce s řezy (slices).
+//
+// tests01/add.go:
+//    Testovaný balíček.
+//
+// tests01/add_test.go:
+//    Implementace jednotkových testů.
+//
+// tests02/add.go:
+//    Testovaný balíček.
+//
+// tests02/add_test.go:
+//    Implementace jednotkových testů.
+//
+// tests03/add.go:
+//    Testovaný balíček.
+//
+// tests03/add_test.go:
+//    Implementace jednotkových testů.
+//
+// tests04/add.go:
+//    Testovaný balíček.
+//
+// tests04/add_test.go:
+//    Implementace jednotkových testů.
+//
+// tests05/add.go:
+//    Testovaný balíček.
+//
+// tests05/add_test.go:
+//    Implementace jednotkových testů.
+//
+// tests06/add.go:
+//    Testovaný balíček.
+//
+// tests06/add_test.go:
+//    Implementace jednotkových testů.
+//
+// tests07/add.go:
+//    Testovaný balíček.
+//
+// tests07/add_test.go:
+//    Implementace jednotkových testů.
+//
+// tests08/add.go:
+//    Testovaný balíček.
+//
+// tests08/add_fast_test.go:
+//    Implementace jednotkových testů.
+//
+// tests08/add_slow_test.go:
+//    Implementace jednotkových testů.
+//
+// tests08/add_test.go:
+//    Implementace jednotkových testů.
+//
