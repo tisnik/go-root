ed93095635d60f439481c96a7e70861979a7af85
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-18 11:54:00 +0200

    Index file for examples used in article #21

---
+++ article_21/00_index.go
@@ -0,0 +1,63 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
+// clockwerk/jobs1.go:
+//    Úloha vytvořená nástrojem clockwerk.
+//
+// clockwerk/jobs2.go:
+//    Dvě úlohy vytvořené nástrojem clockwerk.
+//
+// clockwerk/jobs3.go:
+//    Vylepšená úloha vytvořená nástrojem clockwerk.
+//
+// clockwerk/jobs4.go:
+//    Vylepšené úlohy vytvořené nástrojem clockwerk.
+//
+// clockwork/jobs1.go:
+//    Úloha vytvořená nástrojem clockwork.
+//
+// clockwork/jobs2.go:
+//    Úloha vytvořená nástrojem clockwork.
+//
+// clockwork/jobs3.go:
+//    Tři úlohy vytvořené nástrojem clockwork.
+//
+// clockwork/jobs4.go:
+//    Tři úlohy vytvořené nástrojem clockwork.
+//
+// clockwork/jobs5.go:
+//    Osm naplánovaných úloh vytvořených nástrojem clockwork.
+//
+// duration/duration1.go:
+//    Práce s objektem typu Duration.
+//
+// duration/duration2.go:
+//    Práce s objektem typu Duration.
+//
+// duration/duration3.go:
+//    Práce s objektem typu Duration.
+//
+// duration/duration4.go:
+//    Práce s objektem typu Duration.
+//
+// duration/duration5.go:
+//    Práce s objektem typu Duration.
+//
+// duration/duration6.go:
+//    Práce s objektem typu Duration.
+//
+// go-cron/jobs1.go:
+//    Úloha vytvořená nástrojem go-cron.
+//
+// go-cron/jobs2.go:
+//    Úloha vytvořená nástrojem go-cron.
+//
+// go-cron/jobs3.go:
+//    Tři úlohy vytvořené nástrojem go-cron.
+//
+// go-cron/jobs4.go:
+//    Tři úlohy vytvořené nástrojem go-cron.
+//
+// go-cron/jobs5.go:
+//    Tři úlohy vytvořené nástrojem go-cron.
+//
