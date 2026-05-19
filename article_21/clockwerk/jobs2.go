75837fdd99d99f7c4da80bbfbf73c4a247ce6334
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-19 11:53:47 +0200

    Updated package description, added link to generated doc

--- article_21/clockwerk/jobs2.go
+++ article_21/clockwerk/jobs2.go
@@ -12,13 +12,18 @@
 //    https://github.com/tisnik/go-root/blob/master/article_21/README.md
 //
 // Demonstrační příklad číslo 2:
-//     Dvě úlohy vytvořené nástrojem clockwerk.
+//    Dvě úlohy vytvořené nástrojem clockwerk.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_21/clockwerk/jobs2.html
+//
 
 package main
 
 import (
-	"github.com/onatm/clockwerk"
 	"time"
+
+	"github.com/onatm/clockwerk"
 )
 
 type Task1 struct{}
