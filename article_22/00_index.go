3541cb2f2ca5ac1b74b7231009b987c1122cd689
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-20 09:02:22 +0200

    Updated description, added link to literate documentation

--- article_22/00_index.go
+++ article_22/00_index.go
@@ -2,56 +2,63 @@
 // ------------------------------------------
 //
 // 01_read_byte.go:
-//     Postupné načtení bajtů z řetězce.
+//    Struktura splňující rozhraní Reader.
+//    Postupné načtení bajtů z řetězce.
 //
 // 02_better_read_byte.go:
-//     Postupné načtení bajtů z řetězce, lepší reakce na chyby.
+//    Struktura splňující rozhraní Reader.
+//    Postupné načtení bajtů z řetězce, lepší reakce na chyby.
 //
 // 03_read_byte_from_unicode.go:
-//     Postupné načtení bajtů z řetězce, ukázka použití Unicode.
+//    Struktura splňující rozhraní Reader.
+//    Postupné načtení bajtů z řetězce, ukázka použití Unicode.
 //
 // 04_read_rune_from_unicode.go:
-//     Načítání jednotlivých run (ne bajtů) z řetězce.
+//    Struktura splňující rozhraní Reader.
+//    Načítání jednotlivých run (ne bajtů) z řetězce.
 //
 // 05_write_byte.go:
-//     Zápis bajtu do bufferu.
+//    Struktura splňující rozhraní Writer.
+//    Zápis bajtu do bufferu.
 //
 // 06_write_byte_read_rune.go:
-//     Zápis bajtů du bufferu, přečtení ve formě run.
+//    Čtení a zápis na úrovni run.
+//    Zápis bajtů du bufferu, přečtení ve formě run.
 //
 // 07_write_rune.go:
-//     Zápis runy do bufferu.
+//    Zápis runy do bufferu.
 //
 // 08_unread_byte.go:
-//     Použití metody UnreadByte.
+//    Použití metody UnreadByte.
 //
 // 09_unread_more_bytes.go:
-//     Opakované použití metody UnreadByte.
+//    Opakované použití metody UnreadByte.
 //
 // 10_unread_rune.go:
-//     Použití metody UnreadRune.
+//    Použití metody UnreadRune.
 //
 // 11_io_reader_read.go:
-//     Čtení dat ze souboru.
+//    Čtení dat ze souboru.
 //
 // 12_stdin_read.go:
-//     Čtení dat ze standardního vstupu.
+//    Čtení dat ze standardního vstupu.
 //
 // 13_string_reader_read.go:
-//     StringReader.
+//    Struktura StringReader.
+//    Základní operace čtení.
 //
 // 14_string_reader_as_str.go:
-//     Načítání ze standardního vstupu.
+//    Načítání ze standardního vstupu.
 //
 // 15_multi_reader.go:
-//     Multi reader se třemi vstupy.
+//    Multi reader se třemi vstupy.
 //
 // 16_io_writer_write.go:
-//     Zápis do souboru.
+//    Zápis do souboru.
 //
 // 17_input_via_reader.go:
-//     Čtení ze standardního vstupu přes reader.
+//    Čtení ze standardního vstupu přes reader.
 //
 // 18_file_block_copy.go:
-//     Blokové přenosy dat při kopii souboru.
+//    Blokové přenosy dat při kopii souboru.
 //
