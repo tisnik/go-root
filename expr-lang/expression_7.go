56328cdfbc0f0515ccc0ce5363c8a8c4a75d636d
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-23 09:08:45 +0200

    Expression evaluation

---
+++ expr-lang/expression_7.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/expr-lang/expr"
+)
+
+func main() {
+	env := map[string]interface{}{
+		"foo": -6,
+		"bar": 7,
+	}
+
+	code := `if foo < 0 {-foo*bar} else {foo*bar}`
+
+	fmt.Println("Compiling:", code)
+	program, err := expr.Compile(code, expr.Env(env))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Compiled")
+
+	fmt.Println("Calling compiled program:")
+
+	output, err := expr.Run(program, env)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Output:", output)
+}
