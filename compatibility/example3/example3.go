af45aee9f039cbd83fcd3c0b85b1f954e40e5b9e
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-29 08:37:26 +0200

    3rd example

---
+++ compatibility/example3/example3.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var prints []func()
+	for i := 1; i <= 3; i++ {
+		prints = append(prints, func() { fmt.Println(i) })
+	}
+	for _, print := range prints {
+		print()
+	}
+}
