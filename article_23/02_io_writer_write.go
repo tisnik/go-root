29ca2a67ad7aa78a047930ef93d174067f92adce
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-01 10:58:53 +0200

    Updated description

--- article_23/02_io_writer_write.go
+++ article_23/02_io_writer_write.go
@@ -12,7 +12,10 @@
 //    https://github.com/tisnik/go-root/blob/master/article_23/README.md
 //
 // Demonstrační příklad číslo 2:
-//     Použití metody Write z rozhraní Writer pro zápis do souboru.
+//    Použití metody Write z rozhraní Writer pro zápis do souboru.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_23/01_io_reader_read.html
 
 package main
 
