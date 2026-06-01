61ec53bc895a7defff7d1920938bf564a3939ed3
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-01 10:58:53 +0200

    Updated description

--- article_23/03_memory_pipe.go
+++ article_23/03_memory_pipe.go
@@ -12,7 +12,10 @@
 //    https://github.com/tisnik/go-root/blob/master/article_23/README.md
 //
 // Demonstrační příklad číslo 3:
-//     Přenos dat s využitím roury (pipe) alokované v operační paměti.
+//    Přenos dat s využitím roury (pipe) alokované v operační paměti.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_23/01_io_reader_read.html
 
 package main
 
