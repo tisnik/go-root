f86e620df95497d47733789141d8e9954f064d89
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-03 11:27:48 +0200

    Updated description

--- article_23/09_writer_seeker.go
+++ article_23/09_writer_seeker.go
@@ -12,7 +12,10 @@
 //    https://github.com/tisnik/go-root/blob/master/article_23/README.md
 //
 // Demonstrační příklad číslo 9:
-//     Použití operace Seek při zápisu do souboru.
+//    Použití operace Seek při zápisu do souboru.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_23/01_io_reader_read.html
 
 package main
 
