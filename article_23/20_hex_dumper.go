bad16c95d5cd95df07e47d02481d00be5b9d25f2
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-05 11:01:13 +0200

    Updated descriptions

--- article_23/20_hex_dumper.go
+++ article_23/20_hex_dumper.go
@@ -12,7 +12,10 @@
 //    https://github.com/tisnik/go-root/blob/master/article_23/README.md
 //
 // Demonstrační příklad číslo 20:
-//     Zobrazení libovolných binárních dat ve formě hexadecimálního výpisu.
+//    Zobrazení libovolných binárních dat ve formě hexadecimálního výpisu.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_23/01_io_reader_read.html
 
 package main
 
