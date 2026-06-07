aee07deb454999713ea820abc74318127632a753
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-07 09:44:51 +0200

    Updated index file

--- article_24/00_index.go
+++ article_24/00_index.go
@@ -2,62 +2,81 @@
 // ------------------------------------------
 //
 // 01_server.go:
-//     	Jednoduchý HTTP server posílající dynamicky generovaný obsah
+//    Balíček net/http.
+//    Jednoduchý HTTP server posílající dynamicky generovaný obsah.
 //
 // 02_slow_server.go:
-//     	Zpomalení generování jednotlivých bloků generovaného obsahu
+//    Balíček net/http.
+//    Zpomalení generování jednotlivých bloků generovaného obsahu.
 //
 // 03_flushing_server.go:
-//     	Využití metody Flush z rozhraní Flusher
+//    Balíček net/http.
+//    Využití metody Flush z rozhraní Flusher.
 //
 // 04_close_detector.go:
-//     	Test, zda klient neukončil spojení
+//    Balíček net/http.
+//    Test, zda klient neukončil spojení.
 //
 // 05_list.go:
-//     	Operace nad seznamem
+//    Balíček container/list.
+//    Operace nad seznamem.
 //
 // 06_list_as_stack.go:
-//     	Použití seznamu ve funkci zásobníku
+//    Balíček container/list.
+//    Použití seznamu ve funkci zásobníku.
 //
 // 07_print_stack_content.go:
-//     	Vylepšení předchozího příkladu
+//    Balíček container/list.
+//    Vylepšení předchozího příkladu.
 //
 // 08_empty_ring.go:
-//     	Prázdná cyklická fronta
+//    Balíček container/ring.
+//    Prázdná cyklická fronta.
 //
 // 09_filled_ring.go:
-//     	Cyklická fronta s naplněnými prvky
+//    Balíček container/ring.
+//    Cyklická fronta s naplněnými prvky.
 //
 // 10_ring_empty_items.go:
-//     	Cyklická fronta s některými prázdnými prvky
+//    Balíček container/ring.
+//    Cyklická fronta s některými prázdnými prvky.
 //
 // 11_ring_do_iterator.go:
-//     	Iterace nad prvky cyklické fronty
+//    Balíček container/ring.
+//    Iterace nad prvky cyklické fronty.
 //
 // 12_ring_do_closure.go:
-//     	Vylepšená iterace nad prvky cyklické fronty
+//    Balíček container/ring.
+//    Vylepšená iterace nad prvky cyklické fronty.
 //
 // 13_integer_sort.go:
-//     	Seřazení sekvence celých čísel
+//    Balíček math/rand a sort.
+//    Seřazení sekvence celých čísel.
 //
 // 14_float_sort.go:
-//     	Seřazení sekvence čísel s plovoucí řádovou čárkou
+//    Balíček math a sort.
+//    Seřazení sekvence čísel s plovoucí řádovou čárkou.
 //
 // 15_string_sort.go:
-//     	Načtení textového souboru s jeho rozdělením na slova a seřazením
+//    Kooperace mezi standardními balíčky.
+//    Načtení textového souboru s jeho rozdělením na slova a seřazením.
 //
 // 16_reverse_sort.go:
-//     	Opačně seřazení sekvence/řezu
+//    Balíček math/rand a sort.
+//    Opačně seřazení sekvence/řezu.
 //
 // 17_int_slice.go:
-//     	Použití datového typu IntSlice
+//    Balíček math/rand a sort.
+//    Použití datového typu IntSlice.
 //
 // 18_sort_by.go:
-//     	Seřazení sekvence uživatelských datových struktur
+//    Seřazení sekvence uživatelských datových struktur.
 //
 // 19_heap.go:
-//     	Základní způsob využití datového typu halda
+//    Balíček container/heap.
+//    Základní způsob využití datového typu halda.
 //
 // 20_heap_trace.go:
-//     	Úprava předchozího příkladu, aby zobrazoval prováděné operace
+//    Balíček container/heap.
+//    Úprava předchozího příkladu, aby zobrazoval prováděné operace.
 //
