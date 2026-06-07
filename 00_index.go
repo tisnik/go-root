21e20fdb0b6ba8cb0550b122580df55d49d6575a
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-07 09:44:51 +0200

    Index file with examples for article #24

--- 00_index.go
+++ 00_index.go
@@ -1746,3 +1746,89 @@
 // 21_base_64_encoder.go:
 //    Zakódování binárních dat do tisknutelné podoby podle Base64.
 //
+
+
+
+// ----------------------------------------------------
+// Adresář: article_24
+// ----------------------------------------------------
+//
+// 01_server.go:
+//    Balíček net/http.
+//    Jednoduchý HTTP server posílající dynamicky generovaný obsah.
+//
+// 02_slow_server.go:
+//    Balíček net/http.
+//    Zpomalení generování jednotlivých bloků generovaného obsahu.
+//
+// 03_flushing_server.go:
+//    Balíček net/http.
+//    Využití metody Flush z rozhraní Flusher.
+//
+// 04_close_detector.go:
+//    Balíček net/http.
+//    Test, zda klient neukončil spojení.
+//
+// 05_list.go:
+//    Balíček container/list.
+//    Operace nad seznamem.
+//
+// 06_list_as_stack.go:
+//    Balíček container/list.
+//    Použití seznamu ve funkci zásobníku.
+//
+// 07_print_stack_content.go:
+//    Balíček container/list.
+//    Vylepšení předchozího příkladu.
+//
+// 08_empty_ring.go:
+//    Balíček container/ring.
+//    Prázdná cyklická fronta.
+//
+// 09_filled_ring.go:
+//    Balíček container/ring.
+//    Cyklická fronta s naplněnými prvky.
+//
+// 10_ring_empty_items.go:
+//    Balíček container/ring.
+//    Cyklická fronta s některými prázdnými prvky.
+//
+// 11_ring_do_iterator.go:
+//    Balíček container/ring.
+//    Iterace nad prvky cyklické fronty.
+//
+// 12_ring_do_closure.go:
+//    Balíček container/ring.
+//    Vylepšená iterace nad prvky cyklické fronty.
+//
+// 13_integer_sort.go:
+//    Balíček math/rand a sort.
+//    Seřazení sekvence celých čísel.
+//
+// 14_float_sort.go:
+//    Balíček math a sort.
+//    Seřazení sekvence čísel s plovoucí řádovou čárkou.
+//
+// 15_string_sort.go:
+//    Kooperace mezi standardními balíčky.
+//    Načtení textového souboru s jeho rozdělením na slova a seřazením.
+//
+// 16_reverse_sort.go:
+//    Balíček math/rand a sort.
+//    Opačně seřazení sekvence/řezu.
+//
+// 17_int_slice.go:
+//    Balíček math/rand a sort.
+//    Použití datového typu IntSlice.
+//
+// 18_sort_by.go:
+//    Seřazení sekvence uživatelských datových struktur.
+//
+// 19_heap.go:
+//    Balíček container/heap.
+//    Základní způsob využití datového typu halda.
+//
+// 20_heap_trace.go:
+//    Balíček container/heap.
+//    Úprava předchozího příkladu, aby zobrazoval prováděné operace.
+//
