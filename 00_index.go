c26399a2dd0b84dba6093cd50aa7e2976504e008
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-14 09:49:06 +0200

    Index file with examples for article #25

--- 00_index.go
+++ 00_index.go
@@ -1832,3 +1832,33 @@
 //    Balíček container/heap.
 //    Úprava předchozího příkladu, aby zobrazoval prováděné operace.
 //
+
+
+
+// ----------------------------------------------------
+// Adresář: article_25
+// ----------------------------------------------------
+//
+// 01_add.go:
+//    Jednoduchý program, který sečte dvojici celočíselných hodnot.
+//
+// 02_arrays_slices.go:
+//    Základní operace s poli a s řezy.
+//    Získání řezu z pole a řezu z jiného řezu.
+//
+// 03_structs.go:
+//    Práce se strukturami (záznamy).
+//    Inicializace celého záznamu.
+//    Naplnění jednotlivých prvků záznamu.
+//
+// 04_factorial.go:
+//    Rekurzivní výpočet faktoriálu.
+//
+// 05_goroutines.go:
+//    Konstrukce a spuštění trojice gorutin.
+//
+// 06_channels.go:
+//    Konstrukce a spuštění gorutiny.
+//    Konstrukce kanálu.
+//    Komunikace mezi gorutinami s využitím kanálu.
+//
