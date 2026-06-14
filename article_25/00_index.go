980ba98b38c86742d4e2b027b07d837e857287bf
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-14 09:49:06 +0200

    Updated index file

--- article_25/00_index.go
+++ article_25/00_index.go
@@ -2,20 +2,25 @@
 // ------------------------------------------
 //
 // 01_add.go:
-//     Jednoduchý program, který sečte dvojici celočíselných hodnot
+//    Jednoduchý program, který sečte dvojici celočíselných hodnot.
 //
 // 02_arrays_slices.go:
-//     Základní operace s poli a s řezy
+//    Základní operace s poli a s řezy.
+//    Získání řezu z pole a řezu z jiného řezu.
 //
 // 03_structs.go:
-//     Práce se strukturami (záznamy)
+//    Práce se strukturami (záznamy).
+//    Inicializace celého záznamu.
+//    Naplnění jednotlivých prvků záznamu.
 //
 // 04_factorial.go:
-//     Rekurzivní výpočet faktoriálu
+//    Rekurzivní výpočet faktoriálu.
 //
 // 05_goroutines.go:
-//     Spuštění dvou gorutin
+//    Konstrukce a spuštění trojice gorutin.
 //
 // 06_channels.go:
-//     Gorutiny a kanály
+//    Konstrukce a spuštění gorutiny.
+//    Konstrukce kanálu.
+//    Komunikace mezi gorutinami s využitím kanálu.
 //
