5616a08623fb906e2459e56f02375d2f9d438b22
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-16 08:40:10 +0200

    Index file with examples for article #26

--- article_26/00_index.go
+++ article_26/00_index.go
@@ -2,22 +2,22 @@
 // ------------------------------------------
 //
 // 01_boolean_type.go:
-//    Datový typ boolean
+//    Datový typ boolean.
 //
 // 02_number_types.go:
-//    Numerické datové typy
+//    Numerické datové typy.
 //
 // 03_string_type.go:
-//    Datový typ řetězec
+//    Datový typ řetězec.
 //
 // 04_other_types.go:
-//    Ostatní datové typy
+//    Ostatní datové typy.
 //
 // 05_other_types_better_solution.go:
-//    Ostatní datové typy, lepší přístup
+//    Ostatní datové typy, lepší přístup.
 //
 // 06_structs.go:
-//    Struktury (záznamy)
+//    Struktury (záznamy).
 //
 // 07_nil_as_variable.go:
 //    nil může být korektní jméno konstanty.
@@ -26,65 +26,65 @@
 //    true a false mohou být korektní jméno konstanty.
 //
 // 09_nil_value.go:
-//    Hodnota nil a její význam (bez typu)
+//    Hodnota nil a její význam (bez typu).
 //
 // 10_nil_pointer.go:
-//    Ukazatel inicializovaný na nil
+//    Ukazatel inicializovaný na nil.
 //
 // 10B_nil_pointer.go:
-//    Ukazatel inicializovaný na nil
+//    Ukazatel inicializovaný na nil.
 //
 // 11_other_pointer.go:
-//    Ukazatel na celé číslo
+//    Ukazatel na celé číslo.
 //
 // 12_nil_slice.go:
-//    Nulový řez
+//    Nulový řez.
 //
 // 12B_nil_slice_append.go:
-//    Nulový řez a funkce append
+//    Nulový řez a funkce append.
 //
 // 13_nil_map.go:
-//    Nulová mapa
+//    Nulová mapa.
 //
 // 14_nil_map_is_really_nil.go:
-//    Nulová mapa
+//    Nulová mapa.
 //
 // 15_add_into_nil_map.go:
-//    Přidání prvků do nulové mapy
+//    Přidání prvků do nulové mapy.
 //
 // 16_empty_map.go:
-//    Prázdná mapa a přidání prvků do prázdné mapy
+//    Prázdná mapa a přidání prvků do prázdné mapy.
 //
 // 17_nil_interface.go:
-//    Prázdné rozhraní
+//    Prázdné rozhraní.
 //
 // 18_two_interfaces.go:
-//    Prázdné rozhraní a rozhraní s jednou metodou
+//    Prázdné rozhraní a rozhraní s jednou metodou.
 //
 // 19_nil_is_not_nil.go:
-//    nil se někdy nerovná nil
+//    nil se někdy nerovná nil.
 //
 // 20_two_nil_pointers.go:
-//    Dva ukazatele s nulovou hodnotou
+//    Dva ukazatele s nulovou hodnotou.
 //
 // 21_comparing_interface_to_pointer.go:
-//    Porovnání dvou ukazatelů
+//    Porovnání dvou ukazatelů.
 //
 // 22_compare_two_different_pointer_types.go:
-//    Porovnání dvou ukazatelů různých typů
+//    Porovnání dvou ukazatelů různých typů.
 //
 // 23_nil_channels.go:
-//    Nulový kanál
+//    Nulový kanál.
 //
 // 24_nil_channel_write.go:
-//    Zápis do nulového kanálu
+//    Zápis do nulového kanálu.
 //
 // 25_nil_channel_read.go:
-//    Čtení z nulového kanálu
+//    Čtení z nulového kanálu.
 //
 // 26_nil_and_normal_channel.go:
-//    Nulové i normální kanály
+//    Nulové i normální kanály.
 //
 // 27_three_nil_interfaces.go:
-//    Tři nulová rozhraní
+//    Tři nulová rozhraní.
 //
