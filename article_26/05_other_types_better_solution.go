6c50b30df76dde7927f1ff27f4db67354afc1f98
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-17 09:13:33 +0200

    Updated example description

--- article_26/05_other_types_better_solution.go
+++ article_26/05_other_types_better_solution.go
@@ -12,7 +12,10 @@
 //    https://github.com/tisnik/go-root/blob/master/article_26/README.md
 //
 // Demonstrační příklad číslo 5:
-//    Ostatní datové typy, lepší přístup
+//    Ostatní datové typy, lepší přístup.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_26/05_other_types_better_solution.html
 
 package main
 
