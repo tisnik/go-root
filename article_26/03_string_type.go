8e36c8e6b2d0219c3bef34b0a58d9d5fae7af82e
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-17 09:13:33 +0200

    Updated example description

--- article_26/03_string_type.go
+++ article_26/03_string_type.go
@@ -12,7 +12,10 @@
 //    https://github.com/tisnik/go-root/blob/master/article_26/README.md
 //
 // Demonstrační příklad číslo 3:
-//    Datový typ řetězec
+//    Datový typ řetězec.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_26/03_string_type.html
 
 package main
 
