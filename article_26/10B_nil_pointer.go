b0e77839e1cdf2116902fdcb1acdd2667b832c1b
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-18 09:29:26 +0200

    Updated example description

--- article_26/10B_nil_pointer.go
+++ article_26/10B_nil_pointer.go
@@ -12,7 +12,10 @@
 //    https://github.com/tisnik/go-root/blob/master/article_26/README.md
 //
 // Demonstrační příklad číslo 10B:
-//    Ukazatel inicializovaný na nil
+//    Ukazatel inicializovaný na nil.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_26/10B_nil_pointer.html
 
 package main
 
