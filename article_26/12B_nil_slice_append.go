ae65270c145e360c312cb83abeea59643aee690a
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-20 09:07:38 +0200

    Updated example description

--- article_26/12B_nil_slice_append.go
+++ article_26/12B_nil_slice_append.go
@@ -12,7 +12,10 @@
 //    https://github.com/tisnik/go-root/blob/master/article_26/README.md
 //
 // Demonstrační příklad číslo 12B:
-//    Nulový řez a funkce append
+//    Nulový řez a funkce append.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_26/12B_nil_slice_append.html
 
 package main
 
