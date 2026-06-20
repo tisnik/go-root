1879d64966de04b88bb3cc3505fa1445223067b1
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-20 09:07:38 +0200

    Updated example description

--- article_26/13_nil_map.go
+++ article_26/13_nil_map.go
@@ -12,7 +12,10 @@
 //    https://github.com/tisnik/go-root/blob/master/article_26/README.md
 //
 // Demonstrační příklad číslo 13:
-//    Nulová mapa
+//    Nulová mapa.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_26/13_nil_map.html
 
 package main
 
