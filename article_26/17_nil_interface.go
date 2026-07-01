fb438fa488695ac791afe5b2939b543c191ed1ed
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-07-01 08:56:15 +0200

    Updated description

--- article_26/17_nil_interface.go
+++ article_26/17_nil_interface.go
@@ -12,7 +12,10 @@
 //    https://github.com/tisnik/go-root/blob/master/article_26/README.md
 //
 // Demonstrační příklad číslo 17:
-//    Prázdné rozhraní
+//    Prázdné rozhraní.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_26/17_nil_interface.html
 
 package main
 
