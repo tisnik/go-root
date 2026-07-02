f87160fd1604c53ec42ee3264bc395ffd39bacc1
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-07-02 09:11:53 +0200

    Updated description

--- article_26/22_compare_two_different_pointer_types.go
+++ article_26/22_compare_two_different_pointer_types.go
@@ -12,7 +12,10 @@
 //    https://github.com/tisnik/go-root/blob/master/article_26/README.md
 //
 // Demonstrační příklad číslo 22:
-//    Porovnání dvou ukazatelů různých typů
+//    Porovnání dvou ukazatelů různých typů.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_26/22_compare_two_different_pointer_types.html
 
 package main
 
