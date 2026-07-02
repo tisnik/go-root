dd3739e49d01f46faee36a5363eebec8730f4f0d
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-07-02 09:11:53 +0200

    Updated description

--- article_26/18_two_interfaces.go
+++ article_26/18_two_interfaces.go
@@ -12,7 +12,10 @@
 //    https://github.com/tisnik/go-root/blob/master/article_26/README.md
 //
 // Demonstrační příklad číslo 18:
-//    Prázdné rozhraní a rozhraní s jednou metodou
+//    Prázdné rozhraní a rozhraní s jednou metodou.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_26/18_two_interfaces.html
 
 package main
 
