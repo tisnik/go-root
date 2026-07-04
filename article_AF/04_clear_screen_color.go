5ff236c8b1aa28bd46c723c390d02953f012b1fd
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-07-04 09:36:48 +0200

    gogpu/gg: clear screen with selected color (correct variant)

---
+++ article_AF/04_clear_screen_color.go
@@ -0,0 +1,30 @@
+// Seriál "Programovací jazyk Go"
+//    https://www.root.cz/serialy/programovaci-jazyk-go/
+//
+// Demonstrační příklad číslo 4:
+//    Balíček gogpu/gg.
+//    Vymazání obrazovky (varianta se specifikací barvy).
+//
+
+package main
+
+import "github.com/gogpu/gg"
+
+func main() {
+	// rozměry rastrového obrázku
+	const width = 320
+	const height = 240
+
+	// jméno souboru s výsledným obrázkem
+	const fileName = "04.png"
+
+	// vytvoření kontextu
+	dc := gg.NewContext(width, height)
+
+	// vykreslování s využitím kontextu
+	color := gg.RGBA{R: 0.5, G: 0.9, B: 0.0, A: 0.5}
+	dc.ClearWithColor(color)
+
+	// uložení výsledného rastrového obrázku
+	dc.SavePNG(fileName)
+}
