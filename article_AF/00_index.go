224d08f7c285e2cb582a18da407f08cb60074908
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-07-04 09:36:56 +0200

    Index file

---
+++ article_AF/00_index.go
@@ -0,0 +1,41 @@
+// 01_clear_screen.go:
+//    Balíček gogpu/gg.
+//    Vymazání obsahu rastrového obrázku.
+//
+// 02_fill_rectangle.go:
+//    Balíček gogpu/gg.
+//    Vymazání obrázku vykreslením vyplněného obdélníku.
+//
+// 03_clear_screen_color.go:
+//    Balíček gogpu/gg.
+//    Vymazání obrazovky (lepší varianta).
+//
+// 04_clear_screen_color.go:
+//    Balíček gogpu/gg.
+//    Vymazání obrazovky (varianta se specifikací barvy).
+//
+// 05_clear_screen_color.go:
+//    Balíček gogpu/gg.
+//    Vymazání obrazovky (varianta se specifikací barvy).
+//
+// 06_colors.go:
+//    Balíček gogpu/gg.
+//    Práce s barvami.
+//
+// 07_color_alpha.go:
+//    Balíček gogpu/gg.
+//    Práce s barvami.
+//
+// 08_rgba_color.go:
+//    Balíček gogpu/gg.
+//    Barvový prostor RGB a RGBA.
+//
+// 09_alpha_component.go:
+//    Balíček gogpu/gg.
+//    Barvový prostor RGB a RGBA.
+//    Alfa kanál v barvovém prostoru RGBA.
+//
+// 10_line_width.go:
+//    Balíček gogpu/gg.
+//    Nastavení šířky vykreslovaných úseček a křivek.
+//
