9493572408541163dd7edcde7dbdf346bfd49fe2
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-07-08 09:17:24 +0200

    Gogpu/gg library: complex transformation

---
+++ article_AF/22_transformation_rotate.go
@@ -0,0 +1,82 @@
+// Seriál "Programovací jazyk Go"
+//    https://www.root.cz/serialy/programovaci-jazyk-go/
+//
+// Demonstrační příklad číslo 22:
+//    Balíček gogpu/gg.
+//    Transformace (rotace).
+
+package main
+
+import (
+	"math"
+
+	"github.com/gogpu/gg"
+)
+
+func Radians(angle float64) float64 {
+	return float64(angle / 360.0 * math.Pi)
+}
+
+func main() {
+	// rozměry rastrového obrázku
+	const width = 320
+	const height = 240
+
+	// jméno souboru s výsledným obrázkem
+	const fileName = "22.png"
+
+	// vytvoření kontextu
+	dc := gg.NewContext(width, height)
+
+	dc.DrawRectangle(0, 0, width, height)
+	dc.SetRGB(1.0, 1.0, 1.0)
+	dc.Fill()
+
+	dc.Rotate(Radians(15.0))
+
+	dc.SetRGBA(0.0, 0.0, 0.0, 1)
+
+	dc.DrawLine(32, 20, 288, 20)
+	dc.Stroke()
+
+	dc.SetDash(10)
+	dc.DrawLine(32, 40, 288, 40)
+	dc.Stroke()
+
+	dc.SetDash(10, 10)
+	dc.DrawLine(32, 60, 288, 60)
+	dc.Stroke()
+
+	dc.SetDash(10, 5)
+	dc.DrawLine(32, 80, 288, 80)
+	dc.Stroke()
+
+	dc.SetDash(10, 5, 2, 5)
+	dc.DrawLine(32, 100, 288, 100)
+	dc.Stroke()
+
+	dc.SetLineWidth(4.0)
+	dc.SetLineCap(gg.LineCapButt)
+
+	dc.DrawLine(32, 140, 288, 140)
+	dc.Stroke()
+
+	dc.SetDash(10)
+	dc.DrawLine(32, 160, 288, 160)
+	dc.Stroke()
+
+	dc.SetDash(10, 10)
+	dc.DrawLine(32, 180, 288, 180)
+	dc.Stroke()
+
+	dc.SetDash(10, 5)
+	dc.DrawLine(32, 200, 288, 200)
+	dc.Stroke()
+
+	dc.SetDash(10, 5, 2, 5)
+	dc.DrawLine(32, 220, 288, 220)
+	dc.Stroke()
+
+	// uložení výsledného rastrového obrázku
+	dc.SavePNG(fileName)
+}
