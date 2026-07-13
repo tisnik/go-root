2a5ae6c4f4f9cd32d6e1ca14841fbbaf3bc44c8b
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-07-13 08:44:35 +0200

    List of all examples

--- article_AF/00_index.go
+++ article_AF/00_index.go
@@ -1,3 +1,6 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
 // 01_clear_screen.go:
 //    Balíček gogpu/gg.
 //    Vymazání obsahu rastrového obrázku.
@@ -38,4 +41,81 @@
 // 10_line_width.go:
 //    Balíček gogpu/gg.
 //    Nastavení šířky vykreslovaných úseček a křivek.
+//
+// 11_line_caps.go:
+//    Balíček gogpu/gg.
+//    Nastavení stylu konců úseček.
+//
+// 12_dash.go:
+//    Balíček gogpu/gg.
+//    Nastavení stylu vykreslovaných úseček.
+//
+// 13_simple_path.go:
+//    Balíček gogpu/gg.
+//    Práce s cestou (path).
+//
+// 14_open_path.go:
+//    Balíček gogpu/gg.
+//    Otevřená cesta (open path).
+//
+// 15_closed_path.go:
+//    Balíček gogpu/gg.
+//    Uzavřená cesta (closed path).
+//
+// 16_quadratic_bezier.go:
+//    Balíček gogpu/gg.
+//    Kvadratická Bézierova křivka.
+//
+// 17_quadratic_bezier_control_points.go:
+//    Balíček gogpu/gg.
+//    Kvadratická Bézierova křivka s řídicími body.
+//
+// 18_cubic_bezier.go:
+//    Balíček gogpu/gg.
+//    Kubická Bézierova křivka.
+//
+// 19_cubic_bezier_control_points.go:
+//    Balíček gogpu/gg.
+//    Kubická Bézierova křivka s řídicími body.
+//
+// 20_rotation.go:
+//    Balíček gogpu/gg.
+//    Rotace vykreslovaných objektů.
+//
+// 21_to_image.go:
+//    Balíček gogpu/gg.
+//    Rasterizace do zvoleného rastrového formátu.
+//    Výsledkem bude animovaný GIF.
+//
+// 22_transformation_rotate.go:
+//    Balíček gogpu/gg.
+//    Transformace (rotace).
+//
+// 23_transformation_rotate_about.go:
+//    Balíček gogpu/gg.
+//    Transformace (rotace okolo zvoleného bodu).
+//
+// 24_scale_about.go:
+//    Balíček gogpu/gg.
+//    Transformace (změna měřítka okolo zvoleného bodu).
+//
+// 25_translate_rotate.go:
+//    Balíček gogpu/gg.
+//    Transformace (posun).
+//
+// 26_text.go:
+//    Balíček gogpu/gg.
+//    Vykreslení textu.
+//
+// 27_text_centered.go:
+//    Balíček gogpu/gg.
+//    Vykreslení vycentrovaného textu.
+//
+// 28_text_centered.go:
+//    Balíček gogpu/gg.
+//    Vykreslení vycentrovaného textu.
+//
+// 29_text_rotated.go:
+//    Balíček gogpu/gg.
+//    Vykreslení vycentrovaného textu.
 //
