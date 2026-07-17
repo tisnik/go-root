483046619c0231022047abb7a5cc517b7fcb555f
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-07-17 09:39:01 +0200

    Hex color codes

---
+++ article_B0/03_hex_color.go
@@ -0,0 +1,30 @@
+package main
+
+import "github.com/gogpu/gg"
+
+func main() {
+	// rozměry rastrového obrázku
+	const width = 400
+	const height = 300
+
+	// jméno souboru s výsledným obrázkem
+	const fileName = "03.png"
+
+	// vytvoření kontextu
+	dc := gg.NewContext(width, height)
+
+	// vykreslování s využitím kontextu
+	color := gg.RGBA{R: 0.9, G: 0.9, B: 0.7, A: 1.0}
+	dc.ClearWithColor(color)
+
+	const border = 40
+	dc.DrawRectangle(border, border, width-border*2, height-border*2)
+	// barva zapsaná v hexa triplet formátu
+	dc.SetHexColor("44aadd")
+
+	// vyplnění tvaru
+	dc.Fill()
+
+	// uložení výsledného rastrového obrázku
+	dc.SavePNG(fileName)
+}
