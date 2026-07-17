fe6c684a8707a67478a57343e8134fc48c849f48
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-07-17 09:39:01 +0200

    Fill brush

---
+++ article_B0/05_fill_brush.go
@@ -0,0 +1,31 @@
+package main
+
+import "github.com/gogpu/gg"
+
+func main() {
+	// rozměry rastrového obrázku
+	const width = 400
+	const height = 300
+
+	// jméno souboru s výsledným obrázkem
+	const fileName = "05.png"
+
+	// vytvoření kontextu
+	dc := gg.NewContext(width, height)
+
+	// vykreslování s využitím kontextu
+	color := gg.RGBA{R: 0.9, G: 0.9, B: 0.7, A: 1.0}
+	dc.ClearWithColor(color)
+
+	customColor := gg.Hex("44aadd")
+	dc.SetFillBrush(gg.Solid(customColor))
+
+	const border = 40
+	dc.DrawRectangle(border, border, width-border*2, height-border*2)
+
+	// vykreslení obrysu
+	dc.Fill()
+
+	// uložení výsledného rastrového obrázku
+	dc.SavePNG(fileName)
+}
