062db3a418aea7436f20dcd65a096b340689c6b1
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-07-21 09:36:35 +0200

    Updated description

--- article_AF/03_clear_screen_color.go
+++ article_AF/03_clear_screen_color.go
@@ -1,10 +1,22 @@
 // Seriál "Programovací jazyk Go"
 //    https://www.root.cz/serialy/programovaci-jazyk-go/
 //
+// Sto třináctá část
+//    Tvorba 2D i 3D grafiky a animací v Go s využitím projektu GoGPU
+//    https://www.root.cz/clanky/tvorba-2d-i-3d-grafiky-a-animaci-v-go-s-vyuzitim-projektu-gogpu/
+//
+// Repositář:
+//    https://github.com/tisnik/go-root/
+//
+// Seznam demonstračních příkladů ze čtrnácté části:
+//    https://github.com/tisnik/go-root/blob/master/article_14/README.md
+//
 // Demonstrační příklad číslo 3:
 //    Balíček gogpu/gg.
 //    Vymazání obrazovky (lepší varianta).
 //
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_A0/03_clear_screen_color.html
 
 package main
 
