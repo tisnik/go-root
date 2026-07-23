8ed7beb850559c2cc057181b3bf10e6c325e23ff
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-07-23 09:29:30 +0200

    Updated description

--- article_28/19_combinations_error.go
+++ article_28/19_combinations_error.go
@@ -13,11 +13,16 @@
 //
 // Demonstrační příklad číslo 19:
 //    Kombinace různých metod pro "streaming", zpracování chyb.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_28/19_combinations_error.html
+//
 
 package main
 
 import (
 	"fmt"
+
 	"github.com/wesovilabs/koazee"
 )
 
