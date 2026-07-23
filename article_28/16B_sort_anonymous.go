3e6b9a3d971dfe8338396f314885970d3ab1187e
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-07-23 09:29:30 +0200

    Updated description

--- article_28/16B_sort_anonymous.go
+++ article_28/16B_sort_anonymous.go
@@ -13,11 +13,16 @@
 //
 // Demonstrační příklad číslo 16B:
 //    Metoda Sort() a stream s celočíselnými hodnotami.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_28/16B_sort_anonymous.html
+//
 
 package main
 
 import (
 	"fmt"
+
 	"github.com/wesovilabs/koazee"
 )
 
