998051d48fcc85280b7b0dc5182d08ec4b2673f9
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-08-05 08:54:24 +0200

    Index file with examples for article #30

--- 00_index.go
+++ 00_index.go
@@ -2045,3 +2045,29 @@
 // 21_max_value.go:
 //    Metoda Reduce().
 //
+
+// ----------------------------------------------------
+// Adresář: article_30
+// ----------------------------------------------------
+//
+// 01_image_server.go:
+//    HTTP server, který generuje rastrové obrázky.
+//
+// 02_image_server_with_pprof.go:
+//     HTTP server nabízející i pprof metriky.
+//
+// 03_image_server_fractal.go:
+//    HTTP server generující rastrový obrázek s fraktálem.
+//
+// 04_basic_tracing.go:
+//     Vytvoření souboru s trasovacími informacemi.
+//
+// 05_context.go:
+//    Zpracování kontextu v trasovacích informacích.
+//
+// 06_context_regions.go:
+//    Transakce a regiony v trasovacím souboru.
+//
+// 07_region_hierarchy.go:
+//    Hierarchie regionů v souboru s trasovacími informacemi.
+//
