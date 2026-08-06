80b41e0ec8c05c67bda3e15ee5ecec68e3057b77
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-08-06 10:10:50 +0200

    Index file with examples for article #31

--- 00_index.go
+++ 00_index.go
@@ -2071,3 +2071,41 @@
 // 07_region_hierarchy.go:
 //    Hierarchie regionů v souboru s trasovacími informacemi.
 //
+
+// ----------------------------------------------------
+// Adresář: article_31
+// ----------------------------------------------------
+//
+// 01_image_server.go:
+//    HTTP server, který vrací HTML stránku s generovaným obrázkem.
+//
+// 02_image_server_counter.go:
+//    Přidání jednoduchého čítače do HTTP serveru.
+//
+// 03_image_server_counter_mutex.go:
+//    Čítač měněný v mutexu.
+//
+// 04_image_server_counter_atom.go:
+//    Atomická změna čítače.
+//
+// 05_image_server_expvar.go:
+//    Exportované proměnné - čítač.
+//
+// 06_image_server_expvar_closure.go:
+//    Úprava předchozího příkladu používajícího uzávěry.
+//
+// 07_image_server_expvar_pprof.go:
+//    Použití pprof (profilovací a další metriky)
+//
+// 08_image_server_fractal.go:
+//    Další tři sledované metriky: čas výpočtu, počet vypočtených pixelů, čas v tisknutelné podobě.
+//
+// 09_counter.go:
+//    Čítač zvyšovaný ve více vláknech bez synchronizace.
+//
+// 10_counter_lock.go:
+//    Čítač zvyšovaný ve více vláknech se synchronizací (v mutextu).
+//
+// 11_counter_atomic.go:
+//    Čítač atomicky zvyšovaný ve více vláknech.
+//
