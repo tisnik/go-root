7d83813c86e9ef07ae9c73ac0b63e161b2adaccc
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-08-07 08:59:44 +0200

    Updated example description

--- article_AF/03_clear_screen_color.go
+++ article_AF/03_clear_screen_color.go
@@ -8,15 +8,15 @@
 // Repositář:
 //    https://github.com/tisnik/go-root/
 //
-// Seznam demonstračních příkladů ze čtrnácté části:
-//    https://github.com/tisnik/go-root/blob/master/article_14/README.md
+// Seznam demonstračních příkladů ze sto třinácté části:
+//    https://github.com/tisnik/go-root/blob/master/article_AF/README.md
 //
 // Demonstrační příklad číslo 3:
 //    Balíček gogpu/gg.
 //    Vymazání obrazovky (lepší varianta).
 //
 // Dokumentace ve stylu "literate programming":
-//    https://tisnik.github.io/go-root/article_A0/03_clear_screen_color.html
+//    https://tisnik.github.io/go-root/article_AF/03_clear_screen_color.html
 
 package main
 
