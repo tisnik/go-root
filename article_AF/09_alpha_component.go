e0713c3d052521b225698af9864841f2814005ca
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-08-08 11:17:53 +0200

    RGBA color with alpha channel specification

--- article_AF/09_alpha_component.go
+++ article_AF/09_alpha_component.go
@@ -1,11 +1,26 @@
 // Seriál "Programovací jazyk Go"
 //    https://www.root.cz/serialy/programovaci-jazyk-go/
 //
+// Sto třináctá část
+//    Tvorba 2D i 3D grafiky a animací v Go s využitím projektu GoGPU
+//    https://www.root.cz/clanky/tvorba-2d-i-3d-grafiky-a-animaci-v-go-s-vyuzitim-projektu-gogpu/
+//
+// Repositář:
+//    https://github.com/tisnik/go-root/
+//
+// Seznam demonstračních příkladů ze sto třinácté části:
+//    https://github.com/tisnik/go-root/blob/master/article_AF/README.md
+//
+// Seriál "Programovací jazyk Go"
+//    https://www.root.cz/serialy/programovaci-jazyk-go/
+//
 // Demonstrační příklad číslo 9:
 //    Balíček gogpu/gg.
 //    Barvový prostor RGB a RGBA.
 //    Alfa kanál v barvovém prostoru RGBA.
 //
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_AF/09_alpha_component.html
 
 package main
 
