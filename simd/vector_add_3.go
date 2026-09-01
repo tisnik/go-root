d0f5e991ac0ad153cade330379e5900a2dffe770
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-01 09:16:47 +0200

    Vector add for float32x8 vectors

---
+++ simd/vector_add_3.go
@@ -0,0 +1,39 @@
+// ----------------------------------------------------------------------
+// Podpora nativních SIMD operací v experimentálním balíčku simd/archsimd
+// ----------------------------------------------------------------------
+
+package main
+
+import (
+	"fmt"
+	"simd/archsimd"
+)
+
+func AddTwoVectors(v1, v2 archsimd.Float32x8, result []float32) {
+	// SIMD operace součtu prvků vektorů
+	v3 := v1.Add(v2)
+
+	// zápis prvků vektoru do řezu
+	v3.Store(result)
+}
+
+func main() {
+	// konstrukce a inicializace pole s odvozením počtu prvků
+	a1 := [...]float32{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0}
+	a2 := [...]float32{0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5}
+
+	// výpis obsahu polí
+	fmt.Println("a1", a1)
+	fmt.Println("a2", a2)
+
+	// převod pole na vektor se čtyřmi prvky
+	v1 := archsimd.LoadFloat32x8Array(&a1)
+	v2 := archsimd.LoadFloat32x8Array(&a2)
+
+	// konstrukce řezu
+	result := make([]float32, 8)
+	AddTwoVectors(v1, v2, result)
+
+	// výpis obsahu řezu
+	fmt.Println("a1+a2", result)
+}
