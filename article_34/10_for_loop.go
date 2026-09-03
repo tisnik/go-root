97951e7049b806bdc86e8a2e5dc3ed0283e96b3a
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-03 09:38:48 +0200

    Added link to documenation in literate programming style

--- article_34/10_for_loop.go
+++ article_34/10_for_loop.go
@@ -10,6 +10,10 @@
 //
 // Demonstrační příklad číslo 10:
 //    Implementace programové smyčky typu "for" v jazyce Go.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_34/10_for_loop.html
+//
 
 package main
 
