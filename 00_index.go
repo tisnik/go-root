60511303bf23b95068d6489a737ca52e026e5d55
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-05 11:45:36 +0200

    Index file with examples for article #36

--- 00_index.go
+++ 00_index.go
@@ -2185,6 +2185,9 @@
 // Adresář: article_35
 // ----------------------------------------------------
 //
+// 01_booleans.go:
+//    Pravdivostní hodnoty v jazyce Go.
+//
 // 02_boolean_type_checks.go:
 //    Převod různých hodnot na typ bool.
 //
@@ -2244,4 +2247,66 @@
 //
 // 14_io_reader_read_block.go:
 //    Kopie obsahu jednoho souboru do souboru druhého.
+//
+
+// ----------------------------------------------------
+// Adresář: article_36
+// ----------------------------------------------------
+//
+// 01_import_c.go:
+//    Příkaz import.
+//
+// 02_import_fmt.go:
+//    Příkaz import.
+//
+// 03_missing_include.go:
+//    Chybějící poznámka s příkazem include.
+//
+// 04_puts_go_string.go:
+//    Volání nativní funkce s předáním řetězce jazyka Go.
+//
+// 05_puts_c_string.go:
+//    Volání nativní funkce s předáním řetězce jazyka C.
+//
+// 06_abs.go:
+//    Volání nativní funkce s předáním celočíselné hodnoty.
+//
+// 07_conversion.go:
+//    Volání nativní funkce s předáním celočíselné hodnoty.
+//
+// 08_math.go:
+//    Volání nativní funkce s předáním hodnoty s plovoucí řádovou čárkou.
+//
+// 09_math_link.go:
+//    Volání nativní funkce s předáním hodnoty s plovoucí řádovou čárkou.
+//    Korektní přilinkování knihovny math.
+//
+// 10_oom.go:
+//    OOM při neuvolňování paměti.
+//
+// 11_no_oom.go:
+//    Uvolňování paměti.
+//
+// 12_free_string.go:
+//    Uvolňování paměti.
+//    Záklaní varianta.
+//
+// 13_better_free_string.go:
+//    Uvolňování paměti.
+//    Lepší varianta.
+//
+// 14_free_before_use.go:
+//    Uvolnění paměti před jejím použitím.
+//
+// 15_errno.go:
+//    Čtení hodnoty errno s chybovým stavem.
+//
+// 16_c_function.go:
+//    Volání uživatelem definované céčkové funkce.
+//
+// 17_c_strings.go:
+//    Alokace, konverze a uvolnění céčkových řetězců z paměti.
+//
+// 18_double_free.go:
+//    Dvojité uvolnění paměti.
 //
