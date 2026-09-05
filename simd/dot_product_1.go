130f76ab790d30021f3ba4ca0ddc4f4fdbe8c44e
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-05 11:45:36 +0200

    Dot product (incomplete), example #1

---
+++ simd/dot_product_1.go
@@ -0,0 +1,36 @@
+// ----------------------------------------------------------------------
+// Podpora nativních SIMD operací v experimentálním balíčku simd/archsimd
+// ----------------------------------------------------------------------
+
+package main
+
+import (
+	"fmt"
+	"simd/archsimd"
+)
+
+func main() {
+	// konstrukce a inicializace pole s odvozením počtu prvků
+	a1 := [...]int16{0, 1, 2, 3, 4, 5, 6, 7}
+	a2 := [...]int16{0, 1, 2, 3, 4, 5, 6, 7}
+
+	// výpis obsahu polí
+	fmt.Println("a1 ", a1)
+	fmt.Println("a2 ", a2)
+
+	// převod pole na vektor s osmi prvky
+	v1 := archsimd.LoadInt16x8Array(&a1)
+	v2 := archsimd.LoadInt16x8Array(&a2)
+
+	// SIMD operace součtu prvků vektorů
+	v3 := v1.DotProductPairs(v2)
+
+	// konstrukce řezu
+	result := make([]int32, 4)
+
+	// zápis prvků vektoru do řezu
+	v3.Store(result)
+
+	// výpis obsahu řezu
+	fmt.Println("dot", result)
+}
