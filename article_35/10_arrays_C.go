35378fb18314a5d3a596b31072f1d335a19a7962
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-11 09:16:03 +0200

    Updated description of this example

--- article_35/10_arrays_C.go
+++ article_35/10_arrays_C.go
@@ -10,6 +10,13 @@
 //
 // Seznam demonstračních příkladů ze třicáté páté části:
 //    https://github.com/tisnik/go-root/blob/master/article_35/README.md
+//
+// Demonstrační příklad číslo 10C:
+//    Modifikace obsahu dvourozměrného pole.
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_35/10_arrays_C.html
+//
 
 package main
 
