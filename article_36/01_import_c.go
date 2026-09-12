86ce606e9d2eb25cfd48cf0b403320cea6754832
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-12 10:02:54 +0200

    Updated description

--- article_36/01_import_c.go
+++ article_36/01_import_c.go
@@ -8,8 +8,15 @@
 // Repositář:
 //    https://github.com/tisnik/go-root/
 //
+// Demonstrační příklad číslo 1:
+//    Příkaz import.
+//
 // Seznam demonstračních příkladů ze třicáté šesté části:
 //    https://github.com/tisnik/go-root/blob/master/article_36/README.md
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_36/01_import_c.html
+//
 
 package main
 
