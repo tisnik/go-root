8896d4f9631c87cb7ba127607436f610f81e1668
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-12 10:02:54 +0200

    Updated description

--- article_36/03_missing_include.go
+++ article_36/03_missing_include.go
@@ -8,8 +8,15 @@
 // Repositář:
 //    https://github.com/tisnik/go-root/
 //
+// Demonstrační příklad číslo 3:
+//    Chybějící poznámka s příkazem include.
+//
 // Seznam demonstračních příkladů ze třicáté šesté části:
 //    https://github.com/tisnik/go-root/blob/master/article_36/README.md
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_36/03_missing_include.html
+//
 
 package main
 
