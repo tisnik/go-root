fe619578125961c9cb905a11b88f3648b2b80eba
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-12 10:02:54 +0200

    Updated description

--- article_36/04_puts_go_string.go
+++ article_36/04_puts_go_string.go
@@ -8,8 +8,15 @@
 // Repositář:
 //    https://github.com/tisnik/go-root/
 //
+// Demonstrační příklad číslo 4:
+//    Volání nativní funkce s předáním řetězce jazyka Go.
+//
 // Seznam demonstračních příkladů ze třicáté šesté části:
 //    https://github.com/tisnik/go-root/blob/master/article_36/README.md
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_36/04_puts_html_string.html
+//
 
 package main
 
