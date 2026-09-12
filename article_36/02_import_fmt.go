434b28e0002ba5dd316805b19c148a9e6183a5d1
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-12 10:02:54 +0200

    Updated description

--- article_36/02_import_fmt.go
+++ article_36/02_import_fmt.go
@@ -8,8 +8,15 @@
 // Repositář:
 //    https://github.com/tisnik/go-root/
 //
+// Demonstrační příklad číslo 2:
+//    Příkaz import.
+//
 // Seznam demonstračních příkladů ze třicáté šesté části:
 //    https://github.com/tisnik/go-root/blob/master/article_36/README.md
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_36/02_import_fmt.html
+//
 
 package main
 
