de05e51c5c235eca4fb81417fa858ebad774d817
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-13 10:13:35 +0200

    Comparison of two vectors with floating point values

---
+++ simd/float32_comparison.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"simd"
+)
+
+func main() {
+	// konstrukce a inicializace řezu
+	s1 := []float32{1, 2, 3, 4, 5, 6, 7, 8}
+	s2 := []float32{4, 4, 4, 4, 4, 4, 4, 4}
+
+	// výpis obsahu řezů
+	fmt.Println("s1    ", s1)
+	fmt.Println("s2    ", s2)
+
+	// převod řezu na vektor
+	v1 := simd.LoadFloat32s(s1)
+	v2 := simd.LoadFloat32s(s2)
+
+	// SIMD operace porovnání prvků vektorů
+	v3 := v1.Equal(v2)
+
+	// výpis obsahu řezu
+	fmt.Println("s1==s2", v3.String())
+
+	// SIMD operace porovnání prvků vektorů
+	v4 := v1.Less(v2)
+
+	// výpis obsahu řezu
+	fmt.Println("s1<s2 ", v4.String())
+
+	// SIMD operace porovnání prvků vektorů
+	v5 := v1.LessEqual(v2)
+
+	// výpis obsahu řezu
+	fmt.Println("s1<=s2", v5.String())
+
+	// SIMD operace porovnání prvků vektorů
+	v6 := v1.Greater(v2)
+
+	// výpis obsahu řezu
+	fmt.Println("s1>s2 ", v6.String())
+
+	// SIMD operace porovnání prvků vektorů
+	v7 := v1.GreaterEqual(v2)
+
+	// výpis obsahu řezu
+	fmt.Println("s1>=s2", v7.String())
+
+	// SIMD operace porovnání prvků vektorů
+	v8 := v1.NotEqual(v2)
+
+	// výpis obsahu řezu
+	fmt.Println("s1<>s2", v8.String())
+}
