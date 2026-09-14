3c6670a285e3f0d37e89e5444d789c220bb1d538
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-14 09:53:48 +0200

    Selections by mask, example #2

---
+++ simd/mask_selection_2.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"simd"
+)
+
+func createMask() simd.Mask32s {
+	// konstrukce a inicializace řezu
+	s1 := []float32{1, 2, 3, 4, 5, 6, 7, 8}
+	s2 := []float32{4, 4, 4, 4, 4, 4, 4, 4}
+
+	// převod řezu na vektor
+	v1 := simd.LoadFloat32s(s1)
+	v2 := simd.LoadFloat32s(s2)
+
+	// SIMD operace porovnání prvků vektorů
+	return v1.GreaterEqual(v2)
+}
+
+func main() {
+	mask := createMask()
+
+	// konstrukce a inicializace řezu
+	s1 := []float32{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0}
+	s2 := []float32{99, 99, 99, 99, 99, 99, 99, 99}
+
+	// výpis obsahu řezu
+	fmt.Println("original", s1)
+
+	// převod řezu na vektor
+	v1 := simd.LoadFloat32s(s1)
+	v2 := simd.LoadFloat32s(s2)
+
+	// výběr prvků na základě masky
+	v3 := v1.IfElse(mask, v2)
+
+	// konstrukce řezu
+	result := make([]float32, v3.Len())
+
+	// zápis prvků vektoru do řezu
+	v3.Store(result)
+
+	// výpis obsahu řezu
+	fmt.Println("selected", result)
+}
