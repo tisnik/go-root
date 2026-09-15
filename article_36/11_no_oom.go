29f5d5eafe8ac370652d7c756ca086be600071ab
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-15 09:40:15 +0200

    Updated example description

--- article_36/11_no_oom.go
+++ article_36/11_no_oom.go
@@ -8,8 +8,15 @@
 // Repositář:
 //    https://github.com/tisnik/go-root/
 //
+// Demonstrační příklad číslo 11:
+//    Uvolňování paměti.
+//
 // Seznam demonstračních příkladů ze třicáté šesté části:
 //    https://github.com/tisnik/go-root/blob/master/article_36/README.md
+//
+// Dokumentace ve stylu "literate programming":
+//    https://tisnik.github.io/go-root/article_36/11_no_oom.html
+//
 
 package main
 
