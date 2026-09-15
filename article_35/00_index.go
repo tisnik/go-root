6a4db642d346e49d19b156bd46b30e2be69f8971
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-15 09:40:15 +0200

    Updated example description

--- article_35/00_index.go
+++ article_35/00_index.go
@@ -1,54 +1,66 @@
+// Seznam zdrojových kódů v tomto podadresáři
+// ------------------------------------------
+//
 // 01_booleans.go:
-//     Pravdivostní hodnoty v jazyce Go.
+//    Pravdivostní hodnoty v jazyce Go.
 //
 // 02_boolean_type_checks.go:
-//     Převod různých hodnot na typ bool.
+//    Převod různých hodnot na typ bool.
 //
 // 03_strings.go:
-//     Práce s řetězci v jazyce Go.
+//    Práce s řetězci v jazyce Go.
 //
 // 03_strings_B.go:
-//     Práce s řetězci v jazyce Go.
+//    Práce s řetězci v jazyce Go.
 //
 // 04_immutable_strings.go:
-//     Řetězce jsou v Go neměnitelné.
+//    Řetězce jsou v Go neměnitelné.
 //
 // 05_mutable_strings.go:
 //    Alternativa k měnitelným řetězcům.
 //
 // 06_mutable_utf8_strings.go:
-//     Přístup k bajtům řetězce v UTF-8.
+//    Přístup k bajtům řetězce v UTF-8.
 //
 // 06_mutable_utf8_strings_B.go:
-//     Přístup k bajtům řetězce v UTF-8.
+//    Přístup k bajtům řetězce v UTF-8.
 //
 // 07_string_comparison.go:
-//     Porovnávání řetězců v Go.
+//    Porovnávání řetězců v Go.
 //
 // 07_string_comparison_B.go:
-//     Porovnávání řetězců v Go bez podpory češtiny.
+//    Porovnávání řetězců v Go bez podpory češtiny.
 //
 // 07_string_comparison_C.go:
-//     Porovnávání řetězců v Go s podporou češtiny.
+//    Porovnávání řetězců v Go s podporou češtiny.
 //
 // 08_string_concatenation.go:
-//     Spojení řetězců.
+//    Spojení řetězců.
 //
 // 09_substring.go:
+//    Získání části řetězce.
 //
 // 09_substring_B.go:
+//    Získání části řetězce.
 //
 // 09_substring_C.go:
+//    Získání části řetězce.
 //
 // 10_arrays.go:
+//    Základní práce s poli.
 //
 // 10_arrays_C.go:
+//    Modifikace obsahu dvourozměrného pole.
 //
 // 11_array_copy.go:
+//    Kopie pole operátorem :=.
 //
 // 12_io_reader_read_byte.go:
+//    Základní vstupně-výstupní operace.
 //
 // 13_io_reader_read_ascii_char.go:
+//    Základní vstupně-výstupní operace.
 //
 // 14_io_reader_read_block.go:
+//    Kopie obsahu jednoho souboru do souboru druhého.
 //
