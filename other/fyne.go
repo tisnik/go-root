c079c5df6104cbdfae8f4879e2a87ea7066bc518
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-21 10:14:16 +0200

    Fyne framework: simple example

---
+++ other/fyne.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Hello World")
+
+	w.SetContent(widget.NewLabel("Hello World!"))
+	w.ShowAndRun()
+}
